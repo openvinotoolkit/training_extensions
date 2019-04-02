5962a0c35b569b7511ba642896fb58af33ce374a
Alexader Dokuchaev <alexander.dokuchaev@intel.com>
2019-04-02 17:07:29 +0300

    Fix integration

--- tensorflow_toolkit/veh_ped_nonveh_ssd_mobilenetv2_detector/prepare_data.sh
+++ tensorflow_toolkit/veh_ped_nonveh_ssd_mobilenetv2_detector/prepare_data.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/env bash
+
 echo "Convert image train/val dataset to tf records."
+export PYTHONPATH=${PYTHONPATH}:../../external/models/research:../../external/models/research/slim
 python create_crossroad_extra_tf_records.py --train_image_dir=./dataset/ssd_mbv2_data_train --val_image_dir=./dataset/ssd_mbv2_data_val/ --train_annotations_file=./dataset/annotation_example_train.json --val_annotations_file=./dataset/annotation_example_val.json --output_dir=tfrecords
--- tensorflow_toolkit/veh_ped_nonveh_ssd_mobilenetv2_detector/train.sh
+++ tensorflow_toolkit/veh_ped_nonveh_ssd_mobilenetv2_detector/train.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/env bash
+
 echo "Run training"
+export PYTHONPATH=${PYTHONPATH}:../../external/models/research:../../external/models/research/slim
 python ../../external/models/research/object_detection/legacy/train.py --train_dir=./model --pipeline_config_path=pipeline.config --logtostderr
