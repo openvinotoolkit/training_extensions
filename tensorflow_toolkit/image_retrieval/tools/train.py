674dabd7cfb3b664cade8229cb844bd7cf4d8e29
shixinli <shi_xinli@foxmail.com>
2019-09-03 18:16:48 +0800

    Fix bug for load weights (#136)
    * When --model_weights and --train_dir model are used together, import --model_weight first.

--- tensorflow_toolkit/image_retrieval/tools/train.py
+++ tensorflow_toolkit/image_retrieval/tools/train.py
@@ -161,11 +161,9 @@ def main():
 
     if args.model_weights:
         model.load_weights(args.model_weights)
-
-    if os.path.exists(args.train_dir):
+    elif os.path.exists(args.train_dir):
         latest_checkpoint = tf.train.latest_checkpoint(args.train_dir)
         if latest_checkpoint:
-            assert not args.model_weights
             model.load_weights(latest_checkpoint)
             print('loaded', latest_checkpoint)
     else:
