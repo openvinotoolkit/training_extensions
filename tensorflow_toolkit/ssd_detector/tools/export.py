612fe266651e7e132aed7e5d1a279d98770a0e9c
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2019-10-21 18:31:53 +0300

    Unused import

--- tensorflow_toolkit/ssd_detector/tools/export.py
+++ tensorflow_toolkit/ssd_detector/tools/export.py
@@ -17,7 +17,6 @@
 from __future__ import print_function
 import argparse
 import os
-import subprocess
 
 import tensorflow as tf
 from tfutils.helpers import dump_frozen_graph, load_module, execute_mo
