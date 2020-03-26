ba5cf6f7ef39cee97b5c796b44013db7b77a2ba6
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2020-03-26 18:51:18 +0300

    Add conversion of GNs

--- pytorch_toolkit/person_reidentification/convert_to_onnx.py
+++ pytorch_toolkit/person_reidentification/convert_to_onnx.py
@@ -16,6 +16,8 @@
 
 import argparse
 import torch
+from torch.onnx.symbolic_registry import register_op
+from torch.onnx.symbolic_helper import parse_args
 import numpy as np
 from PIL import Image
 
@@ -27,11 +29,35 @@ from torchreid.utils import load_pretrained_weights
 from models.builder import build_model
 
 
+@parse_args('v', 'i', 'v', 'v', 'f', 'i')
+def group_norm_symbolic(g, input, num_groups, weight, bias, eps, cudnn_enabled):
+    from torch.onnx.symbolic_opset9 import reshape, mul, add, reshape_as
+
+    channels_num = input.type().sizes()[1]
+
+    if num_groups == channels_num:
+        output = g.op('InstanceNormalization', input, weight, bias, epsilon_f=eps)
+    else:
+        # Reshape from [n, g * cg, h, w] to [1, n * g, cg * h, w].
+        x = reshape(g, input, [0, num_groups, -1, 0])
+        x = reshape(g, x, [1, -1, 0, 0])
+        # Normalize channel-wise.
+        x = g.op('MeanVarianceNormalization', x, axes_i=[2, 3])
+        # Reshape back.
+        x = reshape_as(g, x, input)
+        # Apply affine transform.
+        x = mul(g, x, reshape(g, weight, [1, channels_num, 1, 1]))
+        output = add(g, x, reshape(g, bias, [1, channels_num, 1, 1]))
+
+    return output
+
+
 def main():
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--config-file', type=str, default='', help='path to config file')
     parser.add_argument('--output-name', type=str, default='model')
+    parser.add_argument('--opset', type=int, default=9)
     parser.add_argument('--verbose', default=False, action='store_true',
                         help='Verbose mode for onnx.export')
     parser.add_argument('opts', default=None, nargs=argparse.REMAINDER,
@@ -76,10 +102,12 @@ def main():
     im = Image.fromarray(img)
     blob = transform(im).unsqueeze(0)
 
-    torch.onnx.export(model, blob, args.output_name + '.onnx',
-                      verbose=False, export_params=True,
-                      input_names=['data'], output_names=['reid_embedding'],
-                      opset_version=9)  # 9th version resolves nearest upsample issue
+    register_op("group_norm", group_norm_symbolic, "", args.opset)
+    with torch.no_grad():
+        torch.onnx.export(model, blob, args.output_name + '.onnx',
+                          verbose=False, export_params=True,
+                          input_names=['data'], output_names=['reid_embedding'],
+                          opset_version=args.opset)  # 9th version resolves nearest upsample issue
 
 
 if __name__ == '__main__':
