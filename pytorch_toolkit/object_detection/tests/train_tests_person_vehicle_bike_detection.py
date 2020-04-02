aca78ffe154c629714f8af3f581b97d2583b33a0
Ilya Krylov <ilya.krylov@intel.com>
2020-04-02 17:10:11 +0300

    tests for person_vehicle_bike_detection

---
+++ pytorch_toolkit/object_detection/tests/train_tests_person_vehicle_bike_detection.py
@@ -0,0 +1,60 @@
+import json
+import os
+import tempfile
+import unittest
+
+from common import replace_text_in_file, collect_ap
+
+
+class PersonVehicleBikeDetectionCrossroad1016TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.model_name = 'person-vehicle-bike-detection-crossroad-1016'
+        self.snapshot_name = 'person_vehicle_bike_sd512_mb2_clustered_epoch_21.pth'
+
+        self.data_folder = '../../data'
+        self.work_dir = tempfile.mkdtemp()
+        self.configuration_file = f'./configs/{self.model_name}.py'
+        os.system(f'cp {self.configuration_file} {self.work_dir}/')
+        self.configuration_file = os.path.join(self.work_dir,
+                                               os.path.basename(self.configuration_file))
+        self.ote_url = 'https://download.01.org/opencv/openvino_training_extensions'
+        self.url = f'{self.ote_url}/models/object_detection/{self.snapshot_name}'
+        os.system(f'wget {self.url} -P {self.work_dir}')
+
+        assert replace_text_in_file(self.configuration_file, 'total_epochs =',
+                                    'total_epochs = 25#')
+        assert replace_text_in_file(self.configuration_file, 'work_dir =',
+                                    f'work_dir = "{os.path.join(self.work_dir, "outputs")}" #')
+        assert replace_text_in_file(self.configuration_file, 'load_from = None',
+                                    f'load_from = "{os.path.join(self.work_dir, self.snapshot_name)}"')
+
+        assert replace_text_in_file(self.configuration_file, 'annotation_example_train.json',
+                                    'annotation_example_val.json')
+        assert replace_text_in_file(self.configuration_file, '/train', '/val')
+
+        os.system(f'cat {self.configuration_file}')
+
+    def test_fine_tuning(self):
+        log_file = os.path.join(self.work_dir, 'test_fine_tuning.log')
+
+        os.system(
+            f'../../external/mmdetection/tools/dist_train.sh {self.configuration_file} 1 --validate 2>&1 |'
+            f' tee {log_file}')
+        ap = collect_ap(log_file)
+        self.assertEqual(len((ap)), 25)
+        self.assertLess(ap[0], ap[-1])
+
+    def test_quality_metrics(self):
+        log_file = os.path.join(self.work_dir, 'test_quality_metrics.log')
+        os.system(
+            f'python ../../external/mmdetection/tools/test.py '
+            f'{self.configuration_file} '
+            f'{os.path.join(self.work_dir, self.snapshot_name)} '
+            f'--out res.pkl --eval bbox 2>&1 | tee {log_file}')
+        ap = collect_ap(log_file)
+
+        with open(f'tests/expected_outputs/{self.model_name}.json') as read_file:
+            content = json.load(read_file)
+
+        self.assertEqual(content['map'], ap[0])
