fba4c4b16fc7cf9ffdcee6eb4be32e7d50593f11
Ilya Krylov <ilya.krylov@intel.com>
2020-04-03 10:07:34 +0300

    minor

--- pytorch_toolkit/object_detection/tests/train_tests_face_detection.py
+++ pytorch_toolkit/object_detection/tests/train_tests_face_detection.py
@@ -3,27 +3,7 @@ import os
 import tempfile
 import unittest
 
-
-def replace_text_in_file(path, replace_what, replace_by):
-    with open(path) as read_file:
-        content = '\n'.join([line.rstrip() for line in read_file.readlines()])
-        if content.find(replace_what) == -1:
-            return False
-        content = content.replace(replace_what, replace_by)
-    with open(path, 'w') as write_file:
-        write_file.write(content)
-    return True
-
-
-def collect_ap(path):
-    ap = []
-    beginning = 'Average Precision  (AP) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = '
-    with open(path) as read_file:
-        content = [line.strip() for line in read_file.readlines()]
-        for line in content:
-            if line.startswith(beginning):
-                ap.append(float(line.replace(beginning, '')))
-    return ap
+from common import replace_text_in_file, collect_ap
 
 
 def face_detection_test_case(model_name):
