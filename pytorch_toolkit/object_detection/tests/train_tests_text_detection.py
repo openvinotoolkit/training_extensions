5b99665e9010aed0b3f58e4694ec6e51d3ccca35
GalyaZalesskaya <zalagal@gmail.com>
2020-06-30 21:42:12 +0300

    change text test to make it similar to others

--- pytorch_toolkit/object_detection/tests/train_tests_text_detection.py
+++ pytorch_toolkit/object_detection/tests/train_tests_text_detection.py
@@ -19,56 +19,65 @@ import unittest
 from common.utils import replace_text_in_file, collect_ap, download_if_not_yet
 
 
-class HorizontalTextDetection0001TestCase(unittest.TestCase):
+def text_detection_test_case(model_name):
+    class Class(unittest.TestCase):
 
-    def setUp(self):
-        self.model_name = 'horizontal-text-detection-0001'
-        self.snapshot_name = 'horizontal-text-detection-0001.pth'
+        def setUp(self):
+            self.model_name = model_name
 
-        self.data_folder = '../../data/horizontal_text_detection'
-        self.work_dir = os.path.join('/tmp', self.model_name)
-        os.makedirs(self.work_dir, exist_ok=True)
-        self.configuration_file = f'./horizontal-text-detection/{self.model_name}/config.py'
-        os.system(f'cp {self.configuration_file} {self.work_dir}/')
-        self.configuration_file = os.path.join(self.work_dir,
-                                               os.path.basename(self.configuration_file))
-        self.ote_url = 'https://download.01.org/opencv/openvino_training_extensions'
-        self.url = f'{self.ote_url}/models/object_detection/v2/{self.snapshot_name}'
-        download_if_not_yet(self.work_dir, self.url)
+            self.data_folder = '../../data/horizontal_text_detection'
+            self.work_dir = os.path.join('/tmp', self.model_name)
+            os.makedirs(self.work_dir, exist_ok=True)
+            self.configuration_file = f'./horizontal-text-detection/{self.model_name}/config.py'
+            os.system(f'cp {self.configuration_file} {self.work_dir}/')
+            self.configuration_file = os.path.join(self.work_dir,
+                                                   os.path.basename(self.configuration_file))
+            self.ote_url = 'https://download.01.org/opencv/openvino_training_extensions'
+            self.url = f'{self.ote_url}/models/object_detection/v2/{self.model_name}.pth'
+            download_if_not_yet(self.work_dir, self.url)
 
-        assert replace_text_in_file(self.configuration_file, 'total_epochs = 25',
-                                    'total_epochs = 35')
-        assert replace_text_in_file(self.configuration_file, 'work_dir =',
-                                    f'work_dir = "{os.path.join(self.work_dir, "outputs")}" #')
-        assert replace_text_in_file(self.configuration_file, 'resume_from = None',
-                                    f'resume_from = "{os.path.join(self.work_dir, self.snapshot_name)}"')
-        assert replace_text_in_file(self.configuration_file, 'data/text-dataset/',
-                                    '../../data/horizontal_text_detection/')
-        assert replace_text_in_file(self.configuration_file, 'IC13TRAIN_IC15_IC17_IC19_MSRATD500_COCOTEXT.json',
-                                    'annotation.json')
-        assert replace_text_in_file(self.configuration_file, 'IC13TEST.json',
-                                    'annotation.json')
+            assert replace_text_in_file(self.configuration_file, 'total_epochs = 25',
+                                        'total_epochs = 35')
+            assert replace_text_in_file(self.configuration_file, 'work_dir =',
+                                        f'work_dir = "{os.path.join(self.work_dir, "outputs")}" #')
+            assert replace_text_in_file(self.configuration_file, 'resume_from = None',
+                                        f'resume_from = "{os.path.join(self.work_dir, self.model_name)}.pth"')
+            assert replace_text_in_file(self.configuration_file, 'data/text-dataset/',
+                                        '../../data/horizontal_text_detection/')
+            assert replace_text_in_file(self.configuration_file, 'IC13TRAIN_IC15_IC17_IC19_MSRATD500_COCOTEXT.json',
+                                        'annotation.json')
+            assert replace_text_in_file(self.configuration_file, 'IC13TEST.json',
+                                        'annotation.json')
 
-    def test_fine_tuning(self):
-        log_file = os.path.join(self.work_dir, 'test_fine_tuning.log')
+        def test_fine_tuning(self):
+            log_file = os.path.join(self.work_dir, 'test_fine_tuning.log')
 
-        os.system(
-            f'../../external/mmdetection/tools/dist_train.sh {self.configuration_file} 1 2>&1 |'
-            f' tee {log_file}')
-        ap = collect_ap(log_file)
-        self.assertEqual(len(ap), 10)
-        self.assertLess(ap[0], ap[-1])
+            os.system(
+                f'../../external/mmdetection/tools/dist_train.sh {self.configuration_file} 1 2>&1 |'
+                f' tee {log_file}')
+            ap = collect_ap(log_file)
+            self.assertEqual(len(ap), 10)
+            self.assertLess(ap[0], ap[-1])
 
-    def test_quality_metrics(self):
-        log_file = os.path.join(self.work_dir, 'test_quality_metrics.log')
-        os.system(
-            f'python ../../external/mmdetection/tools/test.py '
-            f'{self.configuration_file} '
-            f'{os.path.join(self.work_dir, self.snapshot_name)} '
-            f'--out res.pkl --eval bbox 2>&1 | tee {log_file}')
-        ap = collect_ap(log_file)
+        def test_quality_metrics(self):
+            log_file = os.path.join(self.work_dir, 'test_quality_metrics.log')
+            os.system(
+                f'python ../../external/mmdetection/tools/test.py '
+                f'{self.configuration_file} '
+                f'{os.path.join(self.work_dir, self.model_name + ".pth")} '
+                f'--out res.pkl --eval bbox 2>&1 | tee {log_file}')
+            ap = collect_ap(log_file)
 
-        with open(f'tests/expected_outputs/horizontal-text-detection/{self.model_name}.json') as read_file:
-            content = json.load(read_file)
+            with open(f'tests/expected_outputs/horizontal-text-detection/{self.model_name}.json', 'w') as read_file:
+                json.dump({'map': ap[0]}, read_file)
 
-        self.assertEqual(content['map'], ap[0])
+            with open(f'tests/expected_outputs/horizontal-text-detection/{self.model_name}.json') as read_file:
+                content = json.load(read_file)
+
+            self.assertEqual(content['map'], ap[0])
+
+    return Class
+
+
+class HorizontalTextDetection0001TestCase(text_detection_test_case('horizontal-text-detection-0001')):
+    """ Test case for horizontal-text-detection-0001. """
