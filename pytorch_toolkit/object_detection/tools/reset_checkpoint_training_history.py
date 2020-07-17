f193bffc87b0026c1127fb940443772f3364a70a
Ilya Krylov <ilya.krylov@intel.com>
2020-07-17 13:46:27 +0300

    update reset checkpoint tool

--- pytorch_toolkit/object_detection/tools/reset_checkpoint_training_history.py
+++ pytorch_toolkit/object_detection/tools/reset_checkpoint_training_history.py
@@ -1,17 +1,13 @@
 import argparse
 
-from mmcv import Config
-from mmdet.models import build_detector
-from mmdet.utils import get_root_logger, ExtendedDictAction
-from mmcv.runner.checkpoint import load_checkpoint, save_checkpoint
+import torch
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Resets optimizer, epoch and iter numbers in a checkpoint')
-    parser.add_argument('config', help='train config file path')
-    parser.add_argument('checkpoint', help='checkpoint')
+    parser = argparse.ArgumentParser(
+        description='Resets optimizer, epoch and iter numbers in a checkpoint')
+    parser.add_argument('input', help='train config file path')
     parser.add_argument('output', help='path to output snapshot')
-    parser.add_argument('--update_config', nargs='+', action=ExtendedDictAction, help='arguments in dict')
 
     return parser.parse_args()
 
@@ -19,13 +15,10 @@ def parse_args():
 def main():
     args = parse_args()
 
-    cfg = Config.fromfile(args.config)
-    if args.update_config is not None:
-        cfg.merge_from_dict(args.update_config)
-
-    model = build_detector(cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
-    load_checkpoint(model, args.checkpoint)
-    save_checkpoint(model, args.output, meta={'epoch': 0, 'iter': 0})
+    checkpoint = torch.load(args.input)
+    checkpoint['meta'] = {'epoch': 0, 'iter': 0}
+    del checkpoint['optimizer']
+    torch.save(checkpoint, args.input)
 
     return 0
 
