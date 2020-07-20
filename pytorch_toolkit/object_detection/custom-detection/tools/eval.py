25f336bc61dfee794f4a21b6053a6f1a4cdf51be
Ilya Krylov <ilya.krylov@intel.com>
2020-07-20 13:56:15 +0300

    linter issues for custom detection

--- pytorch_toolkit/object_detection/custom-detection/tools/eval.py
+++ pytorch_toolkit/object_detection/custom-detection/tools/eval.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
+# pylint: disable=C0411,C0413
+
 import argparse
 import os
 import sys
--- pytorch_toolkit/object_detection/custom-detection/tools/train_and_eval.py
+++ pytorch_toolkit/object_detection/custom-detection/tools/train_and_eval.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-# pylint: disable=C0301,W0622,R0914
+# pylint: disable=C0411,C0413
 
 import argparse
 import json
