8095fe02834b5b535b43570506c50a376f57e5e3
Ilya Krylov <ilya.krylov@intel.com>
2020-08-26 11:21:47 +0300

    update interace and template yaml according to agreement

--- pytorch_toolkit/ote/api.py
+++ pytorch_toolkit/ote/api.py
@@ -8,36 +8,35 @@ def train_args_parser(template_path):
     with open(template_path, 'r') as model_definition:
         config = yaml.safe_load(model_definition)
 
-        parser.add_argument('--train_ann_files', required=True,
+        parser.add_argument('--train-ann-files', required=True,
                             help='Comma-separated paths to training annotation files.')
-        parser.add_argument('--train_img_roots', required=True,
+        parser.add_argument('--train-img-roots', required=True,
                             help='Comma-separated paths to training images folders.')
-        parser.add_argument('--val_ann_files', required=True,
+        parser.add_argument('--val-ann-files', required=True,
                             help='Comma-separated paths to validation annotation files.')
-        parser.add_argument('--val_img_roots', required=True,
+        parser.add_argument('--val-img-roots', required=True,
                             help='Comma-separated paths to validation images folders.')
-        parser.add_argument('--resume_from', default='',
+        parser.add_argument('--resume-from', default='',
                             help='Resume training from previously saved checkpoint')
-        parser.add_argument('--load_weights', default='',
+        parser.add_argument('--load-weights', default='',
                             help='Load only weights from previously saved checkpoint')
-        parser.add_argument('--save_checkpoints_to', default='/tmp/checkpoints',
+        parser.add_argument('--save-checkpoints-to', default='/tmp/checkpoints',
                             help='Location where checkpoints will be stored')
         parser.add_argument('--epochs', type=int,
-                            default=config['training_parameters']['epochs'],
+                            default=config['hyper_parameters']['epochs'],
                             help='Number of epochs during training')
-        parser.add_argument('--batch_size', type=int,
-                            default=config['training_parameters']['batch_size'],
+        parser.add_argument('--batch-size', type=int,
+                            default=config['hyper_parameters']['batch_size'],
                             help='Size of a single batch during training per GPU.')
-        parser.add_argument('--gpu_num', type=int,
-                            default=config['training_parameters']['gpu_num'],
-                            help='Number of GPUs that will be used in training, 0 is for CPU mode.')
-        parser.add_argument('--base_learning_rate', type=float,
-                            default=config['training_parameters']['base_learning_rate'],
+        parser.add_argument('--base-learning-rate', type=float,
+                            default=config['hyper_parameters']['base_learning_rate'],
                             help='Starting value of learning rate that might be changed during '
                                  'training according to learning rate schedule that is usually '
                                  'defined in detailed training configuration.')
-        parser.add_argument('--config', default=config['config'],
-                            help='Location of a file describing detailed model configuration.')
+        parser.add_argument('--gpu-num', type=int,
+                            default=config['gpu_num'],
+                            help='Number of GPUs that will be used in training, 0 is for CPU mode.')
+        parser.add_argument('--config', default=config['config'], help=argparse.SUPPRESS)
 
     return parser
 
@@ -47,18 +46,17 @@ def test_args_parser(template_path):
     with open(template_path, 'r') as model_definition:
         config = yaml.safe_load(model_definition)
 
-        parser.add_argument('--test_ann_files', required=True,
+        parser.add_argument('--test-ann-files', required=True,
                             help='Comma-separated paths to test annotation files.')
-        parser.add_argument('--test_img_roots', required=True,
+        parser.add_argument('--test-img-roots', required=True,
                             help='Comma-separated paths to test images folders.')
-        parser.add_argument('--load_weights', required=True,
+        parser.add_argument('--load-weights', required=True,
                             help='Load only weights from previously saved checkpoint')
-        parser.add_argument('--save_metrics_to', required=True,
+        parser.add_argument('--save-metrics-to', required=True,
                             help='Location where evaluated metrics values will be stored (yaml file).')
-        parser.add_argument('--config', default=config['config'],
-                            help='Location of a file describing detailed model configuration.')
-        parser.add_argument('--save_output_images_to', default='',
+        parser.add_argument('--save-output-images-to', default='',
                             help='Location where output images (with displayed result of model work) will be stored.')
+        parser.add_argument('--config', default=config['config'], help=argparse.SUPPRESS)
 
     return parser
 
@@ -67,20 +65,19 @@ def export_args_parser(template_path):
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
     with open(template_path, 'r') as model_definition:
         config = yaml.safe_load(model_definition)
-        parser.add_argument('--load_weights', required=True,
+        parser.add_argument('--load-weights', required=True,
                             help='Load only weights from previously saved checkpoint')
-        parser.add_argument('--config', default=config['config'],
-                            help='Location of a file describing detailed model configuration.')
-        parser.add_argument('--save_exported_model_to', required='True',
+        parser.add_argument('--save-exported-model-to', required='True',
                             help='Location where exported model will be stored.')
         subparsers = parser.add_subparsers(title='target', dest='target',
                                            help='target model format')
         subparsers.required = True
         subparsers.add_parser('onnx', help='export to ONNX')
         parser_openvino = subparsers.add_parser('openvino', help='export to OpenVINO')
-        parser_openvino.add_argument('--alt_ssd_export', action='store_true',
+        parser_openvino.add_argument('--alt-ssd-export', action='store_true',
                                      help='use alternative ONNX representation of SSD net')
-        parser_openvino.add_argument('--input_format', choices=['BGR', 'RGB'], default='BGR',
+        parser_openvino.add_argument('--input-format', choices=['BGR', 'RGB'], default='BGR',
                                      help='Input image format for exported model.')
+        parser.add_argument('--config', default=config['config'], help=argparse.SUPPRESS)
 
     return parser
