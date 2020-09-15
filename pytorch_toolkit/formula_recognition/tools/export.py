2345ad232dc9325fdb1704111673c16cb4fb356c
Vladimir Loginov <vladimir.loginov@intel.com>
2020-09-15 16:42:37 +0300

    add running onnx model and comparing outputs

--- pytorch_toolkit/formula_recognition/tools/export.py
+++ pytorch_toolkit/formula_recognition/tools/export.py
@@ -1,16 +1,16 @@
 import argparse
-import yaml
-import numpy as np
 
 import cv2 as cv
-import torch
-import torch.optim as optim
-from im2latex.data.utils import create_list_of_transforms
-from im2latex.data.vocab import read_vocab
-from im2latex.models.im2latex_model import Im2latexModel
+import numpy as np
 import onnxruntime
+import torch
 import torch.onnx
-from im2latex.data.vocab import START_TOKEN, END_TOKEN
+import torch.optim as optim
+import yaml
+
+from im2latex.data.utils import create_list_of_transforms
+from im2latex.data.vocab import START_TOKEN, END_TOKEN, read_vocab
+from im2latex.models.im2latex_model import Im2latexModel
 
 
 class ONNXExporter():
@@ -22,7 +22,7 @@ class ONNXExporter():
         self.model = Im2latexModel(config.get('backbone_type'), config.get(
             'backbone_config'), len(self.vocab), config.get('head'))
         if self.model_path is not None:
-            self.model.load_weights(self.model_path)
+            self.model.load_weights(self.model_path, old_model=config.get("old_model"))
 
         self.input = config.get("dummy_input")
 
@@ -34,6 +34,20 @@ class ONNXExporter():
         img = self.transform(img)
         img = img[0].unsqueeze(0)
         return img
+
+
+def get_onnx_inputs(model):
+    names = []
+    for inp in model.get_inputs():
+        names.append(inp.name)
+    return names
+
+
+def get_onnx_outputs(model):
+    names = []
+    for out in model.get_outputs():
+        names.append(out.name)
+    return names
 
 
 def parse_args():
@@ -60,30 +74,66 @@ if __name__ == "__main__":
                                     'init_0': {}
                                     },
                       output_names=['row_enc_out', 'hidden', 'context', 'init_0'])
-    sess_pt1 = onnxruntime.InferenceSession(config.get("res_encoder_name"))
-    imgs_name = sess_pt1.get_inputs()[0].name
-    row_enc_out_name = sess_pt1.get_outputs()[0].name
-    h_name = sess_pt1.get_outputs()[1].name
-    c_name = sess_pt1.get_outputs()[2].name
-    init_0_name = sess_pt1.get_outputs()[3].name
-
-    row_enc_out, h, c, O_t = sess_pt1.run([row_enc_out_name, h_name, c_name, init_0_name],
-                                          {imgs_name: np.array(
-                                              img).astype(np.float32)},
-                                          )[:]
+    encoder_onnx = onnxruntime.InferenceSession(config.get("res_encoder_name"))
+    encoder_outputs = get_onnx_outputs(encoder_onnx)
+    encoder_input = get_onnx_inputs(encoder_onnx)[0]
+    row_enc_out, h, c, O_t = encoder_onnx.run(encoder_outputs,
+                                              {encoder_input: np.array(
+                                                  img).astype(np.float32)},
+                                              )
     tgt = torch.tensor([[START_TOKEN]] * img.size(0))
     torch.onnx.export(decoder,
-                        (torch.tensor(h),
-                        torch.tensor(c),
-                        torch.tensor(O_t),
-                        torch.tensor(row_enc_out),
-                        torch.tensor(tgt, dtype=torch.long)),
-                        config.get("res_decoder_name"),
-                        opset_version=11,
-                        input_names=['dec_st_h', 'dec_st_c',
-                                    'output_prev', 'row_enc_out', 'tgt'],
-                        output_names=['dec_st_h_t',
+                      (torch.tensor(h),
+                       torch.tensor(c),
+                       torch.tensor(O_t),
+                       torch.tensor(row_enc_out),
+                       torch.tensor(tgt, dtype=torch.long)),
+                      config.get("res_decoder_name"),
+                      opset_version=11,
+                      input_names=['dec_st_h', 'dec_st_c',
+                                   'output_prev', 'row_enc_out', 'tgt'],
+                      output_names=['dec_st_h_t',
                                     'dec_st_c_t', 'output', 'logit'],
-                        dynamic_axes={row_enc_out_name: {
-                            0: 'batch', 1: 'H', 2: 'W'}}
-                        )
+                      dynamic_axes={'row_enc_out': {
+                          0: 'batch', 1: 'H', 2: 'W'}}
+                      )
+
+    decoder_onnx = onnxruntime.InferenceSession(config.get("res_decoder_name"))
+    decoder_inputs = get_onnx_inputs(decoder_onnx)
+    decoder_outputs = get_onnx_outputs(decoder_onnx)
+    dec_states_out_h, dec_states_out_c, O_t_out_val, logit_val = decoder_onnx.run(
+        decoder_outputs,
+        {
+            decoder_inputs[0]: h,
+            decoder_inputs[1]: c,
+            decoder_inputs[2]: O_t,
+            decoder_inputs[3]: row_enc_out,
+            decoder_inputs[4]: np.array(tgt)
+        })
+    logits = []
+    logits.append(logit_val)
+    for t in range(exporter.model.head.max_len):
+        tgt = torch.reshape(torch.max(torch.tensor(logit_val), dim=1)[
+                            1], (img.size(0), 1)).clone().detach()
+        if tgt == END_TOKEN:
+            break
+        dec_states_out_h, dec_states_out_c, O_t_out_val, logit_val = decoder_onnx.run(
+            decoder_outputs,
+            {
+                decoder_inputs[0]: dec_states_out_h,
+                decoder_inputs[1]: dec_states_out_c,
+                decoder_inputs[2]: O_t_out_val,
+                decoder_inputs[3]: row_enc_out,
+                decoder_inputs[4]: np.array(tgt)
+            })
+        logits.append(logit_val)
+
+
+    logits = torch.tensor(logits)
+    logits = logits.squeeze(1)
+    targets = torch.max(torch.log(logits.clone().detach()).data, dim=1)[1]
+    pred_phrase_str = exporter.vocab.construct_phrase(targets)
+    _, targets = exporter.model(img)
+    pred_phrase_model = exporter.vocab.construct_phrase(targets[0])
+    print("Predicted with ONNX:   \t{}".format(pred_phrase_str))
+    print("Predicted with Pytorch:\t{}".format(pred_phrase_model))
