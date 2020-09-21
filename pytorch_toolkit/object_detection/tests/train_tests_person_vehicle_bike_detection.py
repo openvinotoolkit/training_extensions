d465648bbe0bb3f572cf529727bebd0855888e45
Ilya Krylov <ilya.krylov@intel.com>
2020-09-21 16:48:03 +0300

    person_vehicle_bike_detection and text_detection tests

--- pytorch_toolkit/object_detection/tests/train_tests_person_vehicle_bike_detection.py
+++ pytorch_toolkit/object_detection/tests/train_tests_person_vehicle_bike_detection.py
@@ -12,142 +12,31 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-import json
-import os
-import tempfile
-import unittest
-
-import yaml
-from common.utils import replace_text_in_file, collect_ap, download_if_not_yet, run_through_shell
+from common.test_case import create_test_case
 
 
-def test_case(model_name, snapshot_name):
-    class Class(unittest.TestCase):
-
-        def setUp(self):
-            self.model_name = model_name
-
-            self.data_folder = '../../data'
-            self.work_dir = os.path.join('/tmp/', self.model_name)
-            os.makedirs(self.work_dir, exist_ok=True)
-            self.configuration_file = f'model_templates/person-vehicle-bike-detection/{self.model_name}/config.py'
-            run_through_shell(f'cp {self.configuration_file} {self.work_dir}/')
-            self.configuration_file = os.path.join(self.work_dir,
-                                                   os.path.basename(self.configuration_file))
-            self.ote_url = 'https://download.01.org/opencv/openvino_training_extensions'
-            self.url = f'{self.ote_url}/models/object_detection/v2/{snapshot_name}'
-            download_if_not_yet(self.work_dir, self.url)
-
-            assert replace_text_in_file(self.configuration_file, 'samples_per_gpu=',
-                                        'samples_per_gpu=1 ,#')
-            assert replace_text_in_file(self.configuration_file, 'total_epochs = 20',
-                                        'total_epochs = 25')
-            assert replace_text_in_file(self.configuration_file, 'work_dir =',
-                                        f'work_dir = "{os.path.join(self.work_dir, "outputs")}" #')
-            assert replace_text_in_file(self.configuration_file, 'annotation_example_val.json',
-                                        'annotation_example_train.json')
-            assert replace_text_in_file(self.configuration_file, "data_root + 'val'",
-                                        "data_root + 'train'")
-            assert replace_text_in_file(self.configuration_file, 'resume_from = None',
-                                        f'resume_from = "{os.path.join(self.work_dir, snapshot_name)}"')
-
-        def test_fine_tuning(self):
-            log_file = os.path.join(self.work_dir, 'test_fine_tuning.log')
-            run_through_shell(
-                f'../../external/mmdetection/tools/dist_train.sh {self.configuration_file} 1 2>&1 |'
-                f' tee {log_file}')
-            ap = collect_ap(log_file)
-            self.assertEqual(len((ap)), 5)
-
-        def test_quality_metrics(self):
-            log_file = os.path.join(self.work_dir, 'test_quality_metrics.log')
-            run_through_shell(
-                f'python ../../external/mmdetection/tools/test.py '
-                f'{self.configuration_file} '
-                f'{os.path.join(self.work_dir, snapshot_name)} '
-                f'--out res.pkl --eval bbox 2>&1 | tee {log_file}')
-            ap = collect_ap(log_file)
-
-            with open(f'tests/expected_outputs/person-vehicle-bike-detection/{self.model_name}.json') as read_file:
-                content = json.load(read_file)
-
-            self.assertEqual(content['map'], ap[0])
-
-    return Class
-
-
-class PersonVehicleBikeDetection2000TestCase(test_case('person-vehicle-bike-detection-2000',
-                                                       'vehicle-person-bike-detection-2000-1.pth')):
+class PersonVehicleBikeDetection2000TestCase(create_test_case(
+    'person-vehicle-bike-detection',
+    'person-vehicle-bike-detection-2000',
+    '../../../../../data/airport/annotation_example_train.json',
+    '../../../../../data/airport/train'
+)):
     """ Test case for person-vehicle-bike-detection-2000 model. """
 
 
-class PersonVehicleBikeDetection2001TestCase(test_case('person-vehicle-bike-detection-2001',
-                                                       'vehicle-person-bike-detection-2001-1.pth')):
+class PersonVehicleBikeDetection2001TestCase(create_test_case(
+    'person-vehicle-bike-detection',
+    'person-vehicle-bike-detection-2001',
+    '../../../../../data/airport/annotation_example_train.json',
+    '../../../../../data/airport/train'
+)):
     """ Test case for person-vehicle-bike-detection-2001 model. """
 
 
-class PersonVehicleBikeDetection2002TestCase(test_case('person-vehicle-bike-detection-2002',
-                                                       'vehicle-person-bike-detection-2002-1.pth')):
+class PersonVehicleBikeDetection2002TestCase(create_test_case(
+    'person-vehicle-bike-detection',
+    'person-vehicle-bike-detection-2002',
+    '../../../../../data/airport/annotation_example_train.json',
+    '../../../../../data/airport/train'
+)):
     """ Test case for person-vehicle-bike-detection-2002 model. """
-
-
-class PersonVehicleBikeDetection2000TestCaseOteApi(unittest.TestCase):
-
-    @staticmethod
-    def get_dependencies(template_file):
-        output = {}
-        with open(template_file) as read_file:
-            content = yaml.load(read_file)
-            for dependency in content['dependencies']:
-                output[dependency['destination'].split('.')[0]] = dependency['source']
-        return output
-
-    def test_ok(self):
-        model_name = 'person-vehicle-bike-detection-2000'
-
-        template_file = f'model_templates/person-vehicle-bike-detection/{model_name}/template.yaml'
-        ann_file = '../../../../../data/airport/annotation_example_train.json'
-        img_root = '../../../../../data/airport/train'
-        work_dir = tempfile.mkdtemp()
-        dependencies = self.get_dependencies(template_file)
-
-        download_if_not_yet(work_dir, dependencies['snapshot'])
-
-        run_through_shell(
-            f'cd {os.path.dirname(template_file)};'
-            f'python {dependencies["eval"]}'
-            f' --test-ann-files {ann_file}'
-            f' --test-img-roots {img_root}'
-            f' --save-metrics-to {os.path.join(work_dir, "metrics.yaml")}'
-            f' --load-weights {os.path.join(work_dir, os.path.basename(dependencies["snapshot"]))}')
-
-        with open(os.path.join(work_dir, "metrics.yaml")) as read_file:
-            content = yaml.load(read_file)
-
-        ap0 = [metrics['value'] for metrics in content['metrics'] if metrics['key'] == 'ap'][0]
-
-        run_through_shell(
-            f'cd {os.path.dirname(template_file)};'
-            f'python {dependencies["train"]}'
-            f' --train-ann-files {ann_file}'
-            f' --train-img-roots {img_root}'
-            f' --val-ann-files {ann_file}'
-            f' --val-img-roots {img_root}'
-            f' --resume-from {os.path.join(work_dir, os.path.basename(dependencies["snapshot"]))}'
-            f' --save-checkpoints-to {work_dir}'
-            f' --gpu-num 1'
-            f' --epochs 25')
-
-        run_through_shell(
-            f'cd {os.path.dirname(template_file)};'
-            f'python {dependencies["eval"]}'
-            f' --test-ann-files {ann_file}'
-            f' --test-img-roots {img_root}'
-            f' --save-metrics-to {os.path.join(work_dir, "metrics.yaml")}'
-            f' --load-weights {os.path.join(work_dir, "latest.pth")}')
-
-        with open(os.path.join(work_dir, "metrics.yaml")) as read_file:
-            content = yaml.load(read_file)
-
-        ap = [metrics['value'] for metrics in content['metrics'] if metrics['key'] == 'ap'][0]
-        assert ap > ap0 * 0.9
