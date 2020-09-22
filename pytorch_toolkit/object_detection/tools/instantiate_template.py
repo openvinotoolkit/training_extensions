1e90fc227e4e8f4a22983a586fa088c637139974
Ilya Krylov <ilya.krylov@intel.com>
2020-09-22 12:23:01 +0300

    instantiate template

---
+++ pytorch_toolkit/object_detection/tools/instantiate_template.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+
+import argparse
+import os
+import yaml
+from subprocess import run
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('template', help='Location of model template file (template.yaml).')
+    parser.add_argument('output', help='Location of output directory where template will be instantiated.')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    with open(args.template) as read_file:
+        content = yaml.load(read_file)
+
+    os.makedirs(args.output, exist_ok=True)
+    os.system(f'cp -r {os.path.dirname(args.template)}/* {args.output}')
+
+    for dependency in content['dependencies']:
+        source = dependency['source']
+        destination = dependency['destination']
+        if not source.startswith('http:') and not source.startswith('https:'):
+            rel_source = os.path.join(os.path.dirname(args.template), source)
+            run(f'cp -r {rel_source} {os.path.join(args.output, destination)}', check=True, shell=True)
+        else:
+            run(f'wget -O {os.path.join(args.output, destination)} {source}', check=True, shell=True)
+
+
+if __name__ == '__main__':
+    main()
