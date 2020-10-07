d8ed85c488cc62d2fdc5bb76455644b90bc97ed0
Vladimir Loginov <vladimir.loginov@intel.com>
2020-10-07 11:57:46 +0300

    use inplace relu

--- pytorch_toolkit/formula_recognition/im2latex/models/backbones/original_harvard_bb.py
+++ pytorch_toolkit/formula_recognition/im2latex/models/backbones/original_harvard_bb.py
@@ -46,22 +46,22 @@ class Im2LatexBackbone(nn.Module):
         self.cnn_encoder = nn.Sequential(
             nn.Conv2d(1, 512, 3, 1, 0),
             nn.BatchNorm2d(512),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, 3, 1, 1),
             nn.BatchNorm2d(512),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d((1, 2), (1, 2), (0, 0)),
             nn.Conv2d(512, 256, 3, 1, 1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d((2, 1), (2, 1), (0, 0)),
             nn.Conv2d(256, 256, 3, 1, 1),
             nn.BatchNorm2d(256),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(256, 128, 3, 1, 1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d((2, 2), (2, 2), (0, 0)),
             nn.Conv2d(128, 64, 3, 1, 1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d((2, 2), (2, 2), (1, 1))
         )
 
