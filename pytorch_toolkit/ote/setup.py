5ac4fac0db5ae92da1944a90702428088c88352a
Ilya Krylov <ilya.krylov@intel.com>
2020-10-15 09:29:33 +0300

    fixes

--- pytorch_toolkit/ote/setup.py
+++ pytorch_toolkit/ote/setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as read_file:
-    requirements = [requirement for requirement in read_file]
+    requirements = [requirement.strip() for requirement in read_file]
 
 setup(
     name='ote',
