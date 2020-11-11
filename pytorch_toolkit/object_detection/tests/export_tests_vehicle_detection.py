fd127fad22fe38c5e24f000db5616cd114baf907
yurygoru <yury.gorunov@intel.com>
2020-11-11 11:01:53 +0000

    fix import

--- pytorch_toolkit/object_detection/tests/export_tests_vehicle_detection.py
+++ pytorch_toolkit/object_detection/tests/export_tests_vehicle_detection.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
+import os
 
 from common.test_case import create_export_test_case
 
--- pytorch_toolkit/object_detection/tests/train_tests_vehicle_detection.py
+++ pytorch_toolkit/object_detection/tests/train_tests_vehicle_detection.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
+import os
+
 from common.test_case import create_test_case
 
 
