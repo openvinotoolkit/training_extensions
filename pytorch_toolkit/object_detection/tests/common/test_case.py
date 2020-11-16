87beed01d70420c010ceb07d3a62f9a97c86cf4e
Ilya Krylov <ilya.krylov@intel.com>
2020-11-16 12:45:52 +0300

    removed unused imports

--- pytorch_toolkit/object_detection/tests/common/test_case.py
+++ pytorch_toolkit/object_detection/tests/common/test_case.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-import logging
 import json
 import os
-from subprocess import run
 import unittest
 
 import torch
