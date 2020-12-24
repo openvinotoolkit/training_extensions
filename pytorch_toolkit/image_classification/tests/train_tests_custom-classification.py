09fdc0ad364a1aef41983b237e7ad90e98378650
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2020-12-24 17:32:30 +0300

    Add a stub for train test

---
+++ pytorch_toolkit/image_classification/tests/train_tests_custom-classification.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+
+import os
+
+from common.test_case import create_image_classification_test_case
+
+kwargs = dict(
+    problem_name='custom-classification',
+    ann_file=os.path.dirname(__file__) + '/../../../data/airport/annotation_faces_train.json',
+    img_root=os.path.dirname(__file__) + '/../../../data/airport/'
+)
+
+class CustomClassificationTestCase(
+        create_image_classification_test_case(
+            model_name='mobilenet_v2_w1',
+            **kwargs,
+        )
+):
+    """ Test case for mobilenet_v2_w1 model. """
