3acedf6bc2dc0c430941eaed68c799337d58d666
Leonid Beynenson <Leonid.Beynenson@intel.com>
2020-12-25 15:23:37 +0300

    Add fixes for CI
    * add pylint config file
    * turn of OTE NNCF tests
    * fix ote requirements.txt

--- pytorch_toolkit/tests/run_model_templates_tests.py
+++ pytorch_toolkit/tests/run_model_templates_tests.py
@@ -37,7 +37,7 @@ NNCF_ENABLE_FOR = [
 
 ENABLE_TRAIN_TESTS = True
 ENABLE_EXPORT_TESTS = True
-ENABLE_NNCF_TESTS = False
+ENABLE_NNCF_TESTS = True
 
 def _is_verbose_flag_set():
     if '-v' in sys.argv or '--verbose' in sys.argv:
