3442fa83e38473fe014874c4e7f527e9c9daadb9
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2021-02-05 12:16:25 +0300

    Add classes param to argparser of image classification

--- pytorch_toolkit/ote/ote/modules/arg_parsers/__init__.py
+++ pytorch_toolkit/ote/ote/modules/arg_parsers/__init__.py
@@ -1,9 +1,11 @@
 from .default import DefaultArgParser
 from .custom_detector import CustomDetectorArgParser
 from .face_detector import FaceDetectorArgParser
+from .reid import ReidArgParser
 
 __all__ = [
     'DefaultArgParser',
     'CustomDetectorArgParser',
     'FaceDetectorArgParser',
+    'ReidArgParser',
 ]
