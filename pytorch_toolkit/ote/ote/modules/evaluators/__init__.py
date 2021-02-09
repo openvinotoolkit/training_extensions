7f40077e232674e7d3354e0330b507e4f73833ac
Ilya Krylov <ilya.krylov@intel.com>
2021-02-09 08:08:13 +0300

    bugfix

--- pytorch_toolkit/ote/ote/modules/evaluators/__init__.py
+++ pytorch_toolkit/ote/ote/modules/evaluators/__init__.py
@@ -14,6 +14,6 @@ __all__ = [
     'MMFaceDetectionEvaluator',
     'MMHorizontalTextDetectionEvaluator',
     'InstanceSegmentationEvaluator',
-    'TextSpottingEvaluator'
+    'TextSpottingEvaluator',
     'ReidEvaluator',
 ]
