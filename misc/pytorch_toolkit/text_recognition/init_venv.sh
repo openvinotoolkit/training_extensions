6ed11d8c357878dde32c30bae01f3e7e3fd972e1
Vladimir Loginov <vladimir.loginov@intel.com>
2021-03-25 11:37:35 +0300

    upd init venv

--- misc/pytorch_toolkit/text_recognition/init_venv.sh
+++ misc/pytorch_toolkit/text_recognition/init_venv.sh
@@ -2,24 +2,29 @@
 
 work_dir=$(realpath "$(dirname $0)")
 
+venv_dir=$1
+if [ -z "$venv_dir" ]; then
+  venv_dir=venv
+fi
+
 cd ${work_dir}
 
-if [[ -e venv ]]; then
+if [[ -e ${venv_dir} ]]; then
   echo
   echo "Virtualenv already exists. Use command to start working:"
-  echo "$ . venv/bin/activate"
+  echo "$ . ${venv_dir}/bin/activate"
   exit
 fi
 
 # Create virtual environment
-virtualenv venv -p python3.7 --prompt="(text_recognition)"
+virtualenv ${venv_dir} -p python --prompt="(text_recognition)"
 
 path_openvino_vars="${INTEL_OPENVINO_DIR:-/opt/intel/openvino}/bin/setupvars.sh"
 if [[ -e "${path_openvino_vars}" ]]; then
-  echo ". ${path_openvino_vars}" >>venv/bin/activate
+  echo ". ${path_openvino_vars}" >>${venv_dir}/bin/activate
 fi
 
-. venv/bin/activate
+. ${venv_dir}/bin/activate
 
 cat requirements.txt | xargs -n 1 -L 1 pip3 install
 
@@ -32,4 +37,4 @@ fi
 
 echo
 echo "Activate a virtual environment to start working:"
-echo "$ . venv/bin/activate"
+echo "$ . ${venv_dir}/bin/activate"
