09895607311ec94363097f1184e30a6b1abbedd1
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2021-03-25 17:11:18 +0300

    Enable more features for classification

--- ote/ote/tasks/classification/task.py
+++ ote/ote/tasks/classification/task.py
@@ -38,11 +38,13 @@ from torchreid.data.transforms import build_inference_transform
 from torchreid.data.datasets.image import ExternalDatasetWrapper
 from torchreid.models.common import ModelInterface
 from torchreid.engine import build_engine
-from torchreid.utils import load_pretrained_weights, set_random_seed
+from torchreid.utils import load_pretrained_weights, set_random_seed, resume_from_checkpoint
 from scripts.default_config import (engine_run_kwargs, get_default_config,
                                     imagedata_kwargs, lr_finder_run_kwargs,
                                     lr_scheduler_kwargs, model_kwargs,
                                     optimizer_kwargs)
+from torchreid.ops import DataParallel
+from scripts.script_utils import build_auxiliary_model
 
 
 class ClassificationTask(ITask):
@@ -62,9 +64,17 @@ class ClassificationTask(ITask):
         self.cfg.data.targets = ['val']
         self.cfg.data.save_dir = self.env_parameters.work_dir
 
+        for i, conf in enumerate(self.cfg.mutual_learning.aux_configs):
+            if self.env_parameters.work_dir not in conf:
+                self.cfg.mutual_learning.aux_configs[i] = os.path.join(self.env_parameters.work_dir, conf)
+
         self.device = torch.device("cuda:0") if self.cfg.use_gpu else torch.device("cpu")
+        self.num_devices = min(torch.cuda.device_count(), parameters.gpu_num)
         self.model = self.create_model().to(self.device)
 
+        self.__load_snap_if_exists()
+
+    def __load_snap_if_exists(self):
         if self.env_parameters.snapshot_path:
             with open(self.env_parameters.snapshot_path, "rb") as f:
                 model_bytes = f.read()
@@ -82,12 +92,76 @@ class ClassificationTask(ITask):
 
         set_random_seed(self.cfg.train.seed)
         self.cfg.custom_datasets.roots = [train_dataset, val_dataset]
-        self.model.train()
         datamanager = torchreid.data.ImageDataManager(**imagedata_kwargs(self.cfg))
+        self.num_classes = datamanager.num_train_pids
+
+        self.model = self.create_model()
+        self.__load_snap_if_exists()
+
+        num_aux_models = len(self.cfg.mutual_learning.aux_configs)
+        print(self.cfg.mutual_learning.aux_configs)
+
+        if self.cfg.use_gpu:
+            main_device_ids = list(range(self.num_devices))
+            extra_device_ids = [main_device_ids for _ in range(num_aux_models)]
+            self.model = DataParallel(self.model, device_ids=main_device_ids, output_device=0).cuda(main_device_ids[0])
+        else:
+            extra_device_ids = [None for _ in range(num_aux_models)]
+
         optimizer = torchreid.optim.build_optimizer(self.model, **optimizer_kwargs(self.cfg))
-        scheduler = torchreid.optim.build_lr_scheduler(optimizer, **lr_scheduler_kwargs(self.cfg))
-        engine = build_engine(self.cfg, datamanager, self.model, optimizer, scheduler)
-        engine.run(**engine_run_kwargs(self.cfg), tb_writer=self.monitor)
+
+        if self.cfg.lr_finder.enable and self.cfg.lr_finder.mode == 'automatic' and not parameters.resume_from:
+            scheduler = None
+        else:
+            scheduler = torchreid.optim.build_lr_scheduler(optimizer, **lr_scheduler_kwargs(self.cfg))
+
+        if parameters.resume_from:
+            self.cfg.train.start_epoch = resume_from_checkpoint(
+                parameters.resume_from, self.model, optimizer=optimizer, scheduler=scheduler
+            )
+
+        if self.cfg.lr_finder.enable and not parameters.resume_from:
+            if num_aux_models:
+                print("Mutual learning is enabled. Learning rate will be estimated for the main model only.")
+
+            # build new engine
+            engine = build_engine(self.cfg, datamanager, self.model, optimizer, scheduler)
+            lr = engine.find_lr(**lr_finder_run_kwargs(self.cfg))
+
+            print(f"Estimated learning rate: {lr}")
+            if self.cfg.lr_finder.stop_after:
+                print("Finding learning rate finished. Terminate the training process")
+                return
+
+            # reload random seeds, optimizer with new lr and scheduler for it
+            self.cfg.train.lr = lr
+            self.cfg.lr_finder.enable = False
+            set_random_seed(self.cfg.train.seed)
+
+            optimizer = torchreid.optim.build_optimizer(self.model, **optimizer_kwargs(self.cfg))
+            scheduler = torchreid.optim.build_lr_scheduler(optimizer, **lr_scheduler_kwargs(self.cfg))
+
+        if num_aux_models:
+            print('Enabled mutual learning between {} models.'.format(num_aux_models + 1))
+            weights = [None] * num_aux_models
+
+            models, optimizers, schedulers = [self.model], [optimizer], [scheduler]
+            for config_file, model_weights, device_ids in zip(self.cfg.mutual_learning.aux_configs, weights, extra_device_ids):
+                aux_model, aux_optimizer, aux_scheduler = build_auxiliary_model(
+                    config_file, self.num_classes, self.cfg.use_gpu, device_ids, model_weights
+                )
+
+                models.append(aux_model)
+                optimizers.append(aux_optimizer)
+                schedulers.append(aux_scheduler)
+        else:
+            models, optimizers, schedulers = model, optimizer, scheduler
+
+        print('Building {}-engine for {}-reid'.format(self.cfg.loss.name, self.cfg.data.type))
+        engine = build_engine(self.cfg, datamanager, models, optimizers, schedulers)
+        engine.run(**engine_run_kwargs(self.cfg))
+
+        self.model = self.model.module
 
     def test(self, dataset: IDataset, parameters: BaseTaskParameters.BaseEvaluationParameters) -> (list, dict):
         self.model.eval()
