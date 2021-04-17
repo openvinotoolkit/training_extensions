33687115c1b6de68ffdb304dcf370e3157d44c0d
Ilya Krylov <krylov.ib@gmail.com>
2021-04-17 10:00:00 +0300

    fixes

--- tests/run_model_templates_tests2.py
+++ tests/run_model_templates_tests2.py
@@ -5,16 +5,22 @@ import tempfile
 from subprocess import run
 
 
+ALL_DOMAINS = os.listdir('models')
+
 def parse_args():
-    domains = os.listdir('models')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--domains', nargs='+', choices=domains)
-    parser.add_argument('--work_dir')
+    parser.add_argument('--domains', nargs='+', choices=ALL_DOMAINS,
+                        help='List of domains to test, by default all domains are tested.')
+    parser.add_argument('--work_dir',
+                        help='A location where templates will be instantiated '
+                             'together with corresponding virtual environments.')
     parser.add_argument('--test',
                         help='It is possible to specify test or test case directly: '
-                             'models/text_spotting/tests/train_tests_alphanumeric_text_spotting.py::TextSpotting0003TestCase::test_evaluation_on_gpu')
+                             'models/text_spotting/tests/train_tests_alphanumeric_text_spotting.py::'
+                             'TextSpotting0003TestCase::test_evaluation_on_gpu')
 
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse_args()
@@ -24,8 +30,7 @@ if __name__ == '__main__':
         domains = [args.test.split('/')[1]]
         to_test = args.test
     else:
-        if args.domains is None:
-            domains = os.listdir('models')
+        domains = args.domains if args.domains is not None else ALL_DOMAINS
         to_test = None
 
     if args.work_dir is not None:
@@ -36,10 +41,13 @@ if __name__ == '__main__':
     for domain in domains:
         venv_path = os.path.join(destination, domain, 'venv')
         if not os.path.exists(venv_path):
-            run(f'python3 tools/instantiate.py {destination} --templates-filter models/{domain}/**/template.yaml --verbose',
+            run(f'python3 tools/instantiate.py {destination}'
+                f' --templates-filter models/{domain}/**/template.yaml'
+                '  --verbose',
                 check=True, shell=True)
         else:
-            logging.warning(f'You are running tests inside previously created virtual environments. They might be outdated! {venv_path}')
+            logging.warning(f'You are running tests inside previously created virtual environments. '
+                            f'They might be outdated! {venv_path}')
 
     for domain in domains:
         if args.test is None:
