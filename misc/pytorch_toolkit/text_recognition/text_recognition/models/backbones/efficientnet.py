d7f6b6e3484a3af419a880d2ac18493316a685fb
Vladimir Loginov <vladimir.loginov@intel.com>
2021-05-05 09:59:12 +0300

    adapt efficientnet backbone for 1 ch input

--- misc/pytorch_toolkit/text_recognition/text_recognition/models/backbones/efficientnet.py
+++ misc/pytorch_toolkit/text_recognition/text_recognition/models/backbones/efficientnet.py
@@ -1,12 +1,16 @@
-from efficientnet_pytorch import EfficientNet
-
 import torch.nn as nn
+from efficientnet_pytorch import EfficientNet
+from efficientnet_pytorch.utils import get_same_padding_conv2d, round_filters
 
 
 class EfficientNetLikeBackbone(nn.Module):
-    def __init__(self, model_name, **kwargs):
+    def __init__(self, model_name, in_channels=1, **kwargs):
         super().__init__()
         self.model = EfficientNet.from_pretrained(model_name)
+        if in_channels != 3:
+            out_channels = round_filters(32, self.model._global_params)
+            self.model._conv_stem = get_same_padding_conv2d(image_size=self.model._global_params.image_size)(
+                in_channels, out_channels, kernel_size=3, stride=2, bias=False)
         # Final linear layer
         del self.model._avg_pooling
         if self.model._global_params.include_top:
