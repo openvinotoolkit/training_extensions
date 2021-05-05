6be0c97ee5d5b25c0ce526ec581c179cfda603f7
Vladimir Loginov <vladimir.loginov@intel.com>
2021-05-05 16:48:32 +0300

    add classification head like in CSTR

---
+++ misc/pytorch_toolkit/text_recognition/text_recognition/models/text_recognition_heads/classification_head.py
@@ -0,0 +1,25 @@
+from .attention_based_2d import Encoder
+import torch
+MAX_LEN = 28
+
+
+class ClassificationEncoderDecoderHead(torch.nn.Module):
+    def __init__(self, decoder_vocab_size, dim_input, dim_internal, num_layers, max_len=MAX_LEN):
+        super().__init__()
+        self.encoder = Encoder(dim_input, dim_internal, num_layers=num_layers)
+        self.vocab_size = decoder_vocab_size
+        self.pool = torch.nn.AdaptiveAvgPool2d(1)
+        self.max_len = max_len
+        self.convs = torch.nn.ModuleList([
+            torch.nn.Conv2d(dim_internal, self.vocab_size, 1) for _ in range(self.max_len)
+        ])
+
+    def forward(self, features, texts=None):
+        features = self.encoder(features)
+        features = self.pool(features)
+        logits = []
+        for conv in self.convs:
+            logits.append(conv(features))
+        logits = torch.stack(logits, dim=1).squeeze_(-1).squeeze_(-1)
+        classes = torch.max(logits, dim=2)[1]
+        return logits, classes
