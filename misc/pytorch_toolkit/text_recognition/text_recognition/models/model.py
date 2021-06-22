43dad15ec864bc2442a1d95d38e1caf09d95b579
Vladimir Loginov <vladimir.loginov@intel.com>
2021-06-22 15:35:44 +0300

    add transformation to wrapper

--- misc/pytorch_toolkit/text_recognition/text_recognition/models/model.py
+++ misc/pytorch_toolkit/text_recognition/text_recognition/models/model.py
@@ -48,6 +48,8 @@ class TextRecognitionModel(nn.Module):
             self.model = model
 
         def forward(self, input_images):
+            if hasattr(self, 'transformation'):
+                input_images = self.transformation(input_images)
             encoded = self.model.backbone(input_images)
             return self.model.head.encoder_wrapper(encoded)
 
