ef05f61ae303e12930df7850c0a671233f592d40
Vladimir Loginov <vladimir.loginov@intel.com>
2021-07-27 16:19:40 +0300

    disable setupvars, upgrade pip

--- misc/pytorch_toolkit/text_recognition/init_venv.sh
+++ misc/pytorch_toolkit/text_recognition/init_venv.sh
@@ -19,21 +19,23 @@ fi
 # Create virtual environment
 virtualenv ${venv_dir} -p python --prompt="(text_recognition)"
 
-path_openvino_vars="${INTEL_OPENVINO_DIR:-/opt/intel/openvino}/bin/setupvars.sh"
-if [[ -e "${path_openvino_vars}" ]]; then
-  echo ". ${path_openvino_vars}" >>${venv_dir}/bin/activate
-fi
+# path_openvino_vars="${INTEL_OPENVINO_DIR:-/opt/intel/openvino}/bin/setupvars.sh"
+# if [[ -e "${path_openvino_vars}" ]]; then
+#   echo ". ${path_openvino_vars}" >>${venv_dir}/bin/activate
+# fi
 
 . ${venv_dir}/bin/activate
 
-cat requirements.txt | xargs -n 1 -L 1 pip3 install
+pip install --upgrade pip
 
-mo_requirements_file="${INTEL_OPENVINO_DIR:-/opt/intel/openvino}/deployment_tools/model_optimizer/requirements_onnx.txt"
-if [[ -e "${mo_requirements_file}" ]]; then
-  pip install -qr ${mo_requirements_file}
-else
-  echo "[WARNING] Model optimizer requirements were not installed. Please install the OpenVino toolkit to use one."
-fi
+cat requirements.txt | xargs -n 1 -L 1 pip install
+
+# mo_requirements_file="${INTEL_OPENVINO_DIR:-/opt/intel/openvino}/deployment_tools/model_optimizer/requirements_onnx.txt"
+# if [[ -e "${mo_requirements_file}" ]]; then
+#   pip install -qr ${mo_requirements_file}
+# else
+#   echo "[WARNING] Model optimizer requirements were not installed. Please install the OpenVino toolkit to use one."
+# fi
 
 echo
 echo "Activate a virtual environment to start working:"
