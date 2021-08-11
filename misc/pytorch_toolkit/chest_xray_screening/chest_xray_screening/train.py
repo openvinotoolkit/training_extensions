cf3a462ce241f9bdc11a8daf705fdabc2f1b6791
Rakshith Sathish <rakshith.sathish@gmail.com>
2021-08-11 22:24:44 +0530

    Update misc/pytorch_toolkit/chest_xray_screening/chest_xray_screening/train.py
    Co-authored-by: Vladimir Loginov <vladimir.loginov@intel.com>

--- misc/pytorch_toolkit/chest_xray_screening/chest_xray_screening/train.py
+++ misc/pytorch_toolkit/chest_xray_screening/chest_xray_screening/train.py
@@ -69,7 +69,7 @@ class RSNATrainer():
                              'state_dict': self.model.state_dict(),
                              'best_loss': valid_loss_min,
                              'optimizer' : self.optimizer.state_dict()},
-                             savepath+'/m-epoch'+str(epoch_id)+'.pth')
+                             os.path.join(savepath, f'm-epoch-{epoch_id}.pth'))
             test_auroc = RSNATrainer.test(self)
             print(f"Epoch:{epoch_id + 1}| EndTime:{timestamp_end}| TestAUROC: {test_auroc}| ValidAUROC: {auroc_max}")
 
