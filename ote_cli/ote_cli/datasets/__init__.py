61cecc31fed972c3a011c82a61ae2ef8840795ea
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2021-09-08 14:54:24 +0300

    Add classification dataset import

--- ote_cli/ote_cli/datasets/__init__.py
+++ ote_cli/ote_cli/datasets/__init__.py
@@ -5,15 +5,24 @@ try:
 except ImportError as e:
     print(e)
 
+try:
+    from torchreid.integration.sc.utils import ClassificationDatasetAdapter
+    ClassificationDataset = ClassificationDatasetAdapter
+except ImportError as e:
+    print(e)
+    ClassificationDataset = None
+
 
 def get_dataset_class(name):
     registry = {
-        'detection': ObjectDetectionDataset
+        'detection': ObjectDetectionDataset,
+        'classification': ClassificationDataset
     }
 
     return registry[str(name).lower()]
 
 __all__ = [
     'ObjectDetectionDataset',
+    'ClassificationDataset',
     'get_dataset_class',
 ]
