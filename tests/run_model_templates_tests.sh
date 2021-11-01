be92d43f60b534db517da2c2a8d61b8c40655f63
Ilya Krylov <ilya.krylov@intel.com>
2021-11-01 11:58:32 +0300

    Update run_model_templates_tests.sh

--- tests/run_model_templates_tests.sh
+++ tests/run_model_templates_tests.sh
@@ -4,6 +4,7 @@ pip install -e ote_cli || exit 1
 pip install -e $SC_SDK_REPO/src/ote_sdk || exit 1
 echo ""
 echo ""
+pip install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio===0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 pytest tests/ote_cli/ --collect-only || exit 1
 echo "Sleep 5 sec before actually running tests."
 sleep 5
