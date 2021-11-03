26d6875e0a0256eec481871aeb35f871d8eb16ad
Dick Ameln <dick.ameln@intel.com>
2021-11-03 17:21:34 +0100

    add anomaly heatmap to dataset item as metadata

--- external/anomaly/ote/callbacks/inference.py
+++ external/anomaly/ote/callbacks/inference.py
@@ -20,14 +20,15 @@ from typing import Any, List
 
 import numpy as np
 import pytorch_lightning as pl
+from anomalib.core.model.anomaly_module import AnomalyModule
+from anomalib.utils.post_process import anomaly_map_to_color_map
 from ote_sdk.entities.annotation import Annotation
 from ote_sdk.entities.datasets import DatasetEntity
 from ote_sdk.entities.label import LabelEntity
+from ote_sdk.entities.result_media import ResultMediaEntity
 from ote_sdk.entities.scored_label import ScoredLabel
 from ote_sdk.entities.shapes.rectangle import Rectangle
 from pytorch_lightning.callbacks import Callback
-
-from anomalib.core.model.anomaly_module import AnomalyModule
 
 
 class InferenceCallback(Callback):
@@ -45,9 +46,12 @@ class InferenceCallback(Callback):
         outputs = outputs[0]
         pred_scores = np.hstack([output["pred_scores"].cpu() for output in outputs])
         pred_labels = np.hstack([output["pred_labels"].cpu() for output in outputs])
+        anomaly_maps = np.vstack([output["anomaly_maps"].cpu() for output in outputs])
 
         # Loop over dataset again to assign predictions
-        for dataset_item, pred_score, pred_label in zip(self.ote_dataset, pred_scores, pred_labels):
+        for dataset_item, pred_score, pred_label, anomaly_map in zip(
+            self.ote_dataset, pred_scores, pred_labels, anomaly_maps
+        ):
 
             assigned_label = self.anomalous_label if pred_label else self.normal_label
             shape = Annotation(
@@ -56,3 +60,9 @@ class InferenceCallback(Callback):
             )
 
             dataset_item.append_annotations([shape])
+
+            heatmap = anomaly_map_to_color_map(anomaly_map.squeeze())
+            heatmap_media = ResultMediaEntity(
+                name="Anomaly map", type="anomaly_map", annotation_scene=dataset_item.annotation_scene, numpy=heatmap
+            )
+            dataset_item.append_metadata_item(heatmap_media)
