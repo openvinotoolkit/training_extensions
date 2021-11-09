6cc356e132b45bc43de866ec6984f78417e973b8
Ilya Krylov <ilya.krylov@intel.com>
2021-11-09 13:50:48 +0300

    added license headers and refactored tests/ote_cli/test_template_files.py

--- tests/ote_cli/test_template_files.py
+++ tests/ote_cli/test_template_files.py
@@ -1,26 +1,30 @@
+# Copyright (C) 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+
 import os
+
+import pytest
 
 from ote_sdk.entities.model_template import parse_model_template
 
 from ote_cli.registry import Registry
 
-def gen_parse_model_template_tests():
-    class MyTests:
-        pass
+templates = Registry('external').templates
+paths = [os.path.relpath(template['path']) for template in templates]
+names = [os.path.relpath(template['name']) for template in templates]
 
-    templates_dir = 'external'
-    registry = Registry(templates_dir)
-    for template in registry.templates:
-        path = os.path.relpath(template['path'])
-        def test_template(self, path=path):
-            template = parse_model_template(path)
-            print(template.task_type)
-            assert template.hyper_parameters.data
-        setattr(MyTests, 'test_' + path.replace(' ', '_').replace('/', '_'), test_template)
-
-    return MyTests
-
-
-class TestParseModelTemplates(gen_parse_model_template_tests()):
-    pass
-
+@pytest.mark.parametrize("path", paths, ids=names)
+def test_template(path):
+    template = parse_model_template(path)
+    assert template.hyper_parameters.data
