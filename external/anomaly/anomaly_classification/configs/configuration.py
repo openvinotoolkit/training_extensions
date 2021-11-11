a166ffacc86333a9b82766d7bdf0928977a16bcb
Dick Ameln <dick.ameln@intel.com>
2021-11-11 13:57:48 +0100

    added docstrings

--- external/anomaly/anomaly_classification/configs/configuration.py
+++ external/anomaly/anomaly_classification/configs/configuration.py
@@ -1,3 +1,6 @@
+"""
+Configurable parameters for anomaly classification task
+"""
 # Copyright (C) 2021 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- external/anomaly/anomaly_classification/configs/stfpm/configuration.py
+++ external/anomaly/anomaly_classification/configs/stfpm/configuration.py
@@ -1,3 +1,6 @@
+"""
+Configurable parameters for STFPM anomaly classification task
+"""
 # Copyright (C) 2021 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
