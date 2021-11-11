91064c35b6d44d6d660157d59eb1d91436a78288
Ilya Krylov <ilya.krylov@intel.com>
2021-11-11 12:06:53 +0300

    replaced virtualenv by venv

--- tests/run_model_templates_tests.sh
+++ tests/run_model_templates_tests.sh
@@ -1,4 +1,4 @@
-virtualenv venv -p python3 || exit 1
+python3 -m venv venv || exit 1
 . venv/bin/activate || exit 1
 pip install -e ote_cli || exit 1
 pip install -e $SC_SDK_REPO/src/ote_sdk || exit 1
