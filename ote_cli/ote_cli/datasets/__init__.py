45320a98c2b4747c2bdc553b40d25335e628f02f
Ilya Krylov <ilya.krylov@intel.com>
2021-11-17 10:47:51 +0300

    fixed mypy checks in ote_cli

--- ote_cli/ote_cli/datasets/__init__.py
+++ ote_cli/ote_cli/datasets/__init__.py
@@ -1,9 +1,15 @@
+from typing import Optional, Type
+
+from ote_sdk.ote_sdk.entities.datasets import DatasetEntity
+
 try:
+    ObjectDetectionDataset: Optional[Type[DatasetEntity]]
     from .object_detection.dataset import ObjectDetectionDataset
 except ImportError:
     ObjectDetectionDataset = None
 
 try:
+    ClassificationDataset: Optional[Type[DatasetEntity]]
     from torchreid.integration.sc.utils import ClassificationDatasetAdapter
 
     ClassificationDataset = ClassificationDatasetAdapter
@@ -11,6 +17,7 @@ except ImportError:
     ClassificationDataset = None
 
 try:
+    SemanticSegmentationDataset: Optional[Type[DatasetEntity]]
     from .semantic_segmentation.dataset import SemanticSegmentationDataset
 except ImportError:
     SemanticSegmentationDataset = None
