933138786396ce3101d9ad5ec4a315db89707ada
akorobeinikov <aleksei.korobeinikov@intel.com>
2021-11-23 13:03:25 +0300

    add folder for package

---
+++ ote_sdk/ote_sdk/usecases/exportable_code/demo/demo_package/sync.py
@@ -0,0 +1,98 @@
+# INTEL CONFIDENTIAL
+#
+# Copyright (C) 2021 Intel Corporation
+#
+# This software and the related documents are Intel copyrighted materials, and
+# your use of them is governed by the express license under which they were provided to
+# you ("License"). Unless the License provides otherwise, you may not use, modify, copy,
+# publish, distribute, disclose or transmit this software or the related documents
+# without Intel's prior written permission.
+#
+# This software and the related documents are provided as is,
+# with no express or implied warranties, other than those that are expressly stated
+# in the License.
+
+import os
+import sys
+from argparse import ArgumentParser, SUPPRESS, Namespace
+from pathlib import Path
+
+
+from ote_sdk.usecases.exportable_code.streamer import  get_streamer, get_media_type
+from ote_sdk.usecases.exportable_code.visualization import Visualizer
+from openvino.model_zoo.model_api import models, pipelines
+from openvino.inference_engine import IECore
+from .utils import create_model, create_output_converter
+
+def build_argparser():
+    parser = ArgumentParser(add_help=False)
+    args = parser.add_argument_group('Options')
+    args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
+    args.add_argument('-i', '--input', required=True,
+                      help='Required. An input to process. The input must be a single image.')
+    args.add_argument('-m', '--model', help='Required. Path to an .xml file with a trained model.',
+                      default=None, type=Path)
+    args.add_argument('-d', '--device', default='CPU', type=str,
+                      help='Optional. Specify the target device to infer on; CPU, GPU, HDDL or MYRIAD is '
+                           'acceptable. The demo will look for a suitable plugin for device specified. '
+                           'Default value is CPU.')
+
+    infer_args = parser.add_argument_group('Inference options')
+    infer_args.add_argument('-nireq', '--num_infer_requests', help='Optional. Number of infer requests',
+                            default=0, type=int)
+    infer_args.add_argument('-nstreams', '--num_streams',
+                            help='Optional. Number of streams to use for inference on the CPU or/and GPU in throughput '
+                                 'mode (for HETERO and MULTI device cases use format '
+                                 '<device1>:<nstreams1>,<device2>:<nstreams2> or just <nstreams>).',
+                            default='', type=str)
+    infer_args.add_argument('-nthreads', '--num_threads', default=None, type=int,
+                            help='Optional. Number of threads to use for inference on CPU (including HETERO cases).')
+
+    return parser
+
+def get_infer_parameters(args):
+    params = {
+        'device': args.device,
+        'streams': args.num_streams,
+        'threads': args.num_threads,
+        'infer_requests': args.num_infer_requests
+    }
+    return params
+
+
+class SyncDemo:
+    def __init__(self, model, visualizer, converter) -> None:
+        self.model = model
+        self.visualizer = visualizer
+        self.converter = converter
+
+    def run(self, input_stream):
+        self.streamer = get_streamer(input_stream)
+        for frame in self.streamer:
+            # getting result include preprocessing, infer, postprocessing for sync infer
+            dict_data, input_meta = self.model.preprocess(frame)
+            raw_result = self.model.infer_sync(dict_data)
+            predictions = self.model.postprocess(raw_result, input_meta)
+            annotation_scene = self.converter.convert_to_annotation(predictions, input_meta)
+
+            # any user's visualizer
+            output = self.visualizer.draw(frame, annotation_scene)
+            self.visualizer.show(output)
+
+            if self.visualizer.is_quit():
+                break
+
+def main():
+    args = build_argparser().parse_args()
+    # create components for demo
+
+    model = create_model(Namespace(**get_infer_parameters(args)), args.model)
+    media_type = get_media_type(args.input)
+
+    visualizer = Visualizer(media_type)
+    converter = create_output_converter(model.labels)
+    demo = SyncDemo(model, visualizer, converter)
+    demo.run(args.input)
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)
---
+++ ote_sdk/ote_sdk/usecases/exportable_code/demo/demo_package/utils.py
@@ -0,0 +1,62 @@
+# INTEL CONFIDENTIAL
+#
+# Copyright (C) 2021 Intel Corporation
+#
+# This software and the related documents are Intel copyrighted materials, and
+# your use of them is governed by the express license under which they were provided to
+# you ("License"). Unless the License provides otherwise, you may not use, modify, copy,
+# publish, distribute, disclose or transmit this software or the related documents
+# without Intel's prior written permission.
+#
+# This software and the related documents are provided as is,
+# with no express or implied warranties, other than those that are expressly stated
+# in the License.
+
+import importlib
+import json
+import os
+from pathlib import Path
+from openvino.model_zoo.model_api import models, pipelines
+from openvino.model_zoo.model_api.adapters import create_core, OpenvinoAdapter
+from ote_sdk.entities.label import Domain, LabelEntity
+from ote_sdk.usecases.exportable_code.prediction_to_annotation_converter import create_converter
+
+def get_model_path(path):
+    model_path = path
+    if model_path is None:
+        model_path = Path(__file__).parent / 'model.xml'
+        if not os.path.exists(model_path):
+            raise IOError("The path to the model was not found.")
+
+    return model_path
+
+def get_parameters(path):
+    parameters_path = path
+    if parameters_path is None:
+        parameters_path = Path(__file__).parent / 'config.json'
+        if not os.path.exists(parameters_path):
+            raise IOError("The path to the config was not found.")
+
+    with open(parameters_path, 'r') as f:
+        parameters = json.load(f)
+
+    return parameters
+
+def create_model(infer_parameters, model_path=None, config_file=None):
+    plugin_config = pipelines.get_user_config(infer_parameters.device, infer_parameters.streams, infer_parameters.threads)
+    model_adapter = OpenvinoAdapter(create_core(), get_model_path(model_path), device=infer_parameters.device,
+                                    plugin_config=plugin_config, max_num_requests=infer_parameters.infer_requests)
+    parameters = get_parameters(config_file)
+    try:
+        importlib.import_module('.model', parameters['name_of_model'].lower())
+    except BaseException as e:
+        print("Using model wrapper from Open Model Zoo ModelAPI")
+    model = models.Model.create_model(parameters['type_of_model'], model_adapter, parameters['model_parameters'])
+    model.load()
+
+    return model
+
+def create_output_converter(labels, config_file=None):
+    parameters = get_parameters(config_file)
+    type = Domain[parameters['converter_type']]
+    return create_converter(type, labels)
