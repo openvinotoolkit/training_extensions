bd5c6bc263b6736339da014df8c777b84c46efe0
Rakshith2597 <rakshith.sathish@gmail.com>
2021-11-23 14:55:40 +0530

    replaced os.system with pycurl

--- misc/pytorch_toolkit/chest_xray_screening/chest_xray_screening/utils/download_weights.py
+++ misc/pytorch_toolkit/chest_xray_screening/chest_xray_screening/utils/download_weights.py
@@ -1,6 +1,7 @@
 from .get_config import get_config
 import os
 import zipfile
+import pycurl
 
 def download_checkpoint():
     config = get_config(action = 'download')
@@ -10,10 +11,22 @@ def download_checkpoint():
     densenet_path = config['densenet']['dest_path']
     denseneteff_url = config['densenet_eff']['url']
     denseneteff_path = config['densenet_eff']['dest_path']
-    os.system(f'wget -mnv {densenet_url} -O {densenet_path}')
+
+    with open(densenet_path, 'wb') as f:
+        cl = pycurl.Curl()
+        cl.setopt(cl.URL, densenet_url)
+        cl.setopt(cl.WRITEDATA, f)
+        cl.perform()
+        cl.close()
     with zipfile.ZipFile(config['densenet']['dest_path'], 'r') as zip_ref:
         zip_ref.extractall('model_weights/')
-    os.system(f'wget -mnv {denseneteff_url} -O {denseneteff_path}')
+
+    with open(denseneteff_path, 'wb') as f:
+        cl = pycurl.Curl()
+        cl.setopt(cl.URL, denseneteff_url)
+        cl.setopt(cl.WRITEDATA, f)
+        cl.perform()
+        cl.close()
     with zipfile.ZipFile(config['densenet_eff']['dest_path'], 'r') as zip_ref2:
         zip_ref2.extractall('model_weights/')
 
@@ -24,6 +37,11 @@ def download_data():
         os.makedirs('test_data')
     data_url = config['test_data']['url']
     data_path = config['test_data']['dest_path']
-    os.system(f'wget -mnv {data_url} -O {data_path}')
+    with open(data_path, 'wb') as f:
+        cl = pycurl.Curl()
+        cl.setopt(cl.URL, data_url)
+        cl.setopt(cl.WRITEDATA, f)
+        cl.perform()
+        cl.close()
     with zipfile.ZipFile(config['test_data']['dest_path'], 'r') as zip_ref:
         zip_ref.extractall('test_data/chest_xray_data/')
