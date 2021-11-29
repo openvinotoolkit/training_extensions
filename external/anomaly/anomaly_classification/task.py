c6354cfce0ccbcafd7c8346f2b3fc2188e3937bd
Ilya Krylov <ilya.krylov@intel.com>
2021-11-29 12:32:37 +0300

    updated anomaly

--- external/anomaly/anomaly_classification/task.py
+++ external/anomaly/anomaly_classification/task.py
@@ -147,7 +147,7 @@ class AnomalyClassificationTask(ITrainingTask, IInferenceTask, IEvaluationTask, 
         model_info = {
             "model": self.model.state_dict(),
             "config": config,
-            "labels": labels,
+            "label_schema": self.task_environment.label_schema,
             "VERSION": 1,
         }
         buffer = io.BytesIO()
