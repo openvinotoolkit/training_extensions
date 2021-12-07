08dfae3b128621f1b39b421703e560181e3fee48
Ilya Krylov <ilya.krylov@intel.com>
2021-12-07 14:03:51 +0300

    minor

--- ote_sdk/ote_sdk/tests/serialization/test_datetime_mapper.py
+++ ote_sdk/ote_sdk/tests/serialization/test_datetime_mapper.py
@@ -14,18 +14,27 @@
 # in the License.
 #
 
+import pytest
+
 from ote_sdk.serialization.datetime_mapper import DatetimeMapper
+from ote_sdk.tests.constants.ote_sdk_components import OteSdkComponent
+from ote_sdk.tests.constants.requirements import Requirements
 from ote_sdk.utils.time_utils import now
 
 
-def test_serialization_deserialization():
-    """
-    This test serializes datetime, deserializes serialized datetime and compares with original one.
-    """
+@pytest.mark.components(OteSdkComponent.OTE_SDK)
+class TestDatetimeMapper:
+    @pytest.mark.priority_medium
+    @pytest.mark.component
+    @pytest.mark.reqids(Requirements.REQ_1)
+    def test_serialization_deserialization(self):
+        """
+        This test serializes datetime, deserializes serialized datetime and compares with original one.
+        """
 
-    original_time = now()
-    serialized_time = DatetimeMapper.forward(original_time)
-    assert serialized_time == original_time.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        original_time = now()
+        serialized_time = DatetimeMapper.forward(original_time)
+        assert serialized_time == original_time.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
-    deserialized_time = DatetimeMapper.backward(serialized_time)
-    assert original_time == deserialized_time
+        deserialized_time = DatetimeMapper.backward(serialized_time)
+        assert original_time == deserialized_time
--- ote_sdk/ote_sdk/tests/serialization/test_label_mapper.py
+++ ote_sdk/ote_sdk/tests/serialization/test_label_mapper.py
@@ -17,6 +17,8 @@
 
 from random import randint
 
+import pytest
+
 from ote_sdk.entities.id import ID
 from ote_sdk.entities.label import Color, Domain, LabelEntity
 from ote_sdk.entities.label_schema import LabelSchemaEntity
@@ -26,135 +28,152 @@ from ote_sdk.serialization.label_mapper import (
     LabelMapper,
     LabelSchemaMapper,
 )
+from ote_sdk.tests.constants.ote_sdk_components import OteSdkComponent
+from ote_sdk.tests.constants.requirements import Requirements
 from ote_sdk.utils.time_utils import now
 
 
-def test_color_serialization():
-    """
-    This test serializes Color and checks serialized representation.
-    Then it compares deserialized Color with original one.
-    """
+@pytest.mark.components(OteSdkComponent.OTE_SDK)
+class TestColorMapper:
+    @pytest.mark.priority_medium
+    @pytest.mark.component
+    @pytest.mark.reqids(Requirements.REQ_1)
+    def test_color_serialization(self):
+        """
+        This test serializes Color and checks serialized representation.
+        Then it compares deserialized Color with original one.
+        """
 
-    red = randint(0, 255)
-    green = randint(0, 255)
-    blue = randint(0, 255)
-    alpha = randint(0, 255)
-    color = Color(red, green, blue, alpha)
-    serialized = ColorMapper.forward(color)
-    assert serialized == {"red": red, "green": green, "blue": blue, "alpha": alpha}
+        red = randint(0, 255)
+        green = randint(0, 255)
+        blue = randint(0, 255)
+        alpha = randint(0, 255)
+        color = Color(red, green, blue, alpha)
+        serialized = ColorMapper.forward(color)
+        assert serialized == {"red": red, "green": green, "blue": blue, "alpha": alpha}
 
-    deserialized = ColorMapper.backward(serialized)
-    assert color == deserialized
-
-
-def test_label_entity_serialization():
-    """
-    This test serializes LabelEntity and checks serialized representation.
-    Then it compares deserialized LabelEntity with original one.
-    """
-
-    cur_date = now()
-    red = randint(0, 255)
-    green = randint(0, 255)
-    blue = randint(0, 255)
-    alpha = randint(0, 255)
-
-    label = LabelEntity(
-        name="my_label",
-        domain=Domain.DETECTION,
-        color=Color(red, green, blue, alpha),
-        hotkey="ctrl+1",
-        creation_date=cur_date,
-        is_empty=False,
-        id=ID("0000213"),
-    )
-    serialized = LabelMapper.forward(label)
-
-    assert serialized == {
-        "_id": "0000213",
-        "name": "my_label",
-        "color": {"red": red, "green": green, "blue": blue, "alpha": alpha},
-        "hotkey": "ctrl+1",
-        "domain": "DETECTION",
-        "creation_date": DatetimeMapper.forward(cur_date),
-        "is_empty": False,
-    }
-
-    deserialized = LabelMapper.backward(serialized)
-    assert label == deserialized
+        deserialized = ColorMapper.backward(serialized)
+        assert color == deserialized
 
 
-def test_flat_label_schema_serialization():
-    """
-    This test serializes flat LabelSchema and checks serialized representation.
-    Then it compares deserialized LabelSchema with original one.
-    """
+@pytest.mark.components(OteSdkComponent.OTE_SDK)
+class TestLabelEntityMapper:
+    @pytest.mark.priority_medium
+    @pytest.mark.component
+    @pytest.mark.reqids(Requirements.REQ_1)
+    def test_label_entity_serialization(self):
+        """
+        This test serializes LabelEntity and checks serialized representation.
+        Then it compares deserialized LabelEntity with original one.
+        """
 
-    cur_date = now()
-    names = ["cat", "dog", "mouse"]
-    colors = [
-        Color(randint(0, 255), randint(0, 255), randint(0, 255), randint(0, 255))
-        for _ in range(3)
-    ]
-    labels = [
-        LabelEntity(
-            name=name,
-            domain=Domain.CLASSIFICATION,
+        cur_date = now()
+        red = randint(0, 255)
+        green = randint(0, 255)
+        blue = randint(0, 255)
+        alpha = randint(0, 255)
+
+        label = LabelEntity(
+            name="my_label",
+            domain=Domain.DETECTION,
+            color=Color(red, green, blue, alpha),
+            hotkey="ctrl+1",
             creation_date=cur_date,
-            id=ID(i),
-            color=colors[i],
+            is_empty=False,
+            id=ID("0000213"),
         )
-        for i, name in enumerate(names)
-    ]
-    label_shema = LabelSchemaEntity.from_labels(labels)
-    serialized = LabelSchemaMapper.forward(label_shema)
+        serialized = LabelMapper.forward(label)
 
-    assert serialized == {
-        "label_tree": {"type": "tree", "directed": True, "nodes": [], "edges": []},
-        "exclusivity_graph": {
-            "type": "graph",
-            "directed": False,
-            "nodes": [],
-            "edges": [],
-        },
-        "label_groups": [
-            {
-                "_id": label_shema.get_groups()[0].id,
-                "name": "from_label_list",
-                "label_ids": ["0", "1", "2"],
-                "relation_type": "EXCLUSIVE",
-            }
-        ],
-        "all_labels": {
-            "0": {
-                "_id": "0",
-                "name": "cat",
-                "color": ColorMapper.forward(colors[0]),
-                "hotkey": "",
-                "domain": "CLASSIFICATION",
-                "creation_date": DatetimeMapper.forward(cur_date),
-                "is_empty": False,
-            },
-            "1": {
-                "_id": "1",
-                "name": "dog",
-                "color": ColorMapper.forward(colors[1]),
-                "hotkey": "",
-                "domain": "CLASSIFICATION",
-                "creation_date": DatetimeMapper.forward(cur_date),
-                "is_empty": False,
-            },
-            "2": {
-                "_id": "2",
-                "name": "mouse",
-                "color": ColorMapper.forward(colors[2]),
-                "hotkey": "",
-                "domain": "CLASSIFICATION",
-                "creation_date": DatetimeMapper.forward(cur_date),
-                "is_empty": False,
-            },
-        },
-    }
+        assert serialized == {
+            "_id": "0000213",
+            "name": "my_label",
+            "color": {"red": red, "green": green, "blue": blue, "alpha": alpha},
+            "hotkey": "ctrl+1",
+            "domain": "DETECTION",
+            "creation_date": DatetimeMapper.forward(cur_date),
+            "is_empty": False,
+        }
 
-    deserialized = LabelSchemaMapper.backward(serialized)
-    assert label_shema == deserialized
+        deserialized = LabelMapper.backward(serialized)
+        assert label == deserialized
+
+
+@pytest.mark.components(OteSdkComponent.OTE_SDK)
+class TestLabelSchemaEntityMapper:
+    @pytest.mark.priority_medium
+    @pytest.mark.component
+    @pytest.mark.reqids(Requirements.REQ_1)
+    def test_flat_label_schema_serialization(self):
+        """
+        This test serializes flat LabelSchema and checks serialized representation.
+        Then it compares deserialized LabelSchema with original one.
+        """
+
+        cur_date = now()
+        names = ["cat", "dog", "mouse"]
+        colors = [
+            Color(randint(0, 255), randint(0, 255), randint(0, 255), randint(0, 255))
+            for _ in range(3)
+        ]
+        labels = [
+            LabelEntity(
+                name=name,
+                domain=Domain.CLASSIFICATION,
+                creation_date=cur_date,
+                id=ID(i),
+                color=colors[i],
+            )
+            for i, name in enumerate(names)
+        ]
+        label_shema = LabelSchemaEntity.from_labels(labels)
+        serialized = LabelSchemaMapper.forward(label_shema)
+
+        assert serialized == {
+            "label_tree": {"type": "tree", "directed": True, "nodes": [], "edges": []},
+            "exclusivity_graph": {
+                "type": "graph",
+                "directed": False,
+                "nodes": [],
+                "edges": [],
+            },
+            "label_groups": [
+                {
+                    "_id": label_shema.get_groups()[0].id,
+                    "name": "from_label_list",
+                    "label_ids": ["0", "1", "2"],
+                    "relation_type": "EXCLUSIVE",
+                }
+            ],
+            "all_labels": {
+                "0": {
+                    "_id": "0",
+                    "name": "cat",
+                    "color": ColorMapper.forward(colors[0]),
+                    "hotkey": "",
+                    "domain": "CLASSIFICATION",
+                    "creation_date": DatetimeMapper.forward(cur_date),
+                    "is_empty": False,
+                },
+                "1": {
+                    "_id": "1",
+                    "name": "dog",
+                    "color": ColorMapper.forward(colors[1]),
+                    "hotkey": "",
+                    "domain": "CLASSIFICATION",
+                    "creation_date": DatetimeMapper.forward(cur_date),
+                    "is_empty": False,
+                },
+                "2": {
+                    "_id": "2",
+                    "name": "mouse",
+                    "color": ColorMapper.forward(colors[2]),
+                    "hotkey": "",
+                    "domain": "CLASSIFICATION",
+                    "creation_date": DatetimeMapper.forward(cur_date),
+                    "is_empty": False,
+                },
+            },
+        }
+
+        deserialized = LabelSchemaMapper.backward(serialized)
+        assert label_shema == deserialized
