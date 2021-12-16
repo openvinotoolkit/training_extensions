3dde4debe5c09761e661a57d72957cb162bd84a0
Ilya Krylov <ilya.krylov@intel.com>
2021-12-16 13:28:43 +0300

    Update run_model_templates_tests.sh

--- tests/run_model_templates_tests.sh
+++ tests/run_model_templates_tests.sh
@@ -3,6 +3,10 @@ python3 -m venv venv || exit 1
 pip install --upgrade pip || exit 1
 pip install -e ote_cli || exit 1
 pip install -e $OTE_SDK_PATH || exit 1
+. venv/bin/activate || exit 1
+export PYTHONPATH=${PYTHONPATH}:`pwd`
+pytest tests/ote_cli/ -v --durations=0
+deactivate
 echo ""
 echo ""
 
@@ -12,9 +16,4 @@ bash ./init_venv.sh /tmp/ote-anomaly
 . /tmp/ote-anomaly/bin/activate
 pip install pytest 
 pytest tests/ -v || exit 1
-# pytest tests/ -v && cd ../../ && pytest tests/ote_cli/test_ote_cli_tools_anomaly_classification.py -v || exit 1
 cd -
-
-# . venv/bin/activate || exit 1
-# export PYTHONPATH=${PYTHONPATH}:`pwd`
-# pytest tests/ote_cli/ -v --durations=0
