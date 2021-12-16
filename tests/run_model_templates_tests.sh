2de2f0e2fabbdd4c826f21b82de8acd2fcaeaad2
Ilya Krylov <ilya.krylov@intel.com>
2021-12-16 13:29:54 +0300

    Update run_model_templates_tests.sh

--- tests/run_model_templates_tests.sh
+++ tests/run_model_templates_tests.sh
@@ -3,8 +3,11 @@ python3 -m venv venv || exit 1
 pip install --upgrade pip || exit 1
 pip install -e ote_cli || exit 1
 pip install -e $OTE_SDK_PATH || exit 1
+echo ""
+echo ""
+
 export PYTHONPATH=${PYTHONPATH}:`pwd`
-pytest tests/ote_cli/ -v --durations=0
+pytest tests/ote_cli/ -v --durations=0 || exit 1
 deactivate
 echo ""
 echo ""
