37273a5acbf1301d111cf8e4a7c366dbe35a5931
Junze Wu <junze.wu@intel.com>
2021-12-20 11:16:27 +0800

    Update data/handwritten_text_recognition/create_lmdb_dataset.py
    Co-authored-by: Vladimir Loginov <vladimir.loginov@intel.com>

--- data/handwritten_text_recognition/create_lmdb_dataset.py
+++ data/handwritten_text_recognition/create_lmdb_dataset.py
@@ -1,4 +1,18 @@
-""" a modified version of CRNN torch repository https://github.com/bgshih/crnn/blob/master/tool/create_dataset.py """
+""" 
+ Copyright (c) 2021 Intel Corporation
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ 
+ 
+a modified version of CRNN torch repository https://github.com/bgshih/crnn/blob/master/tool/create_dataset.py 
+"""
 
 import fire
 import os
