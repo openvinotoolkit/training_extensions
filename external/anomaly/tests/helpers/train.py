c4684461f8a4ff05c67524dc2b6fddccc91ef667
Alexander Suslov <alexander.suslov@intel.com>
2021-12-24 14:28:17 +0300

    removed sample.py

--- external/anomaly/tests/helpers/train.py
+++ external/anomaly/tests/helpers/train.py
@@ -26,7 +26,6 @@ from anomaly_classification import (
     AnomalyClassificationTask,
     OpenVINOAnomalyClassificationTask,
 )
-from ote_anomalib.data.helpers import OTEAnomalyDatasetGenerator
 from ote_sdk.configuration.helper import create
 from ote_sdk.entities.inference_parameters import InferenceParameters
 from ote_sdk.entities.label_schema import LabelSchemaEntity
@@ -39,6 +38,7 @@ from ote_sdk.entities.task_environment import TaskEnvironment
 from ote_sdk.entities.train_parameters import TrainParameters
 from ote_sdk.usecases.tasks.interfaces.export_interface import ExportType
 from ote_sdk.usecases.tasks.interfaces.optimization_interface import OptimizationType
+from tests.helpers.dataset import OTEAnomalyDatasetGenerator
 
 logger = logging.getLogger(__name__)
 
