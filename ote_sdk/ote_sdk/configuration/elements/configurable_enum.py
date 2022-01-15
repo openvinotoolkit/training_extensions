32d9122f8bf966f48f0159584368b387a7470ba9
Leonid Beynenson <Leonid.Beynenson@intel.com>
2022-01-15 22:40:57 +0300

    Update licenses where it is required

--- ote_sdk/ote_sdk/configuration/elements/configurable_enum.py
+++ ote_sdk/ote_sdk/configuration/elements/configurable_enum.py
@@ -2,19 +2,8 @@
 This module contains the ConfigurableEnum, that is used to define Enums for which the value can be configured by
 the user
 """
-# INTEL CONFIDENTIAL
-#
-# Copyright (C) 2021 Intel Corporation
-#
-# This software and the related documents are Intel copyrighted materials, and
-# your use of them is governed by the express license under which they were provided to
-# you ("License"). Unless the License provides otherwise, you may not use, modify, copy,
-# publish, distribute, disclose or transmit this software or the related documents
-# without Intel's prior written permission.
-#
-# This software and the related documents are provided as is,
-# with no express or implied warranties, other than those that are expressly stated
-# in the License.
+# Copyright (C) 2021-2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
 #
 
 from enum import Enum
