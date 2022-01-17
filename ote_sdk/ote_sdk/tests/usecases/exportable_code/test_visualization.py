8cd6272047be6c23e59b0517134f60c58ccc88ac
saltykox <valeriyx.saltykov@intel.com>
2022-01-17 10:15:09 +0300

    updated licenses in new tests, removed test to check "is_quit" method of Visualizer class due to required xcb library

--- ote_sdk/ote_sdk/tests/usecases/exportable_code/test_visualization.py
+++ ote_sdk/ote_sdk/tests/usecases/exportable_code/test_visualization.py
@@ -1,16 +1,6 @@
-# Copyright (C) 2020-2021 Intel Corporation
+# Copyright (C) 2021-2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions
-# and limitations under the License.
 
 import cv2
 import numpy as np
@@ -199,21 +189,3 @@ class TestVisualizer:
 
         actual_image = Visualizer().draw(image=image, annotation=annotation_scene)
         assert np.array_equal(actual_image, expected_image)
-
-    @pytest.mark.priority_medium
-    @pytest.mark.component
-    @pytest.mark.reqids(Requirements.REQ_1)
-    def test_visualizer_is_quit(self):
-        """
-        <b>Description:</b>
-        Check "Visualizer" class "is_quit" method
-
-        <b>Input data:</b>
-        "Visualizer" object with specified attributes
-
-        <b>Expected results:</b>
-        Test passes if bool value returned by "is_quit" method is equal to expected
-        """
-        visualizer = Visualizer(delay=1001)
-        is_quit = visualizer.is_quit()
-        assert not is_quit
