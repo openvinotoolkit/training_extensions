e3623e97da0e15ab0e8d54b0d8a915a92d0b701b
Dick Ameln <dick.ameln@intel.com>
2022-01-18 15:14:31 +0100

    make early stopping patience configurable, use f1 as default metric

--- external/anomaly/anomaly_classification/configs/stfpm/configuration.py
+++ external/anomaly/anomaly_classification/configs/stfpm/configuration.py
@@ -16,15 +16,18 @@ Configurable parameters for STFPM anomaly classification task
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
+from attr import attrs
+
 from anomaly_classification.configs.configuration import BaseAnomalyClassificationConfig
 from anomaly_classification.configs.configuration_enums import EarlyStoppingMetrics
-from attr import attrs
 from ote_sdk.configuration.elements import (
     ParameterGroup,
     add_parameter_group,
     selectable,
     string_attribute,
+    configurable_integer,
 )
+from ote_sdk.configuration.model_lifecycle import ModelLifecycle
 
 
 @attrs
@@ -55,9 +58,22 @@ class STFPMConfig(BaseAnomalyClassificationConfig):
             description = header
 
             metric = selectable(
-                default_value=EarlyStoppingMetrics.IMAGE_ROC_AUC,
+                default_value=EarlyStoppingMetrics.IMAGE_F1,
                 header="Early Stopping Metric",
                 description="The metric used to determine if the model should stop training",
+            )
+
+            patience = configurable_integer(
+                default_value=10,
+                min_value=1,
+                max_value=100,
+                header="Early Stopping Patience",
+                description="Number of epochs to wait for an improvement in the monitored metric. If the metric has "
+                            "not improved for this many epochs, the training will stop and the best model will be "
+                            "returned.",
+                warning="Setting this value too low might lead to underfitting. Setting the value too high will "
+                        "increase the training time and might lead to overfitting.",
+                affects_outcome_of=ModelLifecycle.TRAINING,
             )
 
         early_stopping = add_parameter_group(EarlyStoppingParameters)
