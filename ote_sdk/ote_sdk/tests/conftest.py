6f0e21f9cece85309f32c314c033021a382f7ff5
aiarkinx <antonx.iarkin@intel.com>
2022-01-24 22:24:54 +0300

    ote_sdk conftest update

--- ote_sdk/ote_sdk/tests/conftest.py
+++ ote_sdk/ote_sdk/tests/conftest.py
@@ -1,11 +1,33 @@
-"""
-Fixtures.
-"""
-
-# Copyright (C) 2021-2022 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2021 Intel Corporation
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
 
+
+import pytest
+
+from ote_sdk.test_suite.pytest_insertions import *
 from ote_sdk.tests.fixtures.general import (  # noqa #pylint: disable=unused-import
     label_schema_example,
 )
+
+pytest_plugins = get_pytest_plugins_from_ote()
+
+ote_conftest_insertion(default_repository_name='ote/training_extensions/')
+
+
+# pytest magic
+def pytest_generate_tests(metafunc):
+    ote_pytest_generate_tests_insertion(metafunc)
+
+def pytest_addoption(parser):
+    ote_pytest_addoption_insertion(parser)
