b10958efa56ef3a7dc71263dde58a6ec5595e845
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2022-01-24 15:57:59 +0300

    enable cudnn

--- external/anomaly/anomaly_classification/task.py
+++ external/anomaly/anomaly_classification/task.py
@@ -60,7 +60,7 @@ class AnomalyClassificationTask(ITrainingTask, IInferenceTask, IEvaluationTask, 
         Args:
             task_environment (TaskEnvironment): OTE Task environment.
         """
-        torch.backends.cudnn.enabled = False
+        torch.backends.cudnn.enabled = True
         logger.info("Initializing the task environment.")
         logger.info(subprocess.call("nvidia-smi"))
         logger.info("Torch Version '%s'", torch.__version__)
