e7c4c08a78a749cd426d282bfb22ed292777ebb7
Leonid Beynenson <Leonid.Beynenson@intel.com>
2022-02-03 13:34:17 +0300

    Add docstrings to ote_sdk.test_suite.logging

--- ote_sdk/ote_sdk/test_suite/logging.py
+++ ote_sdk/ote_sdk/test_suite/logging.py
@@ -2,15 +2,30 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+"""
+The module with common logger for all OTE training tests.
+"""
+
 import logging
 
 
 def get_logger():
+    """
+    The function returns the common logger for all OTE
+    training tests.
+    """
     logger_name = ".".join(__name__.split(".")[:-1])
     return logging.getLogger(logger_name)
 
 
 def set_log_level(level, recursive=False):
+    """
+    The function sets log level for the common logger for all
+    OTE training tests.
+    The parameter `level` may be either int code or string
+    (e.g. 'DEBUG')
+    """
+
     logger = get_logger()
     logger.setLevel(level)
     if not recursive:
