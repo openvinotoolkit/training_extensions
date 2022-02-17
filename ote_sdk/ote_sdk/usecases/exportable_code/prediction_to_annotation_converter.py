318e4e6050cd6d96cf7744b82c5c2eb4acd0c681
Harmjan <harmjan.de.vries@intel.com>
2022-02-17 09:19:46 +0100

    fix bug causing test breaking

--- ote_sdk/ote_sdk/usecases/exportable_code/prediction_to_annotation_converter.py
+++ ote_sdk/ote_sdk/usecases/exportable_code/prediction_to_annotation_converter.py
@@ -175,7 +175,7 @@ class DetectionBoxToAnnotationConverter(IPredictionToAnnotationConverter):
         annotations = []
         image_size = metadata["original_shape"][1::-1]
         for box in predictions:
-            scored_label = ScoredLabel(self.labels[int(box.id_)], float(box.score))
+            scored_label = ScoredLabel(self.labels[int(box.id)], float(box.score))
             coords = np.array(box.get_coords(), dtype=float) / np.tile(image_size, 2)
             annotations.append(
                 Annotation(
