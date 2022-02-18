14d21ab345bc78bf4cf5adc42dd52054ae179cdc
Ilya Krylov <ilya.krylov@intel.com>
2022-02-18 16:53:20 +0300

    nncf_eval_testing fix

--- tests/ote_cli/test_ote_cli_tools_anomaly_classification.py
+++ tests/ote_cli/test_ote_cli_tools_anomaly_classification.py
@@ -132,7 +132,7 @@ class TestToolsAnomalyClassification:
         if template.entrypoints.nncf is None:
             pytest.skip("nncf entrypoint is none")
 
-        nncf_eval_testing(template, root, ote_dir, args, threshold=0.1)
+        nncf_eval_testing(template, root, ote_dir, args, threshold=0.001)
 
     @e2e_pytest_component
     @pytest.mark.parametrize("template", templates, ids=templates_ids)
