e5eac293129bf2962829de93390c420798e9903b
Ilya Krylov <ilya.krylov@intel.com>
2022-02-21 08:16:45 +0300

    Update run_model_templates_tests.sh

--- tests/run_model_templates_tests.sh
+++ tests/run_model_templates_tests.sh
@@ -2,8 +2,8 @@ python3 -m venv venv || exit 1
 . venv/bin/activate || exit 1
 pip install --upgrade pip || exit 1
 
-pip cache purge
-ls -la `pip cache dir`/
+# pip cache purge
+# ls -la `pip cache dir`/
 
 pip install -e ote_cli || exit 1
 pip install -e $OTE_SDK_PATH || exit 1
@@ -11,7 +11,7 @@ echo ""
 echo ""
 
 export PYTHONPATH=${PYTHONPATH}:`pwd`
-pytest tests/ote_cli/ -v -s --durations=0 || exit 1
+pytest tests/ote_cli/ -v --durations=0 || exit 1
 deactivate
 echo ""
 echo ""
