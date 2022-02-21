f1359318e5012ea1a4623e23523c5da05e3693e8
Fedor Kutsepin <fedorx.kutsepin@intel.com>
2022-02-21 15:44:09 +0300

    fix end line

--- tests/ote_cli/conftest.py
+++ tests/ote_cli/conftest.py
@@ -31,8 +31,6 @@ def pytest_addoption(parser):
 
 
 def pytest_generate_tests(metafunc):
-    # This is called for every test. Only get/set command line arguments
-    # if the argument is specified in the list of test "fixturenames".
     option_value = metafunc.config.option.algo_be
     if 'algo_be' in metafunc.fixturenames and option_value is not None:
         metafunc.parametrize("algo_be", [option_value])
