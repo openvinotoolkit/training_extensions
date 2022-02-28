6e81f67d9baa4a27c9239c75e2d3c4a9a1f121fd
Fedor Kutsepin <fedorx.kutsepin@intel.com>
2022-03-01 01:25:19 +0300

    fix

--- tests/ote_cli/test_ote_cli_params_find.py
+++ tests/ote_cli/test_ote_cli_params_find.py
@@ -14,70 +14,88 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-import pytest
 from subprocess import run
+from copy import deepcopy
 
-from common import wrong_paths
+import os
+import pytest
 
+from ote_sdk.test_suite.e2e_test_system import e2e_pytest_component
+from ote_cli.registry import Registry
 
-@pytest.fixture()
-def task_type(algo_be):
-    return algo_be
+from common import (
+    create_venv,
+    get_some_vars,
+    ote_demo_deployment_testing,
+    ote_demo_testing,
+    ote_demo_openvino_testing,
+    ote_deploy_openvino_testing,
+    ote_eval_deployment_testing,
+    ote_eval_openvino_testing,
+    ote_eval_testing,
+    ote_train_testing,
+    ote_export_testing,
+    pot_optimize_testing,
+    pot_eval_testing,
+    nncf_optimize_testing,
+    nncf_export_testing,
+    nncf_eval_testing,
+    nncf_eval_openvino_testing,
+    wrong_paths,
+    ote_find_common
+)
 
+root = '/tmp/ote_cli/'
+ote_dir = os.getcwd()
 
-valid_paths = {'same_folder': '.',
-               'upper_folder': '..',
-               'external': 'external'
-               }
-
-
-@pytest.mark.parametrize("path", valid_paths.values(), ids=valid_paths.keys())
-def test_ote_cli_find_root(path):
-    cmd = ['ote',
-           'find',
-           '--root',
-           path
-           ]
-    assert run(cmd).returncode == 0
-
-
-def test_ote_cli_task_type(task_type):
-    cmd = ['ote',
-           'find',
-           '--task_type',
-           task_type
-           ]
-    assert run(cmd).returncode == 0
-
-
-@pytest.mark.parametrize("path", wrong_paths.values(), ids=wrong_paths.keys())
-def test_ote_cli_find_root_wrong_path(path):
-    cmd = ['ote',
-           'find'
-           '--root',
-           path
-           ]
-    assert run(cmd).returncode != 0
+templates = Registry('external').templates
+templates_ids = [template.model_template_id for template in templates]
 
 
-def test_ote_cli_find_task_type_not_set():
-    cmd = ['ote',
-           'find'
-           '--task_id',
-           ]
-    assert run(cmd).returncode != 0
+class OTECliFindParams:
+    @e2e_pytest_component
+    def test_create_venv(self):
+        work_dir, template_work_dir, algo_backend_dir = get_some_vars(templates[0], root)
+        create_venv(algo_backend_dir, work_dir, template_work_dir)
 
+    @e2e_pytest_component
+    @pytest.mark.parametrize("template", templates, ids=templates_ids)
+    def test_ote_cli_find(self, template):
+        ret = ote_find_common(template, [])
+        assert ret.returncode == 0
 
-def test_ote_cli_find():
-    cmd = ['ote',
-           'find'
-           ]
-    assert run(cmd).returncode == 0
+    @e2e_pytest_component
+    @pytest.mark.parametrize("template", templates, ids=templates_ids)
+    def test_ote_cli_find_root(self, template):
+        valid_paths = {'same_folder': '.',
+                       'upper_folder': '..',
+                       'external': 'external'
+                       }
+        for path in valid_paths.values():
+            cmd = ['--root', path]
+            ret = ote_find_common(template, cmd)
+            assert ret.returncode == 0
 
+    @e2e_pytest_component
+    @pytest.mark.parametrize("template", templates, ids=templates_ids)
+    def test_ote_cli_task_type(self, template):
+        task_types = ["ANOMALY_CLASSIFICATION", "CLASSIFICATION", "DETECTION", "SEGMENTATION"]
+        for task_type in task_types:
+            cmd = ['--task_type', task_type]
+            ret = ote_find_common(template, cmd)
+            assert ret.returncode == 0
 
-def test_ote_cli_find_help():
-    cmd = ['ote',
-           'find'
-           ]
-    assert run(cmd + ['-h']).returncode == 0
-    assert run(cmd + ['--help']).returncode == 0
+    @e2e_pytest_component
+    @pytest.mark.parametrize("template", templates, ids=templates_ids)
+    def test_ote_cli_find_root_wrong_path(self, template):
+        for path in wrong_paths.values():
+            cmd = ['--root', path]
+            ret = ote_find_common(template, cmd)
+            assert ret.returncode != 0
+
+    @e2e_pytest_component
+    @pytest.mark.parametrize("template", templates, ids=templates_ids)
+    def test_ote_cli_find_task_type_not_set(self, template):
+        cmd = ['--task_id', '']
+        ret = ote_find_common(template, cmd)
+        assert ret.returncode != 0
