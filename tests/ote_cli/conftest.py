8fb45b7da8fbf917ef7cd00864ee2234c2b338cb
Fedor Kutsepin <fedorx.kutsepin@intel.com>
2022-03-15 15:53:29 +0300

    add todo lines

--- tests/ote_cli/conftest.py
+++ tests/ote_cli/conftest.py
@@ -14,7 +14,7 @@
 
 from ote_sdk.test_suite.pytest_insertions import *  # noqa #pylint: disable=unused-import
 
-collect_ignore_glob = ['tests_ote_cli_tools*.py']     # TODO DELETE THE LINE BEFORE MERGE TO DEVELOP!!!
+collect_ignore_glob = ['tests_ote_cli_tools*.py']     # TODO (fkutsepx) DELETE THE LINE BEFORE MERGE TO DEVELOP!!!
 
 pytest_plugins = get_pytest_plugins_from_ote()
 
