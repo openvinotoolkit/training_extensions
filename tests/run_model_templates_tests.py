e4286a3f469314426c63e49f21fe0acfcbaa6e71
Ilya Krylov <ilya.krylov@intel.com>
2022-03-17 09:23:13 +0300

    minor

--- tests/run_model_templates_tests.py
+++ tests/run_model_templates_tests.py
@@ -1,63 +1,78 @@
+""" Runs tests selectively depending on changed files. """
+
 import os
-import re
 import sys
 from subprocess import run
 
 from tests.ote_cli.common import collect_env_vars
 
-algo_root_dir = "external"
-algo_dirs = [
-    os.path.join(algo_root_dir, dir)
-    for dir in os.listdir(algo_root_dir)
-    if os.path.isdir(os.path.join(algo_root_dir, dir))
+ALGO_ROOT_DIR = "external"
+ALGO_DIRS = [
+    os.path.join(ALGO_ROOT_DIR, d)
+    for d in os.listdir(ALGO_ROOT_DIR)
+    if os.path.isdir(os.path.join(ALGO_ROOT_DIR, d))
 ]
-run_all_if_changed = [
+IMPORTANT_DIRS = [
     "data/",
     "ote_cli/",
     "ote_sdk/",
     "tests/",
 ]
 
-run_algo_tests = {algo_dir: False for algo_dir in algo_dirs}
-
-run_all = True
 wd = sys.argv[1]
 
-print(f"{sys.argv=}")
-if len(sys.argv) > 2:
-    run_all = False
-    changed_files = sys.argv[2:]
-    print(f"{changed_files=}")
-    for changed_file in changed_files:
-        print(f"{changed_file=}")
-        for significant_change in run_all_if_changed:
-            if changed_file.startswith(significant_change):
-                print(f"{significant_change=}")
-                run_all = True
+
+def what_to_test():
+    """
+    Returns a dict containing information whether it is needed
+    to run tests for particular algorithm.
+    """
+
+    print(f"{sys.argv=}")
+    run_algo_tests = {d: True for d in ALGO_DIRS}
+    if len(sys.argv) > 2:
+        run_algo_tests = {d: False for d in ALGO_DIRS}
+        changed_files = sys.argv[2:]
+        print(f"{changed_files=}")
+
+        for changed_file in changed_files:
+            if any(changed_file.startswith(d) for d in IMPORTANT_DIRS):
+                run_algo_tests = {d: True for d in ALGO_DIRS}
                 break
-        for algo_dir in algo_dirs:
-            if changed_file.startswith(algo_dir):
-                run_algo_tests[algo_dir] = True
 
-print(f"{run_all=}")
-for k, v in run_algo_tests.items():
-    print("run", k, v)
+            for d in ALGO_DIRS:
+                if changed_file.startswith(d):
+                    run_algo_tests[d] = True
 
-passed = {}
-success = True
-command = ["pytest", os.path.join("tests", "ote_cli", "misc"), "-v"]
-res = run(command, env=collect_env_vars(wd)).returncode == 0
-passed["misc"] = res
-success *= res
-for algo_dir in algo_dirs:
-    if run_all or run_algo_tests[algo_dir]:
-        command = ["pytest", os.path.join("tests", "ote_cli", algo_dir), "-v"]
-        res = run(command, env=collect_env_vars(wd)).returncode == 0
-        passed[algo_dir] = res
-        success *= res
+    for k, v in run_algo_tests.items():
+        print("run", k, v)
 
-for k, v in passed.items():
-    res = "PASSED" if v else "FAILED"
-    print(f"Tests for {k} {res}")
+    return run_algo_tests
 
-sys.exit(1 - success)
+
+def test(run_algo_tests):
+    """
+    Runs tests for algorithms and other stuff (misc).
+    """
+
+    passed = {}
+    success = True
+    command = ["pytest", os.path.join("tests", "ote_cli", "misc"), "-v"]
+    res = run(command, env=collect_env_vars(wd), check=True).returncode == 0
+    passed["misc"] = res
+    success *= res
+    for algo_dir in ALGO_DIRS:
+        if run_algo_tests[algo_dir]:
+            command = ["pytest", os.path.join("tests", "ote_cli", algo_dir), "-v", "-s"]
+            res = run(command, env=collect_env_vars(wd), check=True).returncode == 0
+            passed[algo_dir] = res
+            success *= res
+
+    for k, v in passed.items():
+        res = "PASSED" if v else "FAILED"
+        print(f"Tests for {k} {res}")
+
+    sys.exit(1 - success)
+
+
+test(what_to_test())
