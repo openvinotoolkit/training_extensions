69636eb6b79c3bd1f42b525b4310039212b459e3
Ilya Krylov <ilya.krylov@intel.com>
2022-03-17 18:57:29 +0300

    minor

--- external/mmsegmentation/tests/test_ote/test_api.py
+++ external/mmsegmentation/tests/test_ote/test_api.py
@@ -40,7 +40,7 @@ from ote_sdk.entities.task_environment import TaskEnvironment
 from ote_sdk.entities.train_parameters import TrainParameters
 from ote_sdk.tests.test_helpers import generate_random_annotated_image
 
-from segmentation_tasks.apis.segmentation import OTESegmentationTask
+from segmentation_tasks.apis.segmentation import OTESegmentationTrainingTask
 
 
 DEFAULT_TEMPLATE_DIR = osp.join('configs', 'ote', 'custom-sematic-segmentation', 'ocr-lite-hrnet-18')
