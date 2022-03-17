1c9b9ba1210966b32cc2f43cac60d78238f723b3
Samet Akcay <samet.akcay@intel.com>
2022-03-17 09:45:52 -0700

    removed task type from the config

--- external/anomaly/ote_anomalib/task.py
+++ external/anomaly/ote_anomalib/task.py
@@ -90,12 +90,7 @@ class BaseAnomalyTask(ITrainingTask, IInferenceTask, IEvaluationTask, IExportTas
         # set task type
         # NOTE: Setting the task type is no longer needed and will be deprecated in the
         # future releases of anomalib.
-        if self.task_type == TaskType.ANOMALY_CLASSIFICATION:
-            config.dataset.task = "classification"
-        elif self.task_type in (TaskType.ANOMALY_DETECTION, TaskType.ANOMALY_SEGMENTATION):
-            config.dataset.task = "segmentation"
-        else:
-            raise ValueError(f"Unknown task type: {self.task_type}")
+        config.dataset.task = "classification"
 
         return config
 
