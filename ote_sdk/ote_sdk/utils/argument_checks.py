b028fa281abe08e674fc2dd9bd9e2f095e61d161
saltykox <valeriyx.saltykov@intel.com>
2022-03-23 11:35:18 +0300

    updated check_parameter_type function

--- ote_sdk/ote_sdk/utils/argument_checks.py
+++ ote_sdk/ote_sdk/utils/argument_checks.py
@@ -72,7 +72,7 @@ def check_parameter_type(parameter, parameter_name, expected_type):
     # pylint: disable=W0212
     if expected_type in [typing.Any, inspect._empty]:  # type: ignore
         return
-    if not isinstance(expected_type, typing._GenericAlias):  # type: ignore
+    if (not isinstance(expected_type, typing._GenericAlias)) or (expected_type == typing.Iterable):  # type: ignore
         raise_value_error_if_parameter_has_unexpected_type(
             parameter=parameter,
             parameter_name=parameter_name,
@@ -126,6 +126,8 @@ def check_parameter_type(parameter, parameter_name, expected_type):
                 f"Unexpected type of '{parameter_name}' parameter, expected: {expected_args}, "
                 f"actual type: {actual_type}, actual value: {parameter}"
             )
+    if origin_class == typing.Iterable:
+        check_input_parameters_type()
 
 
 def check_input_parameters_type(checks_types: dict = None):
