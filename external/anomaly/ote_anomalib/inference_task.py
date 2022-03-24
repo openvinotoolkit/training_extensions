bc52ec967d6e3800511730f509d743e910538f95
Slawomir Strehlke <slawomir.strehlke@intel.com>
2022-03-24 15:13:00 +0100

    Anomaly Segmentation train tests Added setting optimization type in BaseAnomalyTask class

--- external/anomaly/ote_anomalib/inference_task.py
+++ external/anomaly/ote_anomalib/inference_task.py
@@ -88,6 +88,10 @@ class AnomalyInferenceTask(IInferenceTask, IEvaluationTask, IExportTask, IUnload
 
         self.trainer: Trainer
 
+        # Set default model attributes.
+        self._optimization_type = ModelOptimizationType.MO
+
+
     def get_config(self) -> Union[DictConfig, ListConfig]:
         """Get Anomalib Config from task environment.
 
@@ -222,6 +226,8 @@ class AnomalyInferenceTask(IInferenceTask, IEvaluationTask, IExportTask, IUnload
             Exception: If export_type is not ExportType.OPENVINO
         """
         assert export_type == ExportType.OPENVINO
+        output_model.model_format = ModelFormat.OPENVINO
+        output_model.optimization_type = self._optimization_type
 
         output_model.model_format = ModelFormat.OPENVINO
         output_model.optimization_type = self.optimization_type
