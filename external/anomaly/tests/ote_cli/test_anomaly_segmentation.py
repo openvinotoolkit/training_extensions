05d02f27c8bf69471172659e3926ad63764877fa
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2022-03-25 16:24:58 +0300

    nncf for anomaly detection

--- external/anomaly/tests/ote_cli/test_anomaly_segmentation.py
+++ external/anomaly/tests/ote_cli/test_anomaly_segmentation.py
@@ -132,7 +132,7 @@ class TestToolsAnomalySegmentation:
         if template.entrypoints.nncf is None:
             pytest.skip("nncf entrypoint is none")
 
-        #TODO(AlexanderDokuchaev): return threshold=0.0001 after fix loading NNCF model
+        # TODO(AlexanderDokuchaev): return threshold=0.0001 after fix loading NNCF model
         nncf_eval_testing(template, root, ote_dir, args, threshold=0.3)
 
     @e2e_pytest_component
