0c10d6368e24ef216c1c3d40265511a56f06e5cd
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2022-03-25 06:26:52 +0300

    linter

--- ote_sdk/ote_sdk/usecases/exportable_code/streamer/streamer.py
+++ ote_sdk/ote_sdk/usecases/exportable_code/streamer/streamer.py
@@ -331,8 +331,8 @@ class ImageStreamer(BaseStreamer):
         self.media_type = MediaType.IMAGE
         self.filenames = _get_filenames(path=path, media_type=MediaType.IMAGE)
 
-    # pylint: disable=arguments-differ
     @staticmethod
+    # pylint: disable=arguments-differ
     def get_stream(stream_input: str) -> Iterable[np.ndarray]:
         image = cv2.imread(stream_input)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
