4cac9afef5c2bfdcb7905a5387122b694f3b6b52
Savelyev <nikita.savelyev@intel.com>
2022-03-25 15:00:56 +0300

    Added a test case for negative update log value

--- ote_sdk/ote_sdk/tests/entities/test_optimization_parameters.py
+++ ote_sdk/ote_sdk/tests/entities/test_optimization_parameters.py
@@ -80,7 +80,7 @@ class TestOptimizationParameters:
             is opt_params.update_progress(1)
             is opt_params.update_progress(0, 0.3)
             is opt_params.update_progress(0.5, 1.4)
-            is opt_params.update_progress(1, 6)
+            is opt_params.update_progress(1, -6.1)
             is None
         )
         assert opt_params.save_model() is None
@@ -93,7 +93,7 @@ class TestOptimizationParameters:
                 is opt_params.update_progress(1)
                 is opt_params.update_progress(0, 0.3)
                 is opt_params.update_progress(0.5, 1.4)
-                is opt_params.update_progress(1, 6)
+                is opt_params.update_progress(1, -6.1)
             is None
         )
         assert opt_params.save_model() is None
