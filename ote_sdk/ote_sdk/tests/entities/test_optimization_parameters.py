15579deee751cf2c140762b66405b94cfefff2b1
Savelyev <nikita.savelyev@intel.com>
2022-03-29 14:10:52 +0300

    Tweaked optimization parameter tests to better reflect general progress update usage

--- ote_sdk/ote_sdk/tests/entities/test_optimization_parameters.py
+++ ote_sdk/ote_sdk/tests/entities/test_optimization_parameters.py
@@ -76,11 +76,11 @@ class TestOptimizationParameters:
         assert opt_params.resume is False
         assert (
             opt_params.update_progress(0)
-            is opt_params.update_progress(0.5)
-            is opt_params.update_progress(1)
+            is opt_params.update_progress(50.5)
+            is opt_params.update_progress(100)
             is opt_params.update_progress(0, 0.3)
-            is opt_params.update_progress(0.5, 1.4)
-            is opt_params.update_progress(1, -6.1)
+            is opt_params.update_progress(50.5, 1.4)
+            is opt_params.update_progress(100, -6.1)
             is None
         )
         assert opt_params.save_model() is None
@@ -89,11 +89,11 @@ class TestOptimizationParameters:
         assert opt_params.resume is True
         assert (
             opt_params.update_progress(0)
-            is opt_params.update_progress(0.5)
-            is opt_params.update_progress(1)
+            is opt_params.update_progress(50.5)
+            is opt_params.update_progress(100)
             is opt_params.update_progress(0, 0.3)
-            is opt_params.update_progress(0.5, 1.4)
-            is opt_params.update_progress(1, -6.1)
+            is opt_params.update_progress(50.5, 1.4)
+            is opt_params.update_progress(100, -6.1)
             is None
         )
         assert opt_params.save_model() is None
