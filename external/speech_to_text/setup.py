1f9eba02891bc36600801eeb298e44cb6d00683f
Alexey Kruglov <Alexey.Kruglov@intel.com>
2022-03-30 16:37:40 +0300

    Rewrite setup.py to get rid of exec()

--- external/speech_to_text/setup.py
+++ external/speech_to_text/setup.py
@@ -1,42 +1,38 @@
-# SPDX-License-Identifier: MIT
-#
-# Copyright (C) 2020-2021 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-#
+import os.path
+import re
 
-import os.path as osp
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, find_packages
 
-repo_root = osp.dirname(osp.realpath(__file__))
 
-def find_version():
-    version_file = osp.join(repo_root, 'speech_to_text/version.py')
-    with open(version_file, 'r') as f:
-        exec(compile(f.read(), version_file, 'exec'))
-    return locals()['__version__']
+# N.B.: Distribution root directory is the current working directory (CWD) because:
+# 1) pip invokes setup.py with cwd=unpacked_source_directory and
+# 2) find_packages() without args looks for packages in CWD.
 
-def get_requirements(filename):
-    requires = []
-    links = []
-    with open(osp.join(repo_root, filename), 'r') as f:
-        for line in f.readlines():
-            line = line.replace('\n', '')
-            if '-f http' in line:
-                links.append(line)
-            else:
-                requires.append(line)
-    return requires, links
+def get_version():
+    re_version = re.compile('^__version__[ \t]*=[ \t]*[\'"](?P<version>[^\'"\\\\]*)[\'"][ \t]*(#.*)?$')
+    with open(os.path.join('speech_to_text', 'version.py'), 'rt') as ver_file:
+        for line in ver_file:
+            m_version = re_version.match(line.rstrip())
+            if m_version:
+                return m_version.group('version')
+    raise NameError("name '__version__' is not defined")
 
-packages, links = get_requirements('requirements.txt')
+def get_requirements():
+    requirements = []
+    with open('requirements.txt', 'rt') as req_file:
+        for line in req_file.readlines():
+            line = line.rstrip()
+            if line != '':
+                requirements.append(line)
+    return requirements
 
 setup(
     name='speech_to_text',
-    version=find_version(),
-    description='A library for deep learning Speech To Text in PyTorch',
+    version=get_version(),
+    description='An OpenVINO Training Extensions backend to train Speech To Text (ASR) models with PyTorch',
     author='Intel Corporation',
     license='Apache-2.0',
-    dependency_links=links,
     packages=find_packages(),
-    install_requires=packages,
+    install_requires=get_requirements(),
     keywords=['Speech To Text', 'STT', 'Deep Learning', 'Speech Recognition'],
 )
