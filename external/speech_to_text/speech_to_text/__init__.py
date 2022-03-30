ca509e0f50a09526b9b33c3860ca948077db7669
Alexey Kruglov <Alexey.Kruglov@intel.com>
2022-03-31 01:23:10 +0300

    Move interface class to the top level module of the TTS backend package

--- external/speech_to_text/speech_to_text/__init__.py
+++ external/speech_to_text/speech_to_text/__init__.py
@@ -1,1 +1,5 @@
 from .version import __version__
+from .ote.task_train import OTESpeechToTextTaskTrain
+
+# All other objects (classes, functions, etc.) in this package be considered internal.
+__all__ = ['__version__', 'OTESpeechToTextTaskTrain']
