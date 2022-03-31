57ca2c32febf79002c22012265e0ecad6ca87f57
Alexey Kruglov <Alexey.Kruglov@intel.com>
2022-03-31 07:00:11 +0300

    Move interface classes to the root module of TTS backend package

--- external/text_to_speech/torchtts/__init__.py
+++ external/text_to_speech/torchtts/__init__.py
@@ -1,3 +1,8 @@
 from __future__ import absolute_import, print_function
 
 from .version import __version__
+from .task import OTETextToSpeechTask
+from .openvino_task import OpenVINOTTSTask
+
+# All other objects (classes, functions, etc.) in this package be considered internal.
+__all__ = ['__version__', 'OTETextToSpeechTask', 'OpenVINOTTSTask']
