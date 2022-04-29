e498287951f63719aee2ec8c432a2ee129177e86
Harim Kang <harim.kang@intel.com>
2022-04-29 13:46:28 +0900

    Add mpa all task validation

--- external/model-preparation-algorithm/tests/test_ote_training.py
+++ external/model-preparation-algorithm/tests/test_ote_training.py
@@ -18,6 +18,7 @@ from ote_sdk.entities.subset import Subset
 
 from torchreid_tasks.utils import ClassificationDatasetAdapter
 from detection_tasks.extension.datasets.data_utils import load_dataset_items_coco_format
+from segmentation_tasks.extension.datasets.mmdataset import load_dataset_items
 
 from ote_sdk.test_suite.e2e_test_system import DataCollector, e2e_pytest_performance
 from ote_sdk.test_suite.training_test_case import (OTETestCaseInterface,
@@ -123,8 +124,31 @@ def _create_object_detection_dataset_and_labels_schema(dataset_params):
     labels_schema = LabelSchemaEntity.from_labels(dataset.get_labels())
     return dataset, labels_schema
 
+def _create_segmentation_dataset_and_labels_schema(dataset_params):
+    logger.debug(f'Using for train annotation file {dataset_params.annotations_train}')
+    logger.debug(f'Using for val annotation file {dataset_params.annotations_val}')
+    labels_list = []
+    items = load_dataset_items(
+        ann_file_path=dataset_params.annotations_train,
+        data_root_dir=dataset_params.images_train_dir,
+        subset=Subset.TRAINING,
+        labels_list=labels_list)
+    items.extend(load_dataset_items(
+        ann_file_path=dataset_params.annotations_val,
+        data_root_dir=dataset_params.images_val_dir,
+        subset=Subset.VALIDATION,
+        labels_list=labels_list))
+    items.extend(load_dataset_items(
+        ann_file_path=dataset_params.annotations_test,
+        data_root_dir=dataset_params.images_test_dir,
+        subset=Subset.TESTING,
+        labels_list=labels_list))
+    dataset = DatasetEntity(items=items)
+    labels_schema = LabelSchemaEntity.from_labels(labels_list)
+    return dataset, labels_schema
 
-class MPAClassificationTrainingTestParameters(DefaultOTETestCreationParametersInterface):
+
+class ClassificationClsIncrTrainingTestParameters(DefaultOTETestCreationParametersInterface):
     def test_case_class(self) -> Type[OTETestCaseInterface]:
         return generate_ote_integration_test_case_class(
             get_test_action_classes()
@@ -139,7 +163,7 @@ class MPAClassificationTrainingTestParameters(DefaultOTETestCreationParametersIn
                        'ClassIncremental_Image_Classification_MobileNet-V3-large-0.75x',
                        'ClassIncremental_Image_Classification_MobileNet-V3-small'
                     ],
-                    dataset_name=['cifar10_airplane_automobile_bird_cat_deer_frog'],
+                    dataset_name=['cifar10_cls_incr'],
                     usecase='precommit',
                 ),
                 dict(
@@ -149,7 +173,7 @@ class MPAClassificationTrainingTestParameters(DefaultOTETestCreationParametersIn
                        'ClassIncremental_Image_Classification_MobileNet-V3-large-0.75x',
                        'ClassIncremental_Image_Classification_MobileNet-V3-small'
                     ],
-                    dataset_name=['cifar10_airplane_automobile_bird_cat_deer_frog'],
+                    dataset_name=['cifar10_cls_incr'],
                     num_training_iters=KEEP_CONFIG_FIELD_VALUE, 
                     batch_size=KEEP_CONFIG_FIELD_VALUE,
                     usecase=REALLIFE_USECASE_CONSTANT,
@@ -166,7 +190,7 @@ class MPAClassificationTrainingTestParameters(DefaultOTETestCreationParametersIn
         return deepcopy(DEFAULT_TEST_PARAMETERS)
 
 
-class MPADetectionTrainingTestParameters(DefaultOTETestCreationParametersInterface):
+class DetectionClsIncrTrainingTestParameters(DefaultOTETestCreationParametersInterface):
     def test_case_class(self) -> Type[OTETestCaseInterface]:
         return generate_ote_integration_test_case_class(
             get_test_action_classes()
@@ -179,7 +203,7 @@ class MPADetectionTrainingTestParameters(DefaultOTETestCreationParametersInterfa
                        'ClassIncremental_Object_Detection_Gen3_ATSS',
                        'ClassIncremental_Object_Detection_Gen3_VFNet',
                     ],
-                    dataset_name='coco_det_person_car',
+                    dataset_name='coco_cls_incr',
                     usecase='precommit',
                 ),
                 dict(
@@ -187,7 +211,7 @@ class MPADetectionTrainingTestParameters(DefaultOTETestCreationParametersInterfa
                        'ClassIncremental_Object_Detection_Gen3_ATSS',
                        'ClassIncremental_Object_Detection_Gen3_VFNet',
                     ],
-                    dataset_name='coco_det_person_car',
+                    dataset_name='coco_cls_incr',
                     num_training_iters=KEEP_CONFIG_FIELD_VALUE,
                     batch_size=KEEP_CONFIG_FIELD_VALUE,
                     usecase=REALLIFE_USECASE_CONSTANT,
@@ -197,12 +221,40 @@ class MPADetectionTrainingTestParameters(DefaultOTETestCreationParametersInterfa
         return deepcopy(test_bunches)
 
 
-class TestOTEReallifeClassificationMPA(OTETrainingTestInterface):
+class SegmentationClsIncrTrainingTestParameters(DefaultOTETestCreationParametersInterface):
+    def test_case_class(self) -> Type[OTETestCaseInterface]:
+        return generate_ote_integration_test_case_class(
+            get_test_action_classes()
+        )
+
+    def test_bunches(self) -> List[Dict[str, Any]]:
+        test_bunches = [
+                dict(
+                    model_name=[
+                       'ClassIncremental_Semantic_Segmentation_Lite-HRNet-18_OCR',
+                    ],
+                    dataset_name='voc_cls_incr',
+                    usecase='precommit',
+                ),
+                dict(
+                    model_name=[
+                       'ClassIncremental_Semantic_Segmentation_Lite-HRNet-18_OCR',
+                    ],
+                    dataset_name='voc_cls_incr',
+                    num_training_iters=KEEP_CONFIG_FIELD_VALUE,
+                    batch_size=KEEP_CONFIG_FIELD_VALUE,
+                    usecase=REALLIFE_USECASE_CONSTANT,
+                ),
+        ]
+        return deepcopy(test_bunches)
+
+
+class TestOTEReallifeClassificationClsIncr(OTETrainingTestInterface):
     """
     The main class of running test in this file.
     """
     PERFORMANCE_RESULTS = None # it is required for e2e system
-    helper = OTETestHelper(MPAClassificationTrainingTestParameters())
+    helper = OTETestHelper(ClassificationClsIncrTrainingTestParameters())
 
     @classmethod
     def get_list_of_tests(cls, usecase: Optional[str] = None):
@@ -285,12 +337,12 @@ class TestOTEReallifeClassificationMPA(OTETrainingTestInterface):
                                cur_test_expected_metrics_callback_fx)
 
 
-class TestOTEReallifeObjectDetectionMPA(OTETrainingTestInterface):
+class TestOTEReallifeObjectDetectionClsIncr(OTETrainingTestInterface):
     """
     The main class of running test in this file.
     """
     PERFORMANCE_RESULTS = None # it is required for e2e system
-    helper = OTETestHelper(MPADetectionTrainingTestParameters())
+    helper = OTETestHelper(DetectionClsIncrTrainingTestParameters())
 
     @classmethod
     def get_list_of_tests(cls, usecase: Optional[str] = None):
@@ -372,7 +424,124 @@ class TestOTEReallifeObjectDetectionMPA(OTETrainingTestInterface):
         setup['test_type'] = os.environ.get('TT_TEST_TYPE', 'no-test-type') # TODO: get from e2e test type
         setup['scenario'] = 'api' # TODO(lbeynens): get from a fixture!
         setup['test'] = request.node.name
-        setup['subject'] = 'custom-object-detection'
+        setup['subject'] = 'detection-cls-incr'
+        setup['project'] = 'ote'
+        if 'test_parameters' in setup:
+            assert isinstance(setup['test_parameters'], dict)
+            if 'dataset_name' not in setup:
+                setup['dataset_name'] = setup['test_parameters'].get('dataset_name')
+            if 'model_name' not in setup:
+                setup['model_name'] = setup['test_parameters'].get('model_name')
+            if 'test_stage' not in setup:
+                setup['test_stage'] = setup['test_parameters'].get('test_stage')
+            if 'usecase' not in setup:
+                setup['usecase'] = setup['test_parameters'].get('usecase')
+        logger.info(f'creating DataCollector: setup=\n{pformat(setup, width=140)}')
+        data_collector = DataCollector(name='TestOTEIntegration',
+                                       setup=setup)
+        with data_collector:
+            logger.info('data_collector is created')
+            yield data_collector
+        logger.info('data_collector is released')
+
+    @e2e_pytest_performance
+    def test(self,
+             test_parameters,
+             test_case_fx, data_collector_fx,
+             cur_test_expected_metrics_callback_fx):
+        test_case_fx.run_stage(test_parameters['test_stage'], data_collector_fx,
+                               cur_test_expected_metrics_callback_fx)
+
+
+class TestOTEReallifeSegmentationClsIncr(OTETrainingTestInterface):
+    """
+    The main class of running test in this file.
+    """
+    PERFORMANCE_RESULTS = None # it is required for e2e system
+    helper = OTETestHelper(SegmentationClsIncrTrainingTestParameters())
+
+    @classmethod
+    def get_list_of_tests(cls, usecase: Optional[str] = None):
+        """
+        This method should be a classmethod. It is called before fixture initialization, during
+        tests discovering.
+        """
+        return cls.helper.get_list_of_tests(usecase)
+
+    @pytest.fixture
+    def params_factories_for_test_actions_fx(self, current_test_parameters_fx,
+                                             dataset_definitions_fx, template_paths_fx,
+                                             ote_current_reference_dir_fx) -> Dict[str,Callable[[], Dict]]:
+        logger.debug('params_factories_for_test_actions_fx: begin')
+
+        test_parameters = deepcopy(current_test_parameters_fx)
+        dataset_definitions = deepcopy(dataset_definitions_fx)
+        template_paths = deepcopy(template_paths_fx)
+        def _training_params_factory() -> Dict:
+            if dataset_definitions is None:
+                pytest.skip('The parameter "--dataset-definitions" is not set')
+
+            model_name = test_parameters['model_name']
+            dataset_name = test_parameters['dataset_name']
+            num_training_iters = test_parameters['num_training_iters']
+            batch_size = test_parameters['batch_size']
+
+            dataset_params = _get_dataset_params_from_dataset_definitions(dataset_definitions, dataset_name)
+
+            if model_name not in template_paths:
+                raise ValueError(f'Model {model_name} is absent in template_paths, '
+                                 f'template_paths.keys={list(template_paths.keys())}')
+            template_path = make_path_be_abs(template_paths[model_name], template_paths[ROOT_PATH_KEY])
+
+            logger.debug('training params factory: Before creating dataset and labels_schema')
+            dataset, labels_schema = _create_segmentation_dataset_and_labels_schema(dataset_params)
+            import os.path as osp
+            ckpt_path = None
+            if hasattr(dataset_params, 'pre_trained_model'):
+                ckpt_path = osp.join(osp.join(dataset_params.pre_trained_model, model_name), "weights.pth")
+            logger.debug('training params factory: After creating dataset and labels_schema')
+
+            return {
+                'dataset': dataset,
+                'labels_schema': labels_schema,
+                'template_path': template_path,
+                'num_training_iters': num_training_iters,
+                'batch_size': batch_size,
+                'checkpoint' : ckpt_path
+            }
+
+        params_factories_for_test_actions = {
+            'training': _training_params_factory,
+        }
+        logger.debug('params_factories_for_test_actions_fx: end')
+        return params_factories_for_test_actions
+
+    @pytest.fixture
+    def test_case_fx(self, current_test_parameters_fx, params_factories_for_test_actions_fx):
+        """
+        This fixture returns the test case class OTEIntegrationTestCase that should be used for the current test.
+        Note that the cache from the test helper allows to store the instance of the class
+        between the tests.
+        If the main parameters used for this test are the same as the main parameters used for the previous test,
+        the instance of the test case class will be kept and re-used. It is helpful for tests that can
+        re-use the result of operations (model training, model optimization, etc) made for the previous tests,
+        if these operations are time-consuming.
+        If the main parameters used for this test differs w.r.t. the previous test, a new instance of
+        test case class will be created.
+        """
+        test_case = type(self).helper.get_test_case(current_test_parameters_fx,
+                                                    params_factories_for_test_actions_fx)
+        return test_case
+
+    # TODO(lbeynens): move to common fixtures
+    @pytest.fixture
+    def data_collector_fx(self, request) -> DataCollector:
+        setup = deepcopy(request.node.callspec.params)
+        setup['environment_name'] = os.environ.get('TT_ENVIRONMENT_NAME', 'no-env')
+        setup['test_type'] = os.environ.get('TT_TEST_TYPE', 'no-test-type') # TODO: get from e2e test type
+        setup['scenario'] = 'api' # TODO(lbeynens): get from a fixture!
+        setup['test'] = request.node.name
+        setup['subject'] = 'segmentation-cls-incr'
         setup['project'] = 'ote'
         if 'test_parameters' in setup:
             assert isinstance(setup['test_parameters'], dict)
