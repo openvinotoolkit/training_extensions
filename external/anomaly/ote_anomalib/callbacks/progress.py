ad032dd5b0b9e68f171250f86752fcf74da9cc4c
SangdaeNam <sangdae.nam@intel.com>
2022-05-12 00:02:14 +0900

    [HPO] Enabling ote_anomalib to report score for hpopt

--- external/anomaly/ote_anomalib/callbacks/progress.py
+++ external/anomaly/ote_anomalib/callbacks/progress.py
@@ -112,6 +112,15 @@ class ProgressCallback(ProgressBar):
             raise ValueError(f"Unknown stage {stage}. Available: train, predict and test")
 
         return self._progress
+    
+    def on_validation_epoch_end(self, trainer, pl_module):
+        super().on_validation_epoch_end(trainer, pl_module)
+        score = None
+        metric = self.update_progress_callback.metric if hasattr(self.update_progress_callback, 'metric') else None
+        if metric in list(trainer.logged_metrics.keys()):
+            score = float(trainer.logged_metrics[metric])
+        progress = int(self._get_progress('predict'))
+        self.update_progress_callback(progress=progress, score=score)
 
     def _update_progress(self, stage: str):
         progress = self._get_progress(stage)
