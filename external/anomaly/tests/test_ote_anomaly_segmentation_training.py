cc0cd2d50bba9951589e8088e986831853dd160a
Slawomir Strehlke <slawomir.strehlke@intel.com>
2022-05-16 19:09:53 +0200

    Enabled temporary blocked tests in pytest.ini

--- external/anomaly/tests/test_ote_anomaly_segmentation_training.py
+++ external/anomaly/tests/test_ote_anomaly_segmentation_training.py
@@ -545,7 +545,5 @@ class TestOTEReallifeAnomalySegmentation(OTETrainingTestInterface):
              test_parameters,
              test_case_fx, data_collector_fx,
              cur_test_expected_metrics_callback_fx):
-        # if "nncf" in test_parameters["test_stage"]:
-        #     pytest.xfail("NNCF not yet supported for Anomaly Segmentation")
         test_case_fx.run_stage(test_parameters['test_stage'], data_collector_fx,
                                cur_test_expected_metrics_callback_fx)
