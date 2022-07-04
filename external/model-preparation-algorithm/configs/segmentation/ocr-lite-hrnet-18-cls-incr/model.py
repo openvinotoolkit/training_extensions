27b837c72edb32a3202c1c4d85896bf76d681ea3
Chun, Emily <emily.chun@intel.com>
2022-07-04 18:29:22 +0900

    add sseg

--- external/model-preparation-algorithm/configs/segmentation/ocr-lite-hrnet-18-cls-incr/model.py
+++
@@ -1,3 +0,0 @@
-_base_ = [
-    '../../../submodule/samples/cfgs/models/backbones/ote_litehrnet18.yaml',
-]
---
+++ external/model-preparation-algorithm/configs/segmentation/ocr-lite-hrnet-x-mod3/model.py
@@ -0,0 +1,5 @@
+_base_ = [
+  '../../../submodule/models/segmentation/ocr_litehrnet_x_mod3.yaml',
+]
+
+load_from = 'https://storage.openvinotoolkit.org/repositories/openvino_training_extensions/models/custom_semantic_segmentation/litehrnetxv3_imagenet1k_rsc.pth'
