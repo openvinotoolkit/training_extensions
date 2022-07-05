cbd3bc801980dbc57666dcbbbf925c14241e3807
Subhadip Roy <sroy84766@gmail.com>
2022-07-06 00:55:18 +0530

    updated __init__.py replaced custom ssim and psnr functions with those defined under torchmetrics

--- misc/pytorch_toolkit/miriad_compression_codes/src/utils/evaluators.py
+++ misc/pytorch_toolkit/miriad_compression_codes/src/utils/evaluators.py
@@ -1,38 +1,40 @@
 import torch
 from skimage.measure import compare_psnr
-import pytorch_msssim
+import pytorch_ssim_psnr
 
-def compare_psnr_batch(original, compressed, **kwargs ):
 
-	assert original.shape == compressed.shape, 'shapes should be same'
-	assert len(original.shape) == 4 # Batch x Channel x Height x Width
+def compare_psnr_batch(original, compressed, **kwargs):
 
-	avg_psnr = 0.0
-	batch_size, n = original.shape[0], 0
+    assert original.shape == compressed.shape, 'shapes should be same'
+    assert len(original.shape) == 4  # Batch x Channel x Height x Width
 
-	for idx in range(original.shape[0]):
-		# take each image in the bacth one by one
+    avg_psnr = 0.0
+    batch_size, n = original.shape[0], 0
 
-		# minor shape adjustment for being compatible with 'compare_psnr()' function
-		one_original = original[idx, 0, ...]
-		one_compressed = compressed[idx, 0, ...]
+    for idx in range(original.shape[0]):
+        # take each image in the bacth one by one
 
-		# measure pSNR
-		psnr = compare_psnr(one_original, one_compressed, **kwargs)
+        # minor shape adjustment for being compatible with 'compare_psnr()' function
+        one_original = original[idx, 0, ...]
+        one_compressed = compressed[idx, 0, ...]
 
-		# running average on the individual pSNRs
-		avg_psnr = ((n * avg_psnr) + psnr) / (n + 1)
-		n += 1
+        # measure pSNR
+        psnr = compare_psnr(one_original, one_compressed, **kwargs)
 
-	return avg_psnr
+        # running average on the individual pSNRs
+        avg_psnr = ((n * avg_psnr) + psnr) / (n + 1)
+        n += 1
 
-def compare_ssim_batch( original, compressed, **kwargs ):
+    return avg_psnr
 
-	assert original.shape == compressed.shape, 'shapes should be same'
-	assert len(original.shape) == 4 # Batch x Channel x Height x Width
 
-	avg_ssim = pytorch_msssim.msssim(
-		torch.tensor(original, requires_grad=False),
-		torch.tensor(compressed, requires_grad=False), **kwargs).item()
+def compare_ssim_batch(original, compressed, **kwargs):
 
-	return avg_ssim
+    assert original.shape == compressed.shape, 'shapes should be same'
+    assert len(original.shape) == 4  # Batch x Channel x Height x Width
+
+    avg_ssim = pytorch_ssim_psnr.msssim(
+        torch.tensor(original, requires_grad=False),
+        torch.tensor(compressed, requires_grad=False), **kwargs).item()
+
+    return avg_ssim
