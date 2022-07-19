291838272b5ab00076ef030e37801c305434009f
Eom, Jihwan <jihwan.eom@intel.com>
2022-07-19 23:54:43 +0900

    Update comment

--- external/mmdetection/detection_tasks/extension/utils/runner.py
+++ external/mmdetection/detection_tasks/extension/utils/runner.py
@@ -57,6 +57,7 @@ class EpochRunnerWithCancel(EpochBasedRunner):
         self.data_loader = data_loader
         self._max_iters = self._max_epochs * len(self.data_loader)
         self.call_hook('before_train_epoch')
+        # TODO: uncomment below line or resolve root cause of deadlock issue if multi-GPUs need to be supported.
         # time.sleep(2)  # Prevent possible multi-gpu deadlock during epoch transition
         for i, data_batch in enumerate(self.data_loader):
             self._inner_iter = i
