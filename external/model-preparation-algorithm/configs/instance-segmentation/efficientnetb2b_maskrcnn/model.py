38b2396b8b09fefe36766feccfb9551eacabc0d6
Lee, Soobee <soobee.lee@intel.com>
2022-08-09 14:42:55 +0900

    Grad_clip cfg in fp16 opt

--- external/model-preparation-algorithm/configs/instance-segmentation/efficientnetb2b_maskrcnn/model.py
+++ external/model-preparation-algorithm/configs/instance-segmentation/efficientnetb2b_maskrcnn/model.py
@@ -2,3 +2,4 @@ _base_ = [
   '../../../submodule/samples/cfgs/models/backbones/efficientnet_b2b.yaml',
   '../../../submodule/recipes/stages/_base_/models/detectors/efficientnetb2b_maskrcnn.custom.py'
 ]
+fp16 = dict(loss_scale=512.)
--- external/model-preparation-algorithm/configs/instance-segmentation/resnet50_maskrcnn/model.py
+++ external/model-preparation-algorithm/configs/instance-segmentation/resnet50_maskrcnn/model.py
@@ -2,3 +2,4 @@ _base_ = [
   '../../../submodule/samples/cfgs/models/backbones/resnet50.yaml',
   '../../../submodule/recipes/stages/_base_/models/detectors/resnet50_maskrcnn.custom.py'
 ]
+fp16 = dict(loss_scale=512.)
