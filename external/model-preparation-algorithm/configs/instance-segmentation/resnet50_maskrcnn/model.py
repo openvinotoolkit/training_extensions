a8f64e42f0c2bb2f80ffb83623b126d44db9b9f4
Lee, Soobee <soobee.lee@intel.com>
2022-08-11 17:32:29 +0900

    Remove fp16 in iseg

--- external/model-preparation-algorithm/configs/instance-segmentation/resnet50_maskrcnn/model.py
+++ external/model-preparation-algorithm/configs/instance-segmentation/resnet50_maskrcnn/model.py
@@ -2,4 +2,3 @@ _base_ = [
   '../../../submodule/samples/cfgs/models/backbones/resnet50.yaml',
   '../../../submodule/recipes/stages/_base_/models/detectors/resnet50_maskrcnn.custom.py'
 ]
-fp16 = dict(loss_scale=512.)
