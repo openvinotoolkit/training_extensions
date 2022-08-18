db69c9895bcecb76cbe52b30146e0579582f07b5
Eom, Jihwan <jihwan.eom@intel.com>
2022-08-19 02:22:15 +0900

    Update multi-label classification templates

--- external/model-preparation-algorithm/configs/classification/efficientnet_v2_s_cls_incr/model_multilabel.py
+++ external/model-preparation-algorithm/configs/classification/efficientnet_v2_s_cls_incr/model_multilabel.py
@@ -1,4 +1,19 @@
 _base_ = [
   '../../../submodule/models/classification/ote_efficientnet_v2_s_multilabel.yaml',
 ]
+
+optimizer = dict(lr=0.025)
+runner = dict(max_epochs=90)
 fp16 = dict(loss_scale=512.)
+
+custom_hooks = [
+    dict(
+        type='LazyEarlyStoppingHook',
+        start=0,
+        patience=10,
+        iteration_patience=0,
+        metric='mAP',
+        interval=1,
+        priority='ABOVE_NORMAL',
+    ),
+]
--- external/model-preparation-algorithm/configs/classification/mobilenet_v3_large_1_cls_incr/model_multilabel.py
+++ external/model-preparation-algorithm/configs/classification/mobilenet_v3_large_1_cls_incr/model_multilabel.py
@@ -1,4 +1,19 @@
 _base_ = [
   '../../../submodule/models/classification/ote_mobilenet_v3_large_multilabel.yaml',
 ]
+
+lr_config=dict(warmup_iters=0)
+runner = dict(max_epochs=90)
 fp16 = dict(loss_scale=512.)
+
+custom_hooks = [
+    dict(
+        type='LazyEarlyStoppingHook',
+        start=0,
+        patience=10,
+        iteration_patience=0,
+        metric='mAP',
+        interval=1,
+        priority='ABOVE_NORMAL',
+    ),
+]
