367c8da2dd7af9d8d67b5c02330fb8599bbb7b87
harimkan <harim.kang@intel.com>
2022-09-21 09:38:58 +0900

    Resolve pylint issues

--- ote_sdk/ote_sdk/utils/vis_utils.py
+++ ote_sdk/ote_sdk/utils/vis_utils.py
@@ -1,3 +1,6 @@
+"""
+This module implements activation map
+"""
 # Copyright (C) 2021-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
@@ -11,14 +14,12 @@ def get_actmap(
     saliency_map: np.ndarray,
     output_res: Union[tuple, list],
 ) -> np.ndarray:
-    """Get activation map (heatmap)  from saliency map
+    """
+    Get activation map (heatmap)  from saliency map
 
-    Args:
-        saliency_map (np.ndarray): Saliency map with pixel values from 0-255
-        output_res (Union[tuple, list]): Output resolution
-
-    Returns:
-        np.ndarray: activation map (heatmap)
+    :param saliency_map: Saliency map with pixel values from 0-255 (np.ndarray)
+    :param output_res: Output resolution (Union[tuple, list])
+    :return: activation map, heatmap (np.ndarray)
     """
     if len(saliency_map.shape) == 3:
         saliency_map = saliency_map[0]
