29206001da2560f7201f900814b9bb836c1ed18e
Ashwin Vaidya <ashwin.vaidya@intel.com>
2022-09-21 16:14:58 +0200

    Fix nncf keys

--- external/anomaly/tasks/nncf.py
+++ external/anomaly/tasks/nncf.py
@@ -132,9 +132,9 @@ class NNCFTask(InferenceTask, IOptimizationTask):
             for key in model_data["model"].keys():
                 if key.startswith("model."):
                     new_key = key.replace("model.", "")
-                    res = re.search(r"nncf_module\.(\w+)_backbone\.(.*)", new_key)
+                    res = re.search(r"nncf_module\.(\w+)_feature_extractor\.(.*)", new_key)
                     if res:
-                        new_key = f"nncf_module.{res.group(1)}_model.backbone.{res.group(2)}"
+                        new_key = f"nncf_module.{res.group(1)}_model.feature_extractor.{res.group(2)}"
                     nncf_modules[new_key] = model_data["model"][key]
                 else:
                     pl_modules[key] = model_data["model"][key]
