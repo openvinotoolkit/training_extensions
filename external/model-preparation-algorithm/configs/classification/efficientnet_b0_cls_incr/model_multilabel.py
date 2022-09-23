7b184d37d0817ebd0c12c27fd765f7c6dd454f65
Jihwan Eom <jihwan.eom@intel.com>
2022-09-23 10:51:40 +0900

    Remove LazyEarlyStopHook in model_multilabel.py (#1281)

--- external/model-preparation-algorithm/configs/classification/efficientnet_b0_cls_incr/model_multilabel.py
+++ external/model-preparation-algorithm/configs/classification/efficientnet_b0_cls_incr/model_multilabel.py
@@ -3,15 +3,3 @@ _base_ = [
 ]
 
 fp16 = dict(loss_scale=512.0)
-
-custom_hooks = [
-    dict(
-        type="LazyEarlyStoppingHook",
-        start=0,
-        patience=10,
-        iteration_patience=0,
-        metric="mAP",
-        interval=1,
-        priority=75,
-    ),
-]
