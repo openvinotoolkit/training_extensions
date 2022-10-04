b0e3c86aeed5d8aab27e10bcfa8311a9aa0a0119
Lee, Soobee <soobee.lee@intel.com>
2022-10-04 18:32:53 +0900

    make init file

---
+++ otx/algorithms/classification/tasks/datasets/__init__.py
@@ -0,0 +1,9 @@
+"""OTX Algorithms - Clasification Dataset."""
+
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+from .mpa_cls_dataset import MPAClsDataset, MPAMultilabelClsDataset, MPAHierarchicalClsDataset
+
+__all__ = ["MPAClsDataset", "MPAMultilabelClsDataset", "MPAHierarchicalClsDataset"]
