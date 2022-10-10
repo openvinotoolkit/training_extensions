73758b6477b229816c73d9d0f745cf1b934e7b5a
Eugene Liu <eugene.liu@intel.com>
2022-10-10 10:38:34 +0100

    fix codacy

--- ote_sdk/ote_sdk/utils/tiler.py
+++ ote_sdk/ote_sdk/utils/tiler.py
@@ -4,12 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from itertools import product
-from typing import Sequence, Union
 
 import numpy as np
 
 
 class Tiler:
+    """Tile Image into (non)overlapping Patches. Images are tiled in order to efficiently process large images.
+
+    Args:
+        tile_size: Tile dimension for each patch
+        overlap: Overlap between adjacent tile
+        batch_size: Batch Size
+    """
+
     def __init__(self, tile_size: int, overlap: float, batch_size=1) -> None:
         self.tile_size = tile_size
         self.overlap = overlap
@@ -22,36 +29,18 @@ class Tiler:
         Args:
             image: Input image to tile.
 
-        Examples:
-            >>> from anomalib.data.tiler import Tiler
-            >>> tiler = Tiler(tile_size=512,stride=256)
-            >>> image = torch.rand(size=(2, 3, 1024, 1024))
-            >>> image.shape
-            torch.Size([1024, 1024, 3])
-            >>> tiles = tiler.tile(image)
-            >>> tiles.shape
-            torch.Size([18, 512, 512, 3])
-
         Returns:
             Tiles generated from the image.
         """
-        height, width, channels = image.shape
-
-        self.num_patches_h = int((height - self.tile_size) / self.stride) + 1
-        self.num_patches_w = int((width - self.tile_size) / self.stride) + 1
+        height, width = image.shape[:2]
 
         tiles, offsets = [], []
-        for (tile_i, tile_j), (loc_i, loc_j) in zip(
-            product(range(self.num_patches_h), range(self.num_patches_w)),
-            product(
-                range(0, height - self.tile_size + 1, self.stride),
-                range(0, width - self.tile_size + 1, self.stride),
-            ),
-        ):
+        for (loc_i, loc_j) in product(range(0, height - self.tile_size + 1, self.stride),
+                                      range(0, width - self.tile_size + 1, self.stride)):
             tiles.append(image[loc_i : (loc_i + self.tile_size), loc_j : (loc_j + self.tile_size), ...])
             offsets.append((loc_i, loc_j))  # offset y, offset x
             if len(tiles) == self.batch_size:
                 yield tiles, offsets
                 tiles, offsets = [], []
-        if len(tiles):
+        if tiles:
             yield tiles, offsets
