371353a83002b74421df2d6eebe6b73b64100e2f
Lee, Soobee <soobee.lee@intel.com>
2022-10-26 15:34:40 +0900

    config name

--- otx/algorithms/classification/adapters/deep_object_reid/configs/base/__init__.py
+++ otx/algorithms/classification/adapters/deep_object_reid/configs/base/__init__.py
@@ -14,6 +14,6 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-from .parameters import ClassificationParameters
+from .configuration import ClassificationParameters
 
 __all__ = ["ClassificationParameters"]
