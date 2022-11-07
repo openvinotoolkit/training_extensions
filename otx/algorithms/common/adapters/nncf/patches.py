f22912546a54af880270155fedd512f9b86a11cd
Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>
2022-11-07 10:35:40 +0900

    feat: add train_step method to NNCFNetwork

--- otx/algorithms/common/adapters/nncf/patches.py
+++ otx/algorithms/common/adapters/nncf/patches.py
@@ -6,6 +6,7 @@ from contextlib import contextmanager
 from functools import partial
 
 import torch
+from nncf.torch.dynamic_graph.io_handling import replicate_same_tensors
 
 from .patchers import NO_TRACE_PATCHER, TRACE_PATCHER
 
@@ -43,3 +44,21 @@ def nncf_trace_context(self, img_metas, nncf_compress_postprocessing=True):
     self.forward = forward_backup
     torch.onnx.utils.__IN_ONNX_EXPORT = onnx_backup
     self = self.to(device_backup)
+
+
+def nncf_train_step(self, data, optimizer):
+    with self._compressed_context as ctx:
+        ctx.base_module_thread_local_replica = self
+        _, data = replicate_same_tensors(((), data))
+        if not self._in_user_dummy_forward:
+            # If a user supplies own dummy forward, he is responsible for
+            # correctly wrapping inputs inside it as well.
+            _, data = self._strip_traced_tensors((), data)
+            _, data = self._wrap_inputs_fn((), data)
+        retval = self.get_nncf_wrapped_model().train_step(data, optimizer)
+        retval = replicate_same_tensors(retval)
+        if not self._in_user_dummy_forward:
+            retval = self._wrap_outputs_fn(retval)
+
+    # TODO: deal with kd_loss_handler in forward method of  NNCFNEtwork
+    return retval
