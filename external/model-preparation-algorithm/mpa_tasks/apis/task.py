4febf0c3bf7bc28f2f49b0a72b193c94d05781b0
Eom, Jihwan <jihwan.eom@intel.com>
2022-11-10 17:47:48 +0900

    Update drop_last in BaseTask

--- external/model-preparation-algorithm/mpa_tasks/apis/task.py
+++ external/model-preparation-algorithm/mpa_tasks/apis/task.py
@@ -20,6 +20,7 @@ from ote_sdk.entities.datasets import DatasetEntity
 from ote_sdk.entities.model import ModelEntity, ModelPrecision
 from ote_sdk.entities.task_environment import TaskEnvironment
 from ote_sdk.serialization.label_mapper import LabelSchemaMapper
+from ote_sdk.entities.subset import Subset
 
 
 logger = get_logger()
@@ -84,8 +85,9 @@ class BaseTask:
         self._model_cfg['model_classes'] = model_classes
         if dataset is not None:
             train_data_cfg = Stage.get_train_data_cfg(self._data_cfg)
+            train_dataset = dataset.get_subset(Subset.TRAINING)
             # if dataset size is smaller than batch size
-            if 0 < len(dataset) < self._recipe_cfg.data.get('samples_per_gpu', 2):
+            if 0 < len(train_dataset) < self._recipe_cfg.data.get('samples_per_gpu', 2):
                 train_data_cfg.drop_last = False
             train_data_cfg['data_classes'] = data_classes
             new_classes = np.setdiff1d(data_classes, model_classes).tolist()
