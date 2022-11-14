286021088bb63a47355f3032a969a7b8f3024265
Harim Kang <harim.kang@intel.com>
2022-11-14 10:45:57 +0900

    Update evaluation iou_thr of ins-seg (#1354)

--- external/model-preparation-algorithm/configs/rotated-detection/resnet50_maskrcnn/model.py
+++ external/model-preparation-algorithm/configs/rotated-detection/resnet50_maskrcnn/model.py
@@ -2,3 +2,4 @@ _base_ = [
     "../../../submodule/samples/cfgs/models/backbones/resnet50.yaml",
     "../../../submodule/recipes/stages/_base_/models/detectors/resnet50_maskrcnn.custom.py",
 ]
+evaluation = dict(interval=1, metric='mAP', save_best='mAP', iou_thr=[0.5])
