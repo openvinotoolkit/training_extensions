fabc265e0e1bcbb9de620781ea8e9b49a9c5caab
Harim Kang <harim.kang@intel.com>
2022-11-15 10:12:54 +0900

    fix black & add xfail test cases (#1367)

--- external/model-preparation-algorithm/configs/rotated-detection/efficientnetb2b_maskrcnn/model.py
+++ external/model-preparation-algorithm/configs/rotated-detection/efficientnetb2b_maskrcnn/model.py
@@ -2,5 +2,5 @@ _base_ = [
     "../../../submodule/samples/cfgs/models/backbones/efficientnet_b2b.yaml",
     "../../../submodule/recipes/stages/_base_/models/detectors/efficientnetb2b_maskrcnn.custom.py",
 ]
-evaluation = dict(interval=1, metric='mAP', save_best='mAP', iou_thr=[0.5])
+evaluation = dict(interval=1, metric="mAP", save_best="mAP", iou_thr=[0.5])
 fp16 = dict(loss_scale=512.0)
