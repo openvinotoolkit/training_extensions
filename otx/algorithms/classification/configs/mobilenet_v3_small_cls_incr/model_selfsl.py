303d46b2cd6f6c388ce680bede978e7757023f93
Adria Arrufat <adria.arrufat@intel.com>
2022-11-17 15:18:44 +0900

    Make the SelfSL classifier inherit from SAMImageClassifier

--- otx/algorithms/classification/configs/mobilenet_v3_small_cls_incr/model_selfsl.py
+++ otx/algorithms/classification/configs/mobilenet_v3_small_cls_incr/model_selfsl.py
@@ -1,25 +1,21 @@
-# _base_ = "../base/models/mobilenet_v3.py"
+# _base_ = [ "../base/models/mobilenet_v3.py" ]
 
 model = dict(
     task="classification",
-    type='SelfSLClassifier',
+    type="SelfSLClassifier",
     backbone=dict(type="OTXMobileNetV3", pretrained=True, mode="small", width_mult=1.0),
-    neck=dict(type='GlobalAveragePooling'),
+    neck=dict(type="GlobalAveragePooling"),
     head=dict(
-        type='SelfSLClsHead',
+        type="SelfSLClsHead",
         num_classes=10,
         in_channels=-1,
         aux_head=dict(hid_channels=0, out_channels=1024),
-        loss=dict(
-            type='BarlowTwinsLoss',
-            off_diag_penality=1. / 128.
-        )
-    )
+        loss=dict(type="BarlowTwinsLoss", off_diag_penality=1.0 / 128.0),
+    ),
 )
 
 checkpoint_config = dict(
-    type='CheckpointHookWithValResults',
+    type="CheckpointHookWithValResults",
     interval=1,
     max_keep_ckpts=1,
 )
-
