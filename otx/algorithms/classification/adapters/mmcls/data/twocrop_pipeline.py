4cf26f9b380f0d3265585511e4392226c9ceeca0
Adria Arrufat <adria.arrufat@intel.com>
2022-11-22 15:22:21 +0900

    Use the same data pipeline as the Baseline model

--- otx/algorithms/classification/adapters/mmcls/data/twocrop_pipeline.py
+++ otx/algorithms/classification/adapters/mmcls/data/twocrop_pipeline.py
@@ -4,46 +4,21 @@ img_norm_cfg = dict(
 __resize_target_size = 224
 
 
-# train_pipeline = [
-#     dict(
-#         type="TwoCropTransform",
-#         pipeline=[
-#             dict(type="Resize", size=__resize_target_size),
-#             dict(type="RandomFlip", flip_prob=0.5, direction="horizontal"),
-#             dict(type="AugMixAugment", config_str="augmix-m5-w3"),
-#             dict(type="RandomRotate", p=0.35, angle=(-10, 10)),
-#             dict(type="ToNumpy"),
-#             dict(type="Normalize", **img_norm_cfg),
-#             dict(type="ImageToTensor", keys=["img"]),
-#             dict(type="ToTensor", keys=["gt_label"]),
-#             dict(type="Collect", keys=["img", "gt_label"]),
-#         ],
-#     )
-# ]
-
 train_pipeline = [
-    dict(type="TwoCropTransform",
+    dict(
+        type="TwoCropTransform",
         pipeline=[
-            dict(type="RandomResizedCrop", size=__resize_target_size, scale=(0.2, 1.0)),
+            dict(type="Resize", size=__resize_target_size),
             dict(type="RandomFlip", flip_prob=0.5, direction="horizontal"),
-            dict(type="RandomAppliedTrans",
-                transforms=[
-                dict(
-                    type="ColorJitter",
-                    brightness=0.4,
-                    contrast=0.4,
-                    saturation=0.4,
-                    hue=0.1
-                )
-            ], p=0.8),
-            dict(type="RandomGrayscale", gray_prob=0.2),
+            dict(type="AugMixAugment", config_str="augmix-m5-w3"),
+            dict(type="RandomRotate", p=0.35, angle=(-10, 10)),
             dict(type="ToNumpy"),
             dict(type="Normalize", **img_norm_cfg),
             dict(type="ImageToTensor", keys=["img"]),
             dict(type="ToTensor", keys=["gt_label"]),
             dict(type="Collect", keys=["img", "gt_label"]),
-        ]
-    ),
+        ],
+    )
 ]
 
 test_pipeline = [
