e638b205beba5251e68261eb2087799f0d820ab0
sungmanc <sungman.cho@intel.com>
2022-11-28 09:00:06 +0900

    Fix: multi-class classification label schema error

--- tests/integration/cli/classification/test_classification.py
+++ tests/integration/cli/classification/test_classification.py
@@ -40,6 +40,7 @@ from tests.test_suite.e2e_test_system import e2e_pytest_component
 # Pre-train w/ 'intel', 'openvino' classes
 args0 = {
     "--train-data-roots": "data/datumaro/imagenet_dataset",
+    "--val-data-roots": "data/datumaro/imagenet_dataset",
     "train_params": [
         "params",
         "--learning_parameters.num_iters",
@@ -52,6 +53,7 @@ args0 = {
 # Pre-train w/ 'intel', 'openvino', 'opencv' classes
 args = {
     "--train-data-roots": "data/datumaro/imagenet_dataset",
+    "--val-data-roots": "data/datumaro/imagenet_dataset",
     "train_params": [
         "params",
         "--learning_parameters.num_iters",
