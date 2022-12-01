23b9b4f3b02ec0edb0acce974955bd00953d6afe
sungmanc <sungman.cho@intel.com>
2022-12-01 09:50:08 +0900

    Add action dataset adapter, remove some useless lines

---
+++ otx/core/anomaly_dataset_adapter.py
@@ -0,0 +1,15 @@
+"""Anomaly Classification / Detection / Segmentation Dataset Adapter."""
+
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# pylint: disable=invalid-name, too-many-locals, no-member
+from otx.core.base_dataset_adapter import BaseDatasetAdapter
+from otx.api.entities.datasets import DatasetEntity
+from otx.utils.logger import get_logger
+
+class AnomalyDatasetAdapter(BaseDatasetAdapter):
+    def convert_to_otx_format(self, datumaro_dataset: dict) -> DatasetEntity:
+        """ Conver DatumaroDataset to DatasetEntity for Anomalytasks. """
+        pass
