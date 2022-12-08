bc228d3ba5ce98df031caf8067778a1004e47f54
dongkwan-kim <dongkwan.kim@intel.com>
2022-12-08 11:48:26 +0900

    [OTE][Release][XAI] Detection fix two stage bbox_head error (#1414)
    * fix two stage bbox_head error
    
    * two-path for saliency map exporting
    
    * fix pylint
    
    * two-stage detector exception for simple test
    
    * fix assertion err in the detection saliency map
    
    * update tiler: handle memory and saliency map dimension issues
    
    * revert to original assertion
    
    * commit head change to develop
    
    * submodule head develop
    
    * submodule heading to releases/OTE_0.4.0
    
    * fix code checker
    
    * add import with flake8 exception
    
    Co-authored-by: Evgeny Tsykunov <e.tsykunov@gmail.com>

--- ote_sdk/ote_sdk/utils/tiler.py
+++ ote_sdk/ote_sdk/utils/tiler.py
@@ -6,6 +6,7 @@ Tiling Module
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import copy
 from itertools import product
 from typing import Any, List, Tuple, Union
 
@@ -79,7 +80,7 @@ class Tiler:
             detections = np.append(detections, output, axis=0)
             # cache full image feature vector and saliency map at 0 index
             if i == 0:
-                features = feats
+                features = copy.deepcopy(feats)
 
         if np.prod(detections.shape):
             dets, keep = multiclass_nms(detections, max_num=self.max_number)
@@ -128,7 +129,7 @@ class Tiler:
             if "feature_vector" in raw_predictions or "saliency_map" in raw_predictions:
                 features = [
                     raw_predictions["feature_vector"].reshape(-1),
-                    raw_predictions["saliency_map"],
+                    raw_predictions["saliency_map"][0],
                 ]
         return features, output
 
