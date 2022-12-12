91d11727513eb507aced0853ea774764da93b024
Kim, Sungchul <sungchul.kim@intel.com>
2022-12-12 15:40:44 +0900

    update cfgs

---
+++ otx/algorithms/classification/configs/efficientnet_v2_s_cls_warmstart/model.py
@@ -0,0 +1,41 @@
+"""EfficientNet-V2 for warmstart config."""
+
+# pylint: disable=invalid-name
+
+_base_ = "../efficientnet_v2_s_cls_incr/model.py"
+
+model = dict(
+    type="BYOL",
+    task="classification",
+    base_momentum=0.996,
+    neck=dict(
+        type="SelfSLMLP",
+        in_channels=1280,
+        hid_channels=4096,
+        out_channels=256,
+        with_avg_pool=True
+    ),
+    head=dict(
+        type="ConstrastiveHead",
+        predictor=dict(
+            type="SelfSLMLP",
+            in_channels=256,
+            hid_channels=4096,
+            out_channels=256,
+            with_avg_pool=False
+        )
+    )
+)
+
+custom_hooks = [
+    dict(
+        type="MomentumUpdateHook",
+        end_momentum=1.
+    )
+]
+
+load_from = None
+
+resume_from = None
+
+fp16 = None
