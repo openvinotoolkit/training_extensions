e933001b4af90064449910a5434b00180b8b80d8
Kim, Sungchul <sungchul.kim@intel.com>
2022-12-15 17:36:08 +0900

    remove duplicated transforms (RandomFlip, Solarize)

--- otx/algorithms/classification/adapters/mmcls/data/__init__.py
+++ otx/algorithms/classification/adapters/mmcls/data/__init__.py
@@ -25,7 +25,6 @@ from .pipelines import (
     LoadImageFromOTXDataset,
     OTXColorJitter,
     RandomAppliedTrans,
-    RandomHorizontalFlip,
 )
 
 __all__ = [
@@ -34,7 +33,6 @@ __all__ = [
     "MPAHierarchicalClsDataset",
     "SelfSLDataset",
     "LoadImageFromOTXDataset",
-    "RandomHorizontalFlip",
     "RandomAppliedTrans",
     "GaussianBlur",
     "OTXColorJitter",
