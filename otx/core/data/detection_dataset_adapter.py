2243df402177357dd8abbd26cd6c0e20f929f18e
sungmanc <sungman.cho@intel.com>
2022-12-16 15:27:46 +0900

    Changed architecture to avoid cyclic import, add docstring

--- otx/core/data/detection_dataset_adapter.py
+++ otx/core/data/detection_dataset_adapter.py
@@ -27,6 +27,7 @@ from otx.core.data.base_dataset_adapter import BaseDatasetAdapter
 
 class DetectionDatasetAdapter(BaseDatasetAdapter):
     """Detection adapter inherited by BaseDatasetAdapter.
+
     It converts DatumaroDataset --> DatasetEntity for object detection, and instance segmentation tasks
     """
 
