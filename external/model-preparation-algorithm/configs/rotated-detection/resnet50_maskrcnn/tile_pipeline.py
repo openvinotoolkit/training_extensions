f2fc56e636a1518c9708862149a06d187c37e933
Eugene Liu <eugene.liu@intel.com>
2022-12-20 01:31:01 +0000

    [OTE][develop] Add tiling in rotated detection (#1420)
    * tiling in rotated detection
    
    * ote sdk
    
    * update ote sdk
    
    * update ote sdk requirement
    
    * update
    
    * fix bug
    
    * update ote sdk
    
    * set xfail to tiling nncf tests
    
    Co-authored-by: Songki Choi <songki.choi@intel.com>

--- external/model-preparation-algorithm/configs/rotated-detection/resnet50_maskrcnn/tile_pipeline.py
+++ external/model-preparation-algorithm/configs/rotated-detection/resnet50_maskrcnn/tile_pipeline.py
@@ -1,12 +1,12 @@
 dataset_type = "CocoDataset"
 
-img_size = (1024, 1024)
+img_size = (1344, 800)
 
 tile_cfg = dict(
     tile_size=400, min_area_ratio=0.9, overlap_ratio=0.2, iou_threshold=0.45, max_per_img=1500, filter_empty_gt=True
 )
 
-img_norm_cfg = dict(mean=(103.53, 116.28, 123.675), std=(1.0, 1.0, 1.0), to_rgb=True)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
 train_pipeline = [
     dict(type="Resize", img_scale=img_size, keep_ratio=False),
