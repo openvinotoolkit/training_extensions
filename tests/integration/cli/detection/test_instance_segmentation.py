c6a0533499c5c801815db617fe7d01e16c0c78ed
sungmanc <sungman.cho@intel.com>
2022-12-21 17:09:56 +0900

    Edit test codes and base_dataset_adapter

--- tests/integration/cli/detection/test_instance_segmentation.py
+++ tests/integration/cli/detection/test_instance_segmentation.py
@@ -1,4 +1,4 @@
-"""Tests for MPA Class-Incremental Learning for object detection with OTX CLI"""
+"""Tests for Class-Incremental Learning for object detection with OTX CLI"""
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
