03827240e650ac5c9a6ca6f01e0b383301ed719f
Kim, Sungchul <sungchul.kim@intel.com>
2022-12-23 11:42:48 +0900

    isort

--- otx/algorithms/classification/adapters/mmcls/models/__init__.py
+++ otx/algorithms/classification/adapters/mmcls/models/__init__.py
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
+from .classifiers import BYOL
 from .heads import ConstrastiveHead
 from .necks import SelfSLMLP
-from .classifiers import BYOL
 
 __all__ = ["BYOL", "SelfSLMLP", "ConstrastiveHead"]
