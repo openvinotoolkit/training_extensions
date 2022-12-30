bc6af29ae1ee77e586068da4e3a6e90cbfa4d25a
Cho, Sungman <sungman.cho@intel.com>
2022-12-30 14:51:10 +0900

    Delte hasattr line, edit anomaly parts, edit for pre-commit

--- otx/cli/tools/train.py
+++ otx/cli/tools/train.py
@@ -33,14 +33,19 @@ from otx.cli.registry import find_and_parse_model_template
 from otx.cli.utils.config import configure_dataset, override_parameters
 from otx.cli.utils.hpo import run_hpo
 from otx.cli.utils.importing import get_impl_class
+<<<<<<< HEAD
 from otx.cli.utils.io import read_binary, read_label_schema, save_model_data
 from otx.cli.utils.multi_gpu import MultiGPUManager
+=======
+>>>>>>> Delte hasattr line, edit anomaly parts, edit for pre-commit
 from otx.cli.utils.io import read_binary, read_label_schema, save_model_data
+from otx.cli.utils.multi_gpu import MultiGPUManager
 from otx.cli.utils.parser import (
     add_hyper_parameters_sub_parser,
     gen_params_dict_from_args,
 )
 from otx.core.data.adapter import get_dataset_adapter
+
 
 def parse_args():
     """Parses command line arguments.
