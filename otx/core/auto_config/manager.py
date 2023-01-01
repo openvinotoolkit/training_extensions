54a6f44a03161375fbbc99e4bc3ec8221f2a1fea
Cho, Sungman <sungman.cho@intel.com>
2023-01-01 14:22:14 +0900

    Minor commit

--- otx/core/auto_config/manager.py
+++ otx/core/auto_config/manager.py
@@ -20,7 +20,7 @@ class AutoConfigManager:
                 'cityscapes',
                 'ade20k2017',
                 'ade20k2020'
-            ]
+            ],
         }
     @classmethod
     def find_task_type(cls, data_root):
@@ -32,20 +32,43 @@ class AutoConfigManager:
                 task_type_candidates.append(task)
     
     def _is_cvat_format(self, path):
-        """Detect whether data path is CVAT format or not."""
-        #TODO: Will be supported by dautmaro detect_dataset. 
+        """Detect whether data path is CVAT format or not.
+        Currently, we used multi-video CVAT format for Action tasks.
+        
+        This function can detect the multi-video CVAT format.
+        
+        Multi-video CVAT format
+        root
+        |--video_0
+            |--images
+                |--frame0001.png
+            |--annotations.xml
+        |--video_1
+        |--video_2
+        
+        """
+        for root, dirs, path in os.listdir(path):
+            sub_folder_path = os.path.join(path, sub_folder)
         pass
 
 
     def _is_mvtec(self, path):
-        """Detect whether data path is MVTec format or not."""
-
-        # condition 1: 'ground_truth', 'train', 'test' folder are located
+        """Detect whether data path is MVTec format or not.
+        Check the first-level architecture folder, to know whether the dataset is MVTec or not.
+        
+        MVTec default structure like as below:
+        root
+        |--ground_truth
+        |--train
+        |--test
+        
+        """
+        # conditio 'ground_truth', 'train', 'test' folder are located
         mvtec_folders = sorted(['ground_truth', 'train', 'test'])
         folder_list = []
-        for sub in os.listdir(path):
-            if os.isdir(sub):
-                folder_list.append(sub)
+        for sub_folder in os.listdir(path):
+            # only use the folder name.
+            if os.isdir(sub_folder):
+                folder_list.append(sub_folder)
         return mvtec_folders == sorted(folder_list)
         
-        # condition 2: 
