08759706a2bc4814f757b7edabe7511197d83cde
Adria Arrufat <adria.arrufat@intel.com>
2023-01-03 14:21:37 +0900

    Add missing docstring in public module

--- otx/algorithms/classification/configs/base/data/supcon/data_pipeline.py
+++ otx/algorithms/classification/configs/base/data/supcon/data_pipeline.py
@@ -1,3 +1,21 @@
+"""Data Pipeline of SupCon model for Classification Task."""
+
+# Copyright (C) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+
+# pylint: disable=invalid-name
+
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 __resize_target_size = 224
 
