b7b53308107eceea6f1fbbe595560593ae8eb27b
Cho, Sungman <sungman.cho@intel.com>
2023-01-09 17:08:57 +0900

    Edit the autoconfig manager, helper

--- otx/core/auto_config/manager.py
+++ otx/core/auto_config/manager.py
@@ -3,9 +3,6 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
-from typing import Any
-import os
-import datumaro
 from datumaro.components.dataset import Dataset
 from otx.core.data.utils.datumaro_helper import DatumaroHelper
 
@@ -31,20 +28,6 @@ class AutoConfigManager:
         }
         
         self.task = None # type: str
-    
-    def _find_data_format(self, data_root: str) -> str:
-        """Find the format of dataset."""
-        data_root = os.path.abspath(data_root)
-        
-        data_format = ""
-        if self._is_cvat_format(data_root):
-            data_format = "multi-cvat"
-        elif self._is_mvtec_format(data_root):
-            data_format = "mvtec"
-        else: 
-            data_formats = datumaro.Environment().detect_dataset(data_root)
-            data_format = data_formats[0] if 'imagenet' not in data_formats else 'imagenet'
-        return data_format
          
     def find_task_type(self, data_root: str) -> str:
         """Detect task type.
@@ -59,7 +42,7 @@ class AutoConfigManager:
         If Datumaro supports the Kinetics, AVA datasets, MVTec, _is_cvat_format(), _is_mvtec_format()
         functions will be deleted.
         """
-        data_format = self._find_data_format(data_root)
+        data_format = DatumaroHelper.find_data_format(data_root)
         
         task = ""
         if data_format == "multi-cvat":
@@ -90,54 +73,3 @@ class AutoConfigManager:
         if val_data_root:
             val_dataset = Dataset.import_from(val_data_root, format=self.task)
             val_set = DatumaroHelper.get_val_dataset(val_dataset)
-        
-         
-            
-     
-    def _is_cvat_format(self, path: str) -> bool:
-        """Detect whether data path is CVAT format or not.
-        Currently, we used multi-video CVAT format for Action tasks.
-        
-        This function can detect the multi-video CVAT format.
-        
-        Multi-video CVAT format
-        root
-        |--video_0
-            |--images
-                |--frame0001.png
-            |--annotations.xml
-        |--video_1
-        |--video_2
-        """
-        
-        cvat_format = sorted(['images', 'annotations.xml'])
-        for sub_folder in os.listdir(path):
-            # video_0, video_1, ...
-            sub_folder_path = os.path.join(path, sub_folder)
-            # files must be same with cvat_format
-            if os.path.isdir(sub_folder_path):
-                files = sorted(os.listdir(sub_folder_path))
-                if files != cvat_format:
-                    return False
-        return True
-
-    def _is_mvtec_format(self, path: str) -> bool:
-        """Detect whether data path is MVTec format or not.
-        Check the first-level architecture folder, to know whether the dataset is MVTec or not.
-        
-        MVTec default structure like as below:
-        root
-        |--ground_truth
-        |--train
-        |--test
-        """
-        
-        mvtec_format = sorted(['ground_truth', 'train', 'test'])
-        folder_list = []
-        for sub_folder in os.listdir(path):
-            sub_folder_path = os.path.join(path, sub_folder)
-            # only use the folder name.
-            if os.path.isdir(sub_folder_path):
-                folder_list.append(sub_folder)
-        return sorted(folder_list) == mvtec_format
-        
--- otx/core/data/utils/datumaro_helper.py
+++ otx/core/data/utils/datumaro_helper.py
@@ -4,11 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import os
+import datumaro
 from datumaro.components.dataset import Dataset
 from datumaro.components.dataset import DatasetSubset
 
 
 class DatumaroHelper:
+    """The aim of DatumaroHelper is support datumaro functions at easy use.
+    All kind of functions implemented in Datumaro are supported by this helper.
+    """
     @staticmethod
     def get_train_dataset(dataset:Dataset) -> DatasetSubset:
         """Returns train dataset."""
@@ -21,3 +26,76 @@ class DatumaroHelper:
         for k, v in dataset.subsets().items():
             if "val" in k or "default" in k:
                 return v
+    
+    @staticmethod 
+    def find_data_format(self, data_root: str) -> str:
+        """Find the format of dataset."""
+        data_root = os.path.abspath(data_root)
+        
+        data_format = ""
+        
+        # TODO #
+        # Currently, below `if/else` statements is mandatory 
+        # because Datumaro can't detect the multi-cvat and mvtec.
+        # After, the upgrade of Datumaro, below codes will be changed. 
+        if self._is_cvat_format(data_root):
+            data_format = "multi-cvat"
+        elif self._is_mvtec_format(data_root):
+            data_format = "mvtec"
+        else: 
+            data_formats = datumaro.Environment().detect_dataset(data_root)
+            #TODO: how to avoid hard-coded part
+            data_format = data_formats[0] if 'imagenet' not in data_formats else 'imagenet'
+        return data_format
+    
+    def _is_cvat_format(self, path: str) -> bool:
+        """Detect whether data path is CVAT format or not.
+        Currently, we used multi-video CVAT format for Action tasks.
+        
+        This function can detect the multi-video CVAT format.
+        
+        Multi-video CVAT format
+        root
+        |--video_0
+            |--images
+                |--frame0001.png
+            |--annotations.xml
+        |--video_1
+        |--video_2
+        
+        will be deprecated soon.
+        """
+        
+        cvat_format = sorted(['images', 'annotations.xml'])
+        for sub_folder in os.listdir(path):
+            # video_0, video_1, ...
+            sub_folder_path = os.path.join(path, sub_folder)
+            # files must be same with cvat_format
+            if os.path.isdir(sub_folder_path):
+                files = sorted(os.listdir(sub_folder_path))
+                if files != cvat_format:
+                    return False
+        return True
+
+    def _is_mvtec_format(self, path: str) -> bool:
+        """Detect whether data path is MVTec format or not.
+        Check the first-level architecture folder, to know whether the dataset is MVTec or not.
+        
+        MVTec default structure like as below:
+        root
+        |--ground_truth
+        |--train
+        |--test
+        
+        will be deprecated soon.
+        """
+        
+        mvtec_format = sorted(['ground_truth', 'train', 'test'])
+        folder_list = []
+        for sub_folder in os.listdir(path):
+            sub_folder_path = os.path.join(path, sub_folder)
+            # only use the folder name.
+            if os.path.isdir(sub_folder_path):
+                folder_list.append(sub_folder)
+        return sorted(folder_list) == mvtec_format
+        
