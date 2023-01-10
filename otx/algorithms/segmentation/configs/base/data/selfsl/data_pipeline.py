c029cfb73a19380bb2ab738ec80073927599c0d6
Kim, Sungchul <sungchul.kim@intel.com>
2023-01-10 17:53:04 +0900

    update data_pipeline

--- otx/algorithms/segmentation/configs/base/data/selfsl/data_pipeline.py
+++ otx/algorithms/segmentation/configs/base/data/selfsl/data_pipeline.py
@@ -16,7 +16,7 @@
 
 # pylint: disable=invalid-name
 
-__resize_target_size = 224
+__resize_target_size = (224, 224)
 __img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
 train_pipeline = [
--- otx/algorithms/segmentation/configs/base/data/supcon/data_pipeline.py
+++ otx/algorithms/segmentation/configs/base/data/supcon/data_pipeline.py
@@ -16,12 +16,42 @@
 
 # pylint: disable=invalid-name
 
-_base_ = ["../selfsl/data_pipeline.py",]
-
 __img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 __img_scale = (544, 544)
+__resize_target_size = (512, 512)
 
-test_pipeline = [
+__train_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations"),
+    dict(
+        type="TwoCropTransform",
+        view0=[
+            dict(type="NDArrayToPILImage", keys=["img"]),
+            dict(type="RandomResizedCrop", size=__resize_target_size),
+            dict(type="RandomColorJitter", brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1, p=0.8),
+            dict(type="RandomGrayscale", p=0.2),
+            dict(type="RandomGaussianBlur", kernel_size=23, p=1.0),
+            dict(type="PILImageToNDArray", keys=["img"]),
+            dict(type="RandomFlip", prob=0.5, direction="horizontal"),
+            dict(type="Normalize", **__img_norm_cfg),
+        ],
+        view1=[
+            dict(type="NDArrayToPILImage", keys=["img"]),
+            dict(type="RandomResizedCrop", size=__resize_target_size),
+            dict(type="RandomColorJitter", brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1, p=0.8),
+            dict(type="RandomGrayscale", p=0.2),
+            dict(type="RandomGaussianBlur", kernel_size=23, p=0.1),
+            dict(type="PILImageToNDArray", keys=["img"]),
+            dict(type="RandomSolarization", threshold=128, p=0.2),
+            dict(type="RandomFlip", prob=0.5, direction="horizontal"),
+            dict(type="Normalize", **__img_norm_cfg),
+        ],
+    ),
+    dict(type="DefaultFormatBundle"),
+    dict(type="Collect", keys=["img", "gt_semantic_seg"]),
+]
+
+__test_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(
         type="MultiScaleFlipAug",
@@ -40,9 +70,7 @@ test_pipeline = [
 # TODO (Sungchul, Soobee) : Remove Repeatdataset in data config
 # when otx/algorithms/segmentation/configs/base/data/data_pipeline.py is updated.
 data = dict(
-    train=dict(
-        _delete_=True,
-        dataset=dict(pipeline={{_base_.train_pipeline}})),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline),
+    train=dict(dataset=dict(pipeline=__train_pipeline)),
+    val=dict(pipeline=__test_pipeline),
+    test=dict(pipeline=__test_pipeline),
 )
