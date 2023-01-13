814cff6351425654845f6fe7756a1896889400c8
Jihwan Eom <jihwan.eom@intel.com>
2023-01-14 01:04:35 +0900

    Improve readability

--- otx/mpa/cls/semisl/stage.py
+++ otx/mpa/cls/semisl/stage.py
@@ -20,19 +20,18 @@ class SemiSLClsStage(ClsStage):
     def configure_data(self, cfg, data_cfg, training, **kwargs):
         """Patch cfg.data."""
         super().configure_data(cfg, data_cfg, training, **kwargs)
-        if training:
-            if "unlabeled" in cfg.data and cfg.train_type == "SEMISUPERVISED":
-                samples_per_gpu = cfg.data.unlabeled.pop("samples_per_gpu", cfg.data.samples_per_gpu)
-                workers_per_gpu = cfg.data.unlabeled.pop("workers_per_gpu", cfg.data.workers_per_gpu)
-                update_or_add_custom_hook(
-                    cfg,
-                    ConfigDict(
-                        type="UnlabeledDataHook",
-                        unlabeled_data_cfg=cfg.data.unlabeled,
-                        samples_per_gpu=samples_per_gpu,
-                        workers_per_gpu=workers_per_gpu,
-                        model_task=cfg.model_task,
-                        seed=cfg.seed,
-                        persistent_workers=True if workers_per_gpu > 0 else False,
-                    ),
-                )
+        if training and "unlabeled" in cfg.data:
+            samples_per_gpu = cfg.data.unlabeled.pop("samples_per_gpu", cfg.data.samples_per_gpu)
+            workers_per_gpu = cfg.data.unlabeled.pop("workers_per_gpu", cfg.data.workers_per_gpu)
+            update_or_add_custom_hook(
+                cfg,
+                ConfigDict(
+                    type="UnlabeledDataHook",
+                    unlabeled_data_cfg=cfg.data.unlabeled,
+                    samples_per_gpu=samples_per_gpu,
+                    workers_per_gpu=workers_per_gpu,
+                    model_task=cfg.model_task,
+                    seed=cfg.seed,
+                    persistent_workers=True if workers_per_gpu > 0 else False,
+                ),
+            )
