2ccb0048549582a01b05e286795081470eedcfae
JihwanEom <jihwan.eom@intel.com>
2023-01-13 14:52:12 +0900

    Seperate IncrStage

--- otx/mpa/cls/incremental/stage.py
+++ otx/mpa/cls/incremental/stage.py
@@ -1,15 +1,24 @@
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
-
+import copy
 from mmcv import ConfigDict
 
-from otx.mpa.cls.stage import ClsStage
+from otx.mpa.cls.stage import Stage, ClsStage
 from otx.mpa.utils.config_utils import update_or_add_custom_hook
 from otx.mpa.utils.logger import get_logger
 
 logger = get_logger()
 
+CLASS_INC_DATASET = [
+    "MPAClsDataset",
+    "MPAMultilabelClsDataset",
+    "MPAHierarchicalClsDataset",
+    "ClsDirDataset",
+    "ClsTVDataset",
+]
+PSEUDO_LABEL_ENABLE_DATASET = ["ClassIncDataset", "LwfTaskIncDataset", "ClsTVDataset"]
+WEIGHT_MIX_CLASSIFIER = ["SAMImageClassifier"]
 
 class IncrClsStage(ClsStage):
     """Patch config to support incremental learning for object Cls"""
@@ -17,27 +26,163 @@ class IncrClsStage(ClsStage):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    # def configure_task(self, cfg, training, **kwargs):
-    #     """Patch config to support incremental learning"""
-    #     super().configure_task(cfg, training, **kwargs)
-    #     if "task_adapt" in cfg and self.task_adapt_type == "mpa":
-    #         self.configure_task_adapt_hook(cfg)
+    def configure_task(self, cfg, training, **kwargs):
+        """Patch config to support incremental learning"""
+        super().configure_task(cfg, training, **kwargs)
+        if "task_adapt" in cfg:
+            self.sub_configure_task(cfg, training, **kwargs)
 
-    # def configure_task_adapt_hook(self, cfg):
-    #     """Add TaskAdaptHook for sampler."""
-    #     sampler_flag = True
-    #     if len(set(self.org_model_classes) & set(self.model_classes)) == 0 or set(self.org_model_classes) == set(
-    #         self.model_classes
-    #     ):
-    #         sampler_flag = False
-    #     update_or_add_custom_hook(
-    #         cfg,
-    #         ConfigDict(
-    #             type="TaskAdaptHook",
-    #             src_classes=self.org_model_classes,
-    #             dst_classes=self.model_classes,
-    #             model_type=cfg.model.type,
-    #             sampler_flag=sampler_flag,
-    #             efficient_mode=cfg["task_adapt"].get("efficient_mode", False),
-    #         ),
-    #     )
+    # noqa: C901
+    def sub_configure_task(self, cfg, training, **kwargs):
+        """Configure for Task Adaptation Task"""
+
+        self.task_type = cfg["task_adapt"].get("type", None)
+        self.adapt_type = cfg["task_adapt"].get("op", "REPLACE")
+
+        model_tasks, self.dst_classes = None, None
+        train_data_cfg = Stage.get_data_cfg(cfg, "train")
+
+        if training:
+            # if Trainer to Stage configure, training = True
+            if train_data_cfg.get("tasks"):
+                # Task Adaptation
+                if self.model_meta.get("tasks", False):
+                    self.refine_tasks(train_data_cfg)
+                else:
+                    raise KeyError(f"can not find task meta data from {cfg.load_from}.")
+                cfg.model.head.update({"old_tasks": self.old_tasks})
+                # update model.head.tasks with training dataset's tasks if it's configured as None
+                if cfg.model.head.get("tasks") is None:
+                    logger.info(
+                        "'tasks' in model.head is None. updated with configuration on train data "
+                        f"{train_data_cfg.get('tasks')}"
+                    )
+                    cfg.model.head.update({"tasks": train_data_cfg.get("tasks")})
+            elif "new_classes" in train_data_cfg:
+                # Class-Incremental
+                self.refine_cls(train_data_cfg)
+            else:
+                raise KeyError('"new_classes" or "tasks" should be defined for incremental learning w/ current model.')
+
+            if self.task_type == "mpa":
+                if train_data_cfg.type not in CLASS_INC_DATASET:  # task incremental is not supported yet
+                    raise NotImplementedError(
+                        f"Class Incremental Learning for {train_data_cfg.type} is not yet supported!"
+                    )
+
+                if cfg.model.type in WEIGHT_MIX_CLASSIFIER:
+                    cfg.model.task_adapt = ConfigDict(
+                        src_classes=self.model_classes,
+                        dst_classes=self.data_classes,
+                    )
+
+                # Train dataset config update
+                train_data_cfg.classes = self.dst_classes
+
+                # model configuration update
+                cfg.model.head.num_classes = len(self.dst_classes)
+                self.configure_task_adapt(cfg)
+
+        else:  # if not training phase (eval)
+            if train_data_cfg.get("tasks"):
+                if self.model_meta.get("tasks", False):
+                    cfg.model.head["tasks"] = self.model_meta["tasks"]
+                else:
+                    raise KeyError(f"can not find task meta data from {cfg.load_from}.")
+            elif train_data_cfg.get("new_classes"):
+                self.refine_cls(train_data_cfg)
+                cfg.model.head.num_classes = len(self.dst_classes)
+
+        self.model_tasks = model_tasks
+        self.model_classes = self.dst_classes
+        self.configure_pseudo_label(cfg, **kwargs)
+
+    def configure_task_adapt(self, cfg):
+        if not cfg.model.get("multilabel", False) and not cfg.model.get("hierarchical", False):
+            efficient_mode = cfg["task_adapt"].get("efficient_mode", True)
+            sampler_type = "balanced"
+            self.configure_loss(cfg)
+        else:
+            efficient_mode = cfg["task_adapt"].get("efficient_mode", False)
+            sampler_type = "cls_incr"
+
+        if len(set(self.model_classes) & set(self.dst_classes)) == 0 or set(self.model_classes) == set(self.dst_classes):
+            sampler_flag = False
+        else:
+            sampler_flag = True
+
+        # Update Task Adapt Hook
+        task_adapt_hook = ConfigDict(
+            type="TaskAdaptHook",
+            src_classes=self.old_classes,
+            dst_classes=self.dst_classes,
+            model_type=cfg.model.type,
+            sampler_flag=sampler_flag,
+            sampler_type=sampler_type,
+            efficient_mode=efficient_mode,
+        )
+        update_or_add_custom_hook(cfg, task_adapt_hook)
+
+    def configure_loss(self, cfg):
+        if len(set(self.model_classes) & set(self.dst_classes)) == 0 or set(self.model_classes) == set(self.dst_classes):
+            cfg.model.head.loss = dict(type="CrossEntropyLoss", loss_weight=1.0)
+        else:
+            cfg.model.head.loss = ConfigDict(
+                type="IBLoss",
+                num_classes=cfg.model.head.num_classes,
+            )
+            ib_loss_hook = ConfigDict(
+                type="IBLossHook",
+                dst_classes=self.dst_classes,
+            )
+            update_or_add_custom_hook(cfg, ib_loss_hook)
+
+    def configure_pseudo_label(self, cfg, **kwargs):
+        # Pseudo label augmentation
+        train_data_cfg = Stage.get_data_cfg(cfg, "train")
+        pre_stage_res = kwargs.get("pre_stage_res", None)
+        if pre_stage_res:
+            logger.info(f"pre-stage dataset: {pre_stage_res}")
+            if train_data_cfg.type not in PSEUDO_LABEL_ENABLE_DATASET:
+                raise NotImplementedError(f"Pseudo label loading for {train_data_cfg.type} is not yet supported!")
+            train_data_cfg.pre_stage_res = pre_stage_res
+            if train_data_cfg.get("tasks"):
+                train_data_cfg.model_tasks = self.model_tasks
+                cfg.model.head.old_tasks = self.old_tasks
+            elif train_data_cfg.get("CLASSES"):
+                train_data_cfg.dst_classes = self.dst_classes
+                cfg.data.val.dst_classes = self.dst_classes
+                cfg.data.test.dst_classes = self.dst_classes
+                cfg.model.head.num_classes = len(self.dst_classes)
+                cfg.model.head.num_old_classes = len(self.old_classes)
+
+    def refine_tasks(self, train_cfg):
+        new_tasks = train_cfg["tasks"]
+        if self.adapt_type == "REPLACE":
+            old_tasks = {}
+            model_tasks = new_tasks
+        elif self.adapt_type == "MERGE":
+            old_tasks = self.model_meta["tasks"]
+            model_tasks = copy.deepcopy(old_tasks)
+            for task, cls in new_tasks.items():
+                if model_tasks.get(task):
+                    model_tasks[task] = model_tasks[task] + [c for c in cls if c not in model_tasks[task]]
+                else:
+                    model_tasks.update({task: cls})
+        else:
+            raise KeyError(f"{self.adapt_type} is not supported for task_adapt options!")
+        self.model_tasks = model_tasks
+        self.old_tasks = old_tasks
+
+    def refine_cls(self, train_cfg):
+        # Get 'new_classes' in data.train_cfg & get 'old_classes' pretreained model meta data CLASSES
+        new_classes = train_cfg["new_classes"]
+        self.old_classes = self.model_meta["CLASSES"]
+        if self.adapt_type == "REPLACE":
+            # if 'REPLACE' operation, then self.dst_classes -> data_classes
+            self.dst_classes = self.data_classes.copy()
+        elif self.adapt_type == "MERGE":
+            # if 'MERGE' operation, then self.dst_classes -> old_classes + new_classes (merge)
+            self.dst_classes = self.old_classes + [cls for cls in new_classes if cls not in self.old_classes]
+        else:
+            raise KeyError(f"{self.adapt_type} is not supported for task_adapt options!")
--- otx/mpa/cls/stage.py
+++ otx/mpa/cls/stage.py
@@ -16,17 +16,6 @@ from otx.mpa.utils.logger import get_logger
 
 logger = get_logger()
 
-CLASS_INC_DATASET = [
-    "MPAClsDataset",
-    "MPAMultilabelClsDataset",
-    "MPAHierarchicalClsDataset",
-    "ClsDirDataset",
-    "ClsTVDataset",
-]
-PSEUDO_LABEL_ENABLE_DATASET = ["ClassIncDataset", "LwfTaskIncDataset", "ClsTVDataset"]
-WEIGHT_MIX_CLASSIFIER = ["SAMImageClassifier"]
-
-
 class ClsStage(Stage):
     def configure(self, model_cfg, model_ckpt, data_cfg, training=True, **kwargs):
         """Create MMCV-consumable config from given inputs"""
@@ -40,24 +29,50 @@ class ClsStage(Stage):
         self.configure_task(cfg, training, **kwargs)
         return cfg
 
-    @staticmethod
-    def sub_configure_model(cfg, training, **kwargs):
-        # verify and update model configurations
-        # check whether in/out of the model layers require updating
+    def configure_model(self, cfg, model_cfg, training, **kwargs):
+        if model_cfg:
+            if hasattr(cfg, "model"):
+                cfg.merge_from_dict(model_cfg._cfg_dict)
+            else:
+                cfg.model = copy.deepcopy(model_cfg.model)
 
-        update_required = False
+        cfg.model_task = cfg.model.pop("task", "classification")
+        if cfg.model_task != "classification":
+            raise ValueError(f"Given model_cfg ({model_cfg.filename}) is not supported by classification recipe")
+
+        # OV-plugin
+        ir_model_path = kwargs.get("ir_model_path")
+        if ir_model_path:
+
+            def is_mmov_model(k, v):
+                if k == "type" and v.startswith("MMOV"):
+                    return True
+                return False
+
+            ir_weight_path = kwargs.get("ir_weight_path", None)
+            ir_weight_init = kwargs.get("ir_weight_init", False)
+            recursively_update_cfg(
+                cfg,
+                is_mmov_model,
+                {"model_path": ir_model_path, "weight_path": ir_weight_path, "init_weight": ir_weight_init},
+            )
+        self.configure_in_channel(cfg)
+        self.configure_topk(cfg)
+
+    @staticmethod
+    def configure_in_channel(cfg):
+        configure_required = False
         if cfg.model.get("neck") is not None:
             if cfg.model.neck.get("in_channels") is not None and cfg.model.neck.in_channels <= 0:
-                update_required = True
-        if not update_required and cfg.model.get("head") is not None:
+                configure_required = True
+        if not configure_required and cfg.model.get("head") is not None:
             if cfg.model.head.get("in_channels") is not None and cfg.model.head.in_channels <= 0:
-                update_required = True
-        if not update_required:
+                configure_required = True
+        if not configure_required:
             return
 
         # update model layer's in/out configuration
         from mmcv.cnn import MODELS as backbone_reg
-
         layer = build_from_cfg(cfg.model.backbone, backbone_reg)
         layer.eval()
         input_shape = [3, 224, 224]
@@ -96,220 +111,12 @@ class ClsStage(Stage):
                 )
                 cfg.model.head.in_channels = in_channels
 
-            # checking task incremental model configurations
-
-    # noqa: C901
     @staticmethod
-    def sub_configure_task(cfg, training, model_meta=None, **kwargs):
-        """Configure for Task Adaptation Task"""
-
-        def refine_tasks(train_cfg, meta, adapt_type):
-            new_tasks = train_cfg["tasks"]
-            if adapt_type == "REPLACE":
-                old_tasks = {}
-                model_tasks = new_tasks
-            elif adapt_type == "MERGE":
-                old_tasks = meta["tasks"]
-                model_tasks = copy.deepcopy(old_tasks)
-                for task, cls in new_tasks.items():
-                    if model_tasks.get(task):
-                        model_tasks[task] = model_tasks[task] + [c for c in cls if c not in model_tasks[task]]
-                    else:
-                        model_tasks.update({task: cls})
-            else:
-                raise KeyError(f"{adapt_type} is not supported for task_adapt options!")
-            return model_tasks, old_tasks
-
-        def refine_cls(train_cfg, data_classes, meta, adapt_type):
-            # Get 'new_classes' in data.train_cfg & get 'old_classes' pretreained model meta data CLASSES
-            new_classes = train_cfg["new_classes"]
-            old_classes = meta["CLASSES"]
-            if adapt_type == "REPLACE":
-                # if 'REPLACE' operation, then dst_classes -> data_classes
-                dst_classes = data_classes.copy()
-            elif adapt_type == "MERGE":
-                # if 'MERGE' operation, then dst_classes -> old_classes + new_classes (merge)
-                dst_classes = old_classes + [cls for cls in new_classes if cls not in old_classes]
-            else:
-                raise KeyError(f"{adapt_type} is not supported for task_adapt options!")
-            return dst_classes, old_classes
-
-        task_adapt_type = cfg["task_adapt"].get("type", None)
-        adapt_type = cfg["task_adapt"].get("op", "REPLACE")
-
-        model_tasks, dst_classes = None, None
-        model_classes, data_classes = [], []
-        train_data_cfg = Stage.get_data_cfg(cfg, "train")
-        if isinstance(train_data_cfg, list):
-            train_data_cfg = train_data_cfg[0]
-
-        model_classes = Stage.get_model_classes(cfg)
-        data_classes = Stage.get_data_classes(cfg)
-        if model_classes:
-            cfg.model.head.num_classes = len(model_classes)
-        elif data_classes:
-            cfg.model.head.num_classes = len(data_classes)
-        model_meta["CLASSES"] = model_classes
-
-        if not train_data_cfg.get("new_classes", False):  # when train_data_cfg doesn't have 'new_classes' key
-            new_classes = np.setdiff1d(data_classes, model_classes).tolist()
-            train_data_cfg["new_classes"] = new_classes
-
-        if training:
-            # if Trainer to Stage configure, training = True
-            if train_data_cfg.get("tasks"):
-                # Task Adaptation
-                if model_meta.get("tasks", False):
-                    model_tasks, old_tasks = refine_tasks(train_data_cfg, model_meta, adapt_type)
-                else:
-                    raise KeyError(f"can not find task meta data from {cfg.load_from}.")
-                cfg.model.head.update({"old_tasks": old_tasks})
-                # update model.head.tasks with training dataset's tasks if it's configured as None
-                if cfg.model.head.get("tasks") is None:
-                    logger.info(
-                        "'tasks' in model.head is None. updated with configuration on train data "
-                        f"{train_data_cfg.get('tasks')}"
-                    )
-                    cfg.model.head.update({"tasks": train_data_cfg.get("tasks")})
-            elif "new_classes" in train_data_cfg:
-                # Class-Incremental
-                dst_classes, old_classes = refine_cls(train_data_cfg, data_classes, model_meta, adapt_type)
-            else:
-                raise KeyError('"new_classes" or "tasks" should be defined for incremental learning w/ current model.')
-
-            if task_adapt_type == "mpa":
-                if train_data_cfg.type not in CLASS_INC_DATASET:  # task incremental is not supported yet
-                    raise NotImplementedError(
-                        f"Class Incremental Learning for {train_data_cfg.type} is not yet supported!"
-                    )
-
-                if cfg.model.type in WEIGHT_MIX_CLASSIFIER:
-                    cfg.model.task_adapt = ConfigDict(
-                        src_classes=model_classes,
-                        dst_classes=data_classes,
-                    )
-
-                # Train dataset config update
-                train_data_cfg.classes = dst_classes
-
-                # model configuration update
-                cfg.model.head.num_classes = len(dst_classes)
-
-                if not cfg.model.get("multilabel", False) and not cfg.model.get("hierarchical", False):
-                    efficient_mode = cfg["task_adapt"].get("efficient_mode", True)
-                    sampler_type = "balanced"
-
-                    if len(set(model_classes) & set(dst_classes)) == 0 or set(model_classes) == set(dst_classes):
-                        cfg.model.head.loss = dict(type="CrossEntropyLoss", loss_weight=1.0)
-                    else:
-                        cfg.model.head.loss = ConfigDict(
-                            type="IBLoss",
-                            num_classes=cfg.model.head.num_classes,
-                        )
-                        ib_loss_hook = ConfigDict(
-                            type="IBLossHook",
-                            dst_classes=dst_classes,
-                        )
-                        update_or_add_custom_hook(cfg, ib_loss_hook)
-                else:
-                    efficient_mode = cfg["task_adapt"].get("efficient_mode", False)
-                    sampler_type = "cls_incr"
-
-                if len(set(model_classes) & set(dst_classes)) == 0 or set(model_classes) == set(dst_classes):
-                    sampler_flag = False
-                else:
-                    sampler_flag = True
-
-                # Update Task Adapt Hook
-                task_adapt_hook = ConfigDict(
-                    type="TaskAdaptHook",
-                    src_classes=old_classes,
-                    dst_classes=dst_classes,
-                    model_type=cfg.model.type,
-                    sampler_flag=sampler_flag,
-                    sampler_type=sampler_type,
-                    efficient_mode=efficient_mode,
-                )
-                update_or_add_custom_hook(cfg, task_adapt_hook)
-
-        else:  # if not training phase (eval)
-            if train_data_cfg.get("tasks"):
-                if model_meta.get("tasks", False):
-                    cfg.model.head["tasks"] = model_meta["tasks"]
-                else:
-                    raise KeyError(f"can not find task meta data from {cfg.load_from}.")
-            elif train_data_cfg.get("new_classes"):
-                dst_classes, _ = refine_cls(train_data_cfg, data_classes, model_meta, adapt_type)
-                cfg.model.head.num_classes = len(dst_classes)
-
-        # Pseudo label augmentation
-        pre_stage_res = kwargs.get("pre_stage_res", None)
-        if pre_stage_res:
-            logger.info(f"pre-stage dataset: {pre_stage_res}")
-            if train_data_cfg.type not in PSEUDO_LABEL_ENABLE_DATASET:
-                raise NotImplementedError(f"Pseudo label loading for {train_data_cfg.type} is not yet supported!")
-            train_data_cfg.pre_stage_res = pre_stage_res
-            if train_data_cfg.get("tasks"):
-                train_data_cfg.model_tasks = model_tasks
-                cfg.model.head.old_tasks = old_tasks
-            elif train_data_cfg.get("CLASSES"):
-                train_data_cfg.dst_classes = dst_classes
-                cfg.data.val.dst_classes = dst_classes
-                cfg.data.test.dst_classes = dst_classes
-                cfg.model.head.num_classes = len(train_data_cfg.dst_classes)
-                cfg.model.head.num_old_classes = len(old_classes)
-        return model_tasks, dst_classes
-
-    def _put_model_on_gpu(self, model, cfg):
-        if torch.cuda.is_available():
-            model = model.cuda()
-            if self.distributed:
-                # put model on gpus
-                find_unused_parameters = cfg.get("find_unused_parameters", False)
-                # Sets the `find_unused_parameters` parameter in
-                # torch.nn.parallel.DistributedDataParallel
-                model = MMDistributedDataParallel(
-                    model,
-                    device_ids=[torch.cuda.current_device()],
-                    broadcast_buffers=False,
-                    find_unused_parameters=find_unused_parameters,
-                )
-            else:
-                model = MMDataParallel(model.cuda(), device_ids=[0])
-        else:
-            model = MMDataCPU(model)
-
-        return model
-
-    def configure_model(self, cfg, model_cfg, training, **kwargs):
-        if model_cfg:
-            if hasattr(cfg, "model"):
-                cfg.merge_from_dict(model_cfg._cfg_dict)
-            else:
-                cfg.model = copy.deepcopy(model_cfg.model)
-
-        cfg.model_task = cfg.model.pop("task", "classification")
-        if cfg.model_task != "classification":
-            raise ValueError(f"Given model_cfg ({model_cfg.filename}) is not supported by classification recipe")
-
-        # OV-plugin
-        ir_model_path = kwargs.get("ir_model_path")
-        if ir_model_path:
-
-            def is_mmov_model(k, v):
-                if k == "type" and v.startswith("MMOV"):
-                    return True
-                return False
-
-            ir_weight_path = kwargs.get("ir_weight_path", None)
-            ir_weight_init = kwargs.get("ir_weight_init", False)
-            recursively_update_cfg(
-                cfg,
-                is_mmov_model,
-                {"model_path": ir_model_path, "weight_path": ir_weight_path, "init_weight": ir_weight_init},
-            )
-
-        self.sub_configure_model(cfg, training, **kwargs)
+    def configure_topk(cfg):
+        if cfg.model.head.get("topk", False) and isinstance(cfg.model.head.topk, tuple):
+            cfg.model.head.topk = (1,) if cfg.model.head.num_classes < 5 else (1, 5)
+            if cfg.model.get("multilabel", False) or cfg.model.get("hierarchical", False):
+                cfg.model.head.pop("topk", None)
 
     def configure_ckpt(self, cfg, model_ckpt, pretrained):
         # Checkpoint
@@ -330,20 +137,49 @@ class ClsStage(Stage):
         Stage.configure_data(cfg, training, **kwargs)
 
     def configure_task(self, cfg, training, **kwargs):
-        # Task
-        if "task_adapt" in cfg:
-            model_meta = self.get_model_meta(cfg)
-            model_tasks, dst_classes = self.sub_configure_task(cfg, training, model_meta, **kwargs)
-            if model_tasks is not None:
-                self.model_tasks = model_tasks
-            if dst_classes is not None:
-                self.model_classes = dst_classes
-        else:
-            if "num_classes" not in cfg.data:
-                cfg.data.num_classes = len(cfg.data.train.get("classes", []))
-            cfg.model.head.num_classes = cfg.data.num_classes
+        self.configure_classes(cfg)
 
-        if cfg.model.head.get("topk", False) and isinstance(cfg.model.head.topk, tuple):
-            cfg.model.head.topk = (1,) if cfg.model.head.num_classes < 5 else (1, 5)
-            if cfg.model.get("multilabel", False) or cfg.model.get("hierarchical", False):
-                cfg.model.head.pop("topk", None)
+    def configure_classes(self, cfg):
+        model_classes, data_classes = [], []
+        self.model_meta = self.get_model_meta(cfg)
+        train_data_cfg = Stage.get_data_cfg(cfg, "train")
+        if isinstance(train_data_cfg, list):
+            train_data_cfg = train_data_cfg[0]
+
+        if "num_classes" not in cfg.data:
+            cfg.data.num_classes = len(cfg.data.train.get("classes", []))
+        cfg.model.head.num_classes = cfg.data.num_classes
+
+        model_classes = Stage.get_model_classes(cfg)
+        data_classes = Stage.get_data_classes(cfg)
+        if model_classes:
+            cfg.model.head.num_classes = len(model_classes)
+        elif data_classes:
+            cfg.model.head.num_classes = len(data_classes)
+        self.model_meta["CLASSES"] = model_classes
+
+        if not train_data_cfg.get("new_classes", False):  # when train_data_cfg doesn't have 'new_classes' key
+            new_classes = np.setdiff1d(data_classes, model_classes).tolist()
+            train_data_cfg["new_classes"] = new_classes
+        self.model_classes = model_classes
+        self.data_classes = data_classes
+
+    def _put_model_on_gpu(self, model, cfg):
+        if torch.cuda.is_available():
+            model = model.cuda()
+            if self.distributed:
+                # put model on gpus
+                find_unused_parameters = cfg.get("find_unused_parameters", False)
+                # Sets the `find_unused_parameters` parameter in
+                # torch.nn.parallel.DistributedDataParallel
+                model = MMDistributedDataParallel(
+                    model,
+                    device_ids=[torch.cuda.current_device()],
+                    broadcast_buffers=False,
+                    find_unused_parameters=find_unused_parameters,
+                )
+            else:
+                model = MMDataParallel(model.cuda(), device_ids=[0])
+        else:
+            model = MMDataCPU(model)
+        return model
