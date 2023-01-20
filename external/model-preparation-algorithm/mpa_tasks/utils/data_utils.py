2e415be77e6573fc1768b19cc392ae240bd53a70
Jihwan Eom <jihwan.eom@intel.com>
2023-01-20 13:11:26 +0900

    [DEV] Remove filelock in videoframe caching (#1555)
    * Remove filelock, use temp filename in videoframe caching

--- external/model-preparation-algorithm/mpa_tasks/utils/data_utils.py
+++ external/model-preparation-algorithm/mpa_tasks/utils/data_utils.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-import fcntl
 import os
 import cv2
 from typing import Any, Dict
@@ -44,40 +43,40 @@ def get_image(results: Dict[str, Any], cache_dir: str, to_float32=False):
         return subset.name in ["TRAINING", "VALIDATION"]
 
     def load_image_from_cache(filename: str, to_float32=False):
-        with open(filename, "rb") as f:
-            fcntl.flock(f, fcntl.LOCK_SH)
-            try:
-                cached_img = cv2.imread(filename)
-                if to_float32:
-                    cached_img = cached_img.astype(np.float32)
-                return cached_img
-            except Exception as e:
-                logger.warning(f"Skip loading cached {filename} \nError msg: {e}")
-            finally:
-                fcntl.flock(f, fcntl.LOCK_UN)
+        try:
+            cached_img = cv2.imread(filename)
+            if to_float32:
+                cached_img = cached_img.astype(np.float32)
+            return cached_img
+        except Exception as e:
+            logger.warning(f"Skip loading cached {filename} \nError msg: {e}")
+            return None
     
     def save_image_to_cache(img: np.array, filename: str):
-        with open(filename, "wb") as f:
-            fcntl.flock(f, fcntl.LOCK_EX)
-            try:
-                cv2.imwrite(filename, img=img)
-            except Exception as e:
-                logger.warning(f"Skip caching for {filename} \nError msg: {e}")
-            finally:
-                fcntl.flock(f, fcntl.LOCK_UN)
+        tmp_filename = filename.replace(".png", "-tmp.png")
+        if os.path.exists(filename) or os.path.exists(tmp_filename):  # if image is cached or caching
+            return None
+        try:
+            cv2.imwrite(tmp_filename, img=img)
+            os.replace(tmp_filename, filename)
+        except Exception as e:
+            logger.warning(f"Skip caching for {filename} \nError msg: {e}")
+            return None
 
     subset = results["dataset_item"].subset
     if is_training_subset(subset) and is_video_frame(results["dataset_item"].media):
         index = results["index"]
         filename = os.path.join(cache_dir, f"{subset}-{index:06d}.png")
         if os.path.exists(filename):
-            return load_image_from_cache(filename, to_float32=to_float32)
+            loaded_img = load_image_from_cache(filename, to_float32=to_float32)
+            if loaded_img is not None:
+                return loaded_img
 
     img = results["dataset_item"].numpy  # this takes long for VideoFrame
     if to_float32:
         img = img.astype(np.float32)
 
-    if is_training_subset(subset) and is_video_frame(results["dataset_item"].media) and not os.path.exists(filename):
+    if is_training_subset(subset) and is_video_frame(results["dataset_item"].media):
         save_image_to_cache(img, filename)
 
     return img
