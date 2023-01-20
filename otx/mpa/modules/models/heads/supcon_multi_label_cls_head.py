6494bf5872028eee1a5bf15b2408e672a3ee6c73
Adria Arrufat <adria.arrufat@intel.com>
2023-01-20 15:36:27 +0900

    Fix some inconsistencies in the multilabel classifier

--- otx/mpa/modules/models/heads/supcon_multi_label_cls_head.py
+++ otx/mpa/modules/models/heads/supcon_multi_label_cls_head.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
-import torch.nn.functional as F
 from mmcls.models.builder import HEADS, build_loss
 from mmcls.models.heads.base_head import BaseHead
 from torch import nn
@@ -27,7 +26,7 @@ class SupConMultiLabelClsHead(BaseHead):
         num_classes: int,
         in_channels: int,
         aux_mlp,
-        loss=dict(type="CrossEntropyLoss", use_sigmoid=True, loss_weight=1.0),
+        loss=dict(type="CrossEntropyLoss", use_sigmoid=True, reduction="mean", loss_weight=1.0),
         aux_loss=dict(type="BarlowTwinsLoss", off_diag_penality=1.0 / 128.0, loss_weight=1.0),
         scale: float = 1.0,
         **kwargs,
@@ -65,52 +64,46 @@ class SupConMultiLabelClsHead(BaseHead):
     def loss(self, cls_score, gt_label, valid_label_mask=None):
         gt_label = gt_label.type_as(cls_score)
         num_samples = len(cls_score)
+        losses = dict()
 
         # map difficult examples to positive ones
         _gt_label = torch.abs(gt_label)
         # compute loss
         loss = self.compute_loss(cls_score, _gt_label, valid_label_mask=valid_label_mask, avg_factor=num_samples)
-        return loss / self.scale
+        losses["loss"] = loss / self.scale
+        return losses
 
     def forward_train(self, x, gt_label, **kwargs):
         img_metas = kwargs.get("img_metas", False)
         gt_label = gt_label.type_as(x)
         cls_score = self.classifier(x) * self.scale
 
-        losses = dict(loss=0.0)
-
         bsz = gt_label.shape[0]
         # make sure we have two views for each label and split them
         assert x.shape[0] == 2 * bsz
-        feats1, feats2 = torch.split(self.aux_mlp(x), [bsz, bsz], dim=0)
         gt_label = torch.cat([gt_label, gt_label], dim=0)
 
         if img_metas:
-            num_samples = len(cls_score)
-            gt_label = gt_label.type_as(cls_score)
-            valid_label_mask = self.get_valid_label_mask(img_metas=img_metas)
+            valid_label_mask = self.get_valid_label_mask(img_metas=img_metas).to(x.device)
             valid_label_mask = torch.cat([valid_label_mask, valid_label_mask], dim=0)
-            loss = self.compute_loss(cls_score, gt_label, valid_label_mask=valid_label_mask, avg_factor=num_samples)
+            losses = self.loss(cls_score, gt_label, valid_label_mask=valid_label_mask)
         else:
-            loss = self.compute_loss(cls_score, gt_label)
+            losses = self.loss(cls_score, gt_label)
+        # return losses
 
+        feats1, feats2 = torch.split(self.aux_mlp(x), [bsz, bsz], dim=0)
         aux_loss = self.aux_loss(feats1, feats2)
-        losses["loss"] = loss + aux_loss
+        losses["loss"] += aux_loss
         return losses
 
     def simple_test(self, img):
-        """
-        Test without data augmentation.
-        """
-        cls_score = self.classifier(img)
-
+        """Test without data augmentation."""
+        cls_score = self.classifier(img) * self.scale
         if isinstance(cls_score, list):
             cls_score = sum(cls_score) / float(len(cls_score))
-
-        pred = F.sigmoid(cls_score) if cls_score is not None else None
-
         if torch.onnx.is_in_onnx_export():
-            return pred
+            return cls_score
+        pred = torch.sigmoid(cls_score) if cls_score is not None else None
         pred = list(pred.detach().cpu().numpy())
         return pred
 
@@ -120,7 +113,6 @@ class SupConMultiLabelClsHead(BaseHead):
             mask = torch.Tensor([1 for _ in range(self.num_classes)])
             if "ignored_labels" in meta and meta["ignored_labels"]:
                 mask[meta["ignored_labels"]] = 0
-            mask = mask.cuda() if torch.cuda.is_available() else mask
             valid_label_mask.append(mask)
         valid_label_mask = torch.stack(valid_label_mask, dim=0)
         return valid_label_mask
