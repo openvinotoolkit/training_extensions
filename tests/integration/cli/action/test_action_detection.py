d065ea997073222005cd4078c77b2e817a9be109
jaegukhyun <jaeguk.hyun@intel.com>
2023-01-31 11:32:09 +0900

    Arrange action tasks tests

--- tests/integration/cli/action/test_action_detection.py
+++ tests/integration/cli/action/test_action_detection.py
@@ -14,8 +14,6 @@ from otx.cli.utils.tests import (
     otx_eval_testing,
     otx_export_testing,
     otx_train_testing,
-    pot_eval_testing,
-    pot_optimize_testing,
 )
 from tests.test_suite.e2e_test_system import e2e_pytest_component
 
@@ -65,15 +63,3 @@ class TestToolsOTXActionDetection:
     @pytest.mark.parametrize("template", templates, ids=templates_ids)
     def test_otx_eval_openvino(self, template, tmp_dir_path):
         otx_eval_openvino_testing(template, tmp_dir_path, otx_dir, args, threshold=1.0)
-
-    @e2e_pytest_component
-    @pytest.mark.skipif(TT_STABILITY_TESTS, reason="This is TT_STABILITY_TESTS")
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_pot_optimize(self, template, tmp_dir_path):
-        pot_optimize_testing(template, tmp_dir_path, otx_dir, args)
-
-    @e2e_pytest_component
-    @pytest.mark.skipif(TT_STABILITY_TESTS, reason="This is TT_STABILITY_TESTS")
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_pot_eval(self, template, tmp_dir_path):
-        pot_eval_testing(template, tmp_dir_path, otx_dir, args)
