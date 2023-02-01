e39d4db213e3c5d96f05d2c8116e9563a1f83651
eunwoosh <eunwoo.shin@intel.com>
2023-02-01 10:21:31 +0900

    update HPO integration test

--- otx/cli/utils/tests.py
+++ otx/cli/utils/tests.py
@@ -18,6 +18,7 @@ import os
 import shutil
 import sys
 from typing import Dict
+from pathlib import Path
 
 import pytest
 import yaml
@@ -184,9 +185,13 @@ def otx_hpo_testing(template, root, otx_dir, args):
 
     command_line.extend(args["train_params"])
     check_run(command_line)
-    assert os.path.exists(f"{template_work_dir}/hpo/hpopt_status.json")
-    with open(f"{template_work_dir}/hpo/hpopt_status.json", "r") as f:
-        assert json.load(f).get("best_config_id", None) is not None
+    trials_json = list(Path(f"{template_work_dir}/hpo/").rglob("*.json"))
+    assert trials_json
+    for trial_json in trials_json:
+        with trial_json.open("r") as f:
+            trial_result = json.load(f)
+        assert trial_result.get("score")
+        
     assert os.path.exists(f"{template_work_dir}/hpo_trained_{template.model_template_id}/weights.pth")
     assert os.path.exists(f"{template_work_dir}/hpo_trained_{template.model_template_id}/label_schema.json")
 
