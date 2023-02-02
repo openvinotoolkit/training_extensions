73097757c2193211d6eab172751728383e2b9028
Lee, Soobee <soobeele@intel.com>
2023-02-03 05:49:34 +0900

    test revised

--- tests/integration/cli/segmentation/test_segmentation.py
+++ tests/integration/cli/segmentation/test_segmentation.py
@@ -49,7 +49,7 @@ args_semisl = {
     "train_params": [
         "params",
         "--learning_parameters.num_iters",
-        "1",
+        "2",
         "--learning_parameters.batch_size",
         "4",
         "--algo_backend.train_type",
@@ -63,7 +63,7 @@ args_selfsl = {
     "train_params": [
         "params",
         "--learning_parameters.num_iters",
-        "1",
+        "2",
         "--learning_parameters.batch_size",
         "4",
         "--algo_backend.train_type",
@@ -91,74 +91,17 @@ templates_ids = [default_template.model_template_id]
 
 
 class TestSegmentationCLI:
+    
     @e2e_pytest_component
     @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_train(self, template, tmp_dir_path):
-        otx_train_testing(template, tmp_dir_path, otx_dir, args)
-
-    @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_resume(self, template, tmp_dir_path):
-        otx_resume_testing(template, tmp_dir_path, otx_dir, args)
-        template_work_dir = get_template_dir(template, tmp_dir_path)
-        args1 = copy.deepcopy(args)
-        args1["train_params"] = resume_params
-        args1["--resume-from"] = f"{template_work_dir}/trained_for_resume_{template.model_template_id}/weights.pth"
-        otx_resume_testing(template, tmp_dir_path, otx_dir, args1)
-
-    @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_export(self, template, tmp_dir_path):
-        otx_export_testing(template, tmp_dir_path)
-
-    @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_eval(self, template, tmp_dir_path):
-        otx_eval_testing(template, tmp_dir_path, otx_dir, args)
-
-    @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_eval_openvino(self, template, tmp_dir_path):
-        otx_eval_openvino_testing(template, tmp_dir_path, otx_dir, args, threshold=1.0)
-
-    @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_deploy_openvino(self, template, tmp_dir_path):
-        otx_deploy_openvino_testing(template, tmp_dir_path, otx_dir, args)
-
-    @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_eval_deployment(self, template, tmp_dir_path):
-        otx_eval_deployment_testing(template, tmp_dir_path, otx_dir, args, threshold=1.0)
-
-    @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_hpo(self, template, tmp_dir_path):
-        otx_hpo_testing(template, tmp_dir_path, otx_dir, args)
-
-    @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_nncf_optimize(self, template, tmp_dir_path):
-        if template.entrypoints.nncf is None:
-            pytest.skip("nncf entrypoint is none")
-
-        nncf_optimize_testing(template, tmp_dir_path, otx_dir, args)
-
-    @e2e_pytest_component
-    @pytest.mark.skip(reason="CVS-101246 Multi-GPU tests are stuck while CI is running")
-    @pytest.mark.skipif(MULTI_GPU_UNAVAILABLE, reason="The number of gpu is insufficient")
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_multi_gpu_train(self, template, tmp_dir_path):
-        args1 = copy.deepcopy(args)
+    def test_otx_multi_gpu_train_semi(self, template, tmp_dir_path):
+        args1 = copy.deepcopy(args_semisl)
         args1["--gpus"] = "0,1"
         otx_train_testing(template, tmp_dir_path, otx_dir, args1)
 
     @e2e_pytest_component
     @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_train_semisl(self, template, tmp_dir_path):
-        otx_train_testing(template, tmp_dir_path, otx_dir, args_semisl)
-
-    @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
-    def test_otx_train_selfsl(self, template, tmp_dir_path):
-        otx_train_testing(template, tmp_dir_path, otx_dir, args_selfsl)
+    def test_otx_multi_gpu_train_self(self, template, tmp_dir_path):
+        args1 = copy.deepcopy(args_selfsl)
+        args1["--gpus"] = "0,1"
+        otx_train_testing(template, tmp_dir_path, otx_dir, args1)
