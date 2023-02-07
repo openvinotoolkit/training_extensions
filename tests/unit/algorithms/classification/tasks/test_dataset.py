4b8f8cc2d93060195ee9598451b8537d09ef7589
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-02-08 02:59:30 +0900

    Update formatting in tests

--- tests/unit/algorithms/classification/tasks/test_dataset.py
+++ tests/unit/algorithms/classification/tasks/test_dataset.py
@@ -2,21 +2,28 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-import pytest
 import numpy as np
+import pytest
 
-from tests.test_suite.e2e_test_system import e2e_pytest_unit
-from tests.integration.api.classification.test_api_classification import ClassificationTaskAPIBase, DEFAULT_CLS_TEMPLATE_DIR
-
+from otx.algorithms.classification.adapters.mmcls.data import (
+    MPAClsDataset,
+    MPAHierarchicalClsDataset,
+    MPAMultilabelClsDataset,
+)
 from otx.algorithms.classification.utils import get_multihead_class_info
-from otx.algorithms.classification.adapters.mmcls.data import MPAClsDataset, MPAMultilabelClsDataset, MPAHierarchicalClsDataset
+from tests.integration.api.classification.test_api_classification import (
+    DEFAULT_CLS_TEMPLATE_DIR,
+    ClassificationTaskAPIBase,
+)
+from tests.test_suite.e2e_test_system import e2e_pytest_unit
 
 
 class TestOTXClsDataset:
     @pytest.fixture(autouse=True)
     def setup(self) -> None:
-        self.hyper_parameters, self.model_template = \
-            ClassificationTaskAPIBase.setup_configurable_parameters(DEFAULT_CLS_TEMPLATE_DIR)
+        self.hyper_parameters, self.model_template = ClassificationTaskAPIBase.setup_configurable_parameters(
+            DEFAULT_CLS_TEMPLATE_DIR
+        )
         self.dataset_len = 20
 
     @pytest.mark.parametrize(
@@ -51,9 +58,9 @@ class TestOTXClsDataset:
 
         assert len(metrics) > 0
         if multilabel:
-            metrics['mAP'] > 0
+            metrics["mAP"] > 0
         else:
-            metrics['accuracy'] > 0
+            metrics["accuracy"] > 0
 
     @e2e_pytest_unit
     def test_create_hierarchical_adapter(self):
@@ -61,9 +68,9 @@ class TestOTXClsDataset:
             self.hyper_parameters, self.model_template, False, True, self.dataset_len
         )
         class_info = get_multihead_class_info(self.task_environment.label_schema)
-        dataset = MPAHierarchicalClsDataset(otx_dataset=self.dataset,
-                                            labels=self.dataset.get_labels(),
-                                            hierarchical_info=class_info)
+        dataset = MPAHierarchicalClsDataset(
+            otx_dataset=self.dataset, labels=self.dataset.get_labels(), hierarchical_info=class_info
+        )
         assert dataset.num_classes == len(self.dataset.get_labels())
         assert len(dataset) == self.dataset_len * dataset.num_classes
         for d in dataset:
@@ -75,13 +82,12 @@ class TestOTXClsDataset:
             self.hyper_parameters, self.model_template, False, True, self.dataset_len
         )
         class_info = get_multihead_class_info(self.task_environment.label_schema)
-        dataset = MPAHierarchicalClsDataset(otx_dataset=self.dataset,
-                                            labels=self.dataset.get_labels(),
-                                            hierarchical_info=class_info)
+        dataset = MPAHierarchicalClsDataset(
+            otx_dataset=self.dataset, labels=self.dataset.get_labels(), hierarchical_info=class_info
+        )
 
         results = np.zeros((len(dataset), dataset.num_classes))
         metrics = dataset.evaluate(results)
 
         assert len(metrics) > 0
-        assert metrics['accuracy'] > 0
-
+        assert metrics["accuracy"] > 0
