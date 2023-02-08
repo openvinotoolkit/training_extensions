8ad4a0160745d74ce3a3aced3564593b42c1ec41
Lee, Soobee <soobeele@intel.com>
2023-02-08 18:58:56 +0900

    remove files in models

--- otx/mpa/cls/__init__.py
+++ otx/mpa/cls/__init__.py
@@ -6,15 +6,12 @@ import otx.mpa.modules.datasets.cls_dir_dataset
 import otx.mpa.modules.datasets.pipelines.transforms.augmix
 import otx.mpa.modules.datasets.pipelines.transforms.ote_transforms
 import otx.mpa.modules.datasets.pipelines.transforms.random_augment
-import otx.mpa.modules.datasets.pipelines.transforms.random_ratio_crop
 import otx.mpa.modules.datasets.pipelines.transforms.twocrop_transform
 import otx.mpa.modules.hooks
 import otx.mpa.modules.models.backbones.efficientnet
 import otx.mpa.modules.models.backbones.efficientnetv2
 import otx.mpa.modules.models.backbones.mobilenetv3
-import otx.mpa.modules.models.backbones.wideresnet
 import otx.mpa.modules.models.classifiers
-import otx.mpa.modules.models.heads.cls_incremental_head
 import otx.mpa.modules.models.heads.custom_cls_head
 import otx.mpa.modules.models.heads.custom_hierarchical_linear_cls_head
 import otx.mpa.modules.models.heads.custom_hierarchical_non_linear_cls_head
@@ -28,11 +25,8 @@ import otx.mpa.modules.models.heads.task_incremental_classifier_head
 import otx.mpa.modules.models.losses.asymmetric_angular_loss_with_ignore
 import otx.mpa.modules.models.losses.asymmetric_loss_with_ignore
 import otx.mpa.modules.models.losses.barlowtwins_loss
-import otx.mpa.modules.models.losses.class_balanced_losses
 import otx.mpa.modules.models.losses.cross_entropy_loss
 import otx.mpa.modules.models.losses.ib_loss
-import otx.mpa.modules.models.losses.mse_loss
-import otx.mpa.modules.models.losses.triplet_loss
 import otx.mpa.modules.optimizer.lars
 
 # flake8: noqa
