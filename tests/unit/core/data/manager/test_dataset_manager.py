a227c8f65e3f55b384f9ab249a6a9373861b8430
Cho, Sungman <sungman.cho@intel.com>
2023-02-10 16:06:13 +0900

    Add unit-test for datasetmanager

--- tests/unit/core/data/manager/test_dataset_manager.py
+++ tests/unit/core/data/manager/test_dataset_manager.py
@@ -2,17 +2,32 @@
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
+from typing import List
 import pytest
+import datumaro as dm
+import shutil
 
 from otx.core.data.manager.dataset_manager import DatasetManager
-from tests.test_helpers import generate_datumaro_dataset
+from otx.cli.manager.config_manager import TASK_TYPE_TO_SUPPORTED_FORMAT
+from tests.unit.core.data.test_helpers import generate_datumaro_dataset_item, generate_datumaro_dataset
 from tests.test_suite.e2e_test_system import e2e_pytest_unit
 
 AVAILABLE_TASKS=["classification", "detection", "segmentation"]
 AVAILABLE_SUBSETS=["train", "val"]
+AVAILABLE_DATA_ROOTS=[
+    "tests/assets/imagenet_dataset",
+    "tests/assets/car_tree_bug",
+    "tests/assets/cityscapes_dataset/dataset",
+    "tests/assets/anomaly/shapes",
+    "tests/assets/cvat_dataset/action_classification/train",
+]
 
+DATA_ROOTS2FORMAT={
+    "tests/assets/imagenet_dataset": "imagenet",
+    "tests/assets/car_tree_bug": "coco",
+    "tests/assets/cityscapes_dataset/dataset": "cityscapes",
+}
 class TestOTXDatasetManager:
-    @pytest.fixture(autouse=True)
     def setup(self) -> None:
         self.dataset = {}
         for subset in AVAILABLE_SUBSETS:
@@ -23,9 +38,88 @@ class TestOTXDatasetManager:
                     task=task
                 )
     
-    @pytest.mark.parametrize("task", AVAILABLE_TASKS)
     @e2e_pytest_unit
-    def test_get_train_dataset(self):
-        train_dataset = DatasetManager.get_train_dataset(
-            self.dataset
+    @pytest.mark.parametrize("task", AVAILABLE_TASKS)
+    @pytest.mark.parametrize("subset", AVAILABLE_SUBSETS)
+    def test_get_train_dataset(self, task: List[str], subset: List[str]):
+        if subset == "val":
+            with pytest.raises(
+                ValueError, 
+                match=f"Can't find training data."
+            ):
+                DatasetManager.get_train_dataset(self.dataset[subset][task])
+        else:
+            train_dataset = DatasetManager.get_train_dataset(
+                self.dataset[subset][task]
+            )
+            assert isinstance(train_dataset, dm.DatasetSubset)
+    
+    @e2e_pytest_unit
+    @pytest.mark.parametrize("task", AVAILABLE_TASKS)
+    @pytest.mark.parametrize("subset", AVAILABLE_SUBSETS)
+    def test_get_val_dataset(self, task: List[str], subset: List[str]):
+        if subset == "train":
+            with pytest.raises(
+                ValueError, 
+                match=f"Can't find validation data."
+            ):
+                DatasetManager.get_val_dataset(self.dataset[subset][task])
+        else:
+            val_dataset = DatasetManager.get_val_dataset(
+                self.dataset[subset][task]
+            )
+            assert isinstance(val_dataset, dm.DatasetSubset)
+    
+    @e2e_pytest_unit
+    @pytest.mark.parametrize("data_root", AVAILABLE_DATA_ROOTS)
+    def test_get_data_format(self, data_root: str):
+        assert isinstance(DatasetManager.get_data_format(data_root), str)
+    
+    @e2e_pytest_unit
+    @pytest.mark.parametrize("task", AVAILABLE_TASKS)
+    @pytest.mark.parametrize("subset", AVAILABLE_SUBSETS)
+    def test_get_image_path(self, task, subset):
+        random_data = DatasetManager.get_image_path(
+            generate_datumaro_dataset_item(
+                item_id="0",
+                subset=subset,
+                task=task
+            )
         )
+        assert random_data is not None
+        
+    @e2e_pytest_unit
+    @pytest.mark.parametrize("task", AVAILABLE_TASKS)
+    @pytest.mark.parametrize("subset", AVAILABLE_SUBSETS)
+    def test_export_dataset(self, task, subset, tmp_dir_path):
+        data_format = TASK_TYPE_TO_SUPPORTED_FORMAT[task.upper()][0]
+        DatasetManager.export_dataset(
+            self.dataset[subset][task],
+            tmp_dir_path,
+            data_format,
+            save_media=False
+        )
+        shutil.rmtree(tmp_dir_path)
+            
+    @e2e_pytest_unit
+    @pytest.mark.parametrize("data_root", AVAILABLE_DATA_ROOTS[:3])
+    def test_import_dataset(self, data_root):
+        data_format = DATA_ROOTS2FORMAT[data_root]
+        assert DatasetManager.import_dataset(data_root, data_format=data_format) is not None
+    
+    @e2e_pytest_unit
+    @pytest.mark.parametrize("data_root", AVAILABLE_DATA_ROOTS[:3])
+    def test_import_dataset(self, data_root):
+        data_format = DATA_ROOTS2FORMAT[data_root]
+        assert DatasetManager.import_dataset(data_root, data_format=data_format) is not None
+    
+    @e2e_pytest_unit
+    @pytest.mark.parametrize("task", AVAILABLE_TASKS)
+    @pytest.mark.parametrize("subset", AVAILABLE_SUBSETS)
+    def test_auto_split(self, task, subset):
+        dataset = DatasetManager.auto_split(
+            task=task,
+            dataset=self.dataset[subset][task],
+            split_ratio=[("train", 0.8), ("val", 0.2)]
+        )
+        assert dataset is not None
