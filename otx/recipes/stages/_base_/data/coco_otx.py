2995b5c9e68b5b6259c4c4a78409745d9a8c847f
Soobee Lee <soobee.lee@intel.com>
2023-02-10 13:11:36 +0900

    Remove unused config files in otx/recipes (#1656)

--- otx/recipes/stages/_base_/data/coco_otx.py
+++ otx/recipes/stages/_base_/data/coco_otx.py
@@ -1,9 +1,10 @@
-_base_ = [
-    "./data.py",
-]
+_base_ = ["./data.py", "./pipelines/coco_otx_pipeline.py"]
 
 __dataset_type = "CocoDataset"
 __data_root = "data/coco/"
+
+__train_pipeline = {{_base_.train_pipeline}}
+__test_pipeline = {{_base_.test_pipeline}}
 
 __samples_per_gpu = 2
 
@@ -14,17 +15,20 @@ data = dict(
         type=__dataset_type,
         ann_file=__data_root + "annotations/instances_train2017.json",
         img_prefix=__data_root + "train2017/",
+        pipeline=__train_pipeline,
     ),
     val=dict(
         type=__dataset_type,
         ann_file=__data_root + "annotations/instances_val2017.json",
         img_prefix=__data_root + "val2017/",
         test_mode=True,
+        pipeline=__test_pipeline,
     ),
     test=dict(
         type=__dataset_type,
         ann_file=__data_root + "annotations/instances_val2017.json",
         img_prefix=__data_root + "val2017/",
         test_mode=True,
+        pipeline=__test_pipeline,
     ),
 )
