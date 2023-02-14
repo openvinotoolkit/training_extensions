90f8c99f017a10ed649969e9e44d3bef27f8641c
Kim, Vinnam <vinnam.kim@intel.com>
2023-02-14 17:59:25 +0900

    Fix DatasetItemEntity.roi getter and setter
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- otx/api/entities/dataset_item.py
+++ otx/api/entities/dataset_item.py
@@ -152,16 +152,13 @@ class DatasetItemEntity(metaclass=abc.ABCMeta):
     def roi(self) -> Annotation:
         """Region Of Interest."""
         with self.__roi_lock:
-            if self.__roi is None:
-                requested_roi = Annotation(Rectangle.generate_full_box(), labels=[])
-                self.__roi = requested_roi
-            else:
-                requested_roi = self.__roi
-            return requested_roi
+            return self.__roi
 
     @roi.setter
     def roi(self, roi: Optional[Annotation]):
         with self.__roi_lock:
+            if roi is None:
+                roi = Annotation(Rectangle.generate_full_box(), labels=[])
             self.__roi = roi
 
     @property
