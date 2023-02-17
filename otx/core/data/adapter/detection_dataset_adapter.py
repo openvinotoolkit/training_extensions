24e43c03faf0e716c43a11ea1059c5e79b9f1b35
sungmanc <sungman.cho@intel.com>
2023-02-17 13:09:05 +0900

    Update detection, base detector

--- otx/core/data/adapter/detection_dataset_adapter.py
+++ otx/core/data/adapter/detection_dataset_adapter.py
@@ -4,10 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# pylint: disable=invalid-name, too-many-locals, no-member
-from typing import List, Optional, Dict
+# pylint: disable=invalid-name, too-many-locals, no-member, too-many-nested-blocks
+from typing import List
 
-from datumaro.components.annotation import Annotation as DatumaroAnnotation
 from datumaro.components.annotation import AnnotationType
 
 from otx.api.entities.dataset_item import DatasetItemEntity
@@ -23,16 +22,6 @@ class DetectionDatasetAdapter(BaseDatasetAdapter):
     It converts DatumaroDataset --> DatasetEntity for object detection, and instance segmentation tasks
     """
 
-    def __init__(
-        self,
-        task_type: TaskType,
-        train_data_roots: Optional[str] = None,
-        val_data_roots: Optional[str] = None,
-        test_data_roots: Optional[str] = None,
-        unlabeled_data_roots: Optional[str] = None,
-    ):
-        super().__init__(task_type, train_data_roots, val_data_roots, test_data_roots, unlabeled_data_roots)
-    
     def get_otx_dataset(self) -> DatasetEntity:
         """Convert DatumaroDataset to DatasetEntity for Detection."""
         # Prepare label information
@@ -42,25 +31,21 @@ class DetectionDatasetAdapter(BaseDatasetAdapter):
         dataset_items: List[DatasetItemEntity] = []
         used_labels: List[int] = []
         for subset, subset_data in self.dataset.items():
-            for datumaro_item in subset_data.get_subset(subset.name.lower()):
-                image = Image(file_path=datumaro_item.media.path)
-                shapes = []
-                for ann in datumaro_item.annotations:
-                    if self.task_type is TaskType.INSTANCE_SEGMENTATION and ann.type == AnnotationType.polygon:
-                        shape = self._get_polygon_entity(ann, image.width, image.height)
-                    if self.task_type is TaskType.DETECTION and ann.type == AnnotationType.bbox:
-                        shape = self._get_normalized_bbox_entity(ann, image.width, image.height)
-                    else:
-                        shape = None
-                        ValueError(f"{ann.type} is not supported.")
-                    
-                    if shape is not None:
-                        shapes.append(shape)
-                        
-                    if ann.label not in used_labels:
-                        used_labels.append(ann.label)
-                dataset_item = DatasetItemEntity(image, self._get_ann_scene_entity(shapes), subset=subset)
-                dataset_items.append(dataset_item)
+            for _, datumaro_items in subset_data.subsets().items():
+                for datumaro_item in datumaro_items:
+                    image = Image(file_path=datumaro_item.media.path)
+                    shapes = []
+                    for ann in datumaro_item.annotations:
+                        if self.task_type is TaskType.INSTANCE_SEGMENTATION and ann.type == AnnotationType.polygon:
+                            self._get_polygon_entity(ann, image.width, image.height)
+                        if self.task_type is TaskType.DETECTION and ann.type == AnnotationType.bbox:
+                            if self._is_normal_bbox(ann.points[0], ann.points[1], ann.points[2], ann.points[3]):
+                                shapes.append(self._get_normalized_bbox_entity(ann, image.width, image.height))
+
+                        if ann.label not in used_labels:
+                            used_labels.append(ann.label)
+                    dataset_item = DatasetItemEntity(image, self._get_ann_scene_entity(shapes), subset=subset)
+                    dataset_items.append(dataset_item)
 
         self.remove_unused_label_entities(used_labels)
         return DatasetEntity(items=dataset_items)
