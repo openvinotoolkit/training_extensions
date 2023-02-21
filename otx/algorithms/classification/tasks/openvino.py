23b145fad61cc35a48816576556f9934d5a4cf0e
Galina <galina.zalesskaya@intel.com>
2023-02-21 12:17:09 +0200

    Solve merge conflict

--- otx/algorithms/classification/tasks/openvino.py
+++ otx/algorithms/classification/tasks/openvino.py
@@ -239,15 +239,24 @@ class ClassificationOpenVINOTask(IDeploymentTask, IInferenceTask, IEvaluationTas
             dataset_item.append_metadata_item(probs_meta, model=self.model)
 
             if dump_features:
-                add_saliency_maps_to_dataset_item(
-                    dataset_item=dataset_item,
-                    saliency_map=saliency_map,
-                    model=self.model,
-                    labels=self.task_environment.get_labels(),
-                    predicted_scored_labels=item_labels,
-                    explain_predicted_classes=explain_predicted_classes,
-                    process_saliency_maps=process_saliency_maps,
-                )
+                if saliency_map is not None and repr_vector is not None:
+                    feature_vec_media = TensorEntity(name="representation_vector", numpy=repr_vector.reshape(-1))
+                    dataset_item.append_metadata_item(feature_vec_media, model=self.model)
+
+                    add_saliency_maps_to_dataset_item(
+                        dataset_item=dataset_item,
+                        saliency_map=saliency_map,
+                        model=self.model,
+                        labels=self.task_environment.get_labels(),
+                        predicted_scored_labels=item_labels,
+                        explain_predicted_classes=explain_predicted_classes,
+                        process_saliency_maps=process_saliency_maps,
+                    )
+                else:
+                    warnings.warn(
+                        "Could not find Feature Vector and Saliency Map in OpenVINO output. "
+                        "Please rerun OpenVINO export or retrain the model."
+                    )
             update_progress_callback(int(i / dataset_size * 100))
         return dataset
 
