614142f88321de0dee652333574c98a76b12b523
Songki Choi <songki.choi@intel.com>
2023-02-24 22:42:53 +0900

    [RELEASE] OTX1.0.0 (#1829)
    * Bump up to 1.0.0
    
    * Fix pre-commit
    
    ---------
    Signed-off-by: Songki Choi <songki.choi@intel.com>

--- otx/__init__.py
+++ otx/__init__.py
@@ -3,4 +3,5 @@
 # Copyright (C) 2021-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = "1.0.0rc1"
+__version__ = "1.0.0"
+# NOTE: Sync w/ otx/api/usecases/exportable_code/demo/requirements.txt
