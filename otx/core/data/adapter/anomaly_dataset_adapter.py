c90eeb340844626dd1b5e835efe1f92c505ec8c1
Dick Ameln <dick.ameln@intel.com>
2023-03-06 21:08:09 +0100

    read annotations directly from datumaro dataset

--- otx/core/data/adapter/anomaly_dataset_adapter.py
+++ otx/core/data/adapter/anomaly_dataset_adapter.py
@@ -5,28 +5,20 @@
 #
 
 # pylint: disable=invalid-name, too-many-locals, no-member
-import os
-from typing import List, Optional, Union
+from typing import Any, Dict, List, Union
 
-import cv2
-import numpy as np
+from datumaro.components.annotation import AnnotationType
+from datumaro.components.dataset import Dataset as DatumaroDataset
+from datumaro.plugins.transforms import MasksToPolygons
 
-from otx.algorithms.common.utils.mask_to_bbox import mask2bbox
-from otx.api.entities.annotation import (
-    Annotation,
-    AnnotationSceneEntity,
-    AnnotationSceneKind,
-    NullAnnotationSceneEntity,
-)
+from otx.api.entities.annotation import Annotation
 from otx.api.entities.dataset_item import DatasetItemEntity
 from otx.api.entities.datasets import DatasetEntity
 from otx.api.entities.id import ID
 from otx.api.entities.image import Image
 from otx.api.entities.label import LabelEntity
 from otx.api.entities.model_template import TaskType
-from otx.api.entities.scored_label import ScoredLabel
-from otx.api.entities.shapes.rectangle import Rectangle
-from otx.api.utils.segmentation_utils import create_annotation_from_segmentation_map
+from otx.api.entities.subset import Subset
 from otx.core.data.adapter.base_dataset_adapter import BaseDatasetAdapter
 
 
@@ -42,7 +34,7 @@ class AnomalyBaseDatasetAdapter(BaseDatasetAdapter):
         Returns:
             str: Selected data type
         """
-        if self.task_type == TaskType.ACTION_CLASSIFICATION:
+        if self.task_type == TaskType.ANOMALY_CLASSIFICATION:
             assert "mvtec_classification" in data_candidates
             data_type = "mvtec_classification"
         elif self.task_type == TaskType.ANOMALY_SEGMENTATION:
@@ -55,7 +47,10 @@ class AnomalyBaseDatasetAdapter(BaseDatasetAdapter):
             raise ValueError(f"Unrecognized anomaly task type: {self.task_type}")
         return data_type
 
-    def _prepare_anomaly_label_information(self) -> List[LabelEntity]:
+    def _prepare_label_information(
+        self,
+        datumaro_dataset: Dict[Subset, DatumaroDataset],
+    ) -> Dict[str, Any]:
         """Prepare LabelEntity List."""
         normal_label = LabelEntity(id=ID(0), name="Normal", domain=self.domain)
         abnormal_label = LabelEntity(
@@ -64,150 +59,162 @@ class AnomalyBaseDatasetAdapter(BaseDatasetAdapter):
             domain=self.domain,
             is_anomalous=True,
         )
-        return [normal_label, abnormal_label]
+
+        label_information = super()._prepare_label_information(datumaro_dataset)
+        label_information["label_entities"] = [
+            normal_label if class_name.name == "good" else abnormal_label
+            for class_name in label_information["category_items"]
+        ]
+
+        return label_information
+
+    def _get_shapes_from_datumaro_item(self, datumaro_item):
+        height, width = datumaro_item.media.size
+        shapes: List[Annotation] = []
+        for ann in datumaro_item.annotations:
+            if ann.type == AnnotationType.label:
+                shapes.append(self._get_label_entity(ann))
+            elif ann.type == AnnotationType.mask:
+                datumaro_polygons = MasksToPolygons.convert_mask(ann)
+                for d_polygon in datumaro_polygons:
+                    shapes.append(self._get_polygon_entity(d_polygon, width, height))
+            elif ann.type == AnnotationType.bbox:
+                if self._is_normal_bbox(ann.points[0], ann.points[1], ann.points[2], ann.points[3]):
+                    shapes.append(self._get_normalized_bbox_entity(ann, width, height))
+        return shapes
 
     def get_otx_dataset(self) -> DatasetEntity:
-        """Get DatasetEntity."""
-        raise NotImplementedError()
+        """Convert DatumaroDataset to DatasetEntity for Anomaly segmentation."""
+        label_information = self._prepare_label_information(self.dataset)
+        self.label_entities = label_information["label_entities"]
+
+        # Convert dataset items
+        dataset_items: List[DatasetItemEntity] = []
+        for subset, subset_data in self.dataset.items():
+            for _, datumaro_items in subset_data.subsets().items():
+                for datumaro_item in datumaro_items:
+
+                    shapes = self._get_shapes_from_datumaro_item(datumaro_item)
+
+                    image = Image(file_path=datumaro_item.media.path)
+                    dataset_item = DatasetItemEntity(image, self._get_ann_scene_entity(shapes), subset=subset)
+                    dataset_items.append(dataset_item)
+
+        self.remove_duplicate_label_entities()
+        return DatasetEntity(items=dataset_items)
+
+    def remove_duplicate_label_entities(self):
+        """Remove duplicate labels from label entities.
+
+        Because label entities will be used to make Label Schema,
+        If there are duplicate labels in the Label Schema, it will hurt the model performance.
+        """
+        self.label_entities = list(set(self.label_entities))
 
 
 class AnomalyClassificationDatasetAdapter(AnomalyBaseDatasetAdapter):
     """Anomaly classification adapter inherited from AnomalyBaseDatasetAdapter."""
 
-    def get_otx_dataset(self) -> DatasetEntity:
-        """Convert DatumaroDataset to DatasetEntity for Anomaly classification."""
-        normal_label, abnormal_label = self._prepare_anomaly_label_information()
-        self.label_entities = [normal_label, abnormal_label]
+    # def get_otx_dataset(self) -> DatasetEntity:
+    #     """Convert DatumaroDataset to DatasetEntity for Anomaly classification."""
+    #     normal_label, abnormal_label = self._prepare_anomaly_label_information()
+    #     self.label_entities = [normal_label, abnormal_label]
 
-        # Prepare
-        dataset_items: List[DatasetItemEntity] = []
-        for subset, subset_data in self.dataset.items():
-            for _, datumaro_items in subset_data.subsets().items():
-                for datumaro_item in datumaro_items:
-                    image = Image(file_path=datumaro_item.media.path)
-                    label = normal_label if os.path.dirname(datumaro_item.id) == "good" else abnormal_label
-                    shapes = [
-                        Annotation(
-                            Rectangle.generate_full_box(),
-                            labels=[ScoredLabel(label=label, probability=1.0)],
-                        )
-                    ]
-                    annotation_scene: Optional[AnnotationSceneEntity] = None
-                    # Unlabeled dataset
-                    if len(shapes) == 0:
-                        annotation_scene = NullAnnotationSceneEntity()
-                    else:
-                        annotation_scene = AnnotationSceneEntity(
-                            kind=AnnotationSceneKind.ANNOTATION, annotations=shapes
-                        )
-                    dataset_item = DatasetItemEntity(image, annotation_scene, subset=subset)
-                    dataset_items.append(dataset_item)
+    #     # Prepare
+    #     dataset_items: List[DatasetItemEntity] = []
+    #     for subset, subset_data in self.dataset.items():
+    #         for _, datumaro_items in subset_data.subsets().items():
+    #             for datumaro_item in datumaro_items:
+    #                 image = Image(file_path=datumaro_item.media.path)
+    #                 label = normal_label if os.path.dirname(datumaro_item.id) == "good" else abnormal_label
+    #                 shapes = [
+    #                     Annotation(
+    #                         Rectangle.generate_full_box(),
+    #                         labels=[ScoredLabel(label=label, probability=1.0)],
+    #                     )
+    #                 ]
+    #                 annotation_scene: Optional[AnnotationSceneEntity] = None
+    #                 # Unlabeled dataset
+    #                 if len(shapes) == 0:
+    #                     annotation_scene = NullAnnotationSceneEntity()
+    #                 else:
+    #                     annotation_scene = AnnotationSceneEntity(
+    #                         kind=AnnotationSceneKind.ANNOTATION, annotations=shapes
+    #                     )
+    #                 dataset_item = DatasetItemEntity(image, annotation_scene, subset=subset)
+    #                 dataset_items.append(dataset_item)
 
-        return DatasetEntity(items=dataset_items)
+    #     return DatasetEntity(items=dataset_items)
 
 
 class AnomalyDetectionDatasetAdapter(AnomalyBaseDatasetAdapter):
     """Anomaly detection adapter inherited from AnomalyBaseDatasetAdapter."""
 
-    def get_otx_dataset(self) -> DatasetEntity:
-        """Conver DatumaroDataset to DatasetEntity for Anomaly detection."""
-        normal_label, abnormal_label = self._prepare_anomaly_label_information()
-        self.label_entities = [normal_label, abnormal_label]
+    # def get_otx_dataset(self) -> DatasetEntity:
+    #     """Conver DatumaroDataset to DatasetEntity for Anomaly detection."""
+    #     normal_label, abnormal_label = self._prepare_anomaly_label_information()
+    #     self.label_entities = [normal_label, abnormal_label]
 
-        # Prepare
-        dataset_items: List[DatasetItemEntity] = []
-        for subset, subset_data in self.dataset.items():
-            for _, datumaro_items in subset_data.subsets().items():
-                for datumaro_item in datumaro_items:
-                    image = Image(file_path=datumaro_item.media.path)
-                    label = normal_label if os.path.dirname(datumaro_item.id) == "good" else abnormal_label
-                    shapes = [
-                        Annotation(
-                            Rectangle.generate_full_box(),
-                            labels=[ScoredLabel(label=label, probability=1.0)],
-                        )
-                    ]
-                    # TODO: avoid hard coding, plan to enable MVTec to Datumaro
-                    mask_file_path = os.path.join(
-                        "/".join(datumaro_item.media.path.split("/")[:-3]),
-                        "ground_truth",
-                        str(datumaro_item.id) + "_mask.png",
-                    )
-                    if os.path.exists(mask_file_path):
-                        mask = (cv2.imread(mask_file_path, cv2.IMREAD_GRAYSCALE) / 255).astype(np.uint8)
-                        bboxes = mask2bbox(mask)
-                        for bbox in bboxes:
-                            x1, y1, x2, y2 = bbox
-                            shapes.append(
-                                Annotation(
-                                    Rectangle(
-                                        x1=x1 / image.width,
-                                        y1=y1 / image.height,
-                                        x2=x2 / image.width,
-                                        y2=y2 / image.height,
-                                    ),
-                                    labels=[ScoredLabel(label=abnormal_label)],
-                                )
-                            )
-                    annotation_scene: Optional[AnnotationSceneEntity] = None
-                    # Unlabeled dataset
-                    if len(shapes) == 0:
-                        annotation_scene = NullAnnotationSceneEntity()
-                    else:
-                        annotation_scene = AnnotationSceneEntity(
-                            kind=AnnotationSceneKind.ANNOTATION, annotations=shapes
-                        )
-                    dataset_item = DatasetItemEntity(image, annotation_scene, subset=subset)
-                    dataset_items.append(dataset_item)
+    #     # Prepare
+    #     dataset_items: List[DatasetItemEntity] = []
+    #     for subset, subset_data in self.dataset.items():
+    #         for _, datumaro_items in subset_data.subsets().items():
+    #             for datumaro_item in datumaro_items:
+    #                 image = Image(file_path=datumaro_item.media.path)
+    #                 label = normal_label if os.path.dirname(datumaro_item.id) == "good" else abnormal_label
+    #                 shapes = [
+    #                     Annotation(
+    #                         Rectangle.generate_full_box(),
+    #                         labels=[ScoredLabel(label=label, probability=1.0)],
+    #                     )
+    #                 ]
+    #                 # TODO: avoid hard coding, plan to enable MVTec to Datumaro
+    #                 mask_file_path = os.path.join(
+    #                     "/".join(datumaro_item.media.path.split("/")[:-3]),
+    #                     "ground_truth",
+    #                     str(datumaro_item.id) + "_mask.png",
+    #                 )
+    #                 if os.path.exists(mask_file_path):
+    #                     mask = (cv2.imread(mask_file_path, cv2.IMREAD_GRAYSCALE) / 255).astype(np.uint8)
+    #                     bboxes = mask2bbox(mask)
+    #                     for bbox in bboxes:
+    #                         x1, y1, x2, y2 = bbox
+    #                         shapes.append(
+    #                             Annotation(
+    #                                 Rectangle(
+    #                                     x1=x1 / image.width,
+    #                                     y1=y1 / image.height,
+    #                                     x2=x2 / image.width,
+    #                                     y2=y2 / image.height,
+    #                                 ),
+    #                                 labels=[ScoredLabel(label=abnormal_label)],
+    #                             )
+    #                         )
+    #                 annotation_scene: Optional[AnnotationSceneEntity] = None
+    #                 # Unlabeled dataset
+    #                 if len(shapes) == 0:
+    #                     annotation_scene = NullAnnotationSceneEntity()
+    #                 else:
+    #                     annotation_scene = AnnotationSceneEntity(
+    #                         kind=AnnotationSceneKind.ANNOTATION, annotations=shapes
+    #                     )
+    #                 dataset_item = DatasetItemEntity(image, annotation_scene, subset=subset)
+    #                 dataset_items.append(dataset_item)
 
-        return DatasetEntity(items=dataset_items)
+    #     return DatasetEntity(items=dataset_items)
 
 
 class AnomalySegmentationDatasetAdapter(AnomalyBaseDatasetAdapter):
     """Anomaly segmentation adapter inherited by AnomalyBaseDatasetAdapter and BaseDatasetAdapter."""
 
-    def get_otx_dataset(self) -> DatasetEntity:
-        """Conver DatumaroDataset to DatasetEntity for Anomaly segmentation."""
-        normal_label, abnormal_label = self._prepare_anomaly_label_information()
-        self.label_entities = [normal_label, abnormal_label]
-
-        # Prepare
-        dataset_items: List[DatasetItemEntity] = []
-        for subset, subset_data in self.dataset.items():
-            for _, datumaro_items in subset_data.subsets().items():
-                for datumaro_item in datumaro_items:
-                    image = Image(file_path=datumaro_item.media.path)
-                    label = normal_label if os.path.dirname(datumaro_item.id) == "good" else abnormal_label
-                    shapes = [
-                        Annotation(
-                            Rectangle.generate_full_box(),
-                            labels=[ScoredLabel(label=label, probability=1.0)],
-                        )
-                    ]
-                    # TODO: avoid hard coding, plan to enable MVTec to Datumaro
-                    mask_file_path = os.path.join(
-                        "/".join(datumaro_item.media.path.split("/")[:-3]),
-                        "ground_truth",
-                        str(datumaro_item.id) + "_mask.png",
-                    )
-                    if os.path.exists(mask_file_path):
-                        mask = (cv2.imread(mask_file_path, cv2.IMREAD_GRAYSCALE) / 255).astype(np.uint8)
-                        shapes.extend(
-                            create_annotation_from_segmentation_map(
-                                hard_prediction=mask,
-                                soft_prediction=np.ones_like(mask),
-                                label_map={0: normal_label, 1: abnormal_label},
-                            )
-                        )
-                    annotation_scene: Optional[AnnotationSceneEntity] = None
-                    # Unlabeled dataset
-                    if len(shapes) == 0:
-                        annotation_scene = NullAnnotationSceneEntity()
-                    else:
-                        annotation_scene = AnnotationSceneEntity(
-                            kind=AnnotationSceneKind.ANNOTATION, annotations=shapes
-                        )
-                    dataset_item = DatasetItemEntity(image, annotation_scene, subset=subset)
-                    dataset_items.append(dataset_item)
-
-        return DatasetEntity(items=dataset_items)
+    # def _get_shapes_from_datumaro_item(self, datumaro_item):
+    #     shapes: List[Annotation] = []
+    #     for ann in datumaro_item.annotations:
+    #         if ann.type == AnnotationType.label:
+    #             shapes.append(self._get_label_entity(ann))
+    #         elif ann.type == AnnotationType.mask:
+    #             datumaro_polygons = MasksToPolygons.convert_mask(ann)
+    #             for d_polygon in datumaro_polygons:
+    #                 height, width = datumaro_item.media.size
+    #                 shapes.append(self._get_polygon_entity(d_polygon, width, height))
+    #     return shapes
