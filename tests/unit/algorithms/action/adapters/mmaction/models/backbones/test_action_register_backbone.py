50af217dad796a41a343b245a51be90271fba29a
JihwanEom <jihwan.eom@intel.com>
2023-03-13 12:19:58 +0900

    Add tests

--- tests/unit/algorithms/action/adapters/mmaction/models/backbones/test_action_register_backbone.py
+++ tests/unit/algorithms/action/adapters/mmaction/models/backbones/test_action_register_backbone.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from mmdet.models import BACKBONES as MMDET_BACKBONES
+from mmaction.models import BACKBONES as MMACTION_BACKBONES
 
 from tests.test_suite.e2e_test_system import e2e_pytest_unit
 
@@ -16,4 +16,5 @@ def test_register_action_backbones() -> None:
     Since this function is called while initialization, X3D should be in mmdet backbone registry
     """
 
-    assert "X3D" in MMDET_BACKBONES
+    assert "X3D" in MMACTION_BACKBONES
+    assert "OTXMoViNet" in MMACTION_BACKBONES
