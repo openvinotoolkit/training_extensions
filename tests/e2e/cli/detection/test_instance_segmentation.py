f78839304cf3cdc474aacb147512ce1833a971f4
Kang, Harim <harim.kang@intel.com>
2023-03-17 10:39:47 +0900

    Add skip instance seg deploy test

--- tests/e2e/cli/detection/test_instance_segmentation.py
+++ tests/e2e/cli/detection/test_instance_segmentation.py
@@ -164,6 +164,7 @@ class TestToolsMPAInstanceSegmentation:
     @e2e_pytest_component
     @pytest.mark.skipif(TT_STABILITY_TESTS, reason="This is TT_STABILITY_TESTS")
     @pytest.mark.parametrize("template", templates, ids=templates_ids)
+    @pytest.mark.skip(reason="CVS-104657")
     def test_otx_eval_deployment(self, template, tmp_dir_path):
         tmp_dir_path = tmp_dir_path / "ins_seg"
         otx_eval_deployment_testing(template, tmp_dir_path, otx_dir, args, threshold=0.0)
@@ -171,6 +172,7 @@ class TestToolsMPAInstanceSegmentation:
     @e2e_pytest_component
     @pytest.mark.skipif(TT_STABILITY_TESTS, reason="This is TT_STABILITY_TESTS")
     @pytest.mark.parametrize("template", templates, ids=templates_ids)
+    @pytest.mark.skip(reason="CVS-104657")
     def test_otx_demo_deployment(self, template, tmp_dir_path):
         tmp_dir_path = tmp_dir_path / "ins_seg"
         otx_demo_deployment_testing(template, tmp_dir_path, otx_dir, args)
