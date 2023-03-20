d695dd6db87298120e6e8708c826ae52965b32ab
Harim Kang <harim.kang@intel.com>
2023-03-20 15:14:57 +0900

    Update otx/mpa/det/exporter.py
    Co-authored-by: Jaeguk Hyun <jaeguk.hyun@intel.com>

--- otx/mpa/det/exporter.py
+++ otx/mpa/det/exporter.py
@@ -46,7 +46,7 @@ class DetectionExporter(ExporterMixin, DetectionStage):
         from mmdet.apis.inference import LoadImage
         from mmdet.datasets.pipelines import Compose
 
-        from ...algorithms.common.adapters.mmdeploy.apis import NaiveExporter
+        from otx.algorithms.common.adapters.mmdeploy.apis import NaiveExporter
 
         def get_fake_data(cfg, orig_img_shape=(128, 128, 3)):
             pipeline = [LoadImage()] + cfg.data.test.pipeline[1:]
