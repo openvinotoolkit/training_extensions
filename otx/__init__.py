248016dbe2deb8a7b1fbc91a7d2df22d960f91f6
Songki Choi <songki.choi@intel.com>
2023-03-28 12:01:15 +0900

    Release 1.1.0 (#1933)
    * Release OTX 1.1.0
    
    * Update known issues in CHANGELOG.md
    
    * Fix PR / issue # to links
    
    * Fix pre-commit

--- otx/__init__.py
+++ otx/__init__.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2021-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = "1.1.0rc1"
+__version__ = "1.1.0"
 # NOTE: Sync w/ otx/api/usecases/exportable_code/demo/requirements.txt on release
 
 MMCLS_AVAILABLE = True
