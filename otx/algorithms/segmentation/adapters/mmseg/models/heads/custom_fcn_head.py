7bab4a32e9fbb43c489a8b57fa481b28ded9b023
kprokofi <kirill.prokofiev@intel.com>
2023-03-29 15:24:14 +0900

    clean

--- otx/algorithms/segmentation/adapters/mmseg/models/heads/custom_fcn_head.py
+++ otx/algorithms/segmentation/adapters/mmseg/models/heads/custom_fcn_head.py
@@ -7,7 +7,6 @@
 from mmseg.models.builder import HEADS
 from mmseg.models.decode_heads.fcn_head import FCNHead
 from mmseg.models.decode_heads.sep_aspp_head import ASPPHead
-from .light_ham import LightHamHead
 
 from .mixin import (
     AggregatorMixin,
@@ -18,7 +17,6 @@ from .mixin import (
 
 KNOWN_HEADS = {
     "FCNHead": FCNHead,
-    "LightHamHead": LightHamHead,
     "ASPPHead": ASPPHead
 }
 
@@ -44,13 +42,13 @@ class CustomFCNHead(
 ):  # pylint: disable=too-many-ancestors
     """Custom Fully Convolution Networks for Semantic Segmentation."""
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-            # get rid of last activation of convs module
-            if self.act_cfg:
-                self.convs[-1].with_activation = False
-                delattr(self.convs[-1], "activate")
+        # get rid of last activation of convs module
+        if self.act_cfg:
+            self.convs[-1].with_activation = False
+            delattr(self.convs[-1], "activate")
 
-            if kwargs.get("init_cfg", {}):
-                self.init_weights()
+        if kwargs.get("init_cfg", {}):
+            self.init_weights()
