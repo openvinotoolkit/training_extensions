07302513031f3c3ac7f8f64bfc6d72a89563259f
kprokofi <kirill.prokofiev@intel.com>
2023-03-29 13:59:55 +0900

    remove mpa and merge conflicts

--- tests/unit/algorithms/segmentation/adapters/mmseg/datasets/__init__.py
+++ tests/unit/algorithms/segmentation/adapters/mmseg/datasets/__init__.py
@@ -2,8 +2,3 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
-<<<<<<< HEAD:tests/unit/algorithms/segmentation/adapters/mmseg/datasets/__init__.py
-=======
-
-# flake8: noqa
->>>>>>> 11a99db59 (some):otx/mpa/modules/models/heads/__init__.py
