81dde6f03a67454df51f90a2845435737f6cd50b
kprokofi <kirill.prokofiev@intel.com>
2023-03-29 13:54:40 +0900

    removed some configs

--- otx/recipes/stages/_base_/models/segmentors/segmentor.py
+++ otx/recipes/stages/_base_/models/segmentors/segmentor.py
@@ -2,4 +2,4 @@ _base_ = "../model.py"
 
 task = "segmentation"
 
-model = dict(train_cfg=dict(), test_cfg=dict())
+model = dict(train_cfg=dict(mix_loss=dict(enable=False, weight=0.1)), test_cfg=dict(mode="whole"))
--- otx/recipes/stages/segmentation/incremental.py
+++ otx/recipes/stages/segmentation/incremental.py
@@ -1,36 +1,26 @@
-_base_ = ["../_base_/models/segmentors/seg_class_incr.py", "../_base_/data/custom_seg.py"]
+_base_ = ["./train.py", "../_base_/models/segmentors/segmentor.py", "../_base_/data/custom_seg.py"]
 
-optimizer = dict(type='AdamW', lr=0.0005, betas=(0.9, 0.999), weight_decay=1e-4, paramwise_cfg={'bias_decay_mult ': 0.0, 'norm_decay_mult ': 0.0})
+optimizer = dict(_delete_=True, type='AdamW', lr=0.0005, betas=(0.9, 0.999), weight_decay=1e-4, paramwise_cfg={'bias_decay_mult ': 0.0, 'norm_decay_mult ': 0.0})
 optimizer_config = dict()
-# optimizer_config = dict(
-#     _delete_=True,
-#     grad_clip=dict(
-#         # method='adaptive',
-#         # clip=0.2,
-#         # method='default',
-#         max_norm=40,
-#         norm_type=2,
-#     ),
-# )
-
-lr_config = dict(policy='poly',  warmup='linear',  warmup_iters=260, warmup_ratio=1e-6, power=0.9,  min_lr=1e-6, by_epoch=False)
+# learning policy
+lr_config = dict(_delete_=True, policy='poly',  warmup='linear',  warmup_iters=260, warmup_ratio=1e-6, power=0.9,  min_lr=1e-6, by_epoch=False)
 
 log_config = dict(
     interval=10,
     hooks=[
         dict(type="TextLoggerHook", by_epoch=True, ignore_last=False),
-        # dict(type='TensorboardLoggerHook')
     ],
 )
 
 dist_params = dict(backend="nccl", linear_scale_lr=False)
 
-runner = dict(type="EpochRunnerWithCancel", max_epochs=300)
+runner = dict(type="EpochRunnerWithCancel", max_epochs=100)
 
 checkpoint_config = dict(by_epoch=True, interval=1)
 
 evaluation = dict(interval=1, metric=["mDice", "mIoU"], show_log=True)
 
+seed = 42
 find_unused_parameters = False
 
 task_adapt = dict(
@@ -39,10 +29,3 @@ task_adapt = dict(
 )
 
 ignore = True
-
-cudnn_benchmark = False
-
-deterministic = False
-
-hparams = dict(dummy=0)
-
