28ab1f55b29bfc0eed198d76edddf18d8bc307b3
Harim Kang <harim.kang@intel.com>
2023-03-30 11:16:36 +0900

    Update tests/integration/cli/test_cli.py
    Co-authored-by: Eunwoo Shin <eunwoo.shin@intel.com>

--- tests/integration/cli/test_cli.py
+++ tests/integration/cli/test_cli.py
@@ -198,7 +198,7 @@ class TestToolsOTXTrainAutoConfig:
         assert os.path.exists(output_path)
         file_list = sorted(os.listdir(output_path))
         assert len(file_list) == 4
-        file_list = file_list[:-1]  # Remove latest from list, then file_list[-1] is latest
+        file_list.remove("latest_trained_model")
         assert os.path.exists(os.path.join(output_path, file_list[-1]))
         assert os.path.exists(os.path.join(output_path, file_list[-1], "nncf"))
         assert os.path.exists(os.path.join(output_path, file_list[-1], "nncf", "weights.pth"))
