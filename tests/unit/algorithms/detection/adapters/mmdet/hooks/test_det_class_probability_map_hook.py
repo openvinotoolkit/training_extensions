27789e97f756b2a156e533b0b1cfa35d28ac6d85
jaegukhyun <jaeguk.hyun@intel.com>
2023-04-06 10:57:29 +0900

    Change test_det_saliency_map_hook to test_det_class_probability_map_hook

--- tests/unit/algorithms/detection/adapters/mmdet/hooks/test_det_class_probability_map_hook.py
+++ tests/unit/algorithms/detection/adapters/mmdet/hooks/test_det_class_probability_map_hook.py
@@ -1,8 +1,8 @@
 import pytest
 import torch
 
-from otx.algorithms.detection.adapters.mmdet.hooks.det_saliency_map_hook import (
-    DetSaliencyMapHook,
+from otx.algorithms.detection.adapters.mmdet.hooks.det_class_probability_map_hook import (
+    DetClassProbabilityMapHook,
 )
 from otx.algorithms.detection.adapters.mmdet.models.heads.custom_atss_head import (
     CustomATSSHead,
@@ -19,8 +19,8 @@ from otx.algorithms.detection.adapters.mmdet.models.heads.custom_yolox_head impo
 from tests.test_suite.e2e_test_system import e2e_pytest_unit
 
 
-class TestDetSaliencyMapHook:
-    """Test class for DetSaliencyMapHook."""
+class TestDetClassProbabilityMapHook:
+    """Test class for DetClassProbabilityMapHook."""
 
     @pytest.fixture(autouse=True)
     def setup(self) -> None:
@@ -37,14 +37,14 @@ class TestDetSaliencyMapHook:
                 return x
 
         self.module = _MockModule()
-        self.hook = DetSaliencyMapHook(self.module)
+        self.hook = DetClassProbabilityMapHook(self.module)
 
     @e2e_pytest_unit
     def test_func(self, mocker) -> None:
         """Test func function."""
 
         mocker.patch.object(
-            DetSaliencyMapHook, "_get_cls_scores_from_feature_map", return_value=[torch.randn(1, 3, 14, 14)]
+            DetClassProbabilityMapHook, "_get_cls_scores_from_feature_map", return_value=[torch.randn(1, 3, 14, 14)]
         )
         assert self.hook.func(torch.randn(1, 3, 14, 14)) is not None
 
@@ -53,14 +53,14 @@ class TestDetSaliencyMapHook:
         """Test _get_cls_scores_from_feature_map function."""
 
         self.module.bbox_head = CustomATSSHead(num_classes=3, in_channels=64)
-        self.hook = DetSaliencyMapHook(self.module)
+        self.hook = DetClassProbabilityMapHook(self.module)
         assert self.hook._get_cls_scores_from_feature_map(torch.Tensor(1, 3, 64, 32, 32)) is not None
         self.module.bbox_head = CustomYOLOXHead(num_classes=3, in_channels=64)
-        self.hook = DetSaliencyMapHook(self.module)
+        self.hook = DetClassProbabilityMapHook(self.module)
         assert self.hook._get_cls_scores_from_feature_map(torch.Tensor(1, 3, 64, 32, 32)) is not None
         self.module.bbox_head = CustomVFNetHead(num_classes=3, in_channels=64)
         self.module.bbox_head.anchor_generator.num_base_anchors = 1
-        self.hook = DetSaliencyMapHook(self.module)
+        self.hook = DetClassProbabilityMapHook(self.module)
         assert self.hook._get_cls_scores_from_feature_map(torch.Tensor(1, 3, 64, 32, 32)) is not None
         self.module.bbox_head = CustomSSDHead(
             anchor_generator=dict(
@@ -71,11 +71,11 @@ class TestDetSaliencyMapHook:
             ),
             act_cfg={},
         )
-        self.hook = DetSaliencyMapHook(self.module)
+        self.hook = DetClassProbabilityMapHook(self.module)
         assert self.hook._get_cls_scores_from_feature_map(torch.Tensor(1, 3, 512, 32, 32)) is not None
 
         self.module.bbox_head = torch.nn.Module()
         self.module.bbox_head.cls_out_channels = 3
-        self.hook = DetSaliencyMapHook(self.module)
+        self.hook = DetClassProbabilityMapHook(self.module)
         with pytest.raises(NotImplementedError):
             self.hook._get_cls_scores_from_feature_map(torch.Tensor(1, 3, 512, 32, 32))
