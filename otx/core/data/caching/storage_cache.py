02dff535257a848ee49e352165bd324d115ee56b
Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>
2023-04-19 08:44:43 +0900

    fix: make force verbose
    Signed-off-by: Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>

--- otx/core/data/caching/storage_cache.py
+++ otx/core/data/caching/storage_cache.py
@@ -5,6 +5,7 @@
 
 import hashlib
 import os
+import shutil
 import stat
 from typing import List, Optional
 
@@ -32,9 +33,11 @@ def arrow_cache_helper(
         force: If true, rebuild arrow even if cache is hit.
     """
 
-    def get_hash(source_path, scheme):
+    def get_hash(dataset, scheme):
+        source_path = dataset.data_path
         _hash = hashlib.sha256()
         _hash.update(f"{source_path}".encode("utf-8"))
+        _hash.update(f"{len(dataset)}".encode("utf-8"))
         _hash.update(f"{scheme}".encode("utf-8"))
         if source_path:
             _hash.update(str(os.stat(source_path)[stat.ST_MTIME]).encode("utf-8"))
@@ -54,23 +57,24 @@ def arrow_cache_helper(
         _hash.update(str(os.stat(file)[stat.ST_MTIME]).encode("utf-8"))
         return _hash.hexdigest()
 
-    cache_dir = os.path.join(cache_dir, get_hash(dataset.data_path, scheme))
-    cache_paths = []
+    cache_dir = os.path.join(cache_dir, get_hash(dataset, scheme))
+    if os.path.exists(cache_dir) and force:
+        shutil.rmtree(cache_dir)
     os.makedirs(cache_dir, exist_ok=True)
 
+    cache_paths = []
     cache_hit = []
-    if not force:
-        for cache_path in os.listdir(cache_dir):
-            if not cache_path.endswith(".arrow"):
-                continue
-            cache_hit.append(False)
-            cache_path = os.path.join(cache_dir, cache_path)
-            cache_paths.append(cache_path)
-            hash_path = f"{cache_path}.hash"
-            if os.path.exists(cache_path) and os.path.exists(hash_path):
-                with open(hash_path, "r", encoding="utf-8") as f:
-                    if get_file_hash(cache_path) == f.read():
-                        cache_hit[-1] = True
+    for cache_path in os.listdir(cache_dir):
+        if not cache_path.endswith(".arrow"):
+            continue
+        cache_hit.append(False)
+        cache_path = os.path.join(cache_dir, cache_path)
+        cache_paths.append(cache_path)
+        hash_path = f"{cache_path}.hash"
+        if os.path.exists(cache_path) and os.path.exists(hash_path):
+            with open(hash_path, "r", encoding="utf-8") as f:
+                if get_file_hash(cache_path) == f.read():
+                    cache_hit[-1] = True
 
     if cache_hit and all(cache_hit):
         return cache_paths
@@ -96,16 +100,16 @@ def arrow_cache_helper(
     return cache_paths
 
 
-def init_arrow_cache(dataset: DatumDataset, scheme: Optional[str] = None, num_workers: int = 0) -> DatumDataset:
+def init_arrow_cache(dataset: DatumDataset, scheme: Optional[str] = None, **kwargs) -> DatumDataset:
     """Init arrow format cache from Datumaro.
 
     Args:
         dataset: Datumaro dataset
         scheme: Datumaro apache arrow image encoding scheme.
-        num_workers: The number of workers to build arrow format.
+        kwargs: kwargs passed to 'arrow_cache_helper'
     """
     if scheme is None or scheme == "NONE":
         return dataset
-    cache_paths = arrow_cache_helper(dataset, scheme, num_workers)
+    cache_paths = arrow_cache_helper(dataset, scheme, **kwargs)
     dataset = DatumDataset.import_from(os.path.dirname(cache_paths[0]), "arrow")
     return dataset
