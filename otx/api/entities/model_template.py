81526c40c9ec135584fd1577708e9a3fcbf7afc1
Galina <galina.zalesskaya@intel.com>
2023-04-18 15:11:52 +0300

    Create Actions domain and task type despite flag

--- otx/api/entities/model_template.py
+++ otx/api/entities/model_template.py
@@ -205,17 +205,16 @@ class TaskType(Enum):
         is_global=False,
         is_local=True,
     )
-    if os.getenv("FEATURE_FLAGS_OTX_ACTION_TASKS", "0") == "1":
-        ACTION_CLASSIFICATION = 14, TaskInfo(
-            domain=Domain.ACTION_CLASSIFICATION,
-            is_trainable=True,
-            is_anomaly=False,
-            is_global=False,
-            is_local=True,
-        )
-        ACTION_DETECTION = 15, TaskInfo(
-            domain=Domain.ACTION_DETECTION, is_trainable=True, is_anomaly=False, is_global=False, is_local=True
-        )
+    ACTION_CLASSIFICATION = 14, TaskInfo(
+        domain=Domain.ACTION_CLASSIFICATION,
+        is_trainable=True,
+        is_anomaly=False,
+        is_global=False,
+        is_local=True,
+    )
+    ACTION_DETECTION = 15, TaskInfo(
+        domain=Domain.ACTION_DETECTION, is_trainable=True, is_anomaly=False, is_global=False, is_local=True
+    )
 
     def __str__(self) -> str:
         """Returns name."""
@@ -247,13 +246,9 @@ def task_type_to_label_domain(task_type: TaskType) -> Domain:
         TaskType.ANOMALY_DETECTION: Domain.ANOMALY_DETECTION,
         TaskType.ANOMALY_SEGMENTATION: Domain.ANOMALY_SEGMENTATION,
         TaskType.ROTATED_DETECTION: Domain.ROTATED_DETECTION,
+        TaskType.ACTION_CLASSIFICATION: Domain.ACTION_CLASSIFICATION,
+        TaskType.ACTION_DETECTION: Domain.ACTION_DETECTION,
     }
-    if os.getenv("FEATURE_FLAGS_OTX_ACTION_TASKS", "0") == "1":
-        mapping = {
-            **mapping,
-            TaskType.ACTION_CLASSIFICATION: Domain.ACTION_CLASSIFICATION,
-            TaskType.ACTION_DETECTION: Domain.ACTION_DETECTION,
-        }
 
     try:
         return mapping[task_type]
