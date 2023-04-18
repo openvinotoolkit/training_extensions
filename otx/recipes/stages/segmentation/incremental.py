29cf1c36bb5f0717b84d85938004e49c2b94161f
kprokofi <kirill.prokofiev@intel.com>
2023-04-19 08:20:16 +0900

    minor

--- otx/recipes/stages/segmentation/incremental.py
+++ otx/recipes/stages/segmentation/incremental.py
@@ -5,7 +5,7 @@ optimizer = dict(
     type="AdamW",
     lr=0.0005,
     betas=(0.9, 0.999),
-    weight_decay=1e-4,
+    weight_decay=0.0001,
     paramwise_cfg={"bias_decay_mult ": 0.0, "norm_decay_mult ": 0.0},
 )
 optimizer_config = dict()
