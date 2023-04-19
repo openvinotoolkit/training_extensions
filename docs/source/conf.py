c5d6cb73dd79970a2938cff82aa393cb7ca2a81b
Songki Choi <songki.choi@intel.com>
2023-04-19 16:39:18 +0900

    HOTFIX: change doc version to 1.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 project = 'OpenVINO™ Training Extensions'
 copyright = '2023, OpenVINO™ Training Extensions Contributors'
 author = 'OpenVINO™ Training Extensions Contributors'
-release = '1.2.0rc0'
+release = '1.2.0'
 
 # -- General configuration --------------------------------------------------- #
 
