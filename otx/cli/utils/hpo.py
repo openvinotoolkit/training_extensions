493005d9950b7314ea8dbaec070e0bd0b187749f
eunwoosh <eunwoo.shin@intel.com>
2023-04-20 16:34:42 +0900

    align with pre commit

--- otx/cli/utils/hpo.py
+++ otx/cli/utils/hpo.py
@@ -715,7 +715,7 @@ class Trainer:
     def _set_hyper_parameter(self, environment: TaskEnvironmentManager):
         environment.set_hyper_parameter_using_str_key(self._hp_config["configuration"])
         if self._task.is_mmcv_framework_task():
-            environment.set_hyper_parameter_using_str_key({'learning_parameters.auto_decrease_batch_size': False})
+            environment.set_hyper_parameter_using_str_key({"learning_parameters.auto_decrease_batch_size": False})
         environment.set_epoch(self._epoch)
 
     def _prepare_environment(self, hyper_parameters, label_schema):
