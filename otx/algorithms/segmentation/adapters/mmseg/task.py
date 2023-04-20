af94e672ecacfb8b635549cb1351b15381bf609f
eunwoosh <eunwoo.shin@intel.com>
2023-04-20 16:02:02 +0900

    remove breakpoint

--- otx/algorithms/segmentation/adapters/mmseg/task.py
+++ otx/algorithms/segmentation/adapters/mmseg/task.py
@@ -375,7 +375,6 @@ class MMSegmentationTask(OTXSegmentationTask):
             validate = isinstance(self, NNCFBaseTask)  # nncf needs eval hooks
             train_func = partial(train_segmentor, meta=deepcopy(meta), model=deepcopy(model), distributed=False)
             adapt_batch_size(train_func, cfg, datasets, validate)
-            breakpoint()
 
         train_segmentor(
             model,
