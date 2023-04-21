882b69c6f6ba58168991adf2707cefc31b8e8583
kprokofi <kirill.prokofiev@intel.com>
2023-04-22 06:17:02 +0900

    revert incremental back

--- otx/recipes/stages/segmentation/incremental_poly.py
+++ otx/recipes/stages/segmentation/incremental_poly.py
@@ -1,29 +1,36 @@
 _base_ = ["./train.py", "../_base_/models/segmentors/segmentor.py", "../_base_/data/custom_seg.py"]
 
-optimizer = dict(_delete_=True, type="Adam", lr=1e-3, eps=1e-08, weight_decay=0.0)
-
-optimizer_config = dict(
+optimizer = dict(
     _delete_=True,
-    grad_clip=dict(
-        # method='adaptive',
-        # clip=0.2,
-        # method='default',
-        max_norm=40,
-        norm_type=2,
-    ),
+    type="AdamW",
+    lr=0.0005,
+    betas=(0.9, 0.999),
+    weight_decay=0.0001,
+    paramwise_cfg={"bias_decay_mult ": 0.0, "norm_decay_mult ": 0.0},
+)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(
+    _delete_=True,
+    policy="poly",
+    warmup="linear",
+    warmup_iters=300,
+    warmup_ratio=1e-6,
+    power=0.9,
+    min_lr=1e-6,
+    by_epoch=False,
 )
 
 log_config = dict(
     interval=10,
     hooks=[
         dict(type="TextLoggerHook", by_epoch=True, ignore_last=False),
-        # dict(type='TensorboardLoggerHook')
     ],
 )
 
 dist_params = dict(backend="nccl", linear_scale_lr=False)
 
-runner = dict(type="EpochRunnerWithCancel", max_epochs=300)
+runner = dict(type="EpochRunnerWithCancel", max_epochs=100)
 
 checkpoint_config = dict(by_epoch=True, interval=1)
 
