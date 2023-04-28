6ad7b60146ddb91ecada521aec156765a9f3bbfe
Jaeguk Hyun <jaeguk.hyun@intel.com>
2023-04-28 10:54:09 +0900

    Add deterministic to training test and Add Github issue link to skip reason (#2084)
    * Add deterministic to training test and Add Github issue link to pytest skip reason
    
    * Add Github issue links to skip reason
    
    * Set deterministic False for anomaly tests
    
    * Add deterministic False to anomaly regression

--- tests/regression/anomaly/test_anomaly_detection.py
+++ tests/regression/anomaly/test_anomaly_detection.py
@@ -81,7 +81,7 @@ class TestRegressionAnomalyDetection:
 
         tmp_dir_path = tmp_dir_path / TASK_TYPE
         train_start_time = timer()
-        otx_train_testing(template, tmp_dir_path, otx_dir, category_data_args)
+        otx_train_testing(template, tmp_dir_path, otx_dir, category_data_args, deterministic=False)
         train_elapsed_time = timer() - train_start_time
 
         infer_start_time = timer()
