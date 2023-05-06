aa3c6592b1ce67d8dbd206761e01c32721caccc7
JihwanEom <jihwan.eom@intel.com>
2023-05-07 01:00:51 +0900

    Fix pre-commit issue

--- otx/algorithms/classification/configs/deit_tiny/model_hierarchical.py
+++ otx/algorithms/classification/configs/deit_tiny/model_hierarchical.py
@@ -19,5 +19,5 @@ model = dict(
     ),
 )
 
-optimizer = dict(_delete_=True, type='AdamW', lr=0.01, weight_decay=0.05)
+optimizer = dict(_delete_=True, type="AdamW", lr=0.01, weight_decay=0.05)
 optimizer_config = dict(_delete_=True)
