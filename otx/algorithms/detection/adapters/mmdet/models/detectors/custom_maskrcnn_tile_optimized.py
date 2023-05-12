9d51e99b79ae53059a2e7880554f92981937d588
Evgeny Tsykunov <evgeny.tsykunov@intel.com>
2023-05-12 22:16:22 +0900

    ActivationMapHook process backbone instead of neck output

--- otx/algorithms/detection/adapters/mmdet/models/detectors/custom_maskrcnn_tile_optimized.py
+++ otx/algorithms/detection/adapters/mmdet/models/detectors/custom_maskrcnn_tile_optimized.py
@@ -309,14 +309,16 @@ if is_mmdeploy_enabled():
         assert self.with_bbox, "Bbox head must be implemented."
         tile_prob = self.tile_classifier.simple_test(img)
 
-        x = self.extract_feat(img)
+        x = backbone_out = self.backbone(img)
+        if self.with_neck:
+            x = self.neck(x)
         if proposals is None:
             proposals, _ = self.rpn_head.simple_test_rpn(x, img_metas)
         out = self.roi_head.simple_test(x, proposals, img_metas, rescale=False)
 
         if ctx.cfg["dump_features"]:
             feature_vector = FeatureVectorHook.func(x)
-            saliency_map = ActivationMapHook.func(x[-1])
+            saliency_map = ActivationMapHook.func(backbone_out)
             return (*out, tile_prob, feature_vector, saliency_map)
 
         return (*out, tile_prob)
