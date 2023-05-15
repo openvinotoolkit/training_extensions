1d5cf0092670aa68be18c5ee7c82ecbc1ca71ae0
Galina <galina.zalesskaya@intel.com>
2023-05-15 18:26:32 +0300

    Fix async demo in exportable code

--- otx/api/usecases/exportable_code/demo/demo_package/executors/asynchronous.py
+++ otx/api/usecases/exportable_code/demo/demo_package/executors/asynchronous.py
@@ -45,6 +45,7 @@ class AsyncExecutor:
         for frame in streamer:
             results = self.async_pipeline.get_result(next_frame_id_to_show)
             while results:
+                start_time = time.perf_counter()
                 output = self.render_result(results)
                 next_frame_id_to_show += 1
                 self.visualizer.show(output)
@@ -52,6 +53,8 @@ class AsyncExecutor:
                     saved_frames.append(output)
                 if self.visualizer.is_quit():
                     stop_visualization = True
+                # visualize video not faster than the original FPS
+                self.visualizer.video_delay(time.perf_counter() - start_time, streamer)
                 results = self.async_pipeline.get_result(next_frame_id_to_show)
             if stop_visualization:
                 break
@@ -63,6 +66,8 @@ class AsyncExecutor:
             results = self.async_pipeline.get_result(next_frame_id_to_show)
             output = self.render_result(results)
             self.visualizer.show(output)
+            if self.visualizer.output:
+                saved_frames.append(output)
             # visualize video not faster than the original FPS
             self.visualizer.video_delay(time.perf_counter() - start_time, streamer)
         dump_frames(saved_frames, self.visualizer.output, input_stream, streamer)
@@ -70,6 +75,8 @@ class AsyncExecutor:
     def render_result(self, results: Tuple[Any, dict]) -> np.ndarray:
         """Render for results of inference."""
         predictions, frame_meta = results
+        predictions = np.array([[pred.id, pred.score, *pred.get_coords()] for pred in predictions])
+        predictions.shape = len(predictions), 6
         annotation_scene = self.converter.convert_to_annotation(predictions, frame_meta)
         current_frame = frame_meta["frame"]
         output = self.visualizer.draw(current_frame, annotation_scene, frame_meta)
--- otx/api/utils/vis_utils.py
+++ otx/api/utils/vis_utils.py
@@ -69,9 +69,10 @@ def dump_frames(saved_frames: list, output: str, input_path: Union[str, int], ca
         out.release()
         print(f"Video was saved to {video_path}")
     else:
-        if len(filenames) < len(saved_frames):
-            filenames = [f"output_{i}" for i, _ in enumerate(saved_frames)]
+        if len(filenames) != len(saved_frames):
+            filenames = [f"output_{i}.jpeg" for i, _ in enumerate(saved_frames)]
         for filename, frame in zip(filenames, saved_frames):
             image_path = str(output_path / filename)
+            print(image_path)
             cv2.imwrite(image_path, frame)
             print(f"Image was saved to {image_path}")
