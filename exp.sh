24bbcdc82d05e1641d772b2310025a6100398aaa
sungmanc <sungman.cho@intel.com>
2023-05-16 17:25:37 +0900

    Fix typo

--- exp.sh
+++ exp.sh
@@ -23,14 +23,14 @@ OUTPUT_PATH="outputs_256_ir_scale_4_trial_2"
 # --load-weights ${OUTPUT_PATH}/instance_segmentation_r50_maskrcnn_${DATASET}/openvino/openvino.xml \
 # --output ${OUTPUT_PATH}/instance_segmentation_r50_maskrcnn_${DATASET}
 
-# benchmark_app -m ${OUTPUT_PATH}/instance_segmentation_effnet_maskrcnn_${DATASET}/openvino/openvino.xml -api sync -niter 100 -nstreams 1 -hint none -nthreads 6
+# benchmark_app -m ${OUTPUT_PATH}/instance_segmentation_r50_maskrcnn_${DATASET}/openvino/openvino.xml -api sync -niter 100 -nstreams 1 -hint none -nthreads 6
 
 ###
 otx train otx/algorithms/detection/configs/instance_segmentation/efficientnetb2b_maskrcnn/template.yaml \
 --train-data-roots ${DATASET_PREFIX}/${DATASET} \
 --val-data-roots ${DATASET_PREFIX}/${DATASET} \
 --workspace ${OUTPUT_PATH}/instance_segmentation_effnet_maskrcnn_${DATASET} \
-params --tiling_parameters.enable_tiling 1 --tiling_parameters.tile_ir_scale_factor 4 --tiling_parameters.tile_sampling_ratio 0.1 \
+params --tiling_parameters.enable_tiling 1 --tiling_parameters.tile_ir_scale_factor 4 --tiling_parameters.tile_sampling_ratio 0.1
 
 otx eval otx/algorithms/detection/configs/instance_segmentation/efficientnetb2b_maskrcnn/template.yaml \
 --test-data-roots ${DATASET_PREFIX}/${DATASET} \
