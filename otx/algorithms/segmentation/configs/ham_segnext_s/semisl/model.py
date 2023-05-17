dcaede5794a92a3dddcc97ef255c05e317db029e
kprokofi <kirill.prokofiev@intel.com>
2023-05-17 09:21:26 +0900

    remove ohem loss: q |

--- otx/algorithms/segmentation/configs/ham_segnext_s/semisl/model.py
+++ otx/algorithms/segmentation/configs/ham_segnext_s/semisl/model.py
@@ -27,6 +27,8 @@ model = dict(
     type="MeanTeacherSegmentor",
     orig_type="OTXEncoderDecoder",
     unsup_weight=0.1,
+    proto_weight=0.1,
+    semisl_start_iter=5,
     train_cfg=dict(mix_loss=dict(enable=False, weight=0.1)),
     test_cfg=dict(mode="whole", output_scale=5.0),
     backbone=dict(embed_dims=[64, 128, 320, 512], depths=[2, 2, 4, 2], norm_cfg=dict(type="BN", requires_grad=True)),
