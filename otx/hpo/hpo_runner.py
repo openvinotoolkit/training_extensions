9cc0eb0522e2a375d877a083b1559e8281fbfcf3
eunwoosh <eunwoo.shin@intel.com>
2023-05-19 17:25:25 +0900

    change pipe to queue

--- otx/hpo/hpo_runner.py
+++ otx/hpo/hpo_runner.py
@@ -17,14 +17,27 @@
 import logging
 import multiprocessing
 import os
+import queue
+import time
+import sys
+import signal
 from copy import deepcopy
 from functools import partial
 from typing import Any, Callable, Dict, Literal, Optional, Union
+from dataclasses import dataclass
 
 from otx.hpo.hpo_base import HpoBase, Trial, TrialStatus
 from otx.hpo.resource_manager import get_resource_manager
 
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class RunningTrial:
+    """Data class for a running trial"""
+    process: multiprocessing.Process
+    trial: Trial
+    queue: multiprocessing.Queue
 
 
 class HpoLoop:
@@ -54,12 +67,17 @@ class HpoLoop:
     ):
         self._hpo_algo = hpo_algo
         self._train_func = train_func
-        self._running_trials: Dict[int, Dict] = {}
+        self._running_trials: Dict[int, RunningTrial] = {}
         self._mp = multiprocessing.get_context("spawn")
+        self._report_queue = self._mp.Queue()
         self._uid_index = 0
         self._resource_manager = get_resource_manager(
             resource_type, num_parallel_trial, num_gpu_for_single_trial, available_gpu
         )
+        self._main_pid = os.getpid()
+
+        signal.signal(signal.SIGINT, self._terminate_signal_handler)
+        signal.signal(signal.SIGTERM, self._terminate_signal_handler)
 
     def run(self):
         """Run a HPO loop."""
@@ -72,6 +90,8 @@ class HpoLoop:
 
             self._remove_finished_process()
             self._get_reports()
+
+            time.sleep(1)
 
         logger.info("HPO loop is done.")
         self._get_reports()
@@ -90,56 +110,49 @@ class HpoLoop:
             for key, val in env.items():
                 os.environ[key] = val
 
-        pipe1, pipe2 = self._mp.Pipe(True)
+        trial_queue = self._mp.Queue()
         process = self._mp.Process(
             target=_run_train,
             args=(
                 self._train_func,
                 trial.get_train_configuration(),
-                partial(_report_score, pipe=pipe2, trial_id=trial.id),
+                partial(_report_score, recv_queue=trial_queue, send_queue=self._report_queue, uid=uid),
             ),
         )
         os.environ = origin_env
-        self._running_trials[uid] = {"process": process, "trial": trial, "pipe": pipe1}
+        self._running_trials[uid] = RunningTrial(process, trial, trial_queue)
         process.start()
 
     def _remove_finished_process(self):
         trial_to_remove = []
-        for uid, val in self._running_trials.items():
-            process = val["process"]
-            if not process.is_alive():
-                val["pipe"].close()
-                process.join()
+        for uid, trial in self._running_trials.items():
+            if not trial.process.is_alive():
+                trial.queue.close()
+                trial.process.join()
                 trial_to_remove.append(uid)
 
         for uid in trial_to_remove:
-            trial = self._running_trials[uid]["trial"]
-            trial.status = TrialStatus.STOP
+            self._running_trials[uid].trial.status = TrialStatus.STOP
             self._resource_manager.release_resource(uid)
             del self._running_trials[uid]
 
     def _get_reports(self):
-        for trial in self._running_trials.values():
-            pipe = trial["pipe"]
-            if pipe.poll():
-                try:
-                    report = pipe.recv()
-                except EOFError:
-                    continue
-                trial_status = self._hpo_algo.report_score(
-                    report["score"], report["progress"], report["trial_id"], report["done"]
-                )
-                pipe.send(trial_status)
+        while not self._report_queue.empty():
+            report = self._report_queue.get_nowait()
+            trial = self._running_trials[report["uid"]]
+            trial_status = self._hpo_algo.report_score(
+                report["score"], report["progress"], trial.trial.id, report["done"]
+            )
+            trial.queue.put_nowait(trial_status)
 
         self._hpo_algo.save_results()
 
     def _join_all_processes(self):
         for val in self._running_trials.values():
-            val["pipe"].close()
+            val.queue.close()
 
         for val in self._running_trials.values():
-            process = val["process"]
-            process.join()
+            val.process.join()
 
         self._running_trials = {}
 
@@ -148,6 +161,25 @@ class HpoLoop:
         self._uid_index += 1
         return uid
 
+    def _terminate_all_running_processes(self):
+        for trial in self._running_trials.values():
+            process = trial.process
+            if process.is_alive():
+                logger.warning(f"Kill child process {process.pid}")
+                process.kill()
+
+    def _terminate_signal_handler(self, signum, _frame):
+        # This code prevents child processses from being killed unintentionally by proccesses forked from main process
+        if self._main_pid != os.getpid():
+            sys.exit()
+
+        self._terminate_all_running_processes()
+
+        singal_name = {2: "SIGINT", 15: "SIGTERM"}
+        logger.warning(f"{singal_name[signum]} is sent. process exited.")
+
+        sys.exit(1)
+
 
 def _run_train(train_func: Callable, hp_config: Dict, report_func: Callable):
     # set multi process method as default
@@ -155,16 +187,29 @@ def _run_train(train_func: Callable, hp_config: Dict, report_func: Callable):
     train_func(hp_config, report_func)
 
 
-def _report_score(score: Union[int, float], progress: Union[int, float], pipe, trial_id: Any, done: bool = False):
-    logger.debug(f"score : {score}, progress : {progress}, trial_id : {trial_id}, pid : {os.getpid()}, done : {done}")
+def _report_score(
+    score: Union[int, float],
+    progress: Union[int, float],
+    recv_queue: multiprocessing.Queue,
+    send_queue: multiprocessing.Queue,
+    uid: Any,
+    done: bool = False
+):
+    logger.debug(f"score : {score}, progress : {progress}, uid : {uid}, pid : {os.getpid()}, done : {done}")
     try:
-        pipe.send({"score": score, "progress": progress, "trial_id": trial_id, "pid": os.getpid(), "done": done})
-    except BrokenPipeError:
+        send_queue.put_nowait(
+            {"score": score, "progress": progress, "uid": uid, "pid": os.getpid(), "done": done}
+        )
+    except ValueError:
         return TrialStatus.STOP
+
     try:
-        trial_status = pipe.recv()
-    except EOFError:
-        return TrialStatus.STOP
+        trial_status = recv_queue.get(timeout=3)
+    except queue.Empty:
+        pass
+
+    while not recv_queue.empty():
+        trial_status = recv_queue.get_nowait()
 
     logger.debug(f"trial_status : {trial_status}")
     return trial_status
