ac79a7b36bca339bdc2947d2a7e9454cf1b45015
sungmanc <sungmanc@intel.com>
2023-05-22 17:43:10 +0900

    Remove some comments

--- otx/algorithms/detection/configs/instance_segmentation/resnet50_maskrcnn/deployment.py
+++ otx/algorithms/detection/configs/instance_segmentation/resnet50_maskrcnn/deployment.py
@@ -6,10 +6,4 @@ scale_ir_input = True
 
 ir_config = dict(
     output_names=["boxes", "labels", "masks"],
-    # input_shape=(1344, 800),
-)
-
-backend_config = dict(
-    # dynamic batch causes forever running openvino process
-    # model_inputs=[dict(opt_shapes=dict(input=[1, 3, 800, 1344]))],
 )
