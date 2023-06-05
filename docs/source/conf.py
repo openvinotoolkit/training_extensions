f1c562315fe9ff15e9d0bd93d85c740f425f5b29
Songki Choi <songki.choi@intel.com>
2023-06-05 10:05:55 +0900

    Fix merge error, revert to pymongo w/o version spec
    Signed-off-by: Songki Choi <songki.choi@intel.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 project = 'OpenVINO™ Training Extensions'
 copyright = '2023, OpenVINO™ Training Extensions Contributors'
 author = 'OpenVINO™ Training Extensions Contributors'
-release = '1.3.1'
+release = '1.4.0'
 
 # -- General configuration --------------------------------------------------- #
 
