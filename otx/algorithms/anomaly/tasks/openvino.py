09a57471c95d5ff7f8970536403d2be9a4127c61
Dick Ameln <dick.ameln@intel.com>
2023-06-08 02:47:28 +0200

    Assign label information to heatmap in resultmedia (#2220)
    * Assign predicted label to heatmap in torch inference
    
    * Assign predicted label to heatmap in openvino inference

--- otx/algorithms/anomaly/tasks/openvino.py
+++ otx/algorithms/anomaly/tasks/openvino.py
@@ -196,6 +196,7 @@ class OpenVINOTask(IInferenceTask, IEvaluationTask, IOptimizationTask, IDeployme
             heatmap_media = ResultMediaEntity(
                 name="Anomaly Map",
                 type="anomaly_map",
+                label=label,
                 annotation_scene=dataset_item.annotation_scene,
                 numpy=anomaly_map,
             )
