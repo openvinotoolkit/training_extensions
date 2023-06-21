bace62e3ebd7f2b4f47cb25eea8cd0e1282cba50
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-06-22 02:47:17 +0900

    Update det ut

--- tests/unit/algorithms/detection/adapters/openvino/test_task.py
+++ tests/unit/algorithms/detection/adapters/openvino/test_task.py
@@ -244,9 +244,9 @@ class TestOpenVINODetectionTask:
         output_model = copy.deepcopy(otx_model)
         self.ov_task.model.set_data("openvino.bin", b"foo")
         self.ov_task.model.set_data("openvino.xml", b"bar")
-        mocker.patch("otx.algorithms.segmentation.adapters.openvino.task.ov.Core.read_model", autospec=True)
-        mocker.patch("otx.algorithms.segmentation.adapters.openvino.task.ov.serialize", new=patch_save_model)
-        fake_quantize = mocker.patch("otx.algorithms.segmentation.adapters.openvino.task.nncf.quantize", autospec=True)
+        mocker.patch("otx.algorithms.detection.adapters.openvino.task.ov.Core.read_model", autospec=True)
+        mocker.patch("otx.algorithms.detection.adapters.openvino.task.ov.serialize", new=patch_save_model)
+        fake_quantize = mocker.patch("otx.algorithms.detection.adapters.openvino.task.nncf.quantize", autospec=True)
         self.ov_task.optimize(OptimizationType.POT, dataset=dataset, output_model=output_model)
 
         fake_quantize.assert_called_once()
