dd06f2d70c2b1fb80e35b27f6e6aed99132ab5be
Kim, Sungchul <sungchul.kim@intel.com>
2023-07-02 23:37:56 +0900

    Add sam ir paths

--- src/otx/cli/utils/io.py
+++ src/otx/cli/utils/io.py
@@ -47,6 +47,10 @@ model_adapter_keys = (
     "config.json",
     "tile_classifier.xml",
     "tile_classifier.bin",
+    "sam_image_encoder.xml",
+    "sam_image_encoder.bin",
+    "sam_decoder.xml",
+    "sam_decoder.bin",
 )
 
 
