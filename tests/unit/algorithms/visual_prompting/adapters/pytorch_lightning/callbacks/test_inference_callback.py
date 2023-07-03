e60374e8f01015230d77dec7c1356b42cc4e45c6
Kim, Sungchul <sungchul.kim@intel.com>
2023-07-03 10:50:01 +0900

    Fix #iou_predictions

--- tests/unit/algorithms/visual_prompting/adapters/pytorch_lightning/callbacks/test_inference_callback.py
+++ tests/unit/algorithms/visual_prompting/adapters/pytorch_lightning/callbacks/test_inference_callback.py
@@ -74,7 +74,7 @@ class TestInferenceCallback:
             [
                 {
                     "masks": [torch.Tensor([[[0, 1, 0], [1, 1, 1], [0, 1, 0]]])],
-                    "iou_predictions": [torch.Tensor([0.9, 0.8, 0.7])],
+                    "iou_predictions": [torch.Tensor([[0.9]])],
                     "labels": [
                         [ScoredLabel(label=LabelEntity("foreground", domain=Domain.VISUAL_PROMPTING), probability=0.0)],
                     ],
