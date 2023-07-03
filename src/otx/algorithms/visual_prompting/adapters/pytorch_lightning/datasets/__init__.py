38d2c3857dce060e27e5003bee48ec30642724a0
Kim, Sungchul <sungchul.kim@intel.com>
2023-07-03 23:02:33 +0900

    Fix typo

--- src/otx/algorithms/visual_prompting/adapters/pytorch_lightning/datasets/__init__.py
+++ src/otx/algorithms/visual_prompting/adapters/pytorch_lightning/datasets/__init__.py
@@ -3,10 +3,10 @@
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from .dataset import OTXVisualPromptingDataModule, OTXVIsualPromptingDataset
+from .dataset import OTXVisualPromptingDataModule, OTXVisualPromptingDataset
 from .pipelines import MultipleInputsCompose, Pad, ResizeLongestSide
 
 __all__ = [
-    "OTXVisualPromptingDataModule", "OTXVIsualPromptingDataset",
+    "OTXVisualPromptingDataModule", "OTXVisualPromptingDataset",
     "ResizeLongestSide", "MultipleInputsCompose", "Pad"
 ]
