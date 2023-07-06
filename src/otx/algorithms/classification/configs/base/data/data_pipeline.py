e15a7adf8c44bbaeb3e66bbe9e49acd2a263afaf
sungmanc <sungman.cho@intel.com>
2023-07-06 19:49:22 +0900

    Change the interpolation type: bicubic --> default

--- src/otx/algorithms/classification/configs/base/data/data_pipeline.py
+++ src/otx/algorithms/classification/configs/base/data/data_pipeline.py
@@ -20,7 +20,7 @@ __img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375
 __resize_target_size = 224
 
 __train_pipeline = [
-    dict(type="RandomResizedCrop", size=224, efficientnet_style=True, interpolation="bicubic"),
+    dict(type="RandomResizedCrop", size=224, efficientnet_style=True),
     dict(type="RandomFlip", flip_prob=0.5, direction="horizontal"),
     dict(type="Normalize", **__img_norm_cfg),
     dict(type="ImageToTensor", keys=["img"]),
