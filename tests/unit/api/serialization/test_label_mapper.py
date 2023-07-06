4fc2e8dfdc5c5b0f3f04e9be1bcf75283b4832cb
Songki Choi <songki.choi@intel.com>
2023-07-06 11:14:37 +0900

    Fix 'None' node issue in label schema mapping in case of label deletion (#2300)
    Signed-off-by: Songki Choi <songki.choi@intel.com>

--- tests/unit/api/serialization/test_label_mapper.py
+++ tests/unit/api/serialization/test_label_mapper.py
@@ -374,3 +374,30 @@ class TestLabelGraphMapper:
         }
         with pytest.raises(ValueError):
             LabelGraphMapper.backward(instance=forward, all_labels=labels)
+        # Checking label deletion case
+        forward = {
+            "type": "tree",
+            "directed": True,
+            "nodes": ["0", "0_1", "0_2", "0_1_1", "0_2_1"],
+            "edges": [("0_1", "0"), ("0_2", "0"), ("0_1_1", "0_1"), ("0_2_1", "0_2")],
+        }
+        labels = {
+            ID("0"): self.label_0,
+            ID("0_1"): self.label_0_1,
+            # ID("0_2"): self.label_0_2,
+            ID("0_1_1"): self.label_0_1_1,
+            # ID("0_1_2"): self.label_0_1_2,
+            ID("0_2_1"): self.label_0_2_1,
+        }
+        expected_backward = LabelTree()
+        for node in labels.values():
+            expected_backward.add_node(node)
+        for parent, child in [
+            (self.label_0, self.label_0_1),
+            # (self.label_0, self.label_0_2),
+            (self.label_0_1, self.label_0_1_1),
+            # (self.label_0_2, self.label_0_2_1),
+        ]:
+            expected_backward.add_child(parent, child)
+        actual_backward = LabelGraphMapper.backward(instance=forward, all_labels=labels)
+        assert LabelGraphMapper.forward(actual_backward) == LabelGraphMapper.forward(expected_backward)
