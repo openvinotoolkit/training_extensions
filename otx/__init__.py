202002c16dd803414c02ba4988b1637e77687e0e
Yunchu Lee <yunchu.lee@intel.com>
2023-07-11 11:58:15 +0900

    update version and release note

--- otx/__init__.py
+++ otx/__init__.py
@@ -3,5 +3,5 @@
 # Copyright (C) 2021-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = "1.2.4rc1"
+__version__ = "1.2.4"
 # NOTE: Sync w/ otx/api/usecases/exportable_code/demo/requirements.txt on release
