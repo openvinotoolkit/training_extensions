ca5f74d58f8e367c9cf725de2541964916684460
Kim, Sungchul <sungchul.kim@intel.com>
2023-07-14 17:11:02 +0900

    Remove custom modelapi patch in visual prompting (#2359)
    * Remove custom modelapi patch
    
    * Update test

--- src/otx/algorithms/visual_prompting/adapters/openvino/model_wrappers/__init__.py
+++ src/otx/algorithms/visual_prompting/adapters/openvino/model_wrappers/__init__.py
@@ -14,5 +14,4 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
-from .openvino_adapters import VisualPromptingOpenvinoAdapter  # noqa: F401
 from .openvino_models import Decoder, ImageEncoder  # noqa: F401
