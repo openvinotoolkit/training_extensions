03f80b50064b2b3149fbcfe416a8d9caa37a873a
Kim, Sungchul <sungchul.kim@intel.com>
2023-07-24 16:16:34 +0900

    Update tests

--- tests/e2e/cli/visual_prompting/test_visual_prompting.py
+++ tests/e2e/cli/visual_prompting/test_visual_prompting.py
@@ -73,11 +73,11 @@ class TestToolsVisualPrompting:
     @pytest.mark.parametrize("template", templates, ids=templates_ids)
     def test_otx_train(self, template, tmp_dir_path):
         tmp_dir_path = tmp_dir_path / "visual_prompting"
-        otx_train_testing(template, tmp_dir_path, otx_dir, args, deterministic=False)
+        otx_train_testing(template, tmp_dir_path, otx_dir, args, deterministic=True)
         template_work_dir = get_template_dir(template, tmp_dir_path)
         args1 = copy.deepcopy(args)
         args1["--load-weights"] = f"{template_work_dir}/trained_{template.model_template_id}/models/weights.pth"
-        otx_train_testing(template, tmp_dir_path, otx_dir, args1, deterministic=False)
+        otx_train_testing(template, tmp_dir_path, otx_dir, args1, deterministic=True)
 
     @e2e_pytest_component
     @pytest.mark.skipif(TT_STABILITY_TESTS, reason="This is TT_STABILITY_TESTS")
