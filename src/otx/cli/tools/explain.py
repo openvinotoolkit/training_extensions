550789bf6dda89123ca27ec20aff405ef16dc220
Jaeguk Hyun <jaeguk.hyun@intel.com>
2023-07-27 10:06:40 +0900

    Enable configurable confidence threshold for ov export and inference (#2388)
    * Enable configurable confidence threshold for ov export and inference
    
    * Update unit tests
    
    * Update task.py
    
    * Update CHANGELOG.md and docs
    
    * Reflect reviews
    
    * Extract getting override params part
    
    ---------
    
    Co-authored-by: Sungman Cho <sungman.cho@intel.com>

--- src/otx/cli/tools/explain.py
+++ src/otx/cli/tools/explain.py
@@ -33,6 +33,7 @@ from otx.cli.utils.io import (
 from otx.cli.utils.nncf import is_checkpoint_nncf
 from otx.cli.utils.parser import (
     add_hyper_parameters_sub_parser,
+    get_override_param,
     get_parser_and_hprams_data,
 )
 
@@ -88,7 +89,7 @@ def get_args():
         help="Weight of the saliency map when overlaying the input image with saliency map.",
     )
     add_hyper_parameters_sub_parser(parser, hyper_parameters, modes=("INFERENCE",))
-    override_param = [f"params.{param[2:].split('=')[0]}" for param in params if param.startswith("--")]
+    override_param = get_override_param(params)
 
     return parser.parse_args(), override_param
 
