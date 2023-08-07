25749bd4cd26a619526770e6fe0f3cdf0bd86b76
Eugene Liu <eugene.liu@intel.com>
2023-08-07 12:00:16 +0100

    remove maskrcnn swin t

--- src/otx/algorithms/detection/configs/instance_segmentation/maskrcnn_swin_s/deployment.py
+++
@@ -1,9 +0,0 @@
-"""MMDployment config of MaskRCNN-SwinS-FP16 model for Instance-Seg Task."""
-
-_base_ = ["../../base/deployments/base_instance_segmentation_dynamic.py"]
-
-ir_config = dict(
-    output_names=["boxes", "labels", "masks"],
-)
-
-scale_ir_input = False
