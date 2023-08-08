378056771fa3ad181ece531c0085bd7d029afac2
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2023-08-08 09:46:46 +0200

    Refactor semantic segmentation inference to use MAPI (#2421)
    * Update sseg ov inference
    
    * Update requirements
    
    * Fix black

--- src/otx/algorithms/segmentation/adapters/openvino/model_wrappers/__init__.py
+++ src/otx/algorithms/segmentation/adapters/openvino/model_wrappers/__init__.py
@@ -13,7 +13,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions
 # and limitations under the License.
-
-from .blur import BlurSegmentation
-
-__all__ = ["BlurSegmentation"]
