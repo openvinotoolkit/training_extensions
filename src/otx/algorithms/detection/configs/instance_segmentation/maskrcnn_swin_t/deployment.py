e85ee761908ea75005d9666189ac47964f2f9861
Eugene Liu <eugene.liu@intel.com>
2023-08-08 15:31:06 +0100

    update opset version

--- src/otx/algorithms/detection/configs/instance_segmentation/maskrcnn_swin_t/deployment.py
+++ src/otx/algorithms/detection/configs/instance_segmentation/maskrcnn_swin_t/deployment.py
@@ -4,7 +4,9 @@ _base_ = ["../../base/deployments/base_instance_segmentation_dynamic.py"]
 
 ir_config = dict(
     output_names=["boxes", "labels", "masks"],
-    opset_version=16,
+    # NOTE: Its necessary to use opset 11 as squeeze does not work in
+    # roi_align_default ONNX in opset 16. Beaware of this for future update.
+    opset_version=11,
 )
 
 scale_ir_input = False
