6f62494d0aa0d98fd2d39d77c1fccb032ccec9c8
Eugene Liu <eugene.liu@intel.com>
2023-08-09 18:34:34 +0100

    add test

--- tests/unit/algorithms/detection/adapters/mmdet/utils/test_detection_config_utils.py
+++ tests/unit/algorithms/detection/adapters/mmdet/utils/test_detection_config_utils.py
@@ -6,6 +6,8 @@
 import pytest
 from mmcv.utils import Config
 
+from pathlib import Path
+
 from otx.algorithms.detection.adapters.mmdet.utils.config_utils import (
     cluster_anchors,
     patch_datasets,
@@ -17,6 +19,17 @@ from tests.test_suite.e2e_test_system import e2e_pytest_unit
 from tests.unit.algorithms.detection.test_helpers import (
     generate_det_dataset,
 )
+
+from otx.algorithms.common.adapters.mmcv.utils.config_utils import MPAConfig
+
+from tests.unit.algorithms.detection.test_helpers import (
+    DEFAULT_DET_MODEL_CONFIG_PATH,
+    DEFAULT_ISEG_MODEL_CONFIG_PATH,
+)
+from otx.api.configuration.helper import create
+from otx.api.entities.model_template import parse_model_template
+
+from otx.algorithms.detection.adapters.mmdet.utils import patch_samples_per_gpu
 
 
 @e2e_pytest_unit
@@ -120,3 +133,17 @@ def test_cluster_anchors(widths, heights):
     )
     dataset, _ = generate_det_dataset(task_type=TaskType.DETECTION)
     cluster_anchors(recipe_config, dataset)
+
+
+@e2e_pytest_unit
+@pytest.mark.parametrize("model_cfg", [DEFAULT_DET_MODEL_CONFIG_PATH, DEFAULT_ISEG_MODEL_CONFIG_PATH])
+def test_patch_samples_per_gpu(model_cfg):
+    """Test that patch_samples_per_gpu function works correctly."""
+    cfg = MPAConfig.fromfile(model_cfg)
+    model_template = parse_model_template(Path(model_cfg).parent / "template.yaml")
+    hyper_parameters = create(model_template.hyper_parameters.data)
+
+    patch_samples_per_gpu(cfg, hyper_parameters)
+    params = hyper_parameters.learning_parameters
+    assert cfg.data.val_dataloader.samples_per_gpu == params.inference_batch_size
+    assert cfg.data.test_dataloader.samples_per_gpu == params.inference_batch_size
