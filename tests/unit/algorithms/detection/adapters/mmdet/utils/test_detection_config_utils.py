892c4b43fe2926babfc5ae0a0055db442708e5b0
jaegukhyun <jaeguk.hyun@intel.com>
2023-08-10 16:29:25 +0900

    Fix unit tests

--- tests/unit/algorithms/detection/adapters/mmdet/utils/test_detection_config_utils.py
+++ tests/unit/algorithms/detection/adapters/mmdet/utils/test_detection_config_utils.py
@@ -8,7 +8,6 @@ from mmcv.utils import Config
 
 from otx.algorithms.detection.adapters.mmdet.utils.config_utils import (
     cluster_anchors,
-    patch_datasets,
     should_cluster_anchors,
 )
 from otx.api.entities.model_template import TaskType
@@ -16,22 +15,6 @@ from tests.test_suite.e2e_test_system import e2e_pytest_unit
 from tests.unit.algorithms.detection.test_helpers import (
     generate_det_dataset,
 )
-
-
-@e2e_pytest_unit
-def test_patch_datasets():
-    """Test patch_datasets function."""
-    config = Config(
-        dict(
-            data=dict(train=dict(), val=dict(), test=dict(), unlabeled=dict()),
-        )
-    )
-    patch_datasets(config, type="FakeType")
-    data_cfg = config.data
-    assert "domain" in data_cfg.train
-    assert "domain" in data_cfg.val
-    assert "domain" in data_cfg.test
-    assert "domain" in data_cfg.unlabeled
 
 
 @e2e_pytest_unit
