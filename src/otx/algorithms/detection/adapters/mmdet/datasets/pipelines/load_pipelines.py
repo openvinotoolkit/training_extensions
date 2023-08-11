8af132b123d1d9e4a6ce7b602f4fd71aba536ebd
jaegukhyun <jaeguk.hyun@intel.com>
2023-08-11 13:41:29 +0900

    Fix Pylint

--- src/otx/algorithms/detection/adapters/mmdet/datasets/pipelines/load_pipelines.py
+++ src/otx/algorithms/detection/adapters/mmdet/datasets/pipelines/load_pipelines.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 import copy
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
 from mmdet.datasets.builder import PIPELINES
 
