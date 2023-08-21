d1d006e353eed9f47c0c4689e4b3305ef3322c2e
Eugene Liu <eugene.liu@intel.com>
2023-08-21 11:07:08 +0100

    Add MaskRCNN SwinTransformer-T (#2423)
    * add maskrcnn swin-transformer
    
    * Update src/otx/algorithms/detection/configs/instance_segmentation/maskrcnn_swin_t/hpo_config.yaml
    
    Co-authored-by: Eunwoo Shin <eunwoo.shin@intel.com>
    
    * patch samples per gpu in config utils
    
    ---------
    
    Co-authored-by: Eunwoo Shin <eunwoo.shin@intel.com>

--- tests/unit/algorithms/detection/adapters/mmdet/utils/test_detection_config_utils.py
+++ tests/unit/algorithms/detection/adapters/mmdet/utils/test_detection_config_utils.py
@@ -6,6 +6,8 @@
 import pytest
 from mmcv.utils import Config
 
+from pathlib import Path
+
 from otx.algorithms.detection.adapters.mmdet.utils.config_utils import (
     cluster_anchors,
     should_cluster_anchors,
@@ -15,6 +17,15 @@ from tests.test_suite.e2e_test_system import e2e_pytest_unit
 from tests.unit.algorithms.detection.test_helpers import (
     generate_det_dataset,
 )
+
+from otx.algorithms.common.adapters.mmcv.utils.config_utils import MPAConfig, patch_from_hyperparams
+
+from tests.unit.algorithms.detection.test_helpers import (
+    DEFAULT_DET_MODEL_CONFIG_PATH,
+    DEFAULT_ISEG_MODEL_CONFIG_PATH,
+)
+from otx.api.configuration.helper import create
+from otx.api.entities.model_template import parse_model_template
 
 
 @e2e_pytest_unit
@@ -56,3 +67,17 @@ def test_cluster_anchors(widths, heights):
     )
     dataset, _ = generate_det_dataset(task_type=TaskType.DETECTION)
     cluster_anchors(recipe_config, dataset)
+
+
+@e2e_pytest_unit
+@pytest.mark.parametrize("model_cfg", [DEFAULT_DET_MODEL_CONFIG_PATH, DEFAULT_ISEG_MODEL_CONFIG_PATH])
+def test_patch_samples_per_gpu(model_cfg):
+    """Test samples per gpu function works correctly."""
+    cfg = MPAConfig.fromfile(model_cfg)
+    model_template = parse_model_template(Path(model_cfg).parent / "template.yaml")
+    hyper_parameters = create(model_template.hyper_parameters.data)
+
+    patch_from_hyperparams(cfg, hyper_parameters)
+    params = hyper_parameters.learning_parameters
+    assert cfg.data.val_dataloader.samples_per_gpu == params.inference_batch_size
+    assert cfg.data.test_dataloader.samples_per_gpu == params.inference_batch_size
