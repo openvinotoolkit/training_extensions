4a986fd6117c1926b15397a3ce5f27191994f45e
Ashwin Vaidya <ashwinitinvaidya@gmail.com>
2023-08-24 14:19:24 +0200

    Specific exceptions

--- src/otx/algorithms/anomaly/tasks/openvino.py
+++ src/otx/algorithms/anomaly/tasks/openvino.py
@@ -221,7 +221,7 @@ class OpenVINOTask(IInferenceTask, IEvaluationTask, IOptimizationTask, IDeployme
                 metadata = json.loads(self.task_environment.model.get_data("metadata").decode())
                 self._populate_metadata(metadata)
                 logger.info("Metadata loaded from model v1.4.")
-            except Exception:
+            except (KeyError, json.decoder.JSONDecodeError):
                 # model is from version 1.2.x
                 metadata = self._populate_metadata_legacy(self.task_environment.model)
                 logger.info("Metadata loaded from model v1.2.x.")
