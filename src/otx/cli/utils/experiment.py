e2428d9eeb32c77e89b0c28821b7c68a07faa437
eunwoosh <eunwoo.shin@intel.com>
2023-09-01 18:23:02 +0900

    refine resource tracker

--- src/otx/cli/utils/experiment.py
+++ src/otx/cli/utils/experiment.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 import pynvml
 import psutil
 import yaml
+import os
 from contextlib import ExitStack
 from typing import Union
 from pathlib import Path
@@ -13,8 +14,10 @@ def run_process_to_check_resource(output_dir: Union[str, Path], exit_stack: Exit
     if isinstance(output_dir, str):
         output_dir = Path(output_dir)
 
+    gpu_used = os.environ.get("CUDA_VISIBLE_DEVICES", 0)
+
     queue = mp.Queue()
-    mem_check_p = mp.Process(target=check_resource, args=(queue,))
+    mem_check_p = mp.Process(target=check_resource, args=(queue, gpu_used))
     mem_check_p.start()
 
     exit_stack.callback(mem_check_proc_callback, mem_check_p, queue, output_dir)
@@ -28,9 +31,9 @@ def mem_check_proc_callback(mem_check_p, queue, output_dir):
     mem_check_p.close()
 
 
-def check_resource(queue: mp.Queue):
+def check_resource(queue: mp.Queue, gpu_idx: int = 0):
     pynvml.nvmlInit()
-    handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+    handle = pynvml.nvmlDeviceGetHandleByIndex(gpu_idx)
     max_gpu_mem = 0
     avg_gpu_util = 0
     max_cpu_mem = 0
@@ -52,12 +55,14 @@ def check_resource(queue: mp.Queue):
             max_gpu_mem = mem_used
 
         # cpu mem
-        cpu_mem = psutil.virtual_memory()[3] / gib
+        # cpu_mem = psutil.virtual_memory()[3] / gib
+        # cpu_mem = target_process.memory_percent()
+        cpu_mem = target_process.memory_info().rss / gib
         if max_cpu_mem < cpu_mem:
             max_cpu_mem = cpu_mem
 
         # cpu util
-        cpu_percent = psutil.cpu_percent()
+        cpu_percent = target_process.cpu_percent()
         avg_cpu_util += cpu_percent
 
         if not queue.empty():
@@ -77,4 +82,3 @@ def check_resource(queue: mp.Queue):
             f,
             default_flow_style=False
         )
-
