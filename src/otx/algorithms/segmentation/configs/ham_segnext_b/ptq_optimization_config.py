01bb34752763532fd0be4b499ccf3eb7dfa64070
Nikita Savelyev <nikita.savelyev@intel.com>
2023-09-04 13:20:35 +0200

    Added configs for segnext t-s-b

---
+++ src/otx/algorithms/segmentation/configs/ham_segnext_b/ptq_optimization_config.py
@@ -0,0 +1,15 @@
+"""PTQ config file."""
+from nncf import IgnoredScope
+
+
+ignored_scope = IgnoredScope(
+    patterns=[
+        "/hamburger/"
+    ],
+    types=[
+        "Add",
+        "MVN",
+        "Divide",
+        "Multiply",
+    ]
+)
