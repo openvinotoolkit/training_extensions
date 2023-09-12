aeb3ba0196ddbbb9894ccfc695322e907f4b20b0
Prokofiev Kirill <kirill.prokofiev@intel.com>
2023-09-12 10:17:53 +0200

    Align batch size in instance segmentation task for Semi-SL to avoid OOM error (#2493)
    align batch size to avoid OOM

--- tests/integration/cli/instance_segmentation/test_instance_segmentation.py
+++ tests/integration/cli/instance_segmentation/test_instance_segmentation.py
@@ -42,7 +42,7 @@ args_semisl = {
     "--test-data-roots": "tests/assets/car_tree_bug",
     "--unlabeled-data-roots": "tests/assets/car_tree_bug",
     "--input": "tests/assets/car_tree_bug/images/train",
-    "train_params": ["params", "--learning_parameters.num_iters", "1", "--learning_parameters.batch_size", "1"],
+    "train_params": ["params", "--learning_parameters.num_iters", "1", "--learning_parameters.batch_size", "2"],
 }
 
 # Training params for resume, num_iters*2
