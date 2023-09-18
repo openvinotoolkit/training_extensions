48080a76e655e0ad6c8836fee8b34e011ae7b418
Eugene Liu <eugene.liu@intel.com>
2023-09-18 17:00:29 +0100

    update polygon validation func

--- src/otx/core/data/adapter/base_dataset_adapter.py
+++ src/otx/core/data/adapter/base_dataset_adapter.py
@@ -272,15 +272,21 @@ class BaseDatasetAdapter(metaclass=abc.ABCMeta):
 
         return {"category_items": category_items, "label_groups": label_groups, "label_entities": label_entities}
 
-    def _is_normal_polygon(self, annotation: DatumAnnotationType.polygon) -> bool:
-        """To filter out the abnormal polygon."""
+    def _is_normal_polygon(self, annotation: DatumAnnotationType.polygon, width: int, height: int) -> bool:
         x_points = [annotation.points[i] for i in range(0, len(annotation.points), 2)]
         y_points = [annotation.points[i + 1] for i in range(0, len(annotation.points), 2)]
-        return min(x_points) < max(x_points) and min(y_points) < max(y_points)
+        return (
+            min(x_points) < max(x_points)
+            and min(y_points) < max(y_points)
+            and min(x_points) >= 0
+            and min(y_points) >= 0
+            and max(x_points) < width
+            and max(y_points) < height
+        )
 
-    def _is_normal_bbox(self, x1: float, y1: float, x2: float, y2: float) -> bool:
+    def _is_normal_bbox(self, x1: float, y1: float, x2: float, y2: float, width: int, height: int) -> bool:
         """To filter out the abrnormal bbox."""
-        return x1 < x2 and y1 < y2
+        return x1 < x2 and y1 < y2 and x1 >= 0 and y1 >= 0 and x2 < width and y2 < height
 
     def _select_data_type(self, data_candidates: Union[List[str], str]) -> str:
         """Select specific type among candidates.
