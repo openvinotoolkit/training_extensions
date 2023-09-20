ef6c0abe5a199c5e2fa01623331f616d021c41db
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-09-21 06:01:31 +0900

    Cleanup in cls train runner

--- src/otx/algorithms/classification/adapters/mmcls/apis/train.py
+++ src/otx/algorithms/classification/adapters/mmcls/apis/train.py
@@ -1,71 +1,17 @@
+"""Train function for classification task."""
+
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
 # Copyright (c) OpenMMLab. All rights reserved.
-import random
 import warnings
 
-import numpy as np
 import torch
-import torch.distributed as dist
 from mmcls.core import DistEvalHook, DistOptimizerHook, EvalHook
 from mmcls.datasets import build_dataloader, build_dataset
-from mmcls.utils import auto_select_device, get_root_logger, wrap_distributed_model, wrap_non_distributed_model
-from mmcv.runner import DistSamplerSeedHook, Fp16OptimizerHook, build_optimizer, build_runner, get_dist_info
+from mmcls.utils import get_root_logger, wrap_distributed_model, wrap_non_distributed_model
+from mmcv.runner import DistSamplerSeedHook, Fp16OptimizerHook, build_optimizer, build_runner
 from otx.algorithms.common.adapters.mmcv.utils import XPUDataParallel
-from otx.algorithms.common.utils import is_xpu_available
-
-
-def init_random_seed(seed=None, device=None):
-    """Initialize random seed.
-
-    If the seed is not set, the seed will be automatically randomized,
-    and then broadcast to all processes to prevent some potential bugs.
-
-    Args:
-        seed (int, Optional): The seed. Default to None.
-        device (str): The device where the seed will be put on.
-            Default to 'cuda'.
-
-    Returns:
-        int: Seed to be used.
-    """
-    if seed is not None:
-        return seed
-    if device is None:
-        device = auto_select_device()
-    # Make sure all ranks share the same random seed to prevent
-    # some potential bugs. Please refer to
-    # https://github.com/open-mmlab/mmdetection/issues/6339
-    rank, world_size = get_dist_info()
-    seed = np.random.randint(2**31)
-    if world_size == 1:
-        return seed
-
-    if rank == 0:
-        random_num = torch.tensor(seed, dtype=torch.int32, device=device)
-    else:
-        random_num = torch.tensor(0, dtype=torch.int32, device=device)
-    dist.broadcast(random_num, src=0)
-    return random_num.item()
-
-
-def set_random_seed(seed, deterministic=False):
-    """Set random seed.
-
-    Args:
-        seed (int): Seed to be used.
-        deterministic (bool): Whether to set the deterministic option for
-            CUDNN backend, i.e., set `torch.backends.cudnn.deterministic`
-            to True and `torch.backends.cudnn.benchmark` to False.
-            Default: False.
-    """
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    if is_xpu_available():
-        torch.xpu.manual_seed_all(seed)
-    if deterministic:
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
 
 
 def train_model(model, dataset, cfg, distributed=False, validate=False, timestamp=None, device=None, meta=None):
@@ -126,12 +72,11 @@ def train_model(model, dataset, cfg, distributed=False, validate=False, timestam
         model = wrap_distributed_model(
             model, cfg.device, broadcast_buffers=False, find_unused_parameters=find_unused_parameters
         )
+    elif cfg.device == "xpu":
+        assert len(cfg.gpu_ids) == 1
+        model = XPUDataParallel(model.xpu(), dim=0, device_ids=cfg.gpu_ids)
     else:
-        if cfg.device == "xpu":
-            assert len(cfg.gpu_ids) == 1
-            model = XPUDataParallel(model.xpu(), dim=0, device_ids=cfg.gpu_ids)
-        else:
-            model = wrap_non_distributed_model(model, cfg.device, device_ids=cfg.gpu_ids)
+        model = wrap_non_distributed_model(model, cfg.device, device_ids=cfg.gpu_ids)
 
     # build runner
     optimizer = build_optimizer(model, cfg.optimizer)
