a09cc65b675ee080824a79574cd99ed2ccbb7d84
Songki Choi <songki.choi@intel.com>
2023-09-25 14:31:48 +0900

    Apply resize-cache to all segmentation pipelines
    Signed-off-by: Songki Choi <songki.choi@intel.com>

--- src/otx/algorithms/segmentation/configs/base/data/semisl/data_pipeline.py
+++ src/otx/algorithms/segmentation/configs/base/data/semisl/data_pipeline.py
@@ -1,12 +1,23 @@
 """Data Pipeline for Semi-SL model of Segmentation Task."""
 
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
 __img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 __img_scale = (544, 544)
 __crop_size = (512, 512)
 
 __common_pipeline = [
-    dict(type="LoadImageFromOTXDataset", enable_memcache=True),
-    dict(type="LoadAnnotationFromOTXDataset"),
+    dict(
+        type="LoadResizeDataFromOTXDataset",
+        load_ann_cfg=dict(type="LoadAnnotationFromOTXDataset"),
+        resize_cfg=dict(
+            type="Resize",
+            img_scale=__img_scale,
+            downscale_only=True,
+        ),  # Resize to intermediate size if org image is bigger
+        enable_memcache=True,  # Cache after resizing image & annotations
+    ),
     dict(type="Resize", img_scale=__img_scale, ratio_range=(0.5, 2.0), keep_ratio=False),
     dict(type="RandomCrop", crop_size=__crop_size, cat_max_ratio=0.75),
     dict(type="RandomFlip", prob=0.5, direction="horizontal"),
@@ -32,6 +43,32 @@ train_pipeline = [
             "flip_direction",
             "ignored_labels",
             "img_shape",
+        ],
+    ),
+]
+
+val_pipeline = [
+    dict(
+        type="LoadResizeDataFromOTXDataset",
+        resize_cfg=dict(
+            type="Resize",
+            img_scale=__img_scale,
+            keep_ratio=False,
+            downscale_only=False,
+        enable_memcache=True,  # Cache after resizing image & annotations
+    ),
+    dict(
+        type="MultiScaleFlipAug",
+        img_scale=__img_scale,
+        flip=False,
+        transforms=[
+            dict(type="RandomFlip"),
+            dict(type="Normalize", **__img_norm_cfg),
+            dict(type="ImageToTensor", keys=["img"]),
+            dict(
+                type="Collect",
+                keys=["img"],
+            ),
         ],
     ),
 ]
@@ -84,7 +121,7 @@ unlabeled_pipeline = [
 
 data = dict(
     train=dict(type="OTXSegDataset", pipeline=train_pipeline),
-    val=dict(type="OTXSegDataset", pipeline=test_pipeline),
+    val=dict(type="OTXSegDataset", pipeline=val_pipeline),
     test=dict(type="OTXSegDataset", pipeline=test_pipeline),
     unlabeled=dict(type="OTXSegDataset", pipeline=unlabeled_pipeline),
 )
