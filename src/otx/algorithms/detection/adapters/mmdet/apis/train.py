ee940a6885ed0750611906e628583aed3050d4c7
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-10-07 08:39:34 +0900

    Update NMS patch

--- src/otx/algorithms/detection/adapters/mmdet/apis/train.py
+++ src/otx/algorithms/detection/adapters/mmdet/apis/train.py
@@ -24,6 +24,10 @@ from mmdet.utils import build_ddp, compat_cfg, find_latest_checkpoint, get_root_
 from mmdet.utils.util_distribution import build_dp, dp_factory
 from otx.algorithms.common.adapters.mmcv.utils import XPUDataParallel
 
+from torchvision.ops import roi_align as tv_roi_align
+from torchvision.ops import nms as tv_nms
+
+
 ext_module = ext_loader.load_ext("_ext", ["nms", "softnms", "nms_match", "nms_rotated", "nms_quadri"])
 dp_factory["xpu"] = XPUDataParallel
 
@@ -201,18 +205,23 @@ def train_detector(model, dataset, cfg, distributed=False, validate=False, times
 
 
 def monkey_patched_xpu_nms(ctx, bboxes, scores, iou_threshold, offset, score_threshold, max_num):
-    """Forces NMS from MMCV to run on CPU."""
+    """Runs MMCVs NMS with torchvision.nms, or forces NMS from MMCV to run on CPU."""
     is_filtering_by_score = score_threshold > 0
     if is_filtering_by_score:
         valid_mask = scores > score_threshold
         bboxes, scores = bboxes[valid_mask], scores[valid_mask]
         valid_inds = torch.nonzero(valid_mask, as_tuple=False).squeeze(dim=1)
-    device = bboxes.device
-    bboxes = bboxes.to("cpu")
-    scores = scores.to("cpu")
-    inds = ext_module.nms(bboxes, scores, iou_threshold=float(iou_threshold), offset=offset)
-    bboxes = bboxes.to(device)
-    scores = scores.to(device)
+
+    if offset == 0:
+        inds = tv_nms(bboxes, scores, float(iou_threshold))
+    else:
+        device = bboxes.device
+        bboxes = bboxes.to("cpu")
+        scores = scores.to("cpu")
+        inds = ext_module.nms(bboxes, scores, iou_threshold=float(iou_threshold), offset=offset)
+        bboxes = bboxes.to(device)
+        scores = scores.to(device)
+
     if max_num > 0:
         inds = inds[:max_num]
     if is_filtering_by_score:
@@ -221,15 +230,13 @@ def monkey_patched_xpu_nms(ctx, bboxes, scores, iou_threshold, offset, score_thr
 
 
 def monkey_patched_xpu_roi_align(self, input, rois):
-    """Replaces MMCVs roi align withe the one from torchvision.
+    """Replaces MMCVs roi align with the one from torchvision.
 
     Args:
         self: patched instance
         input: NCHW images
         rois: Bx5 boxes. First column is the index into N. The other 4 columns are xyxy.
     """
-
-    from torchvision.ops import roi_align as tv_roi_align
 
     if "aligned" in tv_roi_align.__code__.co_varnames:
         return tv_roi_align(input, rois, self.output_size, self.spatial_scale, self.sampling_ratio, self.aligned)
