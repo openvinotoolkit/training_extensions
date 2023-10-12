5f9fc1361a46ce18fc6341f5ee1684c13a23e1b1
kprokofi <kirill.prokofiev@intel.com>
2023-10-13 06:48:58 +0900

    deleted unnecessery files

--- src/otx/algorithms/detection/configs/detection/fasterRCNN50/tile_pipeline.py
+++
@@ -1,7 +0,0 @@
-"""Tiling Pipeline of MobileNetV2-ATSS model for Detection Task."""
-
-# Copyright (C) 2023 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-
-_base_ = ["../../base/data/tiling/atss_tile_pipeline.py"]
