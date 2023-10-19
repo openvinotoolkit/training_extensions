ac8a7dda52114df93bbe09ac43b261c7d40be1e9
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2023-10-19 02:52:50 +0200

    Update ModelAPI configuration (#2564)
    * Update MAPI rt infor for detection
    
    * Upadte export info for cls, det and seg
    
    * Update unit tests

--- tests/unit/algorithms/classification/utils/test_utils.py
+++ tests/unit/algorithms/classification/utils/test_utils.py
@@ -93,3 +93,7 @@ def test_get_cls_model_api_configuration(default_hierarchical_data):
     assert len(model_api_cfg) > 0
     assert model_api_cfg[("model_info", "confidence_threshold")] == str(config["confidence_threshold"])
     assert ("model_info", "hierarchical_config") in model_api_cfg
+    assert ("model_info", "labels") in model_api_cfg
+    assert ("model_info", "label_ids") in model_api_cfg
+    assert len(label_schema.get_labels(include_empty=False)) == len(model_api_cfg[("model_info", "labels")].split())
+    assert len(label_schema.get_labels(include_empty=False)) == len(model_api_cfg[("model_info", "label_ids")].split())
