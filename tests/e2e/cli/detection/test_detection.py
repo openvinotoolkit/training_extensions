da5387df64989f8f69606b95f1800325e70526b4
Yunchu Lee <yunchu.lee@intel.com>
2023-10-20 11:00:20 +0900

    add req_large_memory marker to some tests

--- tests/e2e/cli/detection/test_detection.py
+++ tests/e2e/cli/detection/test_detection.py
@@ -235,7 +235,11 @@ class TestToolsOTXDetection:
 
     @e2e_pytest_component
     @pytest.mark.skipif(TT_STABILITY_TESTS, reason="This is TT_STABILITY_TESTS")
-    @pytest.mark.parametrize("template", templates, ids=templates_ids)
+    @pytest.mark.parametrize(
+        "template",
+        [pytest.param(template, marks=pytest.mark.req_large_memory)
+            if template.name == "ResNeXt101-ATSS" else template for template in templates],
+        ids=templates_ids)
     def test_otx_hpo(self, template, tmp_dir_path):
         tmp_dir_path = tmp_dir_path / "detection/test_hpo"
         otx_hpo_testing(template, tmp_dir_path, otx_dir, args)
