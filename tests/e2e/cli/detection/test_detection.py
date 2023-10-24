b2b809fa0bbc7d898cd299cd25a506912a185f2d
Yunchu Lee <yunchu.lee@intel.com>
2023-10-24 22:41:56 +0900

    test e2e on aws

--- tests/e2e/cli/detection/test_detection.py
+++ tests/e2e/cli/detection/test_detection.py
@@ -222,7 +222,7 @@ class TestToolsOTXDetection:
         "template",
         [
             pytest.param(template, marks=pytest.mark.req_large_memory)
-            if template.name in ["ResNeXt101-ATSS", "YOLOX-Tiny"]
+            if template.name in ["ResNeXt101-ATSS", "YOLOX-Tiny", "YOLOX-L"]
             else template
             for template in templates
         ],
@@ -288,7 +288,9 @@ class TestToolsOTXDetection:
     @pytest.mark.parametrize(
         "template",
         [
-            pytest.param(template, marks=pytest.mark.req_large_memory) if template.name in ["YOLOX-Tiny"] else template
+            pytest.param(template, marks=pytest.mark.req_large_memory)
+            if template.name in ["YOLOX-Tiny", "YOLOX-L"]
+            else template
             for template in templates
         ],
         ids=templates_ids,
--- tests/e2e/cli/instance_segmentation/test_instance_segmentation.py
+++ tests/e2e/cli/instance_segmentation/test_instance_segmentation.py
@@ -100,7 +100,16 @@ else:
 
 class TestToolsOTXInstanceSegmentation:
     @e2e_pytest_component
-    @pytest.mark.parametrize("template", templates_with_experimental, ids=templates_ids_with_experimental)
+    @pytest.mark.parametrize(
+        "template",
+        [
+            pytest.param(template, marks=pytest.mark.req_large_memory)
+            if template.name in ["MaskRCNN-ConvNeXt"]
+            else template
+            for template in templates_with_experimental
+        ],
+        ids=templates_ids_with_experimental,
+    )
     def test_otx_train(self, template, tmp_dir_path):
         tmp_dir_path = tmp_dir_path / "ins_seg"
         otx_train_testing(template, tmp_dir_path, otx_dir, args0)
