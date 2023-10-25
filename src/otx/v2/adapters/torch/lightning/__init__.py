1bb1fb52db50a3ea6ad010be61f77676d7cca818
Kang, Harim <harim.kang@intel.com>
2023-10-26 00:05:06 +0900

    Remove Unnecessary torch imports

--- src/otx/v2/adapters/torch/lightning/__init__.py
+++ src/otx/v2/adapters/torch/lightning/__init__.py
@@ -2,15 +2,3 @@
 
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
-AVAILABLE = True
-VERSION = None
-DEBUG = None
-
-try:
-    import pytorch_lightning
-
-    VERSION = pytorch_lightning.__version__
-except ImportError as e:
-    AVAILABLE = False
-    DEBUG = e
