8000a62b8336bfbc119b16ee8c0ca07508aa2a0d
wonjulee <wonju.lee@intel.com>
2023-10-31 09:31:54 +0900

    boost up numpy accessing speed through PIL

--- src/otx/api/entities/image.py
+++ src/otx/api/entities/image.py
@@ -10,6 +10,7 @@ from typing import Optional, Tuple, Callable, Union
 import cv2
 import imagesize
 import numpy as np
+from PIL import Image as PILImage
 
 from otx.api.entities.annotation import Annotation
 from otx.api.entities.media import IMedia2DEntity
@@ -91,7 +92,9 @@ class Image(IMedia2DEntity):
             np.ndarray: NumPy representation of the image.
         """
         if self.__data is None:
-            return cv2.cvtColor(cv2.imread(self.__file_path), cv2.COLOR_BGR2RGB)
+            img = PILImage.open(self.__file_path)
+            img.draft('RGB', (img.width, img.height))
+            return np.asarray(img)
         if callable(self.__data):
             return self.__data()
         return self.__data
