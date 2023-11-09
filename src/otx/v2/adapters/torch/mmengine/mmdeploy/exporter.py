7808c432123286c627faf95cf1032036875447f3
sungmanc <sungman.cho@intel.com>
2023-11-09 15:46:31 +0900

    Add import

--- src/otx/v2/adapters/torch/mmengine/mmdeploy/exporter.py
+++ src/otx/v2/adapters/torch/mmengine/mmdeploy/exporter.py
@@ -13,6 +13,7 @@ from mmdeploy.apis.core.pipeline_manager import no_mp
 from mmdeploy.apis.onnx import export
 from mmdeploy.backend.openvino.onnx2openvino import from_onnx
 from mmdeploy.backend.openvino.utils import ModelOptimizerOptions
+from mmengine.structures import BaseDataElement
 
 from otx.v2.api.utils.logger import get_logger
 
