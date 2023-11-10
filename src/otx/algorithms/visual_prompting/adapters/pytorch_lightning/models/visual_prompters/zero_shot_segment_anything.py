5fd06358522972b69e63b8098a7b1fbe51ba4f58
Kim, Sungchul <sungchul.kim@intel.com>
2023-11-10 18:02:14 +0900

    (WIP) Update models

--- src/otx/algorithms/visual_prompting/adapters/pytorch_lightning/models/visual_prompters/zero_shot_segment_anything.py
+++ src/otx/algorithms/visual_prompting/adapters/pytorch_lightning/models/visual_prompters/zero_shot_segment_anything.py
@@ -11,6 +11,7 @@ from omegaconf import DictConfig
 from .segment_anything import SegmentAnything
 from otx.algorithms.common.utils.logger import get_logger
 from copy import deepcopy
+from collections import defaultdict
 
 logger = get_logger()
 
@@ -29,7 +30,7 @@ class ZeroShotSegmentAnything(SegmentAnything):
             logger.warning("config.model.freeze_mask_decoder(=False) must be set to True, changed.")
             config.model.freeze_mask_decoder = True
             
-        self.default_threshold_reference = config.default_threshold_reference
+        self.default_threshold_reference = config.model.default_threshold_reference
         super().__init__(config, state_dict)
         
         
@@ -46,18 +47,23 @@ class ZeroShotSegmentAnything(SegmentAnything):
         # self.reference_logit = None
     
     @torch.no_grad()
-    def learn(self, images: torch.Tensor, processed_prompts: Dict[Any]): # TODO (sungchul): fix type information
+    def learn(self, images: torch.Tensor, processed_prompts: Dict[str, Any], **kwargs): # TODO (sungchul): fix type information
         """Get reference features."""
         self._initialize_reference()
         
         _, _, height, width = images.shape
 
+        image_embeddings = self.image_encoder(images)
+        ref_feat = image_embeddings.squeeze().permute(1, 2, 0)
+        
         reference_feats = []
         reference_embeddings = []
         results_prompts = []
-        for label in range(2): # TODO (sungchul): support multi class
+        
+        num_classes = max(processed_prompts.keys()) + 1
+        for label in range(num_classes): # TODO (sungchul): support multi class
             if label == 0:
-                # background
+                # skip background
                 continue
             
             if label not in processed_prompts:
@@ -68,7 +74,7 @@ class ZeroShotSegmentAnything(SegmentAnything):
                 continue
                 
             # generate reference mask
-            results_prompt = torch.zeros((height, width), dtype=torch.uint8)
+            results_prompt = torch.zeros(kwargs["original_size"][0], dtype=torch.uint8, device=images.device)
             input_prompts = processed_prompts.get(label)
             for input_prompt in input_prompts:
                 if "annotation" in input_prompt:
@@ -76,15 +82,27 @@ class ZeroShotSegmentAnything(SegmentAnything):
                     results_prompt[input_prompt.get("annotation") == 1] += 1
                 else:
                     merged_input_prompts = self._merge_prompts(label, input_prompt, processed_prompts)
-                    # merged_input_prompts.update({"mask_input": self.reference_logit})
-                    masks, scores, logits, _ = self.model.predict(**merged_input_prompts, multimask_output=True)
-                    # if params.get("use_logit", DEFAULT_SETTINGS["reference"]["use_logit"]):
-                    #     self.reference_logit = logits[best_idx][None]
+                    # masks, scores = self.mask_decoder.predict_masks(**merged_input_prompts, multimask_output=True)
+                    sparse_embeddings, dense_embeddings = self.prompt_encoder(
+                        boxes=merged_input_prompts["box"], # TODO (sungchul): change key box -> boxes
+                        # TODO (sungchul): support other prompts
+                        points=None,
+                        masks=None,
+                    )
+
+                    low_res_masks, scores = self.mask_decoder(
+                        image_embeddings=image_embeddings,
+                        image_pe=self.prompt_encoder.get_dense_pe(),
+                        sparse_prompt_embeddings=sparse_embeddings,
+                        dense_prompt_embeddings=dense_embeddings,
+                        multimask_output=True,
+                    )
+                    high_res_masks = self.postprocess_masks(low_res_masks, images.shape[2:], kwargs["padding"][0], kwargs["original_size"][0])
+                    masks = high_res_masks > self.config.model.mask_threshold
                     best_idx = torch.argmax(scores)
-                    results_prompt[masks[best_idx]] += 1
+                    results_prompt[masks[0,best_idx]] += 1
             results_prompt = torch.clip(results_prompt, 0, 1)
             
-            ref_feat = self.image_encoder(images).squeeze().permute(1, 2, 0)
             ref_mask = torch.tensor(results_prompt, dtype=torch.float32)
             reference_feat = None
             default_threshold_reference = deepcopy(self.default_threshold_reference)
@@ -115,7 +133,7 @@ class ZeroShotSegmentAnything(SegmentAnything):
     
     @torch.no_grad()
     def infer(self):
-        """Get results from given images & prompts"""
+        """Zero-shot inference with reference features."""
 
     @torch.no_grad()
     def forward(self):
@@ -125,15 +143,169 @@ class ZeroShotSegmentAnything(SegmentAnything):
     def training_step(self, batch, batch_idx):
         """Learn"""
         images = batch["images"]
-        bboxes = batch["bboxes"]
-        points = batch["points"]
-        annotations = batch["annotations"]
         
-        return self.learn(images, ...)
+        # TODO (sungchul): each prompt will be assigned with each label
+        bboxes = batch["bboxes"]
+        labels = batch["labels"]
+        # TODO (sungchul): support other below prompts
+        # points = batch["points"]
+        # annotations = batch["annotations"]
+        
+        # organize prompts based on label
+        processed_prompts, label_dict = self._preprocess_prompts(bboxes=bboxes, labels=labels)
+        
+        kwargs = {
+            "original_size": batch.get("original_size"),
+            "padding": batch.get("padding"),
+        }
+        self.learn(images, processed_prompts, **kwargs)
     
-    def validation_step(self, batch, batch_idx):
-        """No validation step."""
-        pass
+    def _preprocess_prompts(
+        self,
+        bboxes: Optional[List[torch.Tensor]] = None,
+        points: Optional[List[torch.Tensor]] = None,
+        annotations: Optional[List[torch.Tensor]] = None,
+        labels: Optional[List[torch.Tensor]] = None,
+        # height: int = 1024,
+        # width: int = 1024,
+    ) -> Dict[str, Dict[str, Any]]:
+        """Preprocess prompts.
+
+        This function proceeds such below thigs:
+            1. Gather prompts which have the same labels
+            3. If there are box prompts, the key `point_coords` is changed to `box`
+        
+        Args:
+            prompts (list): Given prompts to be processed.
+            height (int): Image height.
+            width (int): Image width.
+            
+        Returns:
+            (dict): Processed and arranged each single prompt using label information as keys.
+                Unlike other prompts, `annotation` prompts will be aggregated as single annotation.
+
+                [Example]
+                processed_prompts = {
+                    0: [ # background
+                        {
+                            "point_coords": torch.Tensor,
+                            "point_labels": torch.Tensor,
+                        },
+                        {
+                            "box": torch.Tensor,
+                        },
+                        {
+                            "annotation": torch.Tensor, # there is only single processed annotation prompt
+                        },
+                        ...
+                    ],
+                    1: [
+                        {
+                            "point_coords": torch.Tensor,
+                            "point_labels": torch.Tensor,
+                        },
+                        {
+                            "point_coords": torch.Tensor,
+                            "point_labels": torch.Tensor,
+                        },
+                        {
+                            "annotation": torch.Tensor, # there is only single processed annotation prompt
+                        },
+                        ...
+                    ],
+                    2: [
+                        {
+                            "box": torch.Tensor,
+                        },
+                        {
+                            "box": torch.Tensor,
+                        },
+                        ...
+                    ]
+                }
+        """
+        processed_prompts = defaultdict(list)
+        label_dict = {}
+        # TODO (sungchul): will be updated
+        if bboxes:
+            for bbox, label in zip(bboxes[0], labels[0]):
+                label_id = int(label.id)
+                processed_prompts[label_id].append({"box": bbox.reshape(-1, 4)})
+                if label_id not in label_dict:
+                    label_dict[label_id] = label
+                
+        if points:
+            pass
+        
+        if annotations:
+            pass
+            
+        # for prompt in prompts:
+        #     processed_prompt = {}
+        #     if prompt.get("type") == "point":
+        #         processed_prompt.update({
+        #             "point_coords": prompt.get("point_coords"),
+        #             "point_labels": prompt.get("point_labels"),
+        #         })
+
+        #     elif prompt.get("type") == "polygon":
+        #         polygon = prompt.get("point_coords")
+        #         if convert_mask:
+        #             # convert polygon to mask
+        #             contour = [[int(point[0]), int(point[1])] for point in polygon]
+        #             gt_mask = np.zeros((height, width), dtype=np.uint8)
+        #             gt_mask = cv2.drawContours(gt_mask, np.asarray([contour]), 0, 1, -1)
+
+        #             # randomly sample points from generated mask
+        #             ys, xs, _ = np.nonzero(gt_mask)
+        #         else:
+        #             ys, xs = polygon[:,1], polygon[:,0]
+
+        #         rand_idx = np.random.permutation(len(ys))[:num_sample]
+        #         _point_coords = []
+        #         _point_labels = []
+        #         for x, y in zip(xs[rand_idx], ys[rand_idx]):
+        #             _point_coords.append([x, y])
+        #             _point_labels.append(prompt.get("point_labels")[0])
+
+        #         processed_prompt.update({
+        #             "point_coords": np.array(_point_coords),
+        #             "point_labels": np.array(_point_labels),
+        #         })
+
+        #     elif prompt.get("type") == "box":
+        #         processed_prompt.update({
+        #             "box": prompt.get("point_coords").reshape(-1, 4),
+        #         })
+
+        #     elif prompt.get("type") == "annotation":
+        #         polygon = prompt.get("point_coords")
+        #         contour = [[int(point[0]), int(point[1])] for point in polygon]
+        #         gt_mask = np.zeros((height, width), dtype=np.uint8)
+        #         gt_mask = cv2.drawContours(gt_mask, np.asarray([contour]), 0, 1, -1)
+        #         processed_prompt.update({"annotation": gt_mask})
+
+        #     processed_prompts[prompt.get("label", 0)].append(processed_prompt)
+
+        # # aggregate annotations
+        # for _, prompts in processed_prompts.items():
+        #     annotations = []
+        #     pop_idx = []
+        #     for idx, prompt in enumerate(prompts):
+        #         if "annotation" in prompt:
+        #             annotations.append(prompt.get("annotation"))
+        #             pop_idx.append(idx)
+
+        #     if len(pop_idx) > 0:
+        #         for idx in pop_idx[::-1]:
+        #             prompts.pop(idx)
+
+        #     if len(annotations) > 0:
+        #         dtype = annotations[0].dtype
+        #         prompts.append({"annotation": np.logical_or.reduce(annotations).astype(dtype)})
+
+        processed_prompts = dict(sorted(processed_prompts.items(), key=lambda x: x[0]))
+        return processed_prompts, label_dict
     
     def _generate_masked_features(self, feats: torch.Tensor, masks: torch.Tensor, threshold_mask: float) -> Tuple[torch.Tensor, ...]:
         """Generate masked features.
