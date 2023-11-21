b206b91c91dea9bcd8bab70152663d2cc7b5a9b3
Dick Ameln <dick.ameln@intel.com>
2023-11-21 14:38:18 +0100

    update license headers

--- src/otx/algorithms/anomaly/adapters/anomalib/config/anomalib_config.py
+++ src/otx/algorithms/anomaly/adapters/anomalib/config/anomalib_config.py
@@ -1,6 +1,6 @@
 """Configurable parameter conversion between OTX and Anomalib."""
 
-# Copyright (C) 2021 Intel Corporation
+# Copyright (C) 2023 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
