5b24f506f7a34c5ee19a93b43e2255d4a7379c3a
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-11-22 23:45:37 +0900

    Add xpu accelerator

---
+++ src/otx/algorithms/anomaly/adapters/anomalib/accelerators/__init__.py
@@ -0,0 +1,9 @@
+"""Accelerators for various devices."""
+
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from .xpu import XPUAccelerator
+
+__all__ = ["XPUAccelerator"]
