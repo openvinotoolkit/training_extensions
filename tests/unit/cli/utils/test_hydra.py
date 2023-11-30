f8e450296e4089709f7e43d8a3b275059160726b
Kang, Harim <harim.kang@intel.com>
2023-11-30 15:50:10 +0900

    Fix unittest for hydra configure

--- tests/unit/cli/utils/test_hydra.py
+++ tests/unit/cli/utils/test_hydra.py
@@ -7,17 +7,16 @@ from omegaconf import DictConfig
 from otx.cli.utils.hydra import configure_hydra_outputs
 
 
-def test_configure_hydra_outputs(mocker) -> None:
+def test_configure_hydra_outputs(mocker, tmp_path) -> None:
     # Mock hydra functions
     mock_configure_log = mocker.patch("otx.cli.utils.hydra.configure_log")
     mock_save_config = mocker.patch("otx.cli.utils.hydra._save_config")
-    mock_set_config = mocker.patch("hydra.core.hydra_config.HydraConfig.set_config")
 
     cfg = DictConfig({
         "hydra": {
             "job": {"name": "test"},
-            "output_subdir": "outputs/.hydra",
-            "runtime": {"output_dir": "outputs"},
+            "output_subdir": "${base.output_dir}",
+            "runtime": {"output_dir": "${base.output_dir}"},
             "overrides": {"task": "test"},
             "verbose": False,
             "job_logging": {
@@ -31,18 +30,13 @@ def test_configure_hydra_outputs(mocker) -> None:
             },
         },
         "base": {
-            'work_dir': 'outputs/work_dir',
+            'work_dir': str(tmp_path / "work_dir"),
             'data_dir': 'inputs/dataset_dir',
             'log_dir': '${base.work_dir}/logs/',
-            'output_dir': 'outputs/output_dir',
+            'output_dir': str(tmp_path / "outputs"),
         },
     })
     configure_hydra_outputs(cfg)
     mock_configure_log.assert_called_once_with(cfg.hydra.job_logging, False)
 
-    updated_cfg = cfg.copy()
-    output_dir = Path(cfg.base.output_dir).resolve()
-    updated_cfg.hydra.job_logging.handlers.file.filename = output_dir / f"{cfg.hydra.job.name}.log"
-    updated_cfg.hydra.runtime.output_dir = output_dir
-    mock_set_config.assert_called_once_with(updated_cfg)
     assert mock_save_config.call_count == 2
