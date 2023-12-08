45bd8f7ba72ce9539966dd86742b0dfa7e2202da
Songki Choi <songki.choi@intel.com>
2023-12-08 09:59:36 +0900

    Fix dataset adapter condition for det/iset

--- src/otx/core/data/adapter/detection_dataset_adapter.py
+++ src/otx/core/data/adapter/detection_dataset_adapter.py
@@ -38,16 +38,15 @@ class DetectionDatasetAdapter(BaseDatasetAdapter):
                     assert isinstance(image, Image)
                     shapes = []
                     for ann in datumaro_item.annotations:
-                        if (
-                            self.task_type in (TaskType.INSTANCE_SEGMENTATION, TaskType.ROTATED_DETECTION)
-                            and ann.type in (DatumAnnotationType.polygon, DatumAnnotationType.ellipse)
-                        ):
+                        if self.task_type in (TaskType.INSTANCE_SEGMENTATION, TaskType.ROTATED_DETECTION):
                             if ann.type == DatumAnnotationType.polygon and self._is_normal_polygon(ann):
                                 shapes.append(self._get_polygon_entity(ann, image.width, image.height))
                             elif ann.type == DatumAnnotationType.ellipse:
                                 shapes.append(self._get_ellipse_entity(ann, image.width, image.height))
-                        if self.task_type is TaskType.DETECTION and ann.type == DatumAnnotationType.bbox:
-                            if self._is_normal_bbox(ann.points[0], ann.points[1], ann.points[2], ann.points[3]):
+                        elif self.task_type is TaskType.DETECTION:
+                            if ann.type == DatumAnnotationType.bbox and self._is_normal_bbox(
+                                ann.points[0], ann.points[1], ann.points[2], ann.points[3]
+                            ):
                                 shapes.append(self._get_normalized_bbox_entity(ann, image.width, image.height))
 
                         if ann.label not in used_labels:
