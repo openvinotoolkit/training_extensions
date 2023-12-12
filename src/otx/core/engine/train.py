b74e57f062adb77d1d2101345952f11c69cf9735
sungmanc <sungman.cho@intel.com>
2023-12-12 17:11:45 +0900

    Add testmetric

--- src/otx/core/engine/train.py
+++ src/otx/core/engine/train.py
@@ -106,6 +106,7 @@ def train(cfg: TrainConfig) -> tuple[Trainer, dict[str, Any]]:
         log.info(f"Best ckpt path: {ckpt_path}")
 
     test_metrics = trainer.callback_metrics
+    log.info(test_metrics)
 
     # merge train and test metrics
     metric_dict = {**train_metrics, **test_metrics}
