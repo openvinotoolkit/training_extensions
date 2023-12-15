d7e55622c32a726fa84e223e1308aa88cc340d7c
jaegukhyun <jaeguk.hyun@intel.com>
2023-12-15 16:22:26 +0900

    Add workaround for loading video

--- src/otx/core/data/dataset/action.py
+++ src/otx/core/data/dataset/action.py
@@ -5,10 +5,15 @@
 
 from __future__ import annotations
 
-from typing import Callable
+from typing import TYPE_CHECKING, Callable
 
+import cv2
 import torch
 from datumaro import Label
+
+if TYPE_CHECKING:
+    import numpy as np
+    from datumaro.components.dataset_base import DatasetItem
 
 from otx.core.data.dataset.base import OTXDataset
 from otx.core.data.entity.action import ActionClsBatchDataEntity, ActionClsDataEntity
@@ -20,9 +25,9 @@ class OTXActionClsDataset(OTXDataset[ActionClsDataEntity]):
 
     def _get_item_impl(self, idx: int) -> ActionClsDataEntity | None:
         item = self.dm_subset.get(id=self.ids[idx], subset=self.dm_subset.name)
-        video = item.media
-        video_data = [frame.data for frame in video]
-        img_shape = video[0].data.shape
+        # video_data = [frame.data for frame in video]
+        video_data = self._get_video_data(item)
+        img_shape = video_data[0].shape
 
         label_anns = [ann for ann in item.annotations if isinstance(ann, Label)]
 
@@ -39,6 +44,25 @@ class OTXActionClsDataset(OTXDataset[ActionClsDataEntity]):
         )
 
         return self._apply_transforms(entity)
+
+    @staticmethod
+    def _get_video_data(item: DatasetItem) -> list[np.ndarray]:
+        """Return frames from datumaro dataset video item.
+
+        This can be handled easily by [frame.data for frame in video],
+        but latest Datumaro raises error after some iterateions.
+        Therefore, this is temporary workaround to get frames.
+        """
+        video = cv2.VideoCapture(item.media.path)
+        video_data = []
+        while video.isOpened():
+            ret, frame = video.read()
+            if ret:
+                video_data.append(frame)
+            else:
+                break
+        video.release()
+        return video_data
 
     @property
     def collate_fn(self) -> Callable:
