5e975702342925857099a8b937c8977e391fb954
kprokofi <kirill.prokofiev@intel.com>
2023-12-18 00:09:40 +0900

    fixed mypy

--- src/otx/algo/segmentation/__init__.py
+++ src/otx/algo/segmentation/__init__.py
@@ -3,6 +3,6 @@
 #
 """Module for OTX segmentation models, hooks, utils, etc."""
 
-from . import backbones, decode_heads
+from . import backbones
 
-__all__ = ["backbones", "decode_heads"]
+__all__ = ["backbones"]
