768f12930f215df2f49aa0fee9ba696428ab319f
Kang, Harim <harim.kang@intel.com>
2023-12-19 14:40:07 +0900

    Fix some docstring for engine

--- src/otx/core/engine/engine.py
+++ src/otx/core/engine/engine.py
@@ -1,7 +1,8 @@
+"""Module for OTX engine components."""
+
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-"""Module for OTX engine components."""
 from __future__ import annotations
 
 from pathlib import Path
@@ -53,6 +54,7 @@ class Engine:
             task (OTXTaskType | None, optional): The Task type want to use in Engine.
             work_dir (str | Path | None, optional): The working directory. Defaults to None.
             device (OTXDeviceType, optional):  The devices to be used. Defaults to "auto".
+            **kwargs: Additional keyword arguments for pl.Trainer configuration.
         """
         self.task = task
         self.work_dir = work_dir
@@ -188,7 +190,7 @@ class Engine:
             val_check_interval (int | float | None, optional): The validation check interval. Defaults to 1.
             callbacks (list[Callback] | Callback | None, optional): The callbacks to be used during training.
             logger (Logger | Iterable[Logger] | bool | None, optional): The logger(s) to be used. Defaults to None.
-            **kwargs: Additional keyword arguments for pl.Trainer.
+            **kwargs: Additional keyword arguments for pl.Trainer configuration.
 
         Returns:
             dict[str, Any]: A dictionary containing the callback metrics from the trainer.
@@ -198,6 +200,7 @@ class Engine:
             model=LightningModule(),
             datamodule=OTXDataModule(),
             checkpoint="checkpoint.ckpt",
+            max_epochs=3,
         )
 
         CLI Usage:
@@ -256,6 +259,7 @@ class Engine:
             datamodule (OTXDataModule | None, optional): The data module containing the test data.
             checkpoint (str | Path | None, optional): Path to the checkpoint file to load the model from.
                 Defaults to None.
+            **kwargs: Additional keyword arguments for pl.Trainer configuration.
 
         Returns:
             dict: Dictionary containing the callback metrics from the trainer.
@@ -295,7 +299,6 @@ class Engine:
         datamodule: OTXDataModule | None = None,
         checkpoint: str | Path | None = None,
         return_predictions: bool | None = None,
-        callbacks: list[Callback] | Callback | None = None,
         **kwargs,
     ) -> list | None:
         """Run predictions using the specified model and data.
@@ -305,6 +308,7 @@ class Engine:
             datamodule (OTXDataModule | None): The data module to use for predictions.
             checkpoint (str | Path | None): The path to the checkpoint file to load the model from.
             return_predictions (bool | None): Whether to return the predictions or not.
+            **kwargs: Additional keyword arguments for pl.Trainer configuration.
 
         Returns:
             list | None: The predictions if `return_predictions` is True, otherwise None.
