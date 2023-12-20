1abbca828bab3fde39e921dd5088b8224afb55ca
Songki Choi <songki.choi@intel.com>
2023-12-20 10:43:50 +0900

    Add API doc for OTXBenchmark

--- tests/perf/benchmark.py
+++ tests/perf/benchmark.py
@@ -3,6 +3,7 @@
 
 
 import os
+import pandas as pd
 import yaml
 from pathlib import Path
 from typing import List
@@ -11,12 +12,38 @@ from tests.test_suite.run_test_command import check_run
 
 
 class OTXBenchmark:
+    """Benchmark runner based on tools/experiment.py in OTX1.x.
+
+    Example:
+        >>> bm = OTXBenchmark(['random_sample1', 'random_sample'], data_root='./data/coco')
+        >>> atss_result = bm.run('MobileNetV2-ATSS')
+        >>> yolox_result = bm.run('YOLOX-TINY')
+
+    Args:
+        datasets (List[str]): Paths to datasets relative to the data_root.
+            Intended for, but not restricted to different sampling based on same dataset.
+        data_root (str): Path to the root of dataset directories. Defaults to './data'.
+        num_epoch (int): Overrides the per-model default number of epoch settings.
+            Defaults to 0, which means no overriding.
+        num_repeat (int): Number for trials with different random seed, which would be set
+            as range(0, num_repeat). Defaults to 1.
+        train_params (dict): Additional training parameters.
+            e.x) {'learning_parameters.num_iters': 2}. Defaults to {}.
+        track_resources (bool): Whether to track CPU & GPU usage metrics. Defaults to False.
+        eval_upto (str): The last serial operation to evaluate. Choose on of ('train', 'export', 'optimize').
+            Operations include the preceeding ones.
+            e.x) Eval up to 'optimize': train -> eval -> export -> eval -> optimize -> eval
+            Default to 'train'.
+        output_root (str): Output path for logs and results. Defaults to './otx-benchmark'.
+        dry_run (bool): Whether to just print the OTX command without execution. Defaults to False.
+        tags (dict): Key-values pair metadata for the experiment. Defaults to {}.
+    """
     def __init__(
         self,
         datasets: List[str],
         data_root: str = "data",
         num_epoch: int = 0,
-        num_repeat: int = 0,
+        num_repeat: int = 1,
         train_params: dict = {},
         track_resources: bool = False,
         eval_upto: str = "train",
@@ -40,7 +67,18 @@ class OTXBenchmark:
         model_id: str,
         train_params: dict = {},
         tags: dict = {},
-    ) -> List[str]:
+    ) -> pd.DataFrame
+        """Run benchmark and return the result.
+
+        Args:
+            model_id (str): Target model identifier
+            train_params (dict): Overrides global benchmark train params
+            tags (dict): Overrides global benchmark tags
+
+        Retruns:
+            pd.DataFrame: Table with benchmark metrics
+        """
+
         # Build config file
         cfg = self._build_config(model_id, train_params, tags)
         cfg_dir = Path(self.output_root)
@@ -59,8 +97,20 @@ class OTXBenchmark:
         # Run benchmark
         check_run(cmd)
         # Load result
-        result = None
+        result = self.load_result()
         return result
+
+    def load_result(self, result_path: str = None) -> pd.DataFrame:
+        """Load result as pd.DataFrame format.
+
+        Args:
+            result_path (str): Result directory or speicific file.
+                Defaults to None to search the benchmark output root.
+
+        Retruns:
+            pd.DataFrame: Table with benchmark metrics
+        """
+        return None
 
     def _build_config(
         self,
