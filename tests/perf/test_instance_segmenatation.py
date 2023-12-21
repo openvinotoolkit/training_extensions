742e8cc489059f7930ba4ed42cceec0b4497ba41
Songki Choi <songki.choi@intel.com>
2023-12-21 14:06:01 +0900

    Add tiling tests

--- tests/perf/test_instance_segmenatation.py
+++ tests/perf/test_instance_segmenatation.py
@@ -67,3 +67,64 @@ class TestPerfInstanceSegmentation:
             model_id=fxt_model_id,
             tags={"benchmark": "speed"},
         )
+
+
+class TestPerfTilingInstanceSegmentation:
+    """Benchmark tiling instance segmentation."""
+
+    TILING_PARAMS = {
+        "tiling_parameters.enable_tiling": 1,
+    }
+    BENCHMARK_CONFIGS = {
+        "small": {
+            "tags": {
+                "task": "instance_segmentation",
+            },
+            "datasets": [
+                "tiling_instance_seg/vitens_aeromonas_small/1",
+                "tiling_instance_seg/vitens_aeromonas_small/2",
+                "tiling_instance_seg/vitens_aeromonas_small/3",
+            ],
+            "num_repeat": 3,
+            "train_params": TILING_PARAMS,
+        },
+        "medium": {
+            "tags": {
+                "task": "instance_segmentation",
+            },
+            "datasets": [
+                "tiling_instance_seg/vitens_aeromonas_medium",
+            ],
+            "num_repeat": 3,
+            "train_params": TILING_PARAMS,
+        },
+        "large": {
+            "tags": {
+                "task": "instance_segmentation",
+            },
+            "datasets": [
+                "tiling_instance_seg/bdd_large",
+            ],
+            "num_repeat": 1,
+            "train_params": TILING_PARAMS,
+        },
+    }
+
+    @pytest.mark.parametrize("fxt_model_id", MODEL_TEMPLATES, ids=MODEL_IDS, indirect=True)
+    @pytest.mark.parametrize("fxt_benchmark", BENCHMARK_CONFIGS.items(), ids=BENCHMARK_CONFIGS.keys(), indirect=True)
+    def test_accuracy(self, fxt_model_id: str, fxt_benchmark: OTXBenchmark):
+        """Benchmark accruacy metrics."""
+        result = fxt_benchmark.run(
+            model_id=fxt_model_id,
+            tags={"benchmark": "accuracy"},
+        )
+
+    @pytest.mark.parametrize("fxt_model_id", MODEL_TEMPLATES, ids=MODEL_IDS, indirect=True)
+    @pytest.mark.parametrize("fxt_benchmark", BENCHMARK_CONFIGS.items(), ids=BENCHMARK_CONFIGS.keys(), indirect=True)
+    def test_speed(self, fxt_model_id: str, fxt_benchmark: OTXBenchmark):
+        """Benchmark train time per iter / infer time per image."""
+        fxt_benchmark.track_resources = True
+        result = fxt_benchmark.run(
+            model_id=fxt_model_id,
+            tags={"benchmark": "speed"},
+        )
