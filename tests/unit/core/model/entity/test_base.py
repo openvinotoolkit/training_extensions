0d52c79f4e7dee7ab96dbda8f39477ef6ef778ad
jaegukhyun <jaeguk.hyun@intel.com>
2024-01-04 15:21:21 +0900

    Add unit test for smart weight loading

---
+++ tests/unit/core/model/entity/test_base.py
@@ -0,0 +1,40 @@
+import torch
+from otx.core.model.entity.base import OTXModel
+
+
+class MockNNModule(torch.nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.backbone = torch.nn.Linear(3, 1024)
+        self.head = torch.nn.Linear(3, num_classes)
+
+
+class TestOTXModel:
+    def test_smart_weight_loading(self, mocker) -> None:
+        mocker.patch.object(OTXModel, "_create_model", return_value=MockNNModule(2))
+        prev_model = OTXModel()
+
+        mocker.patch.object(OTXModel, "_create_model", return_value=MockNNModule(3))
+        current_model = OTXModel()
+
+        prev_classes = ["car", "truck"]
+        current_classes = ["car", "bus", "truck"]
+        indices = torch.Tensor([0, 2]).to(torch.int32)
+
+        current_model.register_load_state_dict_pre_hook(current_classes, prev_classes)
+        current_model.load_state_dict(prev_model.state_dict())
+
+        assert torch.all(
+            current_model.state_dict()["model.backbone.weight"] == prev_model.state_dict()["model.backbone.weight"],
+        )
+        assert torch.all(
+            current_model.state_dict()["model.backbone.bias"] == prev_model.state_dict()["model.backbone.bias"],
+        )
+        assert torch.all(
+            current_model.state_dict()["model.head.weight"].index_select(0, indices)
+            == prev_model.state_dict()["model.head.weight"],
+        )
+        assert torch.all(
+            current_model.state_dict()["model.head.bias"].index_select(0, indices)
+            == prev_model.state_dict()["model.head.bias"],
+        )
