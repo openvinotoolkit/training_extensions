669d935209ac14f857b52b25ce3957fc9ce03f15
Kim, Vinnam <vinnam.kim@intel.com>
2024-01-12 10:31:18 +0900

    Make read_mmconfig() more descriptive
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- src/otx/algo/action_detection/__init__.py
+++ src/otx/algo/action_detection/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
-"""Module for OTX action detection models, hooks, utils, etc."""
+"""Module for OTX action detection models."""
--- src/otx/algo/utils/mmconfig.py
+++ src/otx/algo/utils/mmconfig.py
@@ -14,18 +14,25 @@ def read_mmconfig(model_name: str, subdir_name: str = ".") -> DictConfig:
 
     It try to read MMConfig from the yaml file which exists in
     `<Directory path of __file__ who calls this function>/mmconfigs/<subdir_name>/<model_name>.yaml`
+
+    For example, if this function is called in `otx/algo/action_classification/x3d.py`,
+    `otx/algo/action_classification/mmconfigs/x3d.yaml` will be read.
     """
     frame = inspect.stack()[1]
     module = inspect.getmodule(frame[0])
 
-    if module is None or (mod_fpath := module.__file__) is None:
-        msg = "Cannot get valid model from stack"
+    if module is None or (module_file_path := module.__file__) is None:
+        msg = (
+            "Cannot get Cannot get a valid module from Python function stack. "
+            "Please refer to this function docstring to see how to use correctly."
+        )
         raise RuntimeError(msg)
 
-    root_dir = Path(mod_fpath).parent / "mmconfigs" / subdir_name
+    root_dir = Path(module_file_path).parent / "mmconfigs" / subdir_name
     yaml_fpath = root_dir / f"{model_name}.yaml"
 
     if not yaml_fpath.exists():
-        raise FileNotFoundError
+        msg = f"mmconfig file for {model_name} is not found in {yaml_fpath}"
+        raise FileNotFoundError(msg)
 
     return OmegaConf.load(yaml_fpath)
