66e472eba9fb75ad3483b8bd9489bd4b1e061eac
Eugene Liu <eugene.liu@intel.com>
2024-01-15 10:35:49 +0000

    Fix invalid polygon shape filter

--- src/otx/core/data/adapter/base_dataset_adapter.py
+++ src/otx/core/data/adapter/base_dataset_adapter.py
@@ -272,11 +272,13 @@ class BaseDatasetAdapter(metaclass=abc.ABCMeta):
 
         return {"category_items": category_items, "label_groups": label_groups, "label_entities": label_entities}
 
-    def _is_normal_polygon(self, annotation: DatumAnnotationType.polygon) -> bool:
+    def _is_normal_polygon(self, annotation: DatumAnnotationType.polygon, width: int, height: int) -> bool:
         """To filter out the abnormal polygon."""
         x_points = [annotation.points[i] for i in range(0, len(annotation.points), 2)]
         y_points = [annotation.points[i + 1] for i in range(0, len(annotation.points), 2)]
-        return min(x_points) < max(x_points) and min(y_points) < max(y_points)
+        return min(x_points) < max(x_points) and min(y_points) < max(y_points) and max(x_points) < width and max(
+            y_points
+        ) < height
 
     def _is_normal_bbox(self, x1: float, y1: float, x2: float, y2: float) -> bool:
         """To filter out the abrnormal bbox."""
