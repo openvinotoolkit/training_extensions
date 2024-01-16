58c5d5a3b0cbedfdfed14e180c359756290ad3a2
harimkang <harim.kang@intel.com>
2024-01-16 11:41:57 +0900

    Initial CLI commit

---
+++ src/otx/cli/utils/help_formatter.py
@@ -0,0 +1,222 @@
+"""Custom Help Formatters for OTX CLI."""
+
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import re
+import sys
+from typing import TYPE_CHECKING, Iterable
+
+from jsonargparse import DefaultHelpFormatter
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich_argparse import RichHelpFormatter
+
+if TYPE_CHECKING:
+    import argparse
+
+    from rich.console import RenderableType
+
+
+BASE_ARGUMENTS = {"config", "print_config", "help", "engine", "model", "model.help", "task"}
+ENGINE_ARGUMENTS = {"data_root", "engine.help", "engine.device", "engine.checkpoint", "engine.work_dir"}
+REQUIRED_ARGUMENTS = {
+    "train": {
+        "data",
+        "optimizer",
+        "scheduler",
+    }.union(BASE_ARGUMENTS).union(ENGINE_ARGUMENTS),
+    "test": {
+        "data",
+    }.union(BASE_ARGUMENTS).union(ENGINE_ARGUMENTS),
+}
+
+
+def get_verbosity_subcommand() -> dict:
+    """Parse command line arguments and returns a dictionary of key-value pairs.
+
+    Returns:
+        A dictionary containing the parsed command line arguments.
+
+    Examples:
+        >>> import sys
+        >>> sys.argv = ['otx', 'train', '-h', '-v']
+        >>> get_verbosity_subcommand()
+        {'subcommand': 'train', 'help': True, 'verbosity': 1}
+    """
+    arguments: dict = {"subcommand": None, "help": False, "verbosity": 2}
+    if len(sys.argv) >= 2 and sys.argv[1] not in ("--help", "-h"):
+        arguments["subcommand"] = sys.argv[1]
+    if "--help" in sys.argv or "-h" in sys.argv:
+        arguments["help"] = True
+        if arguments["subcommand"] in REQUIRED_ARGUMENTS:
+            arguments["verbosity"] = 0
+            if "-v" in sys.argv or "--verbose" in sys.argv:
+                arguments["verbosity"] = 1
+            if "-vv" in sys.argv:
+                arguments["verbosity"] = 2
+    return arguments
+
+
+INTRO_MARKDOWN = (
+    "# OpenVINO™ Training Extensions CLI Guide\n\n"
+    "Github Repository: [https://github.com/openvinotoolkit/training_extensions](https://github.com/openvinotoolkit/training_extensions)."
+    "\n\n"
+    "A better guide is provided by the [documentation](https://openvinotoolkit.github.io/training_extensions/stable/)."
+)
+
+VERBOSE_USAGE = (
+    "To get more overridable argument information, run the command below.\n"
+    "```python\n"
+    "# Verbosity Level 1\n"
+    "otx {subcommand} [optional_arguments] -h -v\n"
+    "# Verbosity Level 2\n"
+    "otx {subcommand} [optional_arguments] -h -vv\n"
+    "```"
+)
+
+CLI_USAGE_PATTERN = r"CLI Usage:(.*?)(?=\n{2,}|\Z)"
+
+
+def get_cli_usage_docstring(component: object | None) -> str | None:
+    r"""Get the cli usage from the docstring.
+
+    Args:
+        component (Optional[object]): The component to get the docstring from
+
+    Returns:
+        Optional[str]: The quick-start guide as Markdown format.
+
+    Example:
+        component.__doc__ = '''
+            <Prev Section>
+
+            CLI Usage:
+                1. First Step.
+                2. Second Step.
+
+            <Next Section>
+        '''
+        >>> get_cli_usage_docstring(component)
+        "1. First Step.\n2. Second Step."
+    """
+    if component is None or component.__doc__ is None or "CLI Usage" not in component.__doc__:
+        return None
+    match = re.search(CLI_USAGE_PATTERN, component.__doc__, re.DOTALL)
+    if match:
+        contents = match.group(1).strip().split("\n")
+        return "\n".join([content.strip() for content in contents])
+    return None
+
+
+def render_guide(subcommand: str | None = None) -> list:
+    """Render a guide for the specified subcommand.
+
+    Args:
+        subcommand (Optional[str]): The subcommand to render the guide for.
+
+    Returns:
+        list: A list of contents to be displayed in the guide.
+    """
+    if subcommand is None or subcommand in ("install"):
+        return []
+    from otx.engine import Engine
+
+    contents: list[Panel | Markdown] = [Markdown(INTRO_MARKDOWN)]
+    target_command = getattr(Engine, subcommand)
+    cli_usage = get_cli_usage_docstring(target_command)
+    if cli_usage is not None:
+        cli_usage += f"\n{VERBOSE_USAGE.format(subcommand=subcommand)}"
+        quick_start = Panel(Markdown(cli_usage), border_style="dim", title="Quick-Start", title_align="left")
+        contents.append(quick_start)
+    return contents
+
+
+class CustomHelpFormatter(RichHelpFormatter, DefaultHelpFormatter):
+    """A custom help formatter for OTX CLI.
+
+    This formatter extends the RichHelpFormatter and DefaultHelpFormatter classes to provide
+    a more detailed and customizable help output for OTX CLI.
+
+    Attributes:
+        verbose_level : int
+            The level of verbosity for the help output.
+        subcommand : str | None
+            The subcommand to render the guide for.
+
+    Methods:
+        add_usage(usage, actions, *args, **kwargs)
+            Add usage information to the help output.
+        add_argument(action)
+            Add an argument to the help output.
+        format_help()
+            Format the help output.
+    """
+
+    verbosity_dict = get_verbosity_subcommand()
+    verbose_level = verbosity_dict["verbosity"]
+    subcommand = verbosity_dict["subcommand"]
+
+    def add_usage(self, usage: str | None, actions: Iterable[argparse.Action], *args, **kwargs) -> None:
+        """Add usage information to the formatter.
+
+        Args:
+            usage (str | None): A string describing the usage of the program.
+            actions (Iterable[argparse.Action]): An list of argparse.Action objects.
+            *args (Any): Additional positional arguments to pass to the superclass method.
+            **kwargs (Any): Additional keyword arguments to pass to the superclass method.
+
+        Returns:
+            None
+        """
+        if self.subcommand in REQUIRED_ARGUMENTS:
+            if self.verbose_level == 0:
+                actions = []
+            elif self.verbose_level == 1:
+                actions = [action for action in actions if action.dest in REQUIRED_ARGUMENTS[self.subcommand]]
+
+        super().add_usage(usage, actions, *args, **kwargs)
+
+    def add_argument(self, action: argparse.Action) -> None:
+        """Add an argument to the help formatter.
+
+        If the verbose level is set to 0, the argument is not added.
+        If the verbose level is set to 1 and the argument is not in the non-skip list, the argument is not added.
+
+        Args:
+            action (argparse.Action): The action to add to the help formatter.
+        """
+        if self.subcommand in REQUIRED_ARGUMENTS:
+            if self.verbose_level == 0:
+                return
+            if self.verbose_level == 1 and action.dest not in REQUIRED_ARGUMENTS[self.subcommand]:
+                return
+        super().add_argument(action)
+
+    def format_help(self) -> str:
+        """Format the help message for the current command and returns it as a string.
+
+        The help message includes information about the command's arguments and options,
+        as well as any additional information provided by the command's help guide.
+
+        Returns:
+            str: A string containing the formatted help message.
+        """
+        with self.console.capture() as capture:
+            section = self._root_section
+            _reder: RenderableType = section
+            if self.subcommand in REQUIRED_ARGUMENTS and self.verbose_level in (0, 1) and len(section.rich_items) > 1:
+                contents = render_guide(self.subcommand)
+                for content in contents:
+                    self.console.print(content)
+            if self.verbose_level > 0:
+                if len(section.rich_items) > 1:
+                    _reder = Panel(section, border_style="dim", title="Arguments", title_align="left")
+                self.console.print(_reder, highlight=False, soft_wrap=True)
+        help_msg = capture.get()
+
+        if help_msg:
+            help_msg = self._long_break_matcher.sub("\n\n", help_msg).rstrip() + "\n"
+        return help_msg
---
+++ src/otx/cli/utils/jsonargparse.py
@@ -0,0 +1,140 @@
+"""Functions related to jsonargparse."""
+
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import ast
+from pathlib import Path
+from typing import Any, TypeVar
+
+import docstring_parser
+from jsonargparse import ActionConfigFile, ArgumentParser, Namespace, dict_to_namespace
+
+
+def get_short_docstring(component: TypeVar) -> str | None:
+    """Get the short description from the docstring.
+
+    Args:
+        component (object): The component to get the docstring from
+
+    Returns:
+        Optional[str]: The short description
+    """
+    if component.__doc__ is None:
+        return None
+    docstring = docstring_parser.parse(component.__doc__)
+    return docstring.short_description
+
+
+def flatten_dict(d: dict, parent_key: str = "", sep: str = ".") -> dict:
+    """Flatten a nested dictionary into a single-level dictionary.
+
+    Args:
+        d (dict): The dictionary to be flattened.
+        parent_key (str): The parent key to be used for nested keys.
+        sep (str): The separator to be used between parent and child keys.
+
+    Returns:
+        dict: The flattened dictionary.
+
+    """
+    items: list = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
+# [FIXME]: Overriding Namespce.update to match mmengine.Config (DictConfig | dict)
+# and prevent int, float types from being converted to str
+# https://github.com/omni-us/jsonargparse/issues/236
+def update(
+    self: Namespace,
+    value: Any,  # noqa: ANN401
+    key: str | None = None,
+    only_unset: bool = False,
+) -> Namespace:
+    """Sets or replaces all items from the given nested namespace.
+
+    Args:
+        value: A namespace to update multiple values or other type to set in a single key.
+        key: Branch key where to set the value. Required if value is not namespace.
+        only_unset: Whether to only set the value if not set in namespace.
+    """
+    _dict_type = False
+    if isinstance(value, dict):
+        # Dict -> Nested Namespace for overriding
+        _dict_type = True
+        value = dict_to_namespace(value)
+    if not isinstance(value, (Namespace, dict)):
+        if not key:
+            msg = "Key is required if value not a Namespace."
+            raise KeyError(msg)
+        if not only_unset or key not in self:
+            if key not in self or value is not None:
+                if isinstance(value, str) and (value.isnumeric() or value in ("True", "False")):
+                    value = ast.literal_eval(value)
+                self[key] = value
+            elif value is None:
+                del self[key]
+    else:
+        prefix = key + "." if key else ""
+        for _key, val in value.items():
+            if not only_unset or prefix + _key not in self:
+                self.update(val, prefix + _key)
+    if _dict_type and key is not None:
+        # Dict or Namespace -> Dict
+        self[key] = dict_to_namespace(self[key]).as_dict()
+    return self
+
+
+# To provide overriding of the Config file
+def apply_config(self: ActionConfigFile, parser: ArgumentParser, cfg: Namespace, dest: str, value: str) -> None:  # noqa: ARG001
+    """Applies the configuration to the parser.
+
+    Args:
+        parser: The parser object.
+        cfg: The configuration object.
+        dest: The destination attribute.
+        value: The value to be applied.
+
+    Returns:
+        None
+    """
+    from jsonargparse._actions import _ActionSubCommands, previous_config_context
+    from jsonargparse._link_arguments import skip_apply_links
+    from jsonargparse._loaders_dumpers import get_loader_exceptions, load_value
+    from jsonargparse._optionals import get_config_read_mode
+
+    with _ActionSubCommands.not_single_subcommand(), previous_config_context(cfg), skip_apply_links():
+        kwargs = {"env": False, "defaults": False, "_skip_check": True, "_fail_no_subcommand": False}
+        try:
+            cfg_path: Path | None = Path(value, mode=get_config_read_mode())
+        except TypeError:
+            try:
+                if isinstance(load_value(value), str):
+                    raise
+                cfg_path = None
+                cfg_file = parser.parse_string(value, **kwargs)
+            except (TypeError, *get_loader_exceptions()) as ex_str:
+                msg = f'Parser key "{dest}": {ex_str}'
+                raise TypeError(msg) from ex_str
+        else:
+            cfg_file = parser.parse_path(value, **kwargs)
+        cfg_merged = parser.merge_config(cfg_file, cfg)
+        cfg.__dict__.update(cfg_merged.__dict__)
+        overrides = cfg.__dict__.pop("overrides", None)
+        if overrides is not None:
+            cfg.__dict__.update(overrides)
+        if cfg.get(dest) is None:
+            cfg[dest] = []
+        cfg[dest].append(cfg_path)
+
+
+Namespace.update = update
+ActionConfigFile.apply_config = apply_config
