8498a6e9fad06715e4ba5c392fb4ae312762afb7
sungmanc <sungman.cho@intel.com>
2024-01-22 17:12:47 +0900

    Add the load_ckpt to support backward compat

--- src/otx/algo/classification/efficientnet_b0.py
+++ src/otx/algo/classification/efficientnet_b0.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """EfficientNetB0 model implementation."""
+from typing import Any
 
 from otx.algo.utils.mmconfig import read_mmconfig
 from otx.core.model.entity.classification import (
@@ -10,8 +11,27 @@ from otx.core.model.entity.classification import (
     MMPretrainMultilabelClsModel,
 )
 
+class EfficientNetB0LoadWeightMixin:
+    def convert_previous_otx_ckpt_impl(
+        self, 
+        state_dict: dict[str, Any],
+        label_type: str,
+        add_prefix: str = "", 
+    ):
+        """Convert the previous OTX ckpt according to OTX2.0."""
+        for key in list(state_dict.keys()):
+            val = state_dict.pop(key)
+            if key.startswith("features.") and "activ" not in key:
+                key = "backbone." + key
+            elif key.startswith("output."):
+                key = key.replace("output", "head")
+                if not label_type == "hlabel":
+                    key = key.replace("asl", "fc")
+                val = val.t()
+            state_dict[add_prefix + key] = val
+        return state_dict 
 
-class EfficientNetB0ForHLabelCls(MMPretrainHlabelClsModel):
+class EfficientNetB0ForHLabelCls(EfficientNetB0LoadWeightMixin, MMPretrainHlabelClsModel):
     """EfficientNetB0 Model for hierarchical label classification task."""
 
     def __init__(self, num_classes: int, num_multiclass_heads: int, num_multilabel_classes: int) -> None:
@@ -20,19 +40,30 @@ class EfficientNetB0ForHLabelCls(MMPretrainHlabelClsModel):
         config.head.num_multilabel_classes = num_multilabel_classes
         super().__init__(num_classes=num_classes, config=config)
 
+    def convert_previous_otx_ckpt(self, state_dict, add_prefix: str=""):
+        """Convert the previous OTX ckpt according to OTX2.0."""
+        return self.convert_previous_otx_ckpt_impl(state_dict, "hlabel", add_prefix)
 
-class EfficientNetB0ForMulticlassCls(MMPretrainMulticlassClsModel):
+class EfficientNetB0ForMulticlassCls(EfficientNetB0LoadWeightMixin, MMPretrainMulticlassClsModel):
     """EfficientNetB0 Model for multi-label classification task."""
 
     def __init__(self, num_classes: int, light: bool = False) -> None:
         model_name = "efficientnet_b0_light" if light else "efficientnet_b0"
         config = read_mmconfig(model_name=model_name, subdir_name="multiclass_classification")
         super().__init__(num_classes=num_classes, config=config)
+    
+    def convert_previous_otx_ckpt(self, state_dict, add_prefix: str=""):
+        """Convert the previous OTX ckpt according to OTX2.0."""
+        return self.convert_previous_otx_ckpt_impl(state_dict, "multiclass", add_prefix)
 
 
-class EfficientNetB0ForMultilabelCls(MMPretrainMultilabelClsModel):
+class EfficientNetB0ForMultilabelCls(EfficientNetB0LoadWeightMixin, MMPretrainMultilabelClsModel):
     """EfficientNetB0 Model for multi-class classification task."""
 
     def __init__(self, num_classes: int) -> None:
         config = read_mmconfig(model_name="efficientnet_b0_light", subdir_name="multilabel_classification")
         super().__init__(num_classes=num_classes, config=config)
+
+    def convert_previous_otx_ckpt(self, state_dict, add_prefix: str=""):
+        """Convert the previous OTX ckpt according to OTX2.0."""
+        return self.convert_previous_otx_ckpt_impl(state_dict, "multilabel", add_prefix)
--- src/otx/algo/classification/mobilenet_v3_large.py
+++ src/otx/algo/classification/mobilenet_v3_large.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """MobileNetV3 model implementation."""
+from typing import Any
 
 from otx.algo.utils.mmconfig import read_mmconfig
 from otx.core.model.entity.classification import (
@@ -10,8 +11,30 @@ from otx.core.model.entity.classification import (
     MMPretrainMultilabelClsModel,
 )
 
-
-class MobileNetV3ForHLabelCls(MMPretrainHlabelClsModel):
+class MobileNetV3LoadWeightMixin:
+    def convert_previous_otx_ckpt_impl(
+        self, 
+        state_dict: dict[str, Any], 
+        label_type: str,
+        add_prefix: str = "", 
+    ):
+        """Convert the previous OTX ckpt according to OTX2.0."""
+        for key in list(state_dict.keys()):
+            val = state_dict.pop(key)
+            if key.startswith("classifier."):
+                if "4" in key:
+                    key = "head." + key.replace("4", "3")
+                    if label_type == "multilabel":
+                        val = val.t()
+                else:
+                    key = "head." + key
+            elif key.startswith("act"):
+                key = "head." + key
+            elif not key.startswith("backbone."):
+                key = "backbone." + key
+            state_dict[add_prefix + key] = val
+        return state_dict 
+class MobileNetV3ForHLabelCls(MobileNetV3LoadWeightMixin, MMPretrainHlabelClsModel):
     """MobileNetV3 Model for hierarchical label classification task."""
 
     def __init__(self, num_classes: int, num_multiclass_heads: int, num_multilabel_classes: int) -> None:
@@ -20,8 +43,11 @@ class MobileNetV3ForHLabelCls(MMPretrainHlabelClsModel):
         config.head.num_multilabel_classes = num_multilabel_classes
         super().__init__(num_classes=num_classes, config=config)
 
+    def convert_previous_otx_ckpt(self, state_dict, add_prefix: str=""):
+        """Convert the previous OTX ckpt according to OTX2.0."""
+        return self.convert_previous_otx_ckpt_impl(state_dict, "hlabel", add_prefix)
 
-class MobileNetV3ForMulticlassCls(MMPretrainMulticlassClsModel):
+class MobileNetV3ForMulticlassCls(MobileNetV3LoadWeightMixin, MMPretrainMulticlassClsModel):
     """MobileNetV3 Model for multi-label classification task."""
 
     def __init__(self, num_classes: int, light: bool = False) -> None:
@@ -29,10 +55,17 @@ class MobileNetV3ForMulticlassCls(MMPretrainMulticlassClsModel):
         config = read_mmconfig(model_name=model_name, subdir_name="multiclass_classification")
         super().__init__(num_classes=num_classes, config=config)
 
+    def convert_previous_otx_ckpt(self, state_dict, add_prefix: str=""):
+        """Convert the previous OTX ckpt according to OTX2.0."""
+        return self.convert_previous_otx_ckpt_impl(state_dict, "multiclass", add_prefix)
 
-class MobileNetV3ForMultilabelCls(MMPretrainMultilabelClsModel):
+class MobileNetV3ForMultilabelCls(MobileNetV3LoadWeightMixin, MMPretrainMultilabelClsModel):
     """MobileNetV3 Model for multi-class classification task."""
 
     def __init__(self, num_classes: int) -> None:
         config = read_mmconfig("mobilenet_v3_large_light", subdir_name="multilabel_classification")
         super().__init__(num_classes=num_classes, config=config)
+
+    def convert_previous_otx_ckpt(self, state_dict, add_prefix: str=""):
+        """Convert the previous OTX ckpt according to OTX2.0."""
+        return self.convert_previous_otx_ckpt_impl(state_dict, "multilabel", add_prefix)
