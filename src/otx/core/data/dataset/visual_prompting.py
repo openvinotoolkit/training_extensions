de3b6b33297576ad5ffd7897dbcc0d3e141c435f
Kim, Sungchul <sungchul.kim@intel.com>
2024-01-22 18:00:12 +0900

    Update `OTXVisualPromptingDataset`

--- src/otx/core/data/dataset/visual_prompting.py
+++ src/otx/core/data/dataset/visual_prompting.py
@@ -11,8 +11,9 @@ import numpy as np
 import torch
 from datumaro import DatasetSubset, Image, Polygon
 from torchvision import tv_tensors
+import torchvision.transforms.v2.functional as F
 
-from otx.core.data.entity.base import ImageInfo
+from otx.core.data.entity.base import ImageInfo, Points
 from otx.core.data.entity.visual_prompting import VisualPromptingBatchDataEntity, VisualPromptingDataEntity
 from otx.core.utils.mask_util import polygon_to_bitmap
 
@@ -30,34 +31,73 @@ class OTXVisualPromptingDataset(OTXDataset[VisualPromptingDataEntity]):
         **kwargs: Additional keyword arguments passed to the base class.
     """
 
-    def __init__(self, dm_subset: DatasetSubset, transforms: Transforms, include_polygons: bool, **kwargs) -> None:
+    def __init__(
+        self,
+        dm_subset: DatasetSubset,
+        transforms: Transforms,
+        include_polygons: bool,
+        use_bbox: bool = True,
+        use_point: bool = False,
+        **kwargs
+    ) -> None:
         super().__init__(dm_subset, transforms, **kwargs)
         self.include_polygons = include_polygons
+        if not use_bbox and not use_point:
+            # if both are False, use bbox as default
+            use_bbox = True
+        self.prob = 1. # if using only bbox prompt
+        if use_bbox and use_point:
+            # if using both prompts, divide prob into both
+            self.prob = 0.5
+        if not use_bbox and use_point:
+            # if using only point prompt
+            self.prob = 0.
 
     def _get_item_impl(self, index: int) -> VisualPromptingDataEntity | None:
         item = self.dm_subset.get(id=self.ids[index], subset=self.dm_subset.name)
         img = item.media_as(Image)
         img_data, img_shape = self._get_img_data_and_shape(img)
 
-        gt_bboxes, gt_labels, gt_masks, gt_polygons = [], [], [], []
+        gt_prompts, gt_masks, gt_polygons, gt_labels = [], [], [], []
 
         for annotation in item.annotations:
             if isinstance(annotation, Polygon):
-                bbox = np.array(annotation.get_bbox(), dtype=np.float32)
-                gt_bboxes.append(bbox)
-                gt_labels.append(annotation.label)
-
+                mask = polygon_to_bitmap([annotation], *img_shape)[0]
                 if self.include_polygons:
                     gt_polygons.append(annotation)
                 else:
-                    gt_masks.append(polygon_to_bitmap([annotation], *img_shape)[0])
+                    gt_masks.append(mask)
 
-        # convert xywh to xyxy format
-        bboxes = np.array(gt_bboxes, dtype=np.float32)
-        bboxes[:, 2:] += bboxes[:, :2]
-
-        masks = np.stack(gt_masks, axis=0) if gt_masks else np.zeros((0, *img_shape), dtype=bool)
-        labels = np.array(gt_labels, dtype=np.int64)
+                if np.random.rand() < self.prob:
+                    # get bbox
+                    prompt = tv_tensors.BoundingBoxes(
+                        annotation.get_bbox(),
+                        format=tv_tensors.BoundingBoxFormat.XYWH,
+                        canvas_size=img_shape,
+                        dtype=torch.float32)
+                    prompt = F._meta.convert_bounding_box_format(
+                        prompt,
+                        new_format=tv_tensors.BoundingBoxFormat.XYXY)
+                else:
+                    # get point
+                    if self.dm_subset.name == "train":
+                        # get random point from the mask
+                        mask_points = np.nonzero(mask)
+                        idx_chosen = np.random.permutation(len(mask_points[0]))[0]
+                        prompt = Points(
+                            (mask_points[0][idx_chosen], mask_points[1][idx_chosen]),
+                            canvas_size=img_shape,
+                            dtype=torch.float32)
+                    else:
+                        # get center point
+                        prompt = Points(
+                            np.array(annotation.get_points()).mean(axis=0),
+                            canvas_size=img_shape,
+                            dtype=torch.float32)
+                gt_prompts.append(prompt)
+                gt_labels.append(annotation.label)
+        
+        assert len(gt_prompts) > 0, "#prompts should be greater than 0."
 
         # set entity without masks to avoid resizing masks
         entity = VisualPromptingDataEntity(
@@ -67,18 +107,15 @@ class OTXVisualPromptingDataset(OTXDataset[VisualPromptingDataEntity]):
                 img_shape=img_shape,
                 ori_shape=img_shape,
             ),
-            bboxes=tv_tensors.BoundingBoxes(
-                bboxes,
-                format=tv_tensors.BoundingBoxFormat.XYXY,
-                canvas_size=img_shape,
-            ),
             masks=None,
-            labels=torch.as_tensor(labels),
+            labels=torch.as_tensor(gt_labels, dtype=torch.int64),
             polygons=gt_polygons,
+            prompts=gt_prompts
         )
         transformed_entity = self._apply_transforms(entity)
 
         # insert masks to transformed_entity
+        masks = np.stack(gt_masks, axis=0) if gt_masks else np.zeros((0, *img_shape), dtype=bool)
         transformed_entity.masks = tv_tensors.Mask(masks, dtype=torch.uint8)  # type: ignore[union-attr]
         return transformed_entity
 
---
+++ tests/unit/core/data/dataset/test_visual_prompting.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+"""Unit tests of visual prompting datasets."""
+
+from __future__ import annotations
+
+import pytest
+import numpy as np
+from datumaro import Dataset as DmDataset
+from otx.core.data.dataset.visual_prompting import OTXVisualPromptingDataset
+from otx.core.data.entity.base import ImageInfo, Points
+from torchvision.tv_tensors import Mask, BoundingBoxes
+from torch import Tensor
+from torchvision.transforms.v2 import Transform, Identity
+
+
+class TestOTXVisualPromptingDataset:
+    @pytest.fixture()
+    def fxt_dm_dataset(self) -> DmDataset:
+        return DmDataset.import_from("tests/assets/car_tree_bug", format="coco_instances")
+    
+    @pytest.fixture()
+    def fxt_tvt_transforms(self, mocker) -> Identity:
+        return Identity()
+        
+    @pytest.mark.parametrize("subset", ["train", "val"])
+    @pytest.mark.parametrize("use_bbox", [True, False])
+    @pytest.mark.parametrize("use_point", [True, False])
+    def test_get_item_impl_subset(self, fxt_dm_dataset, fxt_tvt_transforms: Transform, subset: str, use_bbox: bool, use_point: bool) -> None:
+        dataset = OTXVisualPromptingDataset(
+            fxt_dm_dataset.subsets()[subset],
+            fxt_tvt_transforms,
+            include_polygons=False,
+            use_bbox=use_bbox,
+            use_point=use_point
+        )
+        
+        if not use_bbox and not use_point:
+            assert dataset.prob == 1.
+        
+        entity = dataset._get_item_impl(0)
+        
+        assert hasattr(entity, "image")
+        assert isinstance(entity.image, np.ndarray)
+        assert hasattr(entity, "img_info")
+        assert isinstance(entity.img_info, ImageInfo)
+        assert hasattr(entity, "masks")
+        assert isinstance(entity.masks, Mask)
+        assert hasattr(entity, "labels")
+        assert isinstance(entity.labels, Tensor)
+        assert hasattr(entity, "polygons")
+        assert isinstance(entity.polygons, list)
+        assert hasattr(entity, "prompts")
+        assert isinstance(entity.prompts, list)
+        
+        if not use_point:
+            assert isinstance(entity.prompts[0], BoundingBoxes)
+
+        if not use_bbox and use_point:
+            assert isinstance(entity.prompts[0], Points)
