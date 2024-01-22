64ac3a42593e616a6cc712a73ce3ae77bb2417a1
Evgeny Tsykunov <evgeny.tsykunov@intel.com>
2024-01-22 23:40:20 +0900

    Docstring for TargetExplainGroup

--- src/otx/core/types/explain.py
+++ src/otx/core/types/explain.py
@@ -9,9 +9,14 @@ from enum import Enum
 
 
 class TargetExplainGroup(str, Enum):
-    """OTX target explain group definition."""
+    """OTX target explain group definition.
+
+    Enum contains the following values:
+        IMAGE - This implies that single global saliency map will be generated for input image.
+        ALL - This implies that saliency maps will be generated for all possible targets.
+        PREDICTIONS - This implies that saliency map will be generated per each prediction.
+    """
 
     IMAGE = "IMAGE"
     ALL = "ALL"
     PREDICTIONS = "PREDICTIONS"
-    CUSTOM = "CUSTOM"
