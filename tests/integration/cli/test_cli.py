334bdb19a484bd56e3c21f444d16e273c7bd4f65
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2024-01-25 06:25:59 +0900

    Update cli tests

--- tests/integration/cli/test_cli.py
+++ tests/integration/cli/test_cli.py
@@ -149,7 +149,6 @@ def test_otx_e2e(recipe: str, tmp_path: Path, fxt_accelerator: str) -> None:
         task_name in recipe
         for task_name in [
             "h_label_cls",
-            "multi_label_cls",
             "detection",
             "dino_v2",
             "instance_segmentation",
@@ -206,7 +205,7 @@ def test_otx_e2e(recipe: str, tmp_path: Path, fxt_accelerator: str) -> None:
         "--engine.device",
         fxt_accelerator,
         *DATASET[task]["overrides"],
-        "--model.init_args.config.content.model_name",
+        "--model.model_name",
         exported_model_path,
     ]
 
