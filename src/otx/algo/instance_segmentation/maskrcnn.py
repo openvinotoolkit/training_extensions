37e8023332ee2e123f4644e717879521912af3ee
eunwoosh <eunwoo.shin@intel.com>
2024-01-24 14:23:30 +0900

    merge related model calss into the single file

--- src/otx/algo/instance_segmentation/maskrcnn.py
+++ src/otx/algo/instance_segmentation/maskrcnn.py
@@ -34,3 +34,25 @@ class MaskRCNN(MMDetInstanceSegCompatibleModel):
         export_params["swap_rgb"] = False
 
         return export_params
+
+
+class MaskRCNNSwinT(MMDetInstanceSegCompatibleModel):
+    """MaskRCNNSwinT Model."""
+
+    def __init__(self, num_classes: int) -> None:
+        config = read_mmconfig(model_name="maskrcnn_swint")
+        super().__init__(num_classes=num_classes, config=config)
+
+    @property
+    def export_params(self) -> dict[str, Any]:
+        """Parameters for an exporter."""
+        export_params = get_mean_std_from_data_processing(self.config)
+        export_params["model_builder"] = self._create_model
+        export_params["model_cfg"] = copy(self.config)
+        export_params["deploy_cfg"] = "otx.config.mmdeploy.instance_segmentation.maskrcnn_swint"
+        export_params["input_size"] = (1, 3, 1344, 1344)
+        export_params["resize_mode"] = "fit_to_window"
+        export_params["pad_value"] = 0
+        export_params["swap_rgb"] = False
+
+        return export_params
