ca1b581be14800907326ad2b57638982b42b54a2
GitHub Action <action@github.com>
2024-01-26 01:08:56 +0000

    Update documentation

--- releases/1.4.0/_static/documentation_options.js
+++ releases/1.4.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.4',
+    VERSION: '1.4.5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
