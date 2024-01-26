95633f567ce076c746821c6b1f5c5ffbf22ad67e
Yunchu Lee <yunchu.lee@intel.com>
2024-01-26 18:24:37 +0900

    run 4 memory drain tests

---
+++ tests/unit/test_memory4.py
@@ -0,0 +1,17 @@
+import psutil
+import time
+
+# import pytest
+
+
+# @pytest.mark.skip(reason="only for the debugging")
+def test_memory_bound4():
+    print(psutil.virtual_memory())
+    alloc = []
+    alloc_unit = "0123456789" * 1024 * 1024 * 10  # 100m
+    idx = 0
+    while idx < 1024:
+        alloc.append(f"{idx}{alloc_unit}")
+        print(f"[{idx}]{psutil.virtual_memory()}")
+        time.sleep(1)
+        idx += 1
