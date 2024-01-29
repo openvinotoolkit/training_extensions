f54eb1dbf546d35efbf5ab7e7da295504b689456
Jaeguk Hyun <jaeguk.hyun@intel.com>
2024-01-29 11:10:38 +0900

    Add ignored label to all task(cls, det, ins-seg, seg) (#2839)
    * Add atss ignored label
    
    * Add ignore labels for ins-seg
    
    * Set ignored label as default
    
    * Change ins-seg model's cls loss type
    
    * Link DatasetEntity and ignored_label
    
    * Add ignored label for classification
    
    * Link ignored_labels with Datumaro
    
    * Add ignored label to segmentation
    
    * Add unit tests
    
    * Reflect Reviews

--- src/otx/algo/detection/heads/__init__.py
+++ src/otx/algo/detection/heads/__init__.py
@@ -3,6 +3,7 @@
 """Custom head implementations for detection task."""
 
 from .custom_anchor_generator import SSDAnchorGeneratorClustered
+from .custom_atss_head import CustomATSSHead
 from .custom_ssd_head import CustomSSDHead
 
-__all__ = ["SSDAnchorGeneratorClustered", "CustomSSDHead"]
+__all__ = ["SSDAnchorGeneratorClustered", "CustomATSSHead", "CustomSSDHead"]
