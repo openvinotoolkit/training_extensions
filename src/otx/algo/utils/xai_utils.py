c9a1682467122dd0e44c526bd519809f015207e4
Evgeny Tsykunov <evgeny.tsykunov@intel.com>
2024-02-02 00:38:27 +0900

    typing

--- src/otx/algo/utils/xai_utils.py
+++ src/otx/algo/utils/xai_utils.py
@@ -11,7 +11,7 @@ def get_processed_saliency_maps(
     raw_saliency_maps: list,
     explain_config: ExplainConfig | None,
     predictions: list | None,
-) -> list | None:
+) -> list:
     """Implement saliency map filtering and post-processing."""
     # Implement <- TODO(negvet)
     selected_saliency_maps = select_saliency_maps(raw_saliency_maps, explain_config, predictions)
@@ -22,7 +22,7 @@ def select_saliency_maps(
     saliency_maps: list,
     explain_config: ExplainConfig | None,  # noqa: ARG001
     predictions: list | None,  # noqa: ARG001
-) -> list | None:
+) -> list:
     """Select saliency maps in accordance with TargetExplainGroup."""
     return saliency_maps
 
@@ -30,6 +30,6 @@ def select_saliency_maps(
 def process_saliency_maps(
     saliency_maps: list,
     explain_config: ExplainConfig | None,  # noqa: ARG001
-) -> list | None:
+) -> list:
     """Postptocess saliency maps."""
     return saliency_maps
