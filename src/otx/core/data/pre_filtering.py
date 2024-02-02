2fa6227ea7dab0cd33353f3d6382f2b1a576497e
jaegukhyun <jaeguk.hyun@intel.com>
2024-02-02 10:30:20 +0900

    Reflect Reviews

--- src/otx/core/data/pre_filtering.py
+++ src/otx/core/data/pre_filtering.py
@@ -25,31 +25,38 @@ def pre_filtering(dataset: DmDataset, data_format: str) -> DmDataset:
 
 def is_non_empty_item(item: DatasetItem) -> bool:
     """Return whether DatasetItem's annotation is non-empty."""
-    msg = "If there are empty annotation items in train set, they will be filtered out before training."
+    if not (item.subset == "train" and len(item.annotations) == 0):
+        return True
+    msg = "There are empty annotation items in train set, they will be filtered out before training."
     warnings.warn(msg, stacklevel=2)
-    return not (item.subset == "train" and len(item.annotations) == 0)
+    return False
 
 
 def is_valid_annot(item: DatasetItem, annotation: Annotation) -> bool:
     """Return whether DatasetItem's annotation is valid."""
     del item
     if isinstance(annotation, Bbox):
-        msg = "If there are bounding box which is not `x1 < x2 and y1 < y2`, they will be filtered out before training."
-        warnings.warn(msg, stacklevel=2)
         x1, y1, x2, y2 = annotation.points
-        return x1 < x2 and y1 < y2
+        if x1 < x2 and y1 < y2:
+            return True
+        msg = "There are bounding box which is not `x1 < x2 and y1 < y2`, they will be filtered out before training."
+        warnings.warn(msg, stacklevel=2)
+        return False
     if isinstance(annotation, Polygon):
+        # TODO(JaegukHyun): This process is computationally intensive.  # noqa: TD003
+        # We should make pre-filtering user-configurable.
         x_points = [annotation.points[i] for i in range(0, len(annotation.points), 2)]
         y_points = [annotation.points[i + 1] for i in range(0, len(annotation.points), 2)]
-        return min(x_points) < max(x_points) and min(y_points) < max(y_points) and annotation.get_area() > 0
+        if min(x_points) < max(x_points) and min(y_points) < max(y_points) and annotation.get_area() > 0:
+            return True
+        msg = "There are invalid polygon, they will be filtered out before training."
+        return False
     return True
 
 
 def remove_unused_labels(dataset: DmDataset, data_format: str) -> DmDataset:
     """Remove unused labels in Datumaro dataset."""
     original_categories: list[str] = dataset.get_label_cat_names()
-    msg = "If there are unused labels in dataset, they will be filtered out before training."
-    warnings.warn(msg, stacklevel=2)
     used_labels: list[int] = list({ann.label for item in dataset for ann in item.annotations})
     if data_format == "ava":
         used_labels = [0, *used_labels]
@@ -58,5 +65,9 @@ def remove_unused_labels(dataset: DmDataset, data_format: str) -> DmDataset:
             used_labels = [label - 1 for label in used_labels[1:]]
         else:
             used_labels = [label - 1 for label in used_labels]
+    if len(used_labels) == len(original_categories):
+        return dataset
+    msg = "There are unused labels in dataset, they will be filtered out before training."
+    warnings.warn(msg, stacklevel=2)
     mapping = {original_categories[idx]: original_categories[idx] for idx in used_labels}
     return dataset.transform("remap_labels", mapping=mapping, default="delete")
