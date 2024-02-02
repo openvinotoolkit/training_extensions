9d140ed619b3f03176847bb012bdd80fdf40a836
jaegukhyun <jaeguk.hyun@intel.com>
2024-02-02 09:41:37 +0900

    Except h label from pre filtering

--- src/otx/core/data/module.py
+++ src/otx/core/data/module.py
@@ -52,7 +52,8 @@ class OTXDataModule(LightningDataModule):
         VIDEO_EXTENSIONS.append(".mp4")
 
         dataset = DmDataset.import_from(self.config.data_root, format=self.config.data_format)
-        dataset = pre_filtering(dataset, self.config.data_format)
+        if self.task != "H_LABEL_CLS":
+            dataset = pre_filtering(dataset, self.config.data_format)
 
         config_mapping = {
             self.config.train_subset.subset_name: self.config.train_subset,
