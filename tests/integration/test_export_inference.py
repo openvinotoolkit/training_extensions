e6e0eaa3992c1b0bbfb6df026c0ffecd3f88faa1
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2024-02-02 10:25:18 +0900

    Transparent forwarding of deterministic parameter

--- tests/integration/test_export_inference.py
+++ tests/integration/test_export_inference.py
@@ -83,10 +83,7 @@ def test_otx_export_infer(
     if task not in TASK_NAME_TO_MAIN_METRIC_NAME or "dino_v2" in recipe:
         pytest.skip(f"Inference pipeline for {recipe} is not implemented")
 
-    # litehrnet_* models don't support deterministic mode
     model_name = recipe.split("/")[-1].split(".")[0]
-    deterministic_flag = "False" if "litehrnet" in recipe else "True"
-
     # 1) otx train
     tmp_path_train = tmp_path / f"otx_train_{model_name}"
     command_cfg = [
@@ -105,7 +102,7 @@ def test_otx_export_infer(
         "--seed",
         f"{fxt_local_seed}",
         "--deterministic",
-        deterministic_flag,
+        "warn",
         *fxt_cli_override_command_per_task[task],
     ]
 
