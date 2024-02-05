29045059a1ea880ff6ca31e4ef6f2f6184353caf
sungmanc <sungman.cho@intel.com>
2024-02-05 17:07:07 +0900

    Fix precommit

--- src/otx/algo/metrices/accuracy.py
+++ src/otx/algo/metrices/accuracy.py
@@ -4,6 +4,7 @@
 """Module for OTX accuracy metric used for classification tasks."""
 
 from __future__ import annotations
+
 from typing import TYPE_CHECKING, Literal
 
 from torchmetrics import Metric
@@ -13,20 +14,16 @@ if TYPE_CHECKING:
 
 
 class Accuracy(Metric):
+    """Skleton code for future implementation."""
+
     def __init__(self, average: Literal["MICRO", "MACRO"]):
         self.average = average
-    
+
     def update(self, preds: Tensor, target: Tensor) -> None:
         """Update state with predictions and targets."""
-        pass
-    
+
     def compute(self) -> Tensor:
-        pass
-    
-    
-    def _compute_unnormalized_confusion_metrices_from_preds(self, preds: Tensor, ):
-        """Compute an (unnormalized) confusion matriix for every label group.""" 
-        pass
-    
-    def _get_gt_and_pred_label_indices(self, preds: Tensor):
-        pass
+        """Compute the metric."""
+
+    def _compute_unnormalized_confusion_metrices_from_preds(self, preds: Tensor) -> None:
+        """Compute an (unnormalized) confusion matriix for every label group."""
