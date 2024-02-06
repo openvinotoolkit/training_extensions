1535d304b38bb531af407f33501846cc8edf89d8
Kang, Harim <harim.kang@intel.com>
2024-02-06 15:35:50 +0900

    Reduce max value of num_iters to 1k

--- src/otx/algorithms/common/configs/training_base.py
+++ src/otx/algorithms/common/configs/training_base.py
@@ -95,7 +95,7 @@ class BaseConfig(ConfigurableParameters):
         num_iters = configurable_integer(
             default_value=1,
             min_value=1,
-            max_value=100000,
+            max_value=1000,
             header="Number of training iterations",
             description="Increasing this value causes the results to be more robust but training time will be longer.",
             affects_outcome_of=ModelLifecycle.TRAINING,
