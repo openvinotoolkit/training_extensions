2aab09bdabd3c65293475c287ad5c441985107ae
Kang, Harim <harim.kang@intel.com>
2024-02-08 17:37:14 +0900

    Add CLI link between checkpoint and model.model_name

--- tests/integration/cli/test_export_inference.py
+++ tests/integration/cli/test_export_inference.py
@@ -189,7 +189,7 @@ def test_otx_export_infer(
         "--engine.device",
         "cpu",
         *fxt_cli_override_command_per_task[task],
-        "--model.model_name",
+        "--checkpoint",
         exported_model_path,
     ]
 
@@ -232,7 +232,7 @@ def test_otx_export_infer(
         "--engine.device",
         "cpu",
         *fxt_cli_override_command_per_task[task],
-        "--model.model_name",
+        "--checkpoint",
         exported_model_path,
     ]
 
