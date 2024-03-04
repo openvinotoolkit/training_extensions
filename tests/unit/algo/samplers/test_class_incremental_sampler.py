a1545d892ff1d6930c53b662f6184ea07b06cada
Kang, Harim <harim.kang@intel.com>
2024-03-04 15:54:15 +0900

    Fix pre-commit

--- tests/unit/algo/samplers/test_class_incremental_sampler.py
+++ tests/unit/algo/samplers/test_class_incremental_sampler.py
@@ -12,37 +12,41 @@ from otx.core.utils.utils import get_idx_list_per_classes
 
 @pytest.fixture()
 def fxt_old_new_dataset() -> OTXDataset:
-    dataset_items = [
-        DatasetItem(
-            id=f"item00{i}_0",
-            subset="train",
-            media=None,
-            annotations=[
-                Label(label=0),
-            ],
-        )
-        for i in range(1, 101)
-    ] + [
-        DatasetItem(
-            id=f"item00{i}_1",
-            subset="train",
-            media=None,
-            annotations=[
-                Label(label=1),
-            ],
-        )
-        for i in range(1, 9)
-    ] + [
-        DatasetItem(
-            id=f"item00{i}_2",
-            subset="train",
-            media=None,
-            annotations=[
-                Label(label=2),
-            ],
-        )
-        for i in range(1, 9)
-    ]
+    dataset_items = (
+        [
+            DatasetItem(
+                id=f"item00{i}_0",
+                subset="train",
+                media=None,
+                annotations=[
+                    Label(label=0),
+                ],
+            )
+            for i in range(1, 101)
+        ]
+        + [
+            DatasetItem(
+                id=f"item00{i}_1",
+                subset="train",
+                media=None,
+                annotations=[
+                    Label(label=1),
+                ],
+            )
+            for i in range(1, 9)
+        ]
+        + [
+            DatasetItem(
+                id=f"item00{i}_2",
+                subset="train",
+                media=None,
+                annotations=[
+                    Label(label=2),
+                ],
+            )
+            for i in range(1, 9)
+        ]
+    )
 
     dm_dataset = DmDataset.from_iterable(dataset_items, categories=["0", "1", "2"])
     return OTXDataset(
@@ -67,8 +71,8 @@ class TestBalancedSampler:
 
         assert count == len(sampler)
         assert len(sampler.old_indices) == 108  # "0" + "1"
-        assert len(sampler.new_indices) == 8    # "2"
-        assert sampler.old_new_ratio == 3       # np.sqrt(108 / 8)
+        assert len(sampler.new_indices) == 8  # "2"
+        assert sampler.old_new_ratio == 3  # np.sqrt(108 / 8)
         assert sampler.num_samples == len(fxt_old_new_dataset)
 
     def test_sampler_efficient_mode(self, fxt_old_new_dataset):
@@ -87,9 +91,9 @@ class TestBalancedSampler:
 
         assert count == len(sampler)
         assert len(sampler.old_indices) == 108  # "0" + "1"
-        assert len(sampler.new_indices) == 8    # "2"
-        assert sampler.old_new_ratio == 1       # efficient_mode
-        assert sampler.data_length == 37 # 37
+        assert len(sampler.new_indices) == 8  # "2"
+        assert sampler.old_new_ratio == 1  # efficient_mode
+        assert sampler.data_length == 37  # 37
 
     def test_sampler_iter_per_class(self, fxt_old_new_dataset):
         batch_size = 4
@@ -106,5 +110,5 @@ class TestBalancedSampler:
         list_iter = list(iter(sampler))
         for i in range(0, len(sampler), batch_size):
             batch = sorted(list_iter[i : i + batch_size])
-            assert all(idx in old_idx for idx in batch[:sampler.old_new_ratio])
-            assert all(idx in new_idx for idx in batch[sampler.old_new_ratio:])
+            assert all(idx in old_idx for idx in batch[: sampler.old_new_ratio])
+            assert all(idx in new_idx for idx in batch[sampler.old_new_ratio :])
