9e5dc92db3ae2759641b5e3827542d04565ae3c0
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2024-03-07 06:17:34 +0900

    Update data pipeline for Swin-MRCNN is tiling

--- src/otx/algorithms/detection/configs/instance_segmentation/maskrcnn_swin_t/tile_pipeline.py
+++ src/otx/algorithms/detection/configs/instance_segmentation/maskrcnn_swin_t/tile_pipeline.py
@@ -5,3 +5,46 @@
 
 
 _base_ = ["../../base/data/tiling/base_iseg_tile_pipeline.py"]
+
+img_size = (512, 512)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+
+train_pipeline = [
+    dict(type="Resize", img_scale=img_size, keep_ratio=False),
+    dict(type="RandomFlip", flip_ratio=0.5),
+    dict(type="Normalize", **img_norm_cfg),
+    dict(type="Pad", size=img_size),
+    dict(type="DefaultFormatBundle"),
+    dict(
+        type="Collect",
+        keys=["img", "gt_bboxes", "gt_labels", "gt_masks"],
+        meta_keys=[
+            "filename",
+            "ori_filename",
+            "ori_shape",
+            "img_shape",
+            "pad_shape",
+            "scale_factor",
+            "flip",
+            "flip_direction",
+            "img_norm_cfg",
+        ],
+    ),
+]
+
+test_pipeline = [
+    dict(
+        type="MultiScaleFlipAug",
+        img_scale=img_size,
+        flip=False,
+        transforms=[
+            dict(type="Resize", keep_ratio=False),
+            dict(type="RandomFlip"),
+            dict(type="Normalize", **img_norm_cfg),
+            dict(type="Pad", size=img_size),
+            dict(type="ImageToTensor", keys=["img"]),
+            dict(type="Collect", keys=["img"]),
+        ],
+    )
+]
