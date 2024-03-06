1a5ece995834536bef884899e148e1dc62781fb7
Shin, Eunwoo <eunwoo.shin@intel.com>
2024-03-06 08:24:14 +0000

    align with pre-commit

--- src/otx/algorithms/detection/adapters/mmdet/models/patch_mmdeploy.py
+++ src/otx/algorithms/detection/adapters/mmdet/models/patch_mmdeploy.py
@@ -4,14 +4,13 @@
 #
 
 import torch
+
 from otx.algorithms.common.adapters.mmdeploy.utils import is_mmdeploy_enabled
 
 
-def _select_nms_index(scores: torch.Tensor,
-                    boxes: torch.Tensor,
-                    nms_index: torch.Tensor,
-                    batch_size: int,
-                    keep_top_k: int = -1):
+def _select_nms_index(
+    scores: torch.Tensor, boxes: torch.Tensor, nms_index: torch.Tensor, batch_size: int, keep_top_k: int = -1
+):
     """Transform NMS output.
 
     Args:
@@ -37,42 +36,38 @@ def _select_nms_index(scores: torch.Tensor,
 
     # batch all
     batched_dets = dets.unsqueeze(0).repeat(batch_size, 1, 1)
-    batch_template = torch.arange(
-        0, batch_size, dtype=batch_inds.dtype, device=batch_inds.device)
+    batch_template = torch.arange(0, batch_size, dtype=batch_inds.dtype, device=batch_inds.device)
     batched_dets = batched_dets.where(
-        (batch_inds == batch_template.unsqueeze(1)).unsqueeze(-1),
-        batched_dets.new_zeros(1))
+        (batch_inds == batch_template.unsqueeze(1)).unsqueeze(-1), batched_dets.new_zeros(1)
+    )
 
     batched_labels = cls_inds.unsqueeze(0).repeat(batch_size, 1)
     batched_labels = batched_labels.where(
-        (batch_inds == batch_template.unsqueeze(1)),
-        batched_labels.new_ones(1, dtype=batched_labels.dtype) * -1)  # this line is only different line from original code
+        (batch_inds == batch_template.unsqueeze(1)), batched_labels.new_ones(1, dtype=batched_labels.dtype) * -1
+    )  # this line is only different line from original code
 
     N = batched_dets.shape[0]
 
     # expand tensor to eliminate [0, ...] tensor
-    batched_dets = torch.cat((batched_dets, batched_dets.new_zeros((N, 1, 5))),
-                            1)
-    batched_labels = torch.cat((batched_labels, batched_labels.new_zeros(
-        (N, 1))), 1)
+    batched_dets = torch.cat((batched_dets, batched_dets.new_zeros((N, 1, 5))), 1)
+    batched_labels = torch.cat((batched_labels, batched_labels.new_zeros((N, 1))), 1)
 
     # sort
-    is_use_topk = keep_top_k > 0 and \
-        (torch.onnx.is_in_onnx_export() or keep_top_k < batched_dets.shape[1])
+    is_use_topk = keep_top_k > 0 and (torch.onnx.is_in_onnx_export() or keep_top_k < batched_dets.shape[1])
     if is_use_topk:
         _, topk_inds = batched_dets[:, :, -1].topk(keep_top_k, dim=1)
     else:
         _, topk_inds = batched_dets[:, :, -1].sort(dim=1, descending=True)
-    topk_batch_inds = torch.arange(
-        batch_size, dtype=topk_inds.dtype,
-        device=topk_inds.device).view(-1, 1)
+    topk_batch_inds = torch.arange(batch_size, dtype=topk_inds.dtype, device=topk_inds.device).view(-1, 1)
     batched_dets = batched_dets[topk_batch_inds, topk_inds, ...]
     batched_labels = batched_labels[topk_batch_inds, topk_inds, ...]
 
     # slice and recover the tensor
     return batched_dets, batched_labels
 
+
 if is_mmdeploy_enabled():
 
     from mmdeploy.codebase.mmdet.core.post_processing import bbox_nms
+
     bbox_nms.select_nms_index = _select_nms_index
