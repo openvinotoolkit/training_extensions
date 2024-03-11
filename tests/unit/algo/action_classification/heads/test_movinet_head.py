e904e21b98de8d1fe0839aea0286c8a5f50cb7f9
jaegukhyun <jaeguk.hyun@intel.com>
2024-03-11 14:58:08 +0900

    Modify unit test

--- tests/unit/algo/action_classification/heads/test_movinet_head.py
+++ tests/unit/algo/action_classification/heads/test_movinet_head.py
@@ -13,13 +13,13 @@ class TestMoViNetHead:
     def fxt_movinet_head(self) -> MoViNetHead:
         return MoViNetHead(
             5,
-            480,
-            2048,
+            24,
+            48,
             {"type": "CrossEntropyLoss", "loss_weight": 1.0},
             average_clips="prob",
         )
 
     def test_forward(self, fxt_movinet_head: MoViNetHead) -> None:
         fxt_movinet_head.init_weights()
-        x = torch.randn(5, 480, 1, 1, 1)
+        x = torch.randn(5, 24, 1, 1, 1)
         assert fxt_movinet_head(x).shape == torch.Size([5, 5])
