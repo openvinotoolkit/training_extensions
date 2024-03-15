2f6f9f2d119b5d7122da8b4b303a95fbd1a88e68
Kang, Harim <harim.kang@intel.com>
2024-03-15 09:46:52 +0900

    Add from_model_name in Engine

---
+++ tests/unit/engine/test_engine.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from otx.engine import Engine
+
+
+class TestEngine:
+    def test_from_config_with_model_name(self, tmp_path) -> None:
+        model_name = "efficientnet_b0_light"
+        data_root = "tests/assets/classification_dataset"
+        task_type = "MULTI_CLASS_CLS"
+
+        overriding = {
+            "data.config.train_subset.batch_size": 2,
+            "data.config.test_subset.subset_name": "TESTING",
+        }
+
+        engine = Engine.from_model_name(
+            model_name=model_name,
+            data_root=data_root,
+            task=task_type,
+            work_dir=tmp_path,
+            **overriding,
+        )
+
+        assert engine is not None
+        assert engine.datamodule.config.train_subset.batch_size == 2
+        assert engine.datamodule.config.test_subset.subset_name == "TESTING"
