6e6be3b3ebef28554c415504e2bc341f058ce8c5
Kim, Sungchul <sungchul.kim@intel.com>
2024-03-15 14:04:43 +0900

    Refactoring for zero-shot standalone usecase (#3132)
    * Refactoring for standalone usecase
    
    * Fix bug on cascade inference
    
    * precommit
    
    * Fix unittest
    
    * Update regression test
    
    * Update `num_workers` in `train_dataloader` to avoid seg fault
    
    * Update metric names
    
    * Update integration test
    
    * Update `is_cascade` in `infer` to True
    
    * Update vpm recipe
    
    * Remove unused condition
    
    * Revert unnecessary operation
    
    * Remain empty lists
    
    * precommit
    
    * Add ticket no.

--- tests/integration/cli/test_export_inference.py
+++ tests/integration/cli/test_export_inference.py
@@ -44,8 +44,8 @@ TASK_NAME_TO_MAIN_METRIC_NAME = {
     "h_label_cls": "test/accuracy",
     "detection": "test/map_50",
     "instance_segmentation": "test/map_50",
-    "visual_prompting": "test/Dice",
-    "zero_shot_visual_prompting": "test/F1",
+    "visual_prompting": "test/f1-score",
+    "zero_shot_visual_prompting": "test/f1-score",
 }
 
 
@@ -210,11 +210,6 @@ def test_otx_export_infer(
     assert (tmp_path_test / "outputs").exists()
 
     # 5) test optimize
-    if task in ("visual_prompting", "zero_shot_visual_prompting"):
-        pytest.xfail(
-            "Optimize for visual prompting and zero shot visual prompting yields segmentation fault after optimize.",
-        )
-
     command_cfg = [
         "otx",
         "optimize",
