d7ef120b91d91426f80b59c142a5517254ee696a
kprokofi <kirill.prokofiev@intel.com>
2024-03-19 00:44:27 +0900

    add xpu devices

--- tests/perf/test_detection.py
+++ tests/perf/test_detection.py
@@ -118,7 +118,7 @@ class TestPerfObjectDetection(PerfTestBase):
         fxt_dataset: Benchmark.Dataset,
         fxt_benchmark: Benchmark,
     ):
-        fxt_benchmark.accelerator = "gpu"
+        fxt_benchmark.accelerator = "xpu"
         self._test_perf(
             model=fxt_model,
             dataset=fxt_dataset,
