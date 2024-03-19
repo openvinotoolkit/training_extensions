1f3182f9e7471059b2db32ea084d2793440e2382
Kang, Harim <harim.kang@intel.com>
2024-03-19 17:49:09 +0900

    Fix import test

--- tests/unit/core/utils/test_imports.py
+++ tests/unit/core/utils/test_imports.py
@@ -1,8 +1,10 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import inspect
 from pathlib import Path
 
+import otx
 import pytest
 from otx.core.utils.imports import get_otx_root_path
 
@@ -10,7 +12,8 @@ from otx.core.utils.imports import get_otx_root_path
 def test_get_otx_root_path(mocker):
     root_path = get_otx_root_path()
     assert isinstance(root_path, Path)
-    assert str(root_path) == str(Path("./src/otx").resolve())
+    otx_path = inspect.getfile(otx)
+    assert root_path == Path(otx_path).parent
 
     with mocker.patch("importlib.import_module", return_value=None) and pytest.raises(ModuleNotFoundError):
         get_otx_root_path()
