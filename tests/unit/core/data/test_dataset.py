e1253f79744ab3f667f82c48d3ce245ccab12701
jaegukhyun <jaeguk.hyun@intel.com>
2024-03-19 17:21:08 +0900

    Add unit tests for dataset and module

--- tests/unit/core/data/test_dataset.py
+++ tests/unit/core/data/test_dataset.py
@@ -4,6 +4,8 @@
 from unittest.mock import MagicMock
 
 import pytest
+from otx.core.data.dataset.action_classification import OTXActionClsDataset
+from otx.core.data.dataset.classification import HLabelInfo
 
 
 class TestDataset:
@@ -12,14 +14,18 @@ class TestDataset:
         mocker,
         fxt_dataset_and_data_entity_cls,
         fxt_mock_dm_subset: MagicMock,
+        fxt_mock_hlabelinfo,
     ) -> None:
-        dataset_cls, data_entity_cls = fxt_dataset_and_data_entity_cls
+        dataset_cls, data_entity_cls, kwargs = fxt_dataset_and_data_entity_cls
+        mocker.patch.object(HLabelInfo, "from_dm_label_groups", return_value=fxt_mock_hlabelinfo)
         dataset = dataset_cls(
             dm_subset=fxt_mock_dm_subset,
-            transforms=lambda x: x,
+            transforms=[lambda x: x],
             mem_cache_img_max_size=None,
             max_refetch=3,
+            **kwargs,
         )
+        dataset.num_classes = 1
         item = dataset[0]
 
         assert isinstance(item, data_entity_cls)
@@ -31,43 +37,53 @@ class TestDataset:
 
     def test_sample_another_idx(
         self,
+        mocker,
         fxt_dataset_and_data_entity_cls,
         fxt_mock_dm_subset,
+        fxt_mock_hlabelinfo,
     ) -> None:
-        dataset_cls, dataset_entity_cls = fxt_dataset_and_data_entity_cls
+        dataset_cls, data_entity_cls, kwargs = fxt_dataset_and_data_entity_cls
+        mocker.patch.object(HLabelInfo, "from_dm_label_groups", return_value=fxt_mock_hlabelinfo)
         dataset = dataset_cls(
             dm_subset=fxt_mock_dm_subset,
             transforms=lambda x: x,
             mem_cache_img_max_size=None,
+            **kwargs,
         )
+        dataset.num_classes = 1
         assert dataset._sample_another_idx() < len(dataset)
 
     @pytest.mark.parametrize("mem_cache_img_max_size", [(3, 5), (5, 3)])
     def test_mem_cache_resize(
         self,
+        mocker,
         mem_cache_img_max_size,
         fxt_mem_cache_handler,
         fxt_dataset_and_data_entity_cls,
         fxt_mock_dm_subset: MagicMock,
         fxt_dm_item,
+        fxt_mock_hlabelinfo,
     ) -> None:
-        dataset_cls, data_entity_cls = fxt_dataset_and_data_entity_cls
-
+        dataset_cls, data_entity_cls, kwargs = fxt_dataset_and_data_entity_cls
+        mocker.patch.object(HLabelInfo, "from_dm_label_groups", return_value=fxt_mock_hlabelinfo)
         dataset = dataset_cls(
             dm_subset=fxt_mock_dm_subset,
             transforms=lambda x: x,
             mem_cache_handler=fxt_mem_cache_handler,
             mem_cache_img_max_size=mem_cache_img_max_size,
+            **kwargs,
         )
+        dataset.num_classes = 1
 
         item = dataset[0]  # Put in the cache
 
         # The returned image should be resized because it was resized before caching
         h_expected = w_expected = min(mem_cache_img_max_size)
-        assert item.image.shape[:2] == (h_expected, w_expected)
-        assert item.img_info.img_shape == (h_expected, w_expected)
+        if dataset_cls != OTXActionClsDataset:  # Action classification dataset handle video, not image.
+            assert item.image.shape[:2] == (h_expected, w_expected)
+            assert item.img_info.img_shape == (h_expected, w_expected)
 
-        item = dataset[0]  # Take from the cache
+            item = dataset[0]  # Take from the cache
 
-        assert item.image.shape[:2] == (h_expected, w_expected)
-        assert item.img_info.img_shape == (h_expected, w_expected)
+            assert item.image.shape[:2] == (h_expected, w_expected)
+            assert item.img_info.img_shape == (h_expected, w_expected)
