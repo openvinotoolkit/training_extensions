2195fb6461027a52a4b8b0b8b106c4a13f7277bd
Eugene Liu <eugene.liu@intel.com>
2024-03-19 15:54:29 +0000

    cpu device

--- tests/unit/algo/instance_segmentation/heads/test_custom_rtmdet_ins_head.py
+++ tests/unit/algo/instance_segmentation/heads/test_custom_rtmdet_ins_head.py
@@ -74,9 +74,10 @@ class TestCustomRTMDetInsSepBNHead:
             cfg=None,
         )
 
-    def test_predict_by_feat_onnx(self, mocker) -> None:
+    def test_predict_by_feat_onnx(self) -> None:
         lit_module = RTMDetInst(num_classes=1, variant="tiny")
         lit_module.eval()
+        lit_module.to('cpu')
         with tempfile.TemporaryDirectory() as tmpdirname:
             exported_model_path = lit_module.export(
                 output_dir=Path(tmpdirname),
