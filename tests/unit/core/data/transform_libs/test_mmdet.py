f98d7331b74a74e9855173a837c88caeb746b008
jaegukhyun <jaeguk.hyun@intel.com>
2024-03-20 12:45:54 +0900

    Add unit tests for transfrom lib

--- tests/unit/core/data/transform_libs/test_mmdet.py
+++ tests/unit/core/data/transform_libs/test_mmdet.py
@@ -7,9 +7,12 @@ from __future__ import annotations
 import numpy as np
 import pytest
 import torch
+from datumaro import Polygon
+from mmdet.structures.mask import PolygonMasks
 from otx.core.config.data import SubsetConfig
 from otx.core.data.entity.base import ImageInfo
 from otx.core.data.entity.detection import DetDataEntity
+from otx.core.data.entity.instance_segmentation import InstanceSegDataEntity
 from otx.core.data.entity.visual_prompting import VisualPromptingDataEntity
 from otx.core.data.transform_libs.mmcv import LoadImageFromFile
 from otx.core.data.transform_libs.mmdet import LoadAnnotations, MMDetTransformLib, PackDetInputs, PerturbBoundingBoxes
@@ -19,7 +22,7 @@ from torchvision import tv_tensors
 
 
 class TestLoadAnnotations:
-    def test_transform(self) -> None:
+    def test_det_transform(self) -> None:
         data_entity = DetDataEntity(
             tv_tensors.Image(torch.randn(3, 224, 224)),
             ImageInfo(img_idx=0, img_shape=(224, 224), ori_shape=(224, 224)),
@@ -32,6 +35,28 @@ class TestLoadAnnotations:
         results = transform.transform(data_entity)
         assert np.all(results["gt_bboxes"] == np.array([[0.0, 0.0, 50.0, 50.0]]))
         assert results["gt_bboxes_labels"] == np.array([1])
+        assert results["gt_ignore_flags"] == np.array([False])
+
+    def test_vp_transform(self) -> None:
+        data_entity = VisualPromptingDataEntity(
+            image=np.ndarray([3, 10, 10]),
+            img_info=ImageInfo(
+                img_idx=0,
+                img_shape=(10, 10),
+                ori_shape=(10, 10),
+            ),
+            masks=[],
+            labels=LongTensor([0]),
+            polygons=[Polygon(points=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6], label=0)],
+            points=torch.Tensor([[0.1, 0.2, 0.3, 0.4, 0.5, 0.6]]),
+            bboxes=torch.Tensor([[0, 0, 1, 1]]),
+        )
+        data_entity = LoadImageFromFile().transform(data_entity)
+        transform = LoadAnnotations(with_mask=True)
+        results = transform.transform(data_entity)
+        assert np.all(results["gt_bboxes"] == np.array([[0.0, 0.0, 1.0, 1.0]]))
+        assert isinstance(results["gt_masks"], PolygonMasks)
+        assert results["gt_bboxes_labels"] == np.array([0])
         assert results["gt_ignore_flags"] == np.array([False])
 
 
@@ -54,6 +79,22 @@ class TestPackDetInputs:
                 torch.Size([3, 224, 224]),
             ),
             (
+                InstanceSegDataEntity(
+                    image=np.ndarray([10, 10, 3]),
+                    img_info=ImageInfo(
+                        img_idx=0,
+                        img_shape=(10, 10),
+                        ori_shape=(10, 10),
+                    ),
+                    masks=[],
+                    labels=LongTensor([0]),
+                    polygons=[Polygon(points=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6], label=0)],
+                    bboxes=torch.Tensor([[0, 0, 1, 1]]),
+                ),
+                False,
+                torch.Size([3, 10, 10]),
+            ),
+            (
                 VisualPromptingDataEntity(
                     image=np.ndarray((1024, 1024, 3)),
                     img_info=ImageInfo(img_idx=0, img_shape=(1024, 1024), ori_shape=(1024, 1024)),
@@ -63,9 +104,9 @@ class TestPackDetInputs:
                         canvas_size=(1024, 1024),
                     ),
                     points=None,  # TODO(sungchul): add point prompts in mmx # noqa: TD003
-                    masks=None,
+                    masks=[],
                     labels=LongTensor([1]),
-                    polygons=None,
+                    polygons=[Polygon(points=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6], label=0)],
                 ),
                 False,  # TODO(sungchul): add point prompts in mmx # noqa: TD003
                 torch.Size([3, 1024, 1024]),
@@ -79,7 +120,9 @@ class TestPackDetInputs:
         expected: torch.Size,
     ) -> None:
         transform = PackDetInputs()
-        data_entity = LoadAnnotations(with_point=with_point).transform(LoadImageFromFile().transform(data_entity))
+        data_entity = LoadAnnotations(with_point=with_point, with_mask=True).transform(
+            LoadImageFromFile().transform(data_entity),
+        )
 
         results = transform.transform(data_entity)
 
