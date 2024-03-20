6d3dafa25f866b65f180226c4d71ef561c295611
Eugene Liu <eugene.liu@intel.com>
2024-03-20 10:51:03 +0000

    change from cuda to cpu for test

--- tests/unit/algo/instance_segmentation/heads/test_custom_rtmdet_ins_head.py
+++ tests/unit/algo/instance_segmentation/heads/test_custom_rtmdet_ins_head.py
@@ -76,13 +76,12 @@ class TestCustomRTMDetInsSepBNHead:
         )
 
     def test_predict_by_feat_ov(self) -> None:
-        assert torch.cuda.is_available(), "CUDA is not available"
         lit_module = RTMDetInst(num_classes=1, variant="tiny")
         with tempfile.TemporaryDirectory() as tmpdirname, torch.no_grad():
-            lit_module = lit_module.cuda()
+            lit_module = lit_module.cpu()
             lit_module.model.bbox_head.anchor_generator.single_level_grid_priors = partial(
                 lit_module.model.bbox_head.anchor_generator.single_level_grid_priors,
-                device="cuda",
+                device="cpu",
             )
             exported_model_path = lit_module.export(
                 output_dir=Path(tmpdirname),
