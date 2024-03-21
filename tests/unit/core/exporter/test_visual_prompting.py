fde2838724aff36d4f15ab4a13ec7779348e4019
Kim, Sungchul <sungchul.kim@intel.com>
2024-03-21 09:07:54 +0900

    precommit

--- tests/unit/core/exporter/test_visual_prompting.py
+++ tests/unit/core/exporter/test_visual_prompting.py
@@ -29,13 +29,23 @@ class TestOTXVisualPromptingModelExporter:
         mocker_torch_onnx_export = mocker.patch("torch.onnx.export")
         mocker_onnx_load = mocker.patch("onnx.load")
         mocker_onnx_save = mocker.patch("onnx.save")
-        mocker_postprocess_onnx_model = mocker.patch.object(otx_visual_prompting_model_exporter, "_postprocess_onnx_model")
+        mocker_postprocess_onnx_model = mocker.patch.object(
+            otx_visual_prompting_model_exporter,
+            "_postprocess_onnx_model",
+        )
         mocker_openvino_convert_model = mocker.patch("openvino.convert_model")
-        mocker_postprocess_openvino_model = mocker.patch.object(otx_visual_prompting_model_exporter, "_postprocess_openvino_model")
+        mocker_postprocess_openvino_model = mocker.patch.object(
+            otx_visual_prompting_model_exporter,
+            "_postprocess_openvino_model",
+        )
         mocker_openvino_save_model = mocker.patch("openvino.save_model")
-        
-        otx_visual_prompting_model_exporter.export(model=MockModel(), output_dir=tmpdir, export_format=OTXExportFormatType.OPENVINO)
-        
+
+        otx_visual_prompting_model_exporter.export(
+            model=MockModel(),
+            output_dir=tmpdir,
+            export_format=OTXExportFormatType.OPENVINO,
+        )
+
         mocker_torch_onnx_export.assert_called()
         mocker_onnx_load.assert_called()
         mocker_onnx_save.assert_called()
@@ -43,23 +53,33 @@ class TestOTXVisualPromptingModelExporter:
         mocker_openvino_convert_model.assert_called()
         mocker_postprocess_openvino_model.assert_called()
         mocker_openvino_save_model.assert_called()
-        
+
     def test_export_onnx(self, mocker, tmpdir, otx_visual_prompting_model_exporter) -> None:
         """Test export for ONNX."""
         mocker_torch_onnx_export = mocker.patch("torch.onnx.export")
         mocker_onnx_load = mocker.patch("onnx.load")
         mocker_onnx_save = mocker.patch("onnx.save")
-        mocker_postprocess_onnx_model = mocker.patch.object(otx_visual_prompting_model_exporter, "_postprocess_onnx_model")
-        
-        otx_visual_prompting_model_exporter.export(model=MockModel(), output_dir=tmpdir, export_format=OTXExportFormatType.ONNX)
-        
+        mocker_postprocess_onnx_model = mocker.patch.object(
+            otx_visual_prompting_model_exporter,
+            "_postprocess_onnx_model",
+        )
+
+        otx_visual_prompting_model_exporter.export(
+            model=MockModel(),
+            output_dir=tmpdir,
+            export_format=OTXExportFormatType.ONNX,
+        )
+
         mocker_torch_onnx_export.assert_called()
         mocker_onnx_load.assert_called()
         mocker_onnx_save.assert_called()
         mocker_postprocess_onnx_model.assert_called()
-        
+
     def test_export_exportable_code(self, tmpdir, otx_visual_prompting_model_exporter) -> None:
         """Test export for EXPORTABLE_CODE."""
         with pytest.raises(NotImplementedError):
-            otx_visual_prompting_model_exporter.export(model=MockModel(), output_dir=tmpdir, export_format=OTXExportFormatType.EXPORTABLE_CODE)
-        
+            otx_visual_prompting_model_exporter.export(
+                model=MockModel(),
+                output_dir=tmpdir,
+                export_format=OTXExportFormatType.EXPORTABLE_CODE,
+            )
