0ffcdc539c673386863d82069061ae1aaa512ece
Prokofiev Kirill <kirill.prokofiev@intel.com>
2024-03-25 00:30:16 +0100

    Enable training on XPU devices in OTX2.0 (#3094)
    * add raising an error when metric is None
    
    * added accelerators
    
    * fix packages
    
    * fix assigning model
    
    * debug on MAX
    
    * change precision
    
    * update MixedPrecisionXPUPlugin
    
    * debug
    
    * added monkey patching
    
    * minor
    
    * minor
    
    * added patch for mmengine
    
    * fix OD and IS
    
    * benchmark debug
    
    * change device
    
    * quick fix for instance seg
    
    * fix pre-commit
    
    * fix pre-commit
    
    * clean the code
    
    * added additional flag for mmcv
    
    * added unit tests
    
    * fixed unit test
    
    * fix linter
    
    * added unit tests and replied comments
    
    * fix pre-commit
    
    * minor fix
    
    * added documentation
    
    * fix unit test
    
    * add workaround for semantic segmentation
    
    * remove RoiAlignTest due to unstability
    
    * minor
    
    * remove strategy back
    
    * try to patch SingleDeviceStrategy
    
    * added auto xpu configuration
    
    * patch strategy
    
    * small fix
    
    * reply to comments
    
    * move patching xpu packages to accelerator
    
    * fix test_xpu test
    
    * remove do-not-install-mmcv
    
    * fix pre-commit
    
    * remove torch.xpu.optimize for segmentation
    
    ---------
    
    Co-authored-by: Emily <emily.chun@intel.com>

--- src/otx/engine/engine.py
+++ src/otx/engine/engine.py
@@ -15,6 +15,7 @@ from warnings import warn
 import torch
 from lightning import Trainer, seed_everything
 
+from otx.algo.plugins import MixedPrecisionXPUPlugin
 from otx.core.config.device import DeviceConfig
 from otx.core.config.explain import ExplainConfig
 from otx.core.config.hpo import HpoConfig
@@ -26,6 +27,7 @@ from otx.core.types.export import OTXExportFormatType
 from otx.core.types.precision import OTXPrecisionType
 from otx.core.types.task import OTXTaskType
 from otx.core.utils.cache import TrainerArgumentsCache
+from otx.utils.utils import is_xpu_available
 
 from .hpo import execute_hpo, update_hyper_parameter
 from .utils.auto_configurator import DEFAULT_CONFIG_PER_TASK, AutoConfigurator
@@ -830,6 +832,8 @@ class Engine:
 
     @device.setter
     def device(self, device: DeviceType) -> None:
+        if is_xpu_available() and device == DeviceType.auto:
+            device = DeviceType.xpu
         self._device = DeviceConfig(accelerator=device)
         self._cache.update(accelerator=self._device.accelerator, devices=self._device.devices)
         self._cache.is_trainer_args_identical = False
@@ -852,6 +856,14 @@ class Engine:
         """Instantiate the trainer based on the model parameters."""
         if self._cache.requires_update(**kwargs) or self._trainer is None:
             self._cache.update(**kwargs)
+            # set up xpu device
+            if self._device.accelerator == DeviceType.xpu:
+                self._cache.update(strategy="xpu_single")
+                # add plugin for Automatic Mixed Precision on XPU
+                if self._cache.args["precision"] == 16:
+                    self._cache.update(plugins=[MixedPrecisionXPUPlugin()])
+                    self._cache.args["precision"] = None
+
             kwargs = self._cache.args
             self._trainer = Trainer(**kwargs)
             self._cache.is_trainer_args_identical = True
