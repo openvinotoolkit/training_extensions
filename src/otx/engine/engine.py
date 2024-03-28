36bcc17471df4c289762c72c7bcec6ec04677d1f
kprokofi <kirill.prokofiev@intel.com>
2024-03-28 09:02:26 +0900

    quick fix of unit test

--- src/otx/engine/engine.py
+++ src/otx/engine/engine.py
@@ -312,7 +312,7 @@ class Engine:
             datamodule = self._auto_configurator.update_ov_subset_pipeline(datamodule=datamodule, subset="test")
             model = self._auto_configurator.get_ov_model(model_name=str(checkpoint), label_info=datamodule.label_info)
             if self.device.accelerator == "auto":
-                self.device = DeviceConfig(accelerator=DeviceType("cpu"))
+                self.device = DeviceType.cpu
 
         metric = metric if metric is not None else self._auto_configurator.get_metric()
         lit_module = self._build_lightning_module(
