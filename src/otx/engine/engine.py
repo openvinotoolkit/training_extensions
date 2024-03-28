637e98337ed6008348accc033b04aa1b153bbf51
kprokofi <kirill.prokofiev@intel.com>
2024-03-28 09:02:26 +0900

    quick fix of unit test

--- src/otx/engine/engine.py
+++ src/otx/engine/engine.py
@@ -348,7 +348,7 @@ class Engine:
             datamodule = self._auto_configurator.update_ov_subset_pipeline(datamodule=datamodule, subset="test")
             model = self._auto_configurator.get_ov_model(model_name=str(checkpoint), label_info=datamodule.label_info)
             if self.device.accelerator == "auto":
-                self.device = DeviceConfig(accelerator=DeviceType("cpu"))
+                self.device = DeviceType.cpu
 
         # NOTE, trainer.test takes only lightning based checkpoint.
         # So, it can't take the OTX1.x checkpoint.
