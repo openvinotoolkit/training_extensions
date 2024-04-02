f4ce17290d8f2ae47478334e071efaa686cc0d00
Kim, Sungchul <sungchul.kim@intel.com>
2024-04-02 11:59:03 +0900

    precommit

--- src/otx/core/data/transform/detection.py
+++ src/otx/core/data/transform/detection.py
@@ -5,13 +5,13 @@
 
 from __future__ import annotations
 
-from typing import Any, Union
+from typing import Any
 
 import torch
 from torchvision import tv_tensors
 from torchvision.ops.boxes import box_iou
 from torchvision.transforms.v2 import RandomIoUCrop
-from torchvision.transforms.v2 import functional as F
+from torchvision.transforms.v2 import functional as F  # noqa: N812
 from torchvision.transforms.v2._utils import get_bounding_boxes, query_size
 
 
@@ -19,24 +19,25 @@ class MinIoURandomCrop(RandomIoUCrop):
     """MinIoURandomCrop inherited from RandomIoUCrop to align with mmdet.transforms.MinIoURandomCrop.
 
     * Updated
-        - change `ious.max()` to `ious.min()` at L121 because both MinIoURandomCrop and v2.RandomIoUCrop seems similar, 
-          but MinIoURandomCrop uses `overlaps.min()` to check if there is at least one box smaller than `min_iou` (https://github.com/open-mmlab/mmdetection/blob/v3.2.0/mmdet/datasets/transforms/transforms.py#L1432) 
+        - change `ious.max()` to `ious.min()` at L121 because both MinIoURandomCrop and v2.RandomIoUCrop seems similar,
+          but MinIoURandomCrop uses `overlaps.min()` to check if there is at least one box smaller than `min_iou` (https://github.com/open-mmlab/mmdetection/blob/v3.2.0/mmdet/datasets/transforms/transforms.py#L1432)
           and v2.RandomIoUCrop uses `ious.max()` to check if all boxes' IoUs are smaller than `min_jaccard_overlap` (https://github.com/pytorch/vision/blob/v0.16.1/torchvision/transforms/v2/_geometry.py#L1217).
 
         - `trials` in argument from 40 to 50 at L57.
 
     * Applied in other locations
-        - box translation : 
+        - box translation :
             mmdet : https://github.com/open-mmlab/mmdetection/blob/v3.2.0/mmdet/datasets/transforms/transforms.py#L1454
             torchvision : https://github.com/pytorch/vision/blob/v0.16.1/torchvision/transforms/v2/functional/_geometry.py#L1386
-        
+
         - clip border :
             mmdet : https://github.com/open-mmlab/mmdetection/blob/v3.2.0/mmdet/datasets/transforms/transforms.py#L1456-L1457
             torchvision : https://github.com/pytorch/vision/blob/v0.16.1/torchvision/transforms/v2/functional/_geometry.py#L1389
 
         - except invalid bounding boxes :
             mmdet : https://github.com/open-mmlab/mmdetection/blob/v3.2.0/mmdet/datasets/transforms/transforms.py#L1453
-            torchvision : https://github.com/pytorch/vision/blob/v0.16.1/torchvision/transforms/v2/_geometry.py#L1232-L1234 + SanitizeBoundingBoxes
+            torchvision : https://github.com/pytorch/vision/blob/v0.16.1/torchvision/transforms/v2/_geometry.py#L1232-L1234
+                + SanitizeBoundingBoxes
 
     Args:
         min_scale (float, optional): Minimum factors to scale the input size.
@@ -56,7 +57,7 @@ class MinIoURandomCrop(RandomIoUCrop):
         min_aspect_ratio: float = 0.5,
         max_aspect_ratio: float = 2.0,
         sampler_options: list[float] | None = None,
-        trials: int = 50, # 40 -> 50
+        trials: int = 50,  # 40 -> 50
     ):
         super().__init__()
         self.min_scale = min_scale
@@ -78,7 +79,7 @@ class MinIoURandomCrop(RandomIoUCrop):
             idx = int(torch.randint(low=0, high=len(self.options), size=(1,)))
             min_jaccard_overlap = self.options[idx]
             if min_jaccard_overlap >= 1.0:  # a value larger than 1 encodes the leave as-is option
-                return dict()
+                return {}
 
             for _ in range(self.trials):
                 # https://github.com/open-mmlab/mmdetection/blob/v3.2.0/mmdet/datasets/transforms/transforms.py#L1414-L1419
@@ -120,7 +121,13 @@ class MinIoURandomCrop(RandomIoUCrop):
                     xyxy_bboxes,
                     torch.tensor([[left, top, right, bottom]], dtype=xyxy_bboxes.dtype, device=xyxy_bboxes.device),
                 )
-                if ious.min() < min_jaccard_overlap: # max -> min
+                if ious.min() < min_jaccard_overlap:  # max -> min
                     continue
 
-                return dict(top=top, left=left, height=new_h, width=new_w, is_within_crop_area=is_within_crop_area)
+                return {
+                    "top": top,
+                    "left": left,
+                    "height": new_h,
+                    "width": new_w,
+                    "is_within_crop_area": is_within_crop_area,
+                }
