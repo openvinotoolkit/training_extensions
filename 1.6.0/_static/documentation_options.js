b6eddcf730d2e8ea3883a2f9193a9dce66717ae8
Yunchu Lee <yunchu.lee@intel.com>
2024-04-05 21:17:22 +0900

    update 1.6.0 documentation

--- 1.6.0/_static/documentation_options.js
+++ 1.6.0/_static/documentation_options.js
@@ -1,5 +1,4 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+const DOCUMENTATION_OPTIONS = {
     VERSION: '1.6.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
@@ -8,7 +7,7 @@ var DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: true,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
