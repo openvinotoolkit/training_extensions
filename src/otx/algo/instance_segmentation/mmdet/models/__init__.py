47f3744d68af56490668a068f9ab939a4136de8e
Eugene Liu <eugene.liu@intel.com>
2024-04-08 17:10:39 +0100

    add necks

--- src/otx/algo/instance_segmentation/mmdet/models/__init__.py
+++ src/otx/algo/instance_segmentation/mmdet/models/__init__.py
@@ -2,3 +2,4 @@
 from .dense_heads import *  # noqa: F403
 from .detectors import *  # noqa: F403
 from .layers import *  # noqa: F403
+from .necks import *  # noqa: F403
---
+++ src/otx/algo/instance_segmentation/mmdet/models/necks/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .fpn import FPN
+
+__all__ = [
+    'FPN',
+]
