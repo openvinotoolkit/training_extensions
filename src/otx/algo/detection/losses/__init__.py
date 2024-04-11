92dc0bf04a86e53fdfed7021d31b3df6483c79af
Eugene Liu <eugene.liu@intel.com>
2024-04-11 08:58:56 +0100

    update style

--- src/otx/algo/detection/losses/__init__.py
+++ src/otx/algo/detection/losses/__init__.py
@@ -4,13 +4,12 @@
 """Custom OTX Losses for Object Detection."""
 from .accuracy import Accuracy, accuracy
 from .cross_entropy_loss import CrossEntropyLoss
-from .cross_focal_loss import CrossSigmoidFocalLoss, OrdinaryFocalLoss
+from .cross_focal_loss import CrossSigmoidFocalLoss
 from .smooth_l1_loss import L1Loss
 
 __all__ = [
     "CrossEntropyLoss",
     "CrossSigmoidFocalLoss",
-    "OrdinaryFocalLoss",
     "accuracy",
     "Accuracy",
     "L1Loss",
