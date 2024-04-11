27be7a8ff5acfdee695cfb4789d84ccde872bb4f
Galina <galina.zalesskaya@intel.com>
2024-04-11 23:40:10 +0900

    Add tests

--- tests/unit/core/model/test_detection.py
+++ tests/unit/core/model/test_detection.py
@@ -5,13 +5,21 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
 from unittest.mock import create_autospec
 
 import pytest
+import torch
+from importlib_resources import files
 from lightning.pytorch.cli import ReduceLROnPlateau
+from omegaconf import OmegaConf
+from otx.algo.explain.explain_algo import get_feature_vector
 from otx.core.metrics.fmeasure import FMeasureCallable
-from otx.core.model.detection import OTXDetectionModel
+from otx.core.model.detection import MMDetCompatibleModel, OTXDetectionModel
 from torch.optim import Optimizer
+
+if TYPE_CHECKING:
+    from omegaconf.dictconfig import DictConfig
 
 
 class TestOTXDetectionModel:
@@ -39,6 +47,15 @@ class TestOTXDetectionModel:
     def mock_ckpt(self, request):
         return request.param
 
+    @pytest.fixture()
+    def config(self) -> DictConfig:
+        cfg_path = files("otx") / "algo" / "detection" / "mmconfigs" / "yolox_tiny.yaml"
+        return OmegaConf.load(cfg_path)
+
+    @pytest.fixture()
+    def otx_model(self, config) -> MMDetCompatibleModel:
+        return MMDetCompatibleModel(num_classes=1, config=config)
+
     def test_configure_metric_with_ckpt(
         self,
         mock_optimizer,
@@ -56,3 +73,68 @@ class TestOTXDetectionModel:
         model.load_state_dict(mock_ckpt)
 
         assert model.hparams["best_confidence_threshold"] == 0.35
+
+    def test_create_model(self, otx_model) -> None:
+        mmdet_model = otx_model._create_model()
+        assert mmdet_model is not None
+        assert isinstance(mmdet_model, torch.nn.Module)
+
+    def test_get_num_anchors(self, otx_model):
+        num_anchors = otx_model.get_num_anchors()
+        assert isinstance(num_anchors, list)
+        assert all(isinstance(n, int) for n in num_anchors)
+
+    def test_get_explain_fn(self, otx_model):
+        otx_model.explain_mode = True
+        explain_fn = otx_model.get_explain_fn()
+        assert callable(explain_fn)
+
+    def test_forward_explain_detection(self, otx_model, fxt_data_sample):
+        inputs = torch.randn(1, 3, 224, 224)
+        otx_model.model.feature_vector_fn = get_feature_vector
+        otx_model.model.explain_fn = otx_model.get_explain_fn()
+        result = otx_model._forward_explain_detection(otx_model.model, inputs, fxt_data_sample, mode="predict")
+
+        assert "predictions" in result
+        assert "feature_vector" in result
+        assert "saliency_map" in result
+
+    def test_customize_inputs(self, otx_model, fxt_det_data_entity) -> None:
+        output_data = otx_model._customize_inputs(fxt_det_data_entity[2])
+        assert output_data is not None
+        assert "gt_instances" in output_data["data_samples"][-1]
+        assert "bboxes" in output_data["data_samples"][-1].gt_instances
+        assert output_data["data_samples"][-1].metainfo["pad_shape"] == output_data["inputs"].shape[-2:]
+
+    def test_forward_explain(self, otx_model, fxt_det_data_entity):
+        inputs = fxt_det_data_entity[2]
+        otx_model.training = False
+        otx_model.explain_mode = True
+        outputs = otx_model.forward_explain(inputs)
+
+        assert outputs.has_xai_outputs
+        assert outputs.feature_vector is not None
+        assert outputs.saliency_map is not None
+
+    def test_reset_restore_model_forward(self, otx_model):
+        otx_model.explain_mode = True
+        initial_model_forward = otx_model.model.forward
+
+        otx_model._reset_model_forward()
+        assert otx_model.original_model_forward is not None
+        assert str(otx_model.model.forward) != str(otx_model.original_model_forward)
+
+        otx_model._restore_model_forward()
+        assert otx_model.original_model_forward is None
+        assert str(otx_model.model.forward) == str(initial_model_forward)
+
+    def test_export_parameters(self, otx_model):
+        otx_model.image_size = (1, 64, 64, 3)
+        otx_model.explain_mode = False
+        parameters = otx_model._export_parameters
+        assert isinstance(parameters, dict)
+        assert "output_names" in parameters
+
+        otx_model.explain_mode = True
+        parameters = otx_model._export_parameters
+        assert parameters["output_names"] == ["feature_vector", "saliency_map"]
