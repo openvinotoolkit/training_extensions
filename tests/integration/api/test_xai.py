07e87e69e79ca1e914c5c3e7c28a970217f15ed5
Kim, Vinnam <vinnam.kim@intel.com>
2024-04-16 00:57:39 +0900

    Revert test_xai.py
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- tests/integration/api/test_xai.py
+++ tests/integration/api/test_xai.py
@@ -24,7 +24,6 @@ MEAN_TORCH_OV_DIFF = 150
 @pytest.mark.parametrize(
     "recipe",
     EXPLAIN_MODEL_LIST,
-    ids=lambda x: "/".join(Path(x).parts[-2:]),
 )
 def test_forward_explain(
     recipe: str,
@@ -71,7 +70,6 @@ def test_forward_explain(
 @pytest.mark.parametrize(
     "recipe",
     EXPLAIN_MODEL_LIST,
-    ids=lambda x: "/".join(Path(x).parts[-2:]),
 )
 def test_predict_with_explain(
     recipe: str,
