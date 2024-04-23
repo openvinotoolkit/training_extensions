07ef8eace768e525f0f5e8b8937061436f0d480c
Shin, Eunwoo <eunwoo.shin@intel.com>
2024-04-23 10:59:28 +0900

    revert function name

--- src/otx/core/model/base.py
+++ src/otx/core/model/base.py
@@ -736,8 +736,8 @@ class OTXModel(LightningModule, Generic[T_OTXBatchDataEntity, T_OTXBatchPredEnti
 
         return super().lr_scheduler_step(scheduler=scheduler, metric=metric)
 
-    def make_optimizer_and_scheduler_picklable(self) -> None:
-        """Make optimizer and scheduler picklable.
+    def make_optimizer_and_scheduler_hpo(self) -> None:
+        """Patch optimizer and scheduler for hyperparameter optimization and adaptive batch size.
 
         This is inplace function changing inner states (`optimizer_callable` and `scheduler_callable`).
         Both will be changed to be picklable. In addition, `optimizer_callable` is changed
