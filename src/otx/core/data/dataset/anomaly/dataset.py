407a7d137b29d49e13fa1f454319bf8529ee21e2
Ilya Trushkin <ilya.trushkin@intel.com>
2024-04-26 10:01:22 +0300

    Update to datumaro==1.6.1rc3 (#3377)
    * Update datumaro version
    
    Signed-off-by: Ilya Trushkin <ilya.trushkin@intel.com>
    
    * Fix unit tests
    
    Signed-off-by: Ilya Trushkin <ilya.trushkin@intel.com>
    
    * Use dataset indexing
    
    Signed-off-by: Ilya Trushkin <ilya.trushkin@intel.com>
    
    ---------
    
    Signed-off-by: Ilya Trushkin <ilya.trushkin@intel.com>

--- src/otx/core/data/dataset/anomaly/dataset.py
+++ src/otx/core/data/dataset/anomaly/dataset.py
@@ -60,7 +60,7 @@ class AnomalyDataset(OTXDataset):
         self,
         index: int,
     ) -> AnomalyClassificationDataItem | AnomalySegmentationDataBatch | AnomalyDetectionDataBatch:
-        datumaro_item = self.dm_subset.get(id=self.ids[index], subset=self.dm_subset.name)
+        datumaro_item = self.dm_subset.as_dataset()[index]
         img = datumaro_item.media_as(Image)
         # returns image in RGB format if self.image_color_channel is RGB
         img_data, img_shape = self._get_img_data_and_shape(img)
