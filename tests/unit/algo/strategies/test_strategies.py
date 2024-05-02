1fe6e942a95fd81433f2ca36557fc741269da789
kirill prokofiev <kirill.prokofiev@intel.com>
2024-05-02 06:51:21 +0000

    refine unit test

--- tests/unit/algo/strategies/test_strategies.py
+++ tests/unit/algo/strategies/test_strategies.py
@@ -15,25 +15,21 @@ from otx.algo.strategies.xpu_single import SingleXPUStrategy
 class TestSingleXPUStrategy:
     @pytest.fixture()
     def mock_is_xpu_available(self, mocker):
-        return mocker.patch.object(target_file, "is_xpu_available", return_value=False)
+        return mocker.patch.object(target_file, "is_xpu_available", return_value=True)
 
     def test_init(self, mock_is_xpu_available):
-        mock_is_xpu_available.return_value = True
         strategy = SingleXPUStrategy(device="xpu:0")
         assert mock_is_xpu_available.call_count == 1
         assert strategy._root_device.type == "xpu"
         assert strategy.accelerator is None
 
     def test_init_no_xpu(self, mock_is_xpu_available):
+        mock_is_xpu_available.return_value = False
         with pytest.raises(MisconfigurationException):
             SingleXPUStrategy(device="xpu:0")
 
     @pytest.fixture()
-    def strategy(self, mocker):
-        mocker.patch(
-            "otx.algo.strategies.xpu_single.is_xpu_available",
-            return_value=True,
-        )
+    def strategy(self, mock_is_xpu_available):
         return SingleXPUStrategy(device="xpu:0", accelerator="xpu")
 
     def test_is_distributed(self, strategy):
