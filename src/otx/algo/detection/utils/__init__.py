4d40a7d75489a7b6b592720891f25793dbdc4e76
Eunwoo Shin <eunwoo.shin@intel.com>
2024-05-02 20:38:41 +0900

    Remove code to patch nms and roi_align in mmXX for XPU (#3434)
    * remove mm patching code in xpu side
    
    * align with pre-commit
    
    * revert NMSop
    
    * clean up __init__.py
    
    * remove unit test
    
    * update unit test
    
    * refine unit test
    
    ---------
    
    Co-authored-by: kirill prokofiev <kirill.prokofiev@intel.com>

--- src/otx/algo/detection/utils/__init__.py
+++ src/otx/algo/detection/utils/__init__.py
@@ -2,7 +2,3 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """utils for detection task."""
-
-from .mmcv_patched_ops import monkey_patched_nms, monkey_patched_roi_align
-
-__all__ = ["monkey_patched_nms", "monkey_patched_roi_align"]
