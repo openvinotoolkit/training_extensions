c21e26e3281e830af879ecbf09f168d38210c113
Jaeguk Hyun <jaeguk.hyun@intel.com>
2024-05-03 17:52:58 +0900

    Add unit tests for detectors' forward function (#3447)

--- tests/unit/algo/detection/test_atss.py
+++ tests/unit/algo/detection/test_atss.py
@@ -2,8 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 """Test of OTX SSD architecture."""
 
-from otx.algo.detection.atss import MobileNetV2ATSS
+import pytest
+import torch
+from otx.algo.detection.atss import MobileNetV2ATSS, ResNeXt101ATSS
 from otx.algo.utils.support_otx_v1 import OTXv1Helper
+from otx.core.data.entity.detection import DetBatchPredEntity
 from otx.core.exporter.native import OTXModelExporter
 from otx.core.types.export import TaskLevelExportParameters
 
@@ -17,3 +20,26 @@ class TestATSS:
 
         assert isinstance(model._export_parameters, TaskLevelExportParameters)
         assert isinstance(model._exporter, OTXModelExporter)
+
+    @pytest.mark.parametrize("model", [MobileNetV2ATSS(3), ResNeXt101ATSS(3)])
+    def test_loss(self, model, fxt_data_module):
+        data = next(iter(fxt_data_module.train_dataloader()))
+        data.images = [torch.randn(3, 32, 32), torch.randn(3, 48, 48)]
+        output = model(data)
+        assert "loss_cls" in output
+        assert "loss_bbox" in output
+        assert "loss_centerness" in output
+
+    @pytest.mark.parametrize("model", [MobileNetV2ATSS(3), ResNeXt101ATSS(3)])
+    def test_predict(self, model, fxt_data_module):
+        data = next(iter(fxt_data_module.train_dataloader()))
+        data.images = [torch.randn(3, 32, 32), torch.randn(3, 48, 48)]
+        model.eval()
+        output = model(data)
+        assert isinstance(output, DetBatchPredEntity)
+
+    @pytest.mark.parametrize("model", [MobileNetV2ATSS(3), ResNeXt101ATSS(3)])
+    def test_export(self, model):
+        model.eval()
+        output = model.forward_for_tracing(torch.randn(1, 3, 32, 32))
+        assert len(output) == 2
