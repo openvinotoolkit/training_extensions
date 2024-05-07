ade201667f6a4b40778220ef8104e8818adb4707
Wonju Lee <wonju.lee@intel.com>
2024-05-07 22:45:08 +0900

    fix conftest

--- tests/unit/core/data/test_dataset.py
+++ tests/unit/core/data/test_dataset.py
@@ -31,7 +31,7 @@ class TestDataset:
         item = dataset[0]
 
         assert isinstance(item, data_entity_cls)
-        fxt_mock_dm_subset.parent.__getitem__.assert_called_once()
+        fxt_mock_dm_subset.as_dataset().__getitem__.assert_called_once()
 
         mocker.patch.object(dataset, "_get_item_impl", return_value=None)
         with pytest.raises(RuntimeError):
