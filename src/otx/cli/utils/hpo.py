0d97eed937bab8303cbd3ffeaf9cfc1658259ff7
Eunwoo Shin <eunwoo.shin@intel.com>
2024-05-07 14:18:04 +0900

    Remove unnecessary model weights during HPO (#3459)
    * draft implementation
    
    * remove whole directory
    
    * refactor code
    
    * update unit test

--- src/otx/cli/utils/hpo.py
+++ src/otx/cli/utils/hpo.py
@@ -478,6 +478,11 @@ class HpoRunner:
             progress_updater_thread = Thread(target=self._update_hpo_progress, args=[hpo_algo], daemon=True)
             progress_updater_thread.start()
 
+        remove_unused_model_weight = Thread(
+            target=self._remove_unused_weight, args=[hpo_algo, self._hpo_workdir], daemon=True
+        )
+        remove_unused_model_weight.start()
+
         if torch.cuda.is_available():
             resource_type = "gpu"
         elif is_xpu_available():
@@ -577,6 +582,22 @@ class HpoRunner:
             if hpo_algo.is_done():
                 break
             self._progress_updater_callback(hpo_algo.get_progress() * 100)
+            time.sleep(1)
+
+    def _remove_unused_weight(self, hpo_algo: HpoBase, hpo_work_dir: Path):
+        """Function for a thread to report a HPO progress regularly.
+
+        Args:
+            hpo_algo (HpoBase): HPO algorithm instance.
+            hpo_work_dir (Path): HPO work directory.
+        """
+
+        while not hpo_algo.is_done():
+            finished_trials = hpo_algo.get_inferior_trials()
+            for trial in finished_trials:
+                dir_to_remove = hpo_work_dir / "weight" / str(trial.id)
+                if dir_to_remove.exists():
+                    shutil.rmtree(dir_to_remove)
             time.sleep(1)
 
 
@@ -763,7 +784,6 @@ class Trainer:
         score_report_callback = self._prepare_score_report_callback(task)
         task.train(dataset=dataset, output_model=output_model, train_parameters=score_report_callback)
         self._finalize_trial(task)
-        self._delete_unused_model_weight()
 
     def _prepare_hyper_parameter(self):
         return create(self._model_template.hyper_parameters.data)
@@ -831,28 +851,26 @@ class Trainer:
         return self._hpo_workdir / self._initial_weight_name
 
     def _finalize_trial(self, task):
+        self._report_func(0, 0, done=True)
+
         weight_dir_path = self._get_weight_dir_path()
         weight_dir_path.mkdir(parents=True, exist_ok=True)
         self._task.copy_weight(task.project_path, weight_dir_path)
-        self._report_func(0, 0, done=True)
+        necessary_weights = [
+            self._task.get_latest_weight(weight_dir_path),
+            get_best_hpo_weight(self._hpo_workdir, self._hp_config["id"]),
+        ]
+        while None in necessary_weights:
+            necessary_weights.remove(None)
+        for each_model_weight in weight_dir_path.iterdir():
+            for necessary_weight in necessary_weights:
+                if each_model_weight.samefile(necessary_weight):
+                    break
+            else:
+                each_model_weight.unlink()
 
     def _get_weight_dir_path(self) -> Path:
         return self._hpo_workdir / "weight" / self._hp_config["id"]
-
-    def _delete_unused_model_weight(self):
-        """Delete model weights except best and latest model weight."""
-        for json_file in self._hpo_workdir.rglob("*.json"):
-            if not json_file.stem.isnumeric():
-                continue
-            trial_num = json_file.stem
-            weight_dir = self._hpo_workdir / "weight" / trial_num
-            if not weight_dir.exists():
-                continue
-            latest_model_weight = self._task.get_latest_weight(weight_dir)
-            best_model_weight = get_best_hpo_weight(self._hpo_workdir, trial_num)
-            for each_model_weight in weight_dir.iterdir():
-                if str(each_model_weight) not in [latest_model_weight, best_model_weight]:
-                    each_model_weight.unlink()
 
 
 def run_trial(
