d7500711ce6b83d2fca05d83c6033c26201ae282
Kim, Sungchul <sungchul.kim@intel.com>
2024-05-10 11:05:49 +0900

    Enable instance segmentation data pipeline (#3435)
    - Update/Add transforms to enable masks and polygons for iseg
        - Resize
        - Pad
        - RandomFlip
        - CachedMosaic
        - CachedMixUp
        - RandomResize
        - RandomCrop
        - FilterAnnotations
    - Update recipes
        - maskrcnn_efficientnetb2b
        - maskrcnn_r50
        - maskrcnn_swint
        - rtmdet_inst_tiny
    - Update base dataset to avoid None results
    - Refactoring time consumer
    - Fix bugs
        - img_info.img_shape -> img_info.pad_shape when wrapping them with BitmapMasks

--- src/otx/core/data/dataset/base.py
+++ src/otx/core/data/dataset/base.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """Base class for OTXDataset."""
+
 from __future__ import annotations
 
 from abc import abstractmethod
@@ -16,10 +17,10 @@ from datumaro.components.media import ImageFromFile
 from datumaro.util.image import IMAGE_BACKEND, IMAGE_COLOR_CHANNEL, ImageBackend
 from datumaro.util.image import ImageColorChannel as DatumaroImageColorChannel
 from torch.utils.data import Dataset
-from torchvision.transforms.v2 import Compose
 
 from otx.core.data.entity.base import T_OTXDataEntity
 from otx.core.data.mem_cache import NULL_MEM_CACHE_HANDLER
+from otx.core.data.transform_libs.torchvision import Compose
 from otx.core.types.image import ImageColorChannel
 from otx.core.types.label import LabelInfo
 
