37c0775927585bb9fc2126fb432d1c0b3179560e
Kang, Harim <harim.kang@intel.com>
2024-05-10 11:19:40 +0900

    Revert some import

--- src/otx/algo/__init__.py
+++ src/otx/algo/__init__.py
@@ -2,3 +2,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """Module for OTX custom algorithms, e.g., model, losses, hook, etc..."""
+
+from . import (
+    accelerators,
+    plugins,
+    strategies,
+)
+
+__all__ = [
+    "strategies",
+    "accelerators",
+    "plugins",
+]
