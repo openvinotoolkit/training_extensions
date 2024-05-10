31cea8e4f9e01f0208fe341f23d7d85e78bd5c13
Kang, Harim <harim.kang@intel.com>
2024-05-10 09:54:42 +0900

    Update docs and init.py

--- src/otx/algo/__init__.py
+++ src/otx/algo/__init__.py
@@ -2,25 +2,3 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """Module for OTX custom algorithms, e.g., model, losses, hook, etc..."""
-
-from . import (
-    accelerators,
-    action_classification,
-    detection,
-    instance_segmentation,
-    plugins,
-    segmentation,
-    strategies,
-    visual_prompting,
-)
-
-__all__ = [
-    "action_classification",
-    "detection",
-    "segmentation",
-    "visual_prompting",
-    "strategies",
-    "accelerators",
-    "plugins",
-    "instance_segmentation",
-]
