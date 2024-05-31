db32e483fc7c28cde0b52798f7b92f46d2244c64
Kim, Sungchul <sungchul.kim@intel.com>
2024-05-31 11:27:01 +0900

    Change `swap_rgb` to False

--- src/otx/algo/detection/rtmdet.py
+++ src/otx/algo/detection/rtmdet.py
@@ -169,7 +169,7 @@ class RTMDet(ExplainableOTXDetModel):
             std=self.std,
             resize_mode="fit_to_window_letterbox",
             pad_value=114,
-            swap_rgb=True,
+            swap_rgb=False,  # TODO (sungchul): revert to True after merging releases/2.0.0 to develop
             via_onnx=True,
             onnx_export_configuration={
                 "input_names": ["image"],
@@ -179,7 +179,6 @@ class RTMDet(ExplainableOTXDetModel):
                     "boxes": {0: "batch", 1: "num_dets"},
                     "labels": {0: "batch", 1: "num_dets"},
                 },
-                "opset_version": 11,
                 "autograd_inlining": False,
             },
             output_names=["bboxes", "labels", "feature_vector", "saliency_map"] if self.explain_mode else None,
