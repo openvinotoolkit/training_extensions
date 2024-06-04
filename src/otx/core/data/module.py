750e1ae75e5089dc929a55be660aabf3cd5998ea
Kang, Harim <harim.kang@intel.com>
2024-06-04 17:46:00 +0900

    Check and Replace data_format if it's wrong

--- src/otx/core/data/module.py
+++ src/otx/core/data/module.py
@@ -9,6 +9,7 @@ import logging as log
 from typing import TYPE_CHECKING
 
 from datumaro import Dataset as DmDataset
+from datumaro import Environment
 from lightning import LightningDataModule
 from omegaconf import DictConfig, OmegaConf
 from torch.utils.data import DataLoader, RandomSampler
@@ -56,6 +57,18 @@ class OTXDataModule(LightningDataModule):
         from datumaro.plugins.data_formats.video import VIDEO_EXTENSIONS
 
         VIDEO_EXTENSIONS.append(".mp4")
+
+        # Data Format Check
+        available_data_formats = Environment().detect_dataset(str(self.config.data_root))
+        if not available_data_formats:
+            msg = f"Invalid data root: {self.config.data_root}. Please check if the data root is valid."
+            raise ValueError(msg)
+        if self.config.data_format not in available_data_formats:
+            log.warning(
+                f"Invalid data format: {self.config.data_format}. Available formats: {available_data_formats} "
+                f"Replace data_format: {self.config.data_format} -> {available_data_formats[0]}.",
+            )
+            self.config.data_format = available_data_formats[0]
 
         dataset = DmDataset.import_from(self.config.data_root, format=self.config.data_format)
         if self.task != "H_LABEL_CLS":
