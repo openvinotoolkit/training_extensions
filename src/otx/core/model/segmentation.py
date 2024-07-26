043d6f567c69502348b411337e68c991f99e1e04
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2024-07-26 11:36:23 +0900

    Add benchmarking for OV sseg models

--- src/otx/core/model/segmentation.py
+++ src/otx/core/model/segmentation.py
@@ -12,7 +12,7 @@ from typing import TYPE_CHECKING, Any
 import torch
 from torchvision import tv_tensors
 
-from otx.core.data.entity.base import OTXBatchLossEntity
+from otx.core.data.entity.base import ImageInfo, OTXBatchLossEntity
 from otx.core.data.entity.segmentation import SegBatchDataEntity, SegBatchPredEntity
 from otx.core.exporter.base import OTXModelExporter
 from otx.core.exporter.native import OTXNativeModelExporter
@@ -399,3 +399,17 @@ class OVSegmentationModel(OVModel[SegBatchDataEntity, SegBatchPredEntity]):
 
         msg = "Cannot construct LabelInfo from OpenVINO IR. Please check this model is trained by OTX."
         raise ValueError(msg)
+
+    def get_dummy_input(self, batch_size: int = 1) -> SegBatchDataEntity:
+        """Returns a dummy input for classification OV model"""
+        # Resize is embedded to the OV model, which means we don't need to know the actual size
+        images = [torch.rand(3, 224, 224) for _ in range(batch_size)]
+        infos = []
+        for i, img in enumerate(images):
+            infos.append(ImageInfo(
+                img_idx=i,
+                img_shape=img.shape,
+                ori_shape=img.shape,
+            ))
+        data = SegBatchDataEntity(batch_size, images, infos, masks=[])
+        return data
