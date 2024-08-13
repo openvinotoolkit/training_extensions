9dccf2d0550c5bff84e61d09dd080a4017ff2c0a
Shin, Eunwoo <eunwoo.shin@intel.com>
2024-08-13 11:05:22 +0900

    update docstring

--- src/otx/core/data/transform_libs/torchvision.py
+++ src/otx/core/data/transform_libs/torchvision.py
@@ -3508,7 +3508,7 @@ class GenerateTarget(tvt_v2.Transform, NumpytoTVTensorMixin):
             the specific codec for more details.
 
     Args:
-        input_size (tuple[int, int]): Input image size in [w, h]
+        input_size (tuple[int, int]): Input image size in [w, h]  TODO[wonjulee]: need to change order of shape
         is_numpy_to_tvtensor (bool): Whether convert outputs to tensor. Defaults to False.
     """
 
