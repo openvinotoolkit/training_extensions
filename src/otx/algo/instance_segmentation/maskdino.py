e527126b087953c031f62ae7b4dac3a2753f39eb
Eugene Liu <eugene.liu@intel.com>
2024-08-14 15:44:58 +0100

    update postprocess

--- src/otx/algo/instance_segmentation/maskdino.py
+++ src/otx/algo/instance_segmentation/maskdino.py
@@ -7,12 +7,9 @@ from detectron2.config import configurable, get_cfg
 from detectron2.data import MetadataCatalog
 from detectron2.modeling import build_backbone
 from detectron2.modeling.backbone import Backbone
-from detectron2.modeling.postprocessing import sem_seg_postprocess
 from detectron2.projects.deeplab import add_deeplab_config
-from detectron2.structures import Boxes, ImageList, Instances
-from detectron2.utils.memory import retry_if_cuda_oom
-from torch import nn
-from torch.nn import functional as F
+from detectron2.structures import ImageList
+from torch import Tensor, nn
 from torch.nn.modules import Module
 from torchvision import tv_tensors
 
@@ -20,7 +17,7 @@ from otx.algo.instance_segmentation.mask_dino import box_ops
 from otx.algo.instance_segmentation.mask_dino.criterion import SetCriterion
 from otx.algo.instance_segmentation.mask_dino.maskdino_head import MaskDINOHead
 from otx.algo.instance_segmentation.mask_dino.matcher import HungarianMatcher
-from otx.core.data.entity.instance_segmentation import InstanceSegBatchDataEntity
+from otx.core.data.entity.instance_segmentation import InstanceSegBatchDataEntity, InstanceSegBatchPredEntity
 from otx.core.data.entity.utils import stack_batch
 from otx.core.model.instance_segmentation import ExplainableOTXInstanceSegModel
 from otx.core.utils.mask_util import polygon_to_bitmap
@@ -221,7 +218,6 @@ class MaskDINO(nn.Module):
         }
 
     def forward(self, entity: InstanceSegBatchDataEntity):
-        ori_shapes = [img_info.ori_shape for img_info in entity.imgs_info]
         img_shapes = [img_info.img_shape for img_info in entity.imgs_info]
         images = ImageList(entity.images, img_shapes)
 
@@ -259,73 +255,9 @@ class MaskDINO(nn.Module):
                     # remove this loss if not specified in `weight_dict`
                     losses.pop(k)
             return losses
-        else:
-            outputs, _ = self.sem_seg_head(features)
-            mask_cls_results = outputs["pred_logits"]
-            mask_pred_results = outputs["pred_masks"]
-            mask_box_results = outputs["pred_boxes"]
-            # upsample masks
-            mask_pred_results = F.interpolate(
-                mask_pred_results,
-                size=(images.tensor.shape[-2], images.tensor.shape[-1]),
-                mode="bilinear",
-                align_corners=False,
-            )
 
-            del outputs
-
-            processed_results = []
-            for mask_cls_result, mask_pred_result, mask_box_result, input_per_image, image_size in zip(
-                mask_cls_results,
-                mask_pred_results,
-                mask_box_results,
-                batched_inputs,
-                images.image_sizes,
-            ):  # image_size is augmented size, not divisible to 32
-                height = input_per_image.get("height", image_size[0])  # real size
-                width = input_per_image.get("width", image_size[1])
-                processed_results.append({})
-                new_size = mask_pred_result.shape[-2:]  # padded size (divisible to 32)
-
-                if self.sem_seg_postprocess_before_inference:
-                    mask_pred_result = retry_if_cuda_oom(sem_seg_postprocess)(
-                        mask_pred_result,
-                        image_size,
-                        height,
-                        width,
-                    )
-                    mask_cls_result = mask_cls_result.to(mask_pred_result)
-                    # mask_box_result = mask_box_result.to(mask_pred_result)
-                    # mask_box_result = self.box_postprocess(mask_box_result, height, width)
-
-                # semantic segmentation inference
-                if self.semantic_on:
-                    r = retry_if_cuda_oom(self.semantic_inference)(mask_cls_result, mask_pred_result)
-                    if not self.sem_seg_postprocess_before_inference:
-                        r = retry_if_cuda_oom(sem_seg_postprocess)(r, image_size, height, width)
-                    processed_results[-1]["sem_seg"] = r
-
-                # panoptic segmentation inference
-                if self.panoptic_on:
-                    panoptic_r = retry_if_cuda_oom(self.panoptic_inference)(mask_cls_result, mask_pred_result)
-                    processed_results[-1]["panoptic_seg"] = panoptic_r
-
-                # instance segmentation inference
-
-                if self.instance_on:
-                    mask_box_result = mask_box_result.to(mask_pred_result)
-                    height = new_size[0] / image_size[0] * height
-                    width = new_size[1] / image_size[1] * width
-                    mask_box_result = self.box_postprocess(mask_box_result, height, width)
-
-                    instance_r = retry_if_cuda_oom(self.instance_inference)(
-                        mask_cls_result,
-                        mask_pred_result,
-                        mask_box_result,
-                    )
-                    processed_results[-1]["instances"] = instance_r
-
-            return processed_results
+        outputs, _ = self.sem_seg_head(features)
+        return outputs
 
     def prepare_targets(self, targets, images):
         h_pad, w_pad = images.tensor.shape[-2:]
@@ -346,150 +278,6 @@ class MaskDINO(nn.Module):
                 },
             )
         return new_targets
-
-    def prepare_targets_detr(self, targets, images):
-        h_pad, w_pad = images.tensor.shape[-2:]
-        new_targets = []
-        for targets_per_image in targets:
-            # pad gt
-            h, w = targets_per_image.image_size
-            image_size_xyxy = torch.as_tensor([w, h, w, h], dtype=torch.float, device=self.device)
-
-            gt_masks = targets_per_image.gt_masks
-            padded_masks = torch.zeros((gt_masks.shape[0], h_pad, w_pad), dtype=gt_masks.dtype, device=gt_masks.device)
-            padded_masks[:, : gt_masks.shape[1], : gt_masks.shape[2]] = gt_masks
-            new_targets.append(
-                {
-                    "labels": targets_per_image.gt_classes,
-                    "masks": padded_masks,
-                    "boxes": box_ops.box_xyxy_to_cxcywh(targets_per_image.gt_boxes.tensor) / image_size_xyxy,
-                },
-            )
-        return new_targets
-
-    def semantic_inference(self, mask_cls, mask_pred):
-        # if use cross-entropy loss in training, evaluate with softmax
-        if self.semantic_ce_loss:
-            mask_cls = F.softmax(mask_cls, dim=-1)[..., :-1]
-            mask_pred = mask_pred.sigmoid()
-            semseg = torch.einsum("qc,qhw->chw", mask_cls, mask_pred)
-            return semseg
-        # if use focal loss in training, evaluate with sigmoid. As sigmoid is mainly for detection and not sharp
-        # enough for semantic and panoptic segmentation, we additionally use use softmax with a temperature to
-        # make the score sharper.
-        else:
-            T = self.pano_temp
-            mask_cls = mask_cls.sigmoid()
-            if self.transform_eval:
-                mask_cls = F.softmax(mask_cls / T, dim=-1)  # already sigmoid
-            mask_pred = mask_pred.sigmoid()
-            semseg = torch.einsum("qc,qhw->chw", mask_cls, mask_pred)
-            return semseg
-
-    def panoptic_inference(self, mask_cls, mask_pred):
-        # As we use focal loss in training, evaluate with sigmoid. As sigmoid is mainly for detection and not sharp
-        # enough for semantic and panoptic segmentation, we additionally use use softmax with a temperature to
-        # make the score sharper.
-        prob = 0.5
-        T = self.pano_temp
-        scores, labels = mask_cls.sigmoid().max(-1)
-        mask_pred = mask_pred.sigmoid()
-        keep = labels.ne(self.sem_seg_head.num_classes) & (scores > self.object_mask_threshold)
-        # added process
-        if self.transform_eval:
-            scores, labels = F.softmax(mask_cls.sigmoid() / T, dim=-1).max(-1)
-        cur_scores = scores[keep]
-        cur_classes = labels[keep]
-        cur_masks = mask_pred[keep]
-        cur_prob_masks = cur_scores.view(-1, 1, 1) * cur_masks
-
-        h, w = cur_masks.shape[-2:]
-        panoptic_seg = torch.zeros((h, w), dtype=torch.int32, device=cur_masks.device)
-        segments_info = []
-
-        current_segment_id = 0
-
-        if cur_masks.shape[0] == 0:
-            # We didn't detect any mask :(
-            return panoptic_seg, segments_info
-        else:
-            # take argmax
-            cur_mask_ids = cur_prob_masks.argmax(0)
-            stuff_memory_list = {}
-            for k in range(cur_classes.shape[0]):
-                pred_class = cur_classes[k].item()
-                isthing = pred_class in self.metadata.thing_dataset_id_to_contiguous_id.values()
-                mask_area = (cur_mask_ids == k).sum().item()
-                original_area = (cur_masks[k] >= prob).sum().item()
-                mask = (cur_mask_ids == k) & (cur_masks[k] >= prob)
-
-                if mask_area > 0 and original_area > 0 and mask.sum().item() > 0:
-                    if mask_area / original_area < self.overlap_threshold:
-                        continue
-
-                    # merge stuff regions
-                    if not isthing:
-                        if int(pred_class) in stuff_memory_list.keys():
-                            panoptic_seg[mask] = stuff_memory_list[int(pred_class)]
-                            continue
-                        else:
-                            stuff_memory_list[int(pred_class)] = current_segment_id + 1
-
-                    current_segment_id += 1
-                    panoptic_seg[mask] = current_segment_id
-
-                    segments_info.append(
-                        {
-                            "id": current_segment_id,
-                            "isthing": bool(isthing),
-                            "category_id": int(pred_class),
-                        },
-                    )
-
-            return panoptic_seg, segments_info
-
-    def instance_inference(self, mask_cls, mask_pred, mask_box_result):
-        # mask_pred is already processed to have the same shape as original input
-        image_size = mask_pred.shape[-2:]
-        scores = mask_cls.sigmoid()  # [100, 80]
-        labels = (
-            torch.arange(self.sem_seg_head.num_classes, device=self.device)
-            .unsqueeze(0)
-            .repeat(self.num_queries, 1)
-            .flatten(0, 1)
-        )
-        scores_per_image, topk_indices = scores.flatten(0, 1).topk(self.test_topk_per_image, sorted=False)  # select 100
-        labels_per_image = labels[topk_indices]
-        topk_indices = topk_indices // self.sem_seg_head.num_classes
-        mask_pred = mask_pred[topk_indices]
-        # if this is panoptic segmentation, we only keep the "thing" classes
-        if self.panoptic_on:
-            keep = torch.zeros_like(scores_per_image).bool()
-            for i, lab in enumerate(labels_per_image):
-                keep[i] = lab in self.metadata.thing_dataset_id_to_contiguous_id.values()
-            scores_per_image = scores_per_image[keep]
-            labels_per_image = labels_per_image[keep]
-            mask_pred = mask_pred[keep]
-        result = Instances(image_size)
-        # mask (before sigmoid)
-        result.pred_masks = (mask_pred > 0).float()
-        # half mask box half pred box
-        mask_box_result = mask_box_result[topk_indices]
-        if self.panoptic_on:
-            mask_box_result = mask_box_result[keep]
-        result.pred_boxes = Boxes(mask_box_result)
-        # Uncomment the following to get boxes from masks (this is slow)
-        # result.pred_boxes = BitMasks(mask_pred > 0).get_bounding_boxes()
-
-        # calculate average mask prob
-        mask_scores_per_image = (mask_pred.sigmoid().flatten(1) * result.pred_masks.flatten(1)).sum(1) / (
-            result.pred_masks.flatten(1).sum(1) + 1e-6
-        )
-        if self.focus_on_box:
-            mask_scores_per_image = 1.0
-        result.scores = scores_per_image * mask_scores_per_image
-        result.pred_classes = labels_per_image
-        return result
 
     def box_postprocess(self, out_bbox, img_h, img_w):
         # postprocess box height and width
@@ -669,3 +457,103 @@ class MaskDINOR50(ExplainableOTXInstanceSegModel):
         if isinstance(entity.images, list):
             entity.images, entity.imgs_info = stack_batch(entity.images, entity.imgs_info, pad_size_divisor=32)
         return {"entity": entity}
+
+    def _customize_outputs(
+        self,
+        outputs,
+        inputs: InstanceSegBatchDataEntity,
+    ):
+        if self.training:
+            return outputs
+
+        target_sizes = [(max(m.shape), max(m.shape)) for m in inputs.masks]
+        masks, bboxes, labels, scores = self.post_process_instance_segmentation(
+            outputs,
+            inputs.imgs_info,
+            target_sizes=target_sizes,
+        )
+
+        if self.explain_mode:
+            msg = "Explain mode is not supported yet."
+            raise NotImplementedError(msg)
+
+        return InstanceSegBatchPredEntity(
+            batch_size=inputs.batch_size,
+            images=inputs.images,
+            imgs_info=inputs.imgs_info,
+            scores=scores,
+            bboxes=bboxes,
+            masks=masks,
+            polygons=[],
+            labels=labels,
+        )
+
+    def post_process_instance_segmentation(
+        self,
+        outputs,
+        imgs_info,
+        target_sizes: list[tuple[int, int]] | None = None,
+    ) -> tuple[Tensor, Tensor, Tensor, Tensor]:
+        class_queries_logits = outputs["pred_logits"]
+        masks_queries_logits = outputs["pred_masks"]
+        mask_box_results = outputs["pred_boxes"]
+
+        # Scale back to preprocessed image size - (384, 384) for all models
+        masks_queries_logits = torch.nn.functional.interpolate(
+            masks_queries_logits,
+            size=(384, 384),
+            mode="bilinear",
+            align_corners=False,
+        )
+
+        device = masks_queries_logits.device
+        num_classes = self.model.sem_seg_head.num_classes
+        num_queries = self.model.test_topk_per_image
+
+        batch_scores: list[Tensor] = []
+        batch_bboxes: list[tv_tensors.BoundingBoxes] = []
+        batch_labels: list[torch.LongTensor] = []
+        batch_masks: list[tv_tensors.Mask] = []
+
+        for mask_pred, mask_cls, pred_boxes, img_info, target_size in zip(
+            masks_queries_logits,
+            class_queries_logits,
+            mask_box_results,
+            imgs_info,
+            target_sizes,
+        ):
+            ori_h, ori_w = img_info.ori_shape
+            scores = mask_cls.sigmoid()
+            labels = torch.arange(num_classes, device=device).unsqueeze(0).repeat(num_queries, 1).flatten(0, 1)
+
+            scores_per_image, topk_indices = scores.flatten(0, 1).topk(num_queries, sorted=False)
+            labels_per_image = labels[topk_indices]
+
+            topk_indices = torch.div(topk_indices, num_classes, rounding_mode="floor")
+            mask_pred = mask_pred[topk_indices]
+            pred_masks = (mask_pred > 0).float()
+
+            # Calculate average mask prob
+            mask_scores_per_image = (mask_pred.sigmoid().flatten(1) * pred_masks.flatten(1)).sum(1) / (
+                pred_masks.flatten(1).sum(1) + 1e-6
+            )
+            pred_scores = scores_per_image * mask_scores_per_image
+            pred_classes = labels_per_image
+            pred_masks = torch.nn.functional.interpolate(
+                pred_masks.unsqueeze(0),
+                size=target_size,
+                mode="nearest",
+            )[0][:, :ori_h, :ori_w]
+
+            pred_boxes = box_ops.box_cxcywh_to_xyxy(pred_boxes)
+            pred_boxes *= pred_boxes.new_tensor(img_info.img_shape).repeat((1, 2))
+            pred_boxes /= pred_boxes.new_tensor(img_info.scale_factor[::-1]).repeat((1, 2))
+            area = (pred_boxes[:, 2] - pred_boxes[:, 0]) * (pred_boxes[:, 3] - pred_boxes[:, 1])
+
+            keep = (pred_masks.sum((1, 2)) > 10) & (pred_scores > 0.05) & (area > 10)
+            batch_masks.append(pred_masks[keep])
+            batch_bboxes.append(pred_boxes[keep])
+            batch_labels.append(pred_classes[keep])
+            batch_scores.append(pred_scores[keep])
+
+        return batch_masks, batch_bboxes, batch_labels, batch_scores
