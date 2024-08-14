a3e1e95056a77982af79498f97ea49b3d864f653
kprokofi <kirill.prokofiev@intel.com>
2024-08-15 03:22:40 +0900

    started instance_seg refactoring

--- src/otx/algo/instance_segmentation/maskrcnn_tv.py
+++ src/otx/algo/instance_segmentation/maskrcnn_tv.py
@@ -5,23 +5,17 @@
 
 from __future__ import annotations
 
-from collections import OrderedDict
 from typing import TYPE_CHECKING, Any
 
 import torch
 from torch import Tensor, nn
 from torchvision import tv_tensors
-from torchvision.models.detection.backbone_utils import _resnet_fpn_extractor, _validate_trainable_layers
-from torchvision.models.detection.faster_rcnn import FastRCNNConvFCHead, FastRCNNPredictor, RPNHead, _default_anchorgen
-from torchvision.models.detection.image_list import ImageList
+from otx.algo.instance_segmentation.segmentors.maskrcnn_tv import FastRCNNConvFCHead, RPNHead, MaskRCNNHeads, MaskRCNNBackbone, MaskRCNNTV
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, _default_anchorgen
 from torchvision.models.detection.mask_rcnn import (
-    MaskRCNN,
     MaskRCNN_ResNet50_FPN_V2_Weights,
-    MaskRCNNHeads,
     MaskRCNNPredictor,
 )
-from torchvision.models.detection.roi_heads import paste_masks_in_image
-from torchvision.models.resnet import resnet50
 
 from otx.algo.instance_segmentation.heads import TVRoIHeads
 from otx.core.config.data import TileConfig
@@ -42,103 +36,90 @@ if TYPE_CHECKING:
     from otx.core.types.label import LabelInfoTypes
 
 
-class _TVMaskRCNN(MaskRCNN):
-    """Torchvision MaskRCNN model with forward method accepting InstanceSegBatchDataEntity."""
-
-    def forward(self, entity: InstanceSegBatchDataEntity) -> dict[str, Tensor] | list[dict[str, Tensor]]:
-        """Overwrite GeneralizedRCNN forward method to accept InstanceSegBatchDataEntity."""
-        ori_shapes = [img_info.ori_shape for img_info in entity.imgs_info]
-        img_shapes = [img_info.img_shape for img_info in entity.imgs_info]
-
-        image_list = ImageList(entity.images, img_shapes)
-        targets = []
-        for bboxes, labels, masks, polygons in zip(
-            entity.bboxes,
-            entity.labels,
-            entity.masks,
-            entity.polygons,
-        ):
-            # NOTE: shift labels by 1 as 0 is reserved for background
-            _labels = labels + 1 if len(labels) else labels
-            targets.append(
-                {
-                    "boxes": bboxes,
-                    "labels": _labels,
-                    "masks": masks,
-                    "polygons": polygons,
-                },
-            )
-
-        features = self.backbone(image_list.tensors)
-        if isinstance(features, Tensor):
-            features = OrderedDict([("0", features)])
-        proposals, proposal_losses = self.rpn(image_list, features, targets)
-
-        detections, detector_losses = self.roi_heads(
-            features,
-            proposals,
-            image_list.image_sizes,
-            targets,
-        )
-
-        losses = {}
-        losses.update(detector_losses)
-        losses.update(proposal_losses)
-
-        if self.training:
-            return losses
-        scale_factors = [
-            img_meta.scale_factor if img_meta.scale_factor else (1.0, 1.0) for img_meta in entity.imgs_info
-        ]
-
-        return self.postprocess(detections, ori_shapes, scale_factors)
-
-    def postprocess(
-        self,
-        result: list[dict[str, Tensor]],
-        ori_shapes: list[tuple[int, int]],
-        scale_factors: list[tuple[float, float]],
-        mask_thr_binary: float = 0.5,
-    ) -> list[dict[str, Tensor]]:
-        """Postprocess the output of the model."""
-        for i, (pred, scale_factor, ori_shape) in enumerate(zip(result, scale_factors, ori_shapes)):
-            boxes = pred["boxes"]
-            labels = pred["labels"]
-            _scale_factor = [1 / s for s in scale_factor]  # (H, W)
-            boxes = boxes * boxes.new_tensor(_scale_factor[::-1]).repeat((1, int(boxes.size(-1) / 2)))
-            h, w = ori_shape
-            boxes[:, 0::2].clamp_(min=0, max=w - 1)
-            boxes[:, 1::2].clamp_(min=0, max=h - 1)
-            keep_indices = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1]) > 0
-            boxes = boxes[keep_indices > 0]
-            labels = labels[keep_indices > 0]
-            result[i]["boxes"] = boxes
-            result[i]["labels"] = labels - 1  # Convert back to 0-indexed labels
-            if "masks" in pred:
-                masks = pred["masks"][keep_indices]
-                masks = paste_masks_in_image(masks, boxes, ori_shape)
-                masks = (masks >= mask_thr_binary).to(dtype=torch.bool)
-                masks = masks.squeeze(1)
-                result[i]["masks"] = masks
-        return result
-
-    def export(
-        self,
-        batch_inputs: Tensor,
-        batch_img_metas: list[dict],
-    ) -> tuple[list[Tensor], list[Tensor], list[Tensor]]:
-        """Export the model with the given inputs and image metas."""
-        img_shapes = [img_meta["image_shape"] for img_meta in batch_img_metas]
-        image_list = ImageList(batch_inputs, img_shapes)
-        features = self.backbone(batch_inputs)
-        proposals, _ = self.rpn(image_list, features)
-        boxes, labels, masks_probs = self.roi_heads.export(features, proposals, image_list.image_sizes)
-        labels = [label - 1 for label in labels]  # Convert back to 0-indexed labels
-        return boxes, labels, masks_probs
-
-
 class TVMaskRCNN(ExplainableOTXInstanceSegModel):
     """Implementation of torchvision MaskRCNN for instance segmentation."""
+    load_from = {"resnet_50": MaskRCNN_ResNet50_FPN_V2_Weights.verify("DEFAULT")}
+    mean = (123.675, 116.28, 103.53)
+    std = (58.395, 57.12, 57.375)
+
+    def __init__(
+        self,
+        label_info: LabelInfoTypes,
+        input_size: tuple[int, int] = (1024, 1024),
+        optimizer: OptimizerCallable = DefaultOptimizerCallable,
+        scheduler: LRSchedulerCallable | LRSchedulerListCallable = DefaultSchedulerCallable,
+        metric: MetricCallable = MaskRLEMeanAPFMeasureCallable,
+        torch_compile: bool = False,
+        tile_config: TileConfig = TileConfig(enable_tiler=False),
+    ) -> None:
+        super().__init__(
+            label_info=label_info,
+            input_size=input_size,
+            optimizer=optimizer,
+            scheduler=scheduler,
+            metric=metric,
+            torch_compile=torch_compile,
+            tile_config=tile_config,
+        )
+
+    def _create_model(self) -> nn.Module:
+        """create MaskRCNN model with TV implementation."""
+        # NOTE: Add 1 to num_classes to account for background class.
+        num_classes = self.label_info.num_classes + 1
+        weights = self.load_from[self.model_version]
+
+        rpn_anchor_generator = _default_anchorgen()
+        backbone = MaskRCNNBackbone(version=self.model_version)
+        rpn_head = RPNHead(version=self.model_version, anchorgen=rpn_anchor_generator)
+        box_head = FastRCNNConvFCHead(version=self.model_version)
+        mask_head = MaskRCNNHeads(version=self.model_version)
+
+        model = MaskRCNNTV(
+            backbone,
+            num_classes=91,
+            rpn_anchor_generator=rpn_anchor_generator,
+            rpn_head=rpn_head,
+            box_head=box_head,
+            mask_head=mask_head,
+        )
+
+        model.load_state_dict(weights.get_state_dict(progress=True, check_hash=True))
+
+        # Replace RoIHeads since torchvision does not allow customized roi_heads.
+        model.roi_heads = TVRoIHeads(
+            model.roi_heads.box_roi_pool,
+            model.roi_heads.box_head,
+            model.roi_heads.box_predictor,
+            fg_iou_thresh=0.5,
+            bg_iou_thresh=0.5,
+            batch_size_per_image=512,
+            positive_fraction=0.25,
+            bbox_reg_weights=None,
+            score_thresh=model.roi_heads.score_thresh,
+            nms_thresh=model.roi_heads.nms_thresh,
+            detections_per_img=model.roi_heads.detections_per_img,
+            mask_roi_pool=model.roi_heads.mask_roi_pool,
+            mask_head=model.roi_heads.mask_head,
+            mask_predictor=model.roi_heads.mask_predictor,
+        )
+
+        # get number of input features for the classifier
+        in_features = model.roi_heads.box_predictor.cls_score.in_features
+        # replace the pre-trained head with a new one
+        model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+        # now get the number of input features for the mask classifier
+        in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+        hidden_layer = model.roi_heads.mask_predictor.conv5_mask.out_channels
+
+        # and replace the mask predictor with a new one
+        model.roi_heads.mask_predictor = MaskRCNNPredictor(
+            in_features_mask,
+            hidden_layer,
+            num_classes,
+        )
+
+        return model
 
     def _customize_inputs(self, entity: InstanceSegBatchDataEntity) -> dict[str, Any]:
         if isinstance(entity.images, list):
@@ -264,103 +245,3 @@ class TVMaskRCNN(ExplainableOTXInstanceSegModel):
         }
         meta_info_list = [meta_info] * len(inputs)
         return self.model.export(inputs, meta_info_list)
-
-
-class TVMaskRCNNR50(TVMaskRCNN):
-    """Torchvision MaskRCNN model with ResNet50 backbone."""
-
-    mean = (123.675, 116.28, 103.53)
-    std = (58.395, 57.12, 57.375)
-
-    def __init__(
-        self,
-        label_info: LabelInfoTypes,
-        input_size: tuple[int, int] = (1024, 1024),
-        optimizer: OptimizerCallable = DefaultOptimizerCallable,
-        scheduler: LRSchedulerCallable | LRSchedulerListCallable = DefaultSchedulerCallable,
-        metric: MetricCallable = MaskRLEMeanAPFMeasureCallable,
-        torch_compile: bool = False,
-        tile_config: TileConfig = TileConfig(enable_tiler=False),
-    ) -> None:
-        super().__init__(
-            label_info=label_info,
-            input_size=input_size,
-            optimizer=optimizer,
-            scheduler=scheduler,
-            metric=metric,
-            torch_compile=torch_compile,
-            tile_config=tile_config,
-        )
-
-    def _create_model(self) -> nn.Module:
-        """From torchvision tutorial."""
-        # NOTE: Add 1 to num_classes to account for background class.
-        num_classes = self.label_info.num_classes + 1
-
-        weights = MaskRCNN_ResNet50_FPN_V2_Weights.verify("DEFAULT")
-
-        trainable_backbone_layers = _validate_trainable_layers(
-            is_trained=True,
-            trainable_backbone_layers=None,
-            max_value=5,
-            default_value=3,
-        )
-
-        backbone = resnet50(progress=True)
-        backbone = _resnet_fpn_extractor(backbone, trainable_backbone_layers, norm_layer=nn.BatchNorm2d)
-        rpn_anchor_generator = _default_anchorgen()
-        rpn_head = RPNHead(backbone.out_channels, rpn_anchor_generator.num_anchors_per_location()[0], conv_depth=2)
-        box_head = FastRCNNConvFCHead(
-            (backbone.out_channels, 7, 7),
-            [256, 256, 256, 256],
-            [1024],
-            norm_layer=nn.BatchNorm2d,
-        )
-        mask_head = MaskRCNNHeads(backbone.out_channels, [256, 256, 256, 256], 1, norm_layer=nn.BatchNorm2d)
-
-        model = _TVMaskRCNN(
-            backbone,
-            num_classes=91,
-            rpn_anchor_generator=rpn_anchor_generator,
-            rpn_head=rpn_head,
-            box_head=box_head,
-            mask_head=mask_head,
-        )
-
-        model.load_state_dict(weights.get_state_dict(progress=True, check_hash=True))
-
-        # Replace RoIHeads since torchvision does not allow customized roi_heads.
-        model.roi_heads = TVRoIHeads(
-            model.roi_heads.box_roi_pool,
-            model.roi_heads.box_head,
-            model.roi_heads.box_predictor,
-            fg_iou_thresh=0.5,
-            bg_iou_thresh=0.5,
-            batch_size_per_image=512,
-            positive_fraction=0.25,
-            bbox_reg_weights=None,
-            score_thresh=model.roi_heads.score_thresh,
-            nms_thresh=model.roi_heads.nms_thresh,
-            detections_per_img=model.roi_heads.detections_per_img,
-            mask_roi_pool=model.roi_heads.mask_roi_pool,
-            mask_head=model.roi_heads.mask_head,
-            mask_predictor=model.roi_heads.mask_predictor,
-        )
-
-        # get number of input features for the classifier
-        in_features = model.roi_heads.box_predictor.cls_score.in_features
-        # replace the pre-trained head with a new one
-        model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-        # now get the number of input features for the mask classifier
-        in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-        hidden_layer = model.roi_heads.mask_predictor.conv5_mask.out_channels
-
-        # and replace the mask predictor with a new one
-        model.roi_heads.mask_predictor = MaskRCNNPredictor(
-            in_features_mask,
-            hidden_layer,
-            num_classes,
-        )
-
-        return model
