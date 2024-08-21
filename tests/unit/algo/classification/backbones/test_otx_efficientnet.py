1e8ec38e7e83bdc11f18281a0d838a128b8e99d6
Harim Kang <harim.kang@intel.com>
2024-08-21 15:32:37 +0900

    Refactor Classification Backbone to Factory Class Design (#3864)
    * update for releases 2.2.0rc0
    
    * Refactor Classification Backbone to Factory Design
    
    * Revert "update for releases 2.2.0rc0"
    
    This reverts commit d1bd1d5d04d62cdcf57ff270ba2a3a33f52a333f.
    
    * Remove deprecated docstring
    
    ---------
    
    Co-authored-by: Yunchu Lee <yunchu.lee@intel.com>

--- tests/unit/algo/classification/backbones/test_otx_efficientnet.py
+++ tests/unit/algo/classification/backbones/test_otx_efficientnet.py
@@ -4,17 +4,17 @@
 
 import pytest
 import torch
-from otx.algo.classification.backbones.efficientnet import OTXEfficientNet
+from otx.algo.classification.backbones.efficientnet import EfficientNetBackbone
 
 
 class TestOTXEfficientNet:
     @pytest.mark.parametrize("version", ["b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8"])
     def test_forward(self, version):
-        model = OTXEfficientNet(version, pretrained=None)
+        model = EfficientNetBackbone(version, pretrained=None)
         assert model(torch.randn(1, 3, 244, 244))[0].shape[-1] == 8
         assert model(torch.randn(1, 3, 244, 244))[0].shape[-2] == 8
 
     def test_set_input_size(self):
         input_size = (300, 300)
-        model = OTXEfficientNet("b0", input_size=input_size, pretrained=None)
+        model = EfficientNetBackbone("b0", input_size=input_size, pretrained=None)
         assert model.in_size == input_size
