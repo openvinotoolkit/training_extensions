283e0b5749403f9833b8b98d4b3f2f886634beea
kprokofi <kirill.prokofiev@intel.com>
2024-08-27 07:30:58 +0900

    remove transformer layers

--- src/otx/algo/modules/transformer.py
+++ src/otx/algo/modules/transformer.py
@@ -8,9 +8,10 @@ from __future__ import annotations
 
 import math
 from functools import partial
-from typing import Callable
+from typing import Callable, Sequence
 
 import torch
+from timm.models.layers import to_2tuple
 from torch import nn
 
 from otx.algo.modules.base_module import BaseModule, Sequential
@@ -122,8 +123,6 @@ class PatchEmbed(BaseModule):
     """Image to Patch Embedding.
 
     We use a conv layer to implement PatchEmbed.
-
-    TODO (sungchul): it is duplicated with otx.algo.instance_segmentation.layers.transformer.PatchEmbed
 
     Args:
         in_channels (int): The num of input channels. Default: 3
@@ -366,3 +365,130 @@ def deformable_attention_core_func(
     )
 
     return output.permute(0, 2, 1)
+
+
+class PatchMerging(BaseModule):
+    """Merge patch feature map.
+
+    This layer groups feature map by kernel_size, and applies norm and linear
+    layers to the grouped feature map. Our implementation uses `nn.Unfold` to
+    merge patch, which is about 25% faster than original implementation.
+    Instead, we need to modify pretrained models for compatibility.
+
+    Args:
+        in_channels (int): The num of input channels.
+            to gets fully covered by filter and stride you specified..
+            Default: True.
+        out_channels (int): The num of output channels.
+        kernel_size (int | tuple, optional): the kernel size in the unfold
+            layer. Defaults to 2.
+        stride (int | tuple, optional): the stride of the sliding blocks in the
+            unfold layer. Default: None. (Would be set as `kernel_size`)
+        padding (int | tuple | string ): The padding length of
+            embedding conv. When it is a string, it means the mode
+            of adaptive padding, support "same" and "corner" now.
+            Default: "corner".
+        dilation (int | tuple, optional): dilation parameter in the unfold
+            layer. Default: 1.
+        bias (bool, optional): Whether to add bias in linear layer or not.
+            Defaults: False.
+        normalization (Callable[..., nn.Module] | None): Normalization layer module.
+            Defaults to ``nn.LayerNorm``.
+        init_cfg (dict, optional): The extra config for initialization.
+            Default: None.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int | tuple[int, int] = 2,
+        stride: int | tuple[int, int] = 1,
+        padding: int | tuple | str = "corner",
+        dilation: int | tuple[int, int] = 1,
+        bias: bool = False,
+        normalization: Callable[..., nn.Module] | None = nn.LayerNorm,
+        init_cfg: dict | None = None,
+    ) -> None:
+        super().__init__(init_cfg=init_cfg)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        stride = stride if stride else kernel_size
+
+        _kernel_size = to_2tuple(kernel_size)
+        stride = to_2tuple(stride)
+        dilation = to_2tuple(dilation)
+
+        self.adap_padding: nn.Module | None
+        if isinstance(padding, str):
+            self.adap_padding = AdaptivePadding(
+                kernel_size=_kernel_size,
+                stride=stride,
+                dilation=dilation,
+                padding=padding,
+            )
+            # disable the padding of unfold
+            padding = 0
+        else:
+            self.adap_padding = None
+
+        padding = to_2tuple(padding)
+        self.sampler = nn.Unfold(kernel_size=_kernel_size, dilation=dilation, padding=padding, stride=stride)
+
+        sample_dim = _kernel_size[0] * _kernel_size[1] * in_channels
+
+        if normalization is not None:
+            self.norm = build_norm_layer(normalization, sample_dim)[1]
+        else:
+            self.norm = None
+
+        self.reduction = nn.Linear(sample_dim, out_channels, bias=bias)
+
+    def forward(self, x: torch.Tensor, input_size: tuple[int, ...]) -> tuple[torch.Tensor, tuple[int, int]]:
+        """Forward function for PatchMerging.
+
+        Args:
+            x (Tensor): Has shape (B, H*W, C_in).
+            input_size (tuple[int]): The spatial shape of x, arrange as (H, W).
+                Default: None.
+
+        Returns:
+            tuple: Contains merged results and its spatial shape.
+
+                - x (Tensor): Has shape (B, Merged_H * Merged_W, C_out)
+                - out_size (tuple[int]): Spatial shape of x, arrange as
+                    (Merged_H, Merged_W).
+        """
+        batch_size, length, channels = x.shape
+        if not isinstance(input_size, Sequence):
+            msg = f"Expect input_size is `Sequence` but get {input_size}"
+            raise TypeError(msg)
+
+        h, w = input_size
+        if h * w != length:
+            msg = "input feature has wrong size"
+            raise ValueError(msg)
+
+        x = x.view(batch_size, h, w, channels).permute([0, 3, 1, 2])  # B, C, H, W
+        # Use nn.Unfold to merge patch. About 25% faster than original method,
+        # but need to modify pretrained model for compatibility
+
+        if self.adap_padding:
+            x = self.adap_padding(x)
+            h, w = x.shape[-2:]
+
+        x = self.sampler(x)
+        # if kernel_size=2 and stride=2, x should has shape (B, 4*C, H/2*W/2)
+
+        out_h = (
+            h + 2 * self.sampler.padding[0] - self.sampler.dilation[0] * (self.sampler.kernel_size[0] - 1) - 1
+        ) // self.sampler.stride[0] + 1
+        out_w = (
+            w + 2 * self.sampler.padding[1] - self.sampler.dilation[1] * (self.sampler.kernel_size[1] - 1) - 1
+        ) // self.sampler.stride[1] + 1
+
+        output_size = (out_h, out_w)
+        x = x.transpose(1, 2)  # B, H/2*W/2, 4*C
+        x = self.norm(x) if self.norm else x
+        x = self.reduction(x)
+        return x, output_size
