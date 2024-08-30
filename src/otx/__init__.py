9f3ce8836a4423528c6a107c9ddc9fbf90b4c01b
Kang, Harim <harim.kang@intel.com>
2024-08-30 14:08:33 +0900

    Fix cached dir

--- src/otx/__init__.py
+++ src/otx/__init__.py
@@ -5,7 +5,13 @@
 
 __version__ = "2.2.0rc0"
 
+import os
+from pathlib import Path
+
 from otx.core.types import *  # noqa: F403
+
+# Pretrained weight is saved into ~/.cache/torch/hub/checkpoints
+os.environ["XDG_CACHE_HOME"] = str(Path.home() / ".cache" / "torch" / "hub" / "checkpoints")
 
 OTX_LOGO: str = """
 
--- tests/unit/algo/classification/backbones/test_timm.py
+++ tests/unit/algo/classification/backbones/test_timm.py
@@ -1,16 +1,25 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+from pathlib import Path
+
 import torch
 from otx.algo.classification.backbones.timm import TimmBackbone
 
 
 class TestOTXEfficientNetV2:
     def test_forward(self):
-        model = TimmBackbone(backbone="efficientnetv2_s_21k")
+        model = TimmBackbone(backbone="tf_efficientnetv2_s.in21k")
         assert model(torch.randn(1, 3, 244, 244))[0].shape == torch.Size([1, 1280, 8, 8])
 
     def test_get_config_optim(self):
-        model = TimmBackbone(backbone="efficientnetv2_s_21k")
+        model = TimmBackbone(backbone="tf_efficientnetv2_s.in21k")
         assert model.get_config_optim([0.01])[0]["lr"] == 0.01
         assert model.get_config_optim(0.01)[0]["lr"] == 0.01
+
+    def test_check_pretrained_weight(self):
+        TimmBackbone(backbone="tf_efficientnetv2_s.in21k", pretrained=True)
+        assert Path(
+            Path.home(),
+            ".cache/torch/hub/checkpoints/huggingface/hub/models--timm--tf_efficientnetv2_s.in21k",
+        ).exists()
