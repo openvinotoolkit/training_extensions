7fc1f59ae656fa2902c6cb6531985c3f9d37d0d4
Kang, Harim <harim.kang@intel.com>
2024-08-30 16:02:13 +0900

    Fix user setting

--- src/otx/__init__.py
+++ src/otx/__init__.py
@@ -10,8 +10,12 @@ from pathlib import Path
 
 from otx.core.types import *  # noqa: F403
 
-# Pretrained weight is saved into ~/.cache/torch/hub/checkpoints
-os.environ["XDG_CACHE_HOME"] = str(Path.home() / ".cache" / "torch" / "hub" / "checkpoints")
+# Set the value of XDG_CACHE_HOME to set the cache folder that stores the pretrained weights for timm and huggingface.
+# Default, Pretrained weight is saved into ~/.cache/torch/hub/checkpoints
+os.environ["XDG_CACHE_HOME"] = os.getenv(
+    "XDG_CACHE_HOME",
+    str(Path.home() / ".cache" / "torch" / "hub" / "checkpoints"),
+)
 
 OTX_LOGO: str = """
 
