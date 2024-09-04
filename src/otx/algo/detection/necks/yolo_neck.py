9a95f849dcab49d13a7701950dbceaec08d779b5
Kim, Sungchul <sungchul.kim@intel.com>
2024-09-04 13:16:30 +0900

    Refactoring yolo neck

--- src/otx/algo/detection/necks/yolo_neck.py
+++ src/otx/algo/detection/necks/yolo_neck.py
@@ -12,47 +12,73 @@ from typing import Any, ClassVar
 import torch
 from torch import Tensor, nn
 
-from otx.algo.detection.gelan import Conv, Pool, RepNCSPELAN
+from otx.algo.detection.backbones.gelan import Pool, RepNCSPELAN
 from otx.algo.detection.utils.yolov7_v9_utils import set_info_into_module
+from otx.algo.modules import Conv2dModule
 
 
 class SPPELAN(nn.Module):
-    """SPPELAN module comprising multiple pooling and convolution layers."""
+    """SPPELAN module comprising multiple pooling and convolution layers.
 
-    def __init__(self, in_channels: int, out_channels: int, neck_channels: int | None = None):
+    Args:
+        in_channels (int): The number of input channels.
+        out_channels (int): The number of output channels.
+        neck_channels (int | None): The number of neck channels. Defaults to None.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, neck_channels: int | None = None) -> None:
         super().__init__()
         neck_channels = neck_channels or out_channels // 2
 
-        self.conv1 = Conv(in_channels, neck_channels, kernel_size=1)
+        self.conv1 = Conv2dModule(
+            in_channels,
+            neck_channels,
+            kernel_size=1,
+            normalization=nn.BatchNorm2d(neck_channels, eps=1e-3, momentum=3e-2),
+            activation=nn.SiLU(inplace=True),
+        )
         self.pools = nn.ModuleList([Pool("max", 5, stride=1) for _ in range(3)])
-        self.conv5 = Conv(4 * neck_channels, out_channels, kernel_size=1)
+        self.conv5 = Conv2dModule(
+            4 * neck_channels,
+            out_channels,
+            kernel_size=1,
+            normalization=nn.BatchNorm2d(out_channels, eps=1e-3, momentum=3e-2),
+            activation=nn.SiLU(inplace=True),
+        )
 
     def forward(self, x: Tensor) -> Tensor:
+        """Forward function."""
         features = [self.conv1(x)]
         for pool in self.pools:
             features.append(pool(features[-1]))
         return self.conv5(torch.cat(features, dim=1))
 
 
-class UpSample(nn.Module):
-    def __init__(self, **kwargs):
-        super().__init__()
-        self.UpSample = nn.Upsample(**kwargs)
-
-    def forward(self, x):
-        return self.UpSample(x)
-
-
 class Concat(nn.Module):
-    def __init__(self, dim=1):
+    """Concat module.
+
+    Args:
+        dim (int): The dimension to concatenate. Defaults to 1.
+    """
+
+    def __init__(self, dim: int = 1) -> None:
         super().__init__()
         self.dim = dim
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
+        """Forward function."""
         return torch.cat(x, self.dim)
 
 
 class YOLONeckModule(nn.Module):
+    """Neck module for YOLOv7 and v9.
+
+    Args:
+        elan_channels (list[dict[str, int]]): The ELAN channels.
+        concat_sources (list[list[str, int]]): The sources to concatenate.
+        csp_args (dict[str, Any] | None): The arguments for CSP. Defaults to None.
+    """
+
     def __init__(
         self,
         elan_channels: list[dict[str, int]],
@@ -74,14 +100,15 @@ class YOLONeckModule(nn.Module):
                     {
                         "module": layer(**elan_channel["args"], **_csp_args),
                         "tags": elan_channel["tags"],
-                    }
-                )
+                    },
+                ),
             )
             if len(concat_sources) > idx:
-                self.module.append(UpSample(scale_factor=2, mode="nearest"))
+                self.module.append(nn.Upsample(scale_factor=2, mode="nearest"))
                 self.module.append(set_info_into_module({"module": Concat(), "source": concat_sources[idx]}))
 
     def forward(self, x: Tensor | dict[str, Tensor], *args, **kwargs) -> dict[str, Tensor]:
+        """Forward function."""
         outputs: dict[str, Tensor] = {0: x} if isinstance(x, Tensor) else x
         for layer in self.module:
             if hasattr(layer, "source") and isinstance(layer.source, list):
@@ -96,6 +123,8 @@ class YOLONeckModule(nn.Module):
 
 
 class YOLONeck:
+    """YOLONeck factory for detection."""
+
     YOLONECK_CFG: ClassVar[dict[str, Any]] = {
         "yolov9-s": {
             "elan_channels": [
