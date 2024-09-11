e628c5fbef0d6e1d892ffbbee79c8c889e209c2b
Eugene Liu <eugene.liu@intel.com>
2024-09-11 10:55:43 +0100

    remove unnecessary files

--- bash_scripts/export.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-otx export --work_dir otx-workspace/maskrcnn_swint/vitens_coliform/1
-otx export --work_dir otx-workspace/maskrcnn_swint/vitens_coliform/2
-otx export --work_dir otx-workspace/maskrcnn_swint/vitens_coliform/3
-otx export --work_dir otx-workspace/maskrcnn_swint/vitens_coliform/4
