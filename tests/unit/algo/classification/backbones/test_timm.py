368a91c0dda0d832a293602a8c6ec445632794f2
Kang, Harim <harim.kang@intel.com>
2024-09-26 10:34:03 +0900

    Fix unit-test

--- tests/unit/algo/classification/backbones/test_timm.py
+++ tests/unit/algo/classification/backbones/test_timm.py
@@ -11,11 +11,11 @@ from otx.algo.classification.backbones.timm import TimmBackbone
 
 class TestOTXEfficientNetV2:
     def test_forward(self):
-        model = TimmBackbone(backbone="tf_efficientnetv2_s.in21k")
+        model = TimmBackbone(model_name="tf_efficientnetv2_s.in21k")
         assert model(torch.randn(1, 3, 244, 244))[0].shape == torch.Size([1, 1280, 8, 8])
 
     def test_get_config_optim(self):
-        model = TimmBackbone(backbone="tf_efficientnetv2_s.in21k")
+        model = TimmBackbone(model_name="tf_efficientnetv2_s.in21k")
         assert model.get_config_optim([0.01])[0]["lr"] == 0.01
         assert model.get_config_optim(0.01)[0]["lr"] == 0.01
 
@@ -24,5 +24,5 @@ class TestOTXEfficientNetV2:
         if target.exists():
             shutil.rmtree(target)
         assert not target.exists()
-        TimmBackbone(backbone="tf_efficientnetv2_s.in21k", pretrained=True)
+        TimmBackbone(model_name="tf_efficientnetv2_s.in21k", pretrained=True)
         assert target.exists()
