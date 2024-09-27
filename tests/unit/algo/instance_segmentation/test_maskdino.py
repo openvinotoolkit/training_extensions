816743d96abbc226fad17ef5d5a6761a346df173
Eugene Liu <eugene.liu@intel.com>
2024-09-27 17:27:54 +0100

    update test

--- tests/unit/algo/instance_segmentation/test_maskdino.py
+++ tests/unit/algo/instance_segmentation/test_maskdino.py
@@ -19,7 +19,7 @@ class TestMaskDINO:
     @pytest.mark.parametrize("model", [MaskDINOR50(3, "maskdino_resnet_50")])
     def test_predict(self, model, fxt_data_module):
         data = next(iter(fxt_data_module.train_dataloader()))
-        data.images = [torch.randn(3, 32, 32), torch.randn(3, 48, 48)]
+        data.images = [torch.randn(3, 320, 320), torch.randn(3, 320, 320)]
         model.eval()
         output = model(data)
         assert isinstance(output, InstanceSegBatchPredEntity)
@@ -27,5 +27,5 @@ class TestMaskDINO:
     @pytest.mark.parametrize("model", [MaskDINOR50(3, "maskdino_resnet_50")])
     def test_export(self, model):
         model.eval()
-        output = model.forward_for_tracing(torch.randn(1, 3, 32, 32))
+        output = model.forward_for_tracing(torch.randn(1, 3, 320, 320))
         assert len(output) == 3
