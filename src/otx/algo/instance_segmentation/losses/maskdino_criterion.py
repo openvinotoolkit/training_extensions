d5576f86cf4548b3f439f5184aa071a5ae28fe7a
Eugene Liu <eugene.liu@intel.com>
2024-10-07 10:12:08 +0100

    update docstring

--- src/otx/algo/instance_segmentation/losses/maskdino_criterion.py
+++ src/otx/algo/instance_segmentation/losses/maskdino_criterion.py
@@ -25,18 +25,14 @@ def sigmoid_focal_loss(
     """Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
 
     Args:
-        inputs: A float tensor of arbitrary shape.
-                The predictions for each example.
-        targets: A float tensor with the same shape as inputs. Stores the binary
-                 classification label for each element in inputs
-                (0 for the negative class and 1 for the positive class).
-        alpha: (optional) Weighting factor in range (0,1) to balance
-                positive vs negative examples. Default = -1 (no weighting).
-        gamma: Exponent of the modulating factor (1 - p_t) to
-               balance easy vs hard examples.
+        inputs (torch.Tensor): A float tensor of arbitrary shape. The predictions for each example.
+        targets (torch.Tesnor): A float tensor with the same shape as inputs. Stores the binary classification label.
+        num_boxes (float): Number of boxes.
+        alpha (float): Weighting factor in the loss function. Default to 0.25.
+        gamma (float): Exponent of the modulating factor (1 - p_t) to balance easy vs hard examples. Default to 2.
 
     Returns:
-        Loss tensor
+        torch.Tensor: Loss tensor
     """
     prob = inputs.sigmoid()
     ce_loss = f.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
